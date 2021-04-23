0d3cebcb750da22e85065d5ee019fb45bf9ec56c
JunhoYou <youjh@rsupport.com>
2021-04-23 13:29:09 +0900

    chore: template 및 언어 constant 파일로 분리

---
+++ src/js/Constant/Language.js
@@ -0,0 +1,25 @@
+export const LABEL = {
+  TITLE: '🎱 행운의 로또',
+  BUY_TITLE: '구입할 금액을 입력해주세요.',
+  ALL_COUNT_TITLE: '총 #개를 구매하였습니다.',
+  LAST_WEEK_TITLE: '지난 주 당첨번호 6개와 보너스 넘버 1개를 입력해주세요.',
+  WIN_NUMBER : '당첨 번호',
+  BONUS_NUMBER : '보너스 번호'
+}
+
+export const BTN_LABEL = {
+  CHECK : '확인',
+  CHECK_NUMBER : '번호보기',
+  CHECK_RESULT : '결과 확인하기'
+}
+
+export const MODAL_LABEL = {
+  RESULT_TITLE: '🏆 당첨 통계 🏆',
+  MATCH_COUNT : '일치 갯수',
+  REWARD: '당첨금',
+  WIN_COUNT: '당첨 갯수',
+  COUNT: '개',
+  BONUS_BALL: '+ 보너스볼',
+  RESULT_PERCENT: '당신의 총 수익률은 #%입니다.',
+  RESTART_BTN: '다시 시작하기'
+}
