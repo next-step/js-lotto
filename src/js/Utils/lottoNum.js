6ca424ddf470bdf3478efcbf84a7dad4484093c9
Will <kode15333@gmail.com>
2021-04-26 14:31:53 +0900

    feat: util(로또번호 구매함수 생성)

---
+++ src/js/Utils/lottoNum.js
@@ -0,0 +1,14 @@
+const NumberArr = Array.from({ length: 45 }, (v, i) => i + 1)
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+
+  return array
+}
+
+export const getTicker = () => {
+  return shuffleArray(NumberArr).slice(0, 6)
+}
