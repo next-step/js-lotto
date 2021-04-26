16da0bfed3dcdd303997b9a9b12f8a42ebe3f83d
YimJiYoung <chiy323@gmail.com>
2021-04-26 21:24:54 +0900

    feat: 로또 번호 토글 기능 구현

--- src/js/Lottos.js
+++ src/js/Lottos.js
@@ -1,4 +1,4 @@
-import { $ } from './utils/dom.js';
+import { $, $$ } from './utils/dom.js';
 import { getRandomInt } from './utils/random.js';
 
 const PRICE_PER_LOTTO = 1000;
@@ -6,22 +6,42 @@ const PRICE_PER_LOTTO = 1000;
 export class Lottos {
   constructor() {
     this.lottos = [];
+    this.$container = $('#purchased-lottos');
     this.$lottoSwitch = $('#lotto-switch');
+    this.$numOfLotto = $('#total-purchased');
     this.initEventListener();
   }
 
   setState({ price }) {
     const numOfLotto = price / PRICE_PER_LOTTO;
     this.lottos = Array.from({ length: numOfLotto }, () => Lottos.generateLotto());
-    console.log(this.lottos);
+    this.render();
   }
 
   initEventListener() {
-    this.$lottoSwitch.addEventListener('change', this.handleToggle.bind(this));
+    this.$lottoSwitch.addEventListener('change', Lottos.handleToggle);
   }
 
-  handleToggle(e) {
-    console.log(e);
+  static handleToggle() {
+    const lottoNumbers = $$('.lotto-numbers');
+    lottoNumbers.forEach((element) => {
+      element.classList.toggle('d-none');
+    });
+  }
+
+  render() {
+    this.$container.hidden = false;
+    this.$numOfLotto.textContent = this.lottos.length;
+    this.$container.insertAdjacentHTML('beforeend', `
+    <ul class="d-flex flex-wrap">
+      ${this.lottos.reduce((html, numbers) => `${html}
+        <li class="mx-1 d-flex"> 
+          <span class="mr-4 text-4xl"> 🎟️ </span>
+          <span class="lotto-numbers text-xl d-none">${numbers.join(', ')}</span>
+        </li>
+      `, '')}
+    </ul>
+    `);
   }
 
   static generateLotto() {
