905b2d5739f44d9efeeb591e6d2a15dec8199531
mjchoi <mjchoi@ex-em.com>
2021-04-28 17:53:33 +0900

    [fixed] 불필요 코드 제거

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -6,12 +6,6 @@ describe("로또", () => {
     cy.visit("http://127.0.0.1:8888/");
   });
 
-  /**
-   * - [x] 로또 구입 금액을 입력하면, 금액에 해당하는 로또를 발급해야 한다.
-   * - [x] 로또 1장의 가격은 1,000원이다.
-   * - [x] 소비자는 **자동 구매**를 할 수 있어야 한다.
-   * - [x] 복권 번호는 번호보기 토글 버튼을 클릭하면, 볼 수 있어야 한다.
-   * */
   const commClass = {
     priceInput: ".price-area input",
     priceButton: ".price-area button",
