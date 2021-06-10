378ae4126d9481aea77aa97e58fde210c2ee563e
Sanghyeok Nam <sanam@c3r3s7.42seoul.kr>
2021-06-10 16:02:47 +0900

    feat: 템플릿화

--- src/js/index.js
+++ src/js/index.js
@@ -2,21 +2,3 @@ import App from './App.js';
 import { $ } from './utils/helpers.js';
 
 const app = new App($('#app'));
-
-// const $showResultButton = document.querySelector('.open-result-modal-button')
-// const $modalClose = document.querySelector('.modal-close')
-// const $modal = document.querySelector('.modal')
-// const $lottoNumbersToggleButton = document.querySelector(
-//   '.lotto-numbers-toggle-button'
-// )
-
-// const onModalShow = () => {
-//   $modal.classList.add('open')
-// }
-
-// const onModalClose = () => {
-//   $modal.classList.remove('open')
-// }
-
-// $showResultButton.addEventListener('click', onModalShow)
-// $modalClose.addEventListener('click', onModalClose)
