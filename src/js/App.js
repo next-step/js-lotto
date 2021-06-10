8ac5870e8731a0f616933a468976e26e671a2d65
sanam <simian114@gmail.com>
2021-06-10 10:40:06 +0900

    temp

--- src/js/App.js
+++ src/js/App.js
@@ -5,11 +5,29 @@ import WinningNumberForm from './components/WinningNumberForm.js';
 import Modal from './components/Modal.js';
 import getRandomNumber from './utils/getRandomInt.js';
 
+function lottoNum() {
+  const lotto = [];
+  function makeNum() {
+    if (lotto.length < 7) {
+      let n = Math.floor(Math.random() * 45) + 1;
+      if (notSame(n)) lotto.push(n);
+      makeNum();
+    }
+    function notSame(n) {
+      return lotto.every((e) => n !== e);
+    }
+  }
+  makeNum();
+  return lotto;
+}
 
 const initialState = {
   // NOTE: lottos 배열 안에는 lotto의 문자열 값이 들어간다.
-  lottos: [],
   purchasedPrice: 0,
+  lottos: [],
+  isShowNumber: false,
+
+  // TODO:
   winningNumber: [],
   numberOfWinner: {
     1: 0,
@@ -18,91 +36,100 @@ const initialState = {
     4: 0,
     5: 0,
   },
+
   isLottosExist: false,
   isModalHidden: true,
-}
+};
 
 function App($root) {
   this.$root = $root;
-
-  this.generateLottoNumber = () => [...Array(7).keys()].map(() => getRandomNumber(0, 46));
+  this.state = initialState;
 
   // TODO: setState가 호출되는 경우는 무엇무엇인가? 구입금액 눌렀을 때,
   // TODO: 다시시작하기 눌렀을 때?
   this.setState = (nextState) => {
     this.state = nextState;
-    if (!this.components) return;
-    Object.values(this.components).map(component => {
-      component.updateProps && component.updateProps(this.state);
-    })
     this.render();
-  }
+  };
+
+  // this.calculateWinner = (winningNumber) => {
+  //   console.log(winningNumber);
+  //   const normalOfWinning = winningNumber.slice(0, 6);
+  //   const bonusOfWinning = winningNumber[6];
+  //   console.log(normalOfWinning);
+  //   const numberOfWinner = {
+  //     1: 0,
+  //     2: 0,
+  //     3: 0,
+  //     4: 0,
+  //     5: 0,
+  //   };
+  //   const lottos = this.state.lottos;
+  //   lottos.map((lotto) => {
+  //     const normal = lotto.slice(0, 6);
+  //     const bonus = lotto[6];
+  //     let count = 0;
+  //     normal.map((val) => {
+  //       console.log(normalOfWinning);
+  //       if (normalOfWinning.indexOf(val) !== -1) count += 1;
+  //     });
+  //     console.log(bonus);
+  //     console.log(bonusOfWinning);
+  //     if (count === 3) numberOfWinner[1] += 1;
+  //     else if (count === 4) numberOfWinner[2] += 1;
+  //     else if (count === 5) numberOfWinner[3] += 1;
+  //     else if (count === 5 && bonus === bonusOfWinning) numberOfWinner[4] += 1;
+  //     else if (count === 6) numberOfWinner[5] += 1;
+  //   });
+  //   return numberOfWinner;
+  // };
 
   this.handleSubmitPurchase = (purchasedPrice) => {
     // TODO: reset 하고 다시시작하게 만들기
-    const lottos = [...Array(purchasedPrice / 1000).keys()].map(() => this.generateLottoNumber());
-    this.setState({...this.state, lottos, purchasedPrice, isLottosExist: true });
-  }
-
-  this.calculateWinner = (winningNumber) => {
-    console.log(winningNumber)
-    const normalOfWinning = winningNumber.slice(0, 6);
-    const bonusOfWinning = winningNumber[6];
-    console.log(normalOfWinning)
-    const numberOfWinner = {
-      1: 0,
-      2: 0,
-      3: 0,
-      4: 0,
-      5: 0,
-    };
-    const lottos = this.state.lottos;
-    lottos.map((lotto) => {
-      const normal = lotto.slice(0,6);
-      const bonus = lotto[6];
-      let count = 0;
-      normal.map((val) => {
-        console.log(normalOfWinning)
-        if (normalOfWinning.indexOf(val) !== -1) count += 1;
-      })
-      console.log(bonus)
-      console.log(bonusOfWinning)
-      if (count === 3) numberOfWinner[1] += 1;
-      else if (count === 4) numberOfWinner[2] += 1;
-      else if (count === 5) numberOfWinner[3] += 1;
-      else if (count === 5 && bonus === bonusOfWinning) numberOfWinner[4] += 1;
-      else if (count === 6) numberOfWinner[5] += 1;
-    })
-    return numberOfWinner;  
-  }
+    const nums = [...Array(purchasedPrice / 1000).keys()].map(() => lottoNum());
+    lottos.setState({ ...this.state, lottos: nums });
+    // this.setState({
+    //   ...this.state,
+    //   lottos,
+    //   purchasedPrice,
+    //   isLottosExist: true,
+    // });
+  };
 
   // NOTE: Get Number Of Winner
   this.handleSubmitResult = (winningNumber) => {
-    this.setState({...this.state, numberOfWinner: this.calculateWinner(winningNumber) });
-    console.log(this.state.numberOfWinner);
-  }
+    this.setState({
+      ...this.state,
+      numberOfWinner: this.calculateWinner(winningNumber),
+    });
+  };
 
-  this.mountComponents = () => {
-    this.components = {
-      priceForm: new PriceForm($('#price-form'), this.handleSubmitPurchase),
-      lottos: new Lottos($('#lottos'), { lottos: this.state.lottos, isLottosExist: this.state.isLottosExist }),
-      winningNumberForm:
-        new WinningNumberForm($('#winning-number'),
-        { isLottosExist: this.state.isLottosExist },
-        this.handleSubmitResult
-      ),
-      modal: new Modal($('#modal')),
-    }
-  }
+  // this.mountComponents = () => {
+  //   this.components = {
+  //     priceForm: new PriceForm($('#price-form'), this.handleSubmitPurchase),
+  //     lottos: new Lottos($('#lottos'), {
+  //       lottos: this.state.lottos,
+  //       isLottosExist: this.state.isLottosExist,
+  //     }),
+  //     winningNumberForm: new WinningNumberForm(
+  //       $('#winning-number'),
+  //       { isLottosExist: this.state.isLottosExist },
+  //       this.handleSubmitResult
+  //     ),
+  //     modal: new Modal($('#modal')),
+  //   };
+  // };
 
   // TODO: state가 변화될 때 마다 render가 호출된다.
-  this.render = () => {
-  }
+  // this.render = () => {};
 
   // NOTE: Construction
-  this.setState(initialState);
-  this.mountComponents();
-  this.render();
+  // this.setState(initialState);
+  // this.mountComponents();
+  // this.render();
+
+  const priceForm = new PriceForm($('#price-form'), this.handleSubmitPurchase);
+  const lottos = new Lottos($('#lottos'), { state: this.state });
 }
 
 export default App;
--- src/js/components/Lottos.js
+++ src/js/components/Lottos.js
@@ -1,65 +1,64 @@
-import { $, $on } from '../utils/helpers.js';
-import Lotto from './Lotto.js'
+import {
+  $,
+  $addClass,
+  $removeClass,
+  $hide,
+  $on,
+  $show,
+} from '../utils/helpers.js';
+import Lotto from './Lotto.js';
 
 const initialState = {
+  lottos: [],
   isShowNumber: false,
-  isComponentsMounted: false,
 };
 
-// function Lottos($root, { lottos, isLottosExist }) {
-function Lottos($root, props) {
+function Lottos($root, { state }) {
   this.$root = $root;
   this.$lottoIcons = $('#lotto-icons', $root);
   this.$checkBox = $('input', $root);
-  this.state = initialState;
-  this.props = props;
-  this.components = {};
+  this.state = state;
 
   this.setState = (nextState) => {
     this.state = nextState;
-    Object.values(this.components).map(component => {
-      component.updateProps && component.updateProps(this.state);
-    })
     this.render();
-  }
-
-  this.updateProps = (props) => {
-    const keys = Object.keys(this.props);
-    keys && keys.map((key) => {
-      if (!props[key]) return;
-      this.props[key] = props[key];
-    })
-    this.render();
-  }
+  };
 
   this.mountComponents = () => {
-    this.components = this.props.lottos.map((lotto, index) =>
-      new Lotto($('#lotto-icons'), { id: index, lotto , isShowNumber: this.state.isShowNumber }));
-    this.setState({...this.state, isComponentsMounted: true });
-  }
+    this.components = this.state.lottos.map(
+      (lotto, index) =>
+        new Lotto($('#lotto-icons'), {
+          state: {
+            id: index,
+            lotto,
+            isShowNumber: this.state.isShowNumber,
+          },
+        })
+    );
+  };
 
   this.render = () => {
-    if (!this.props.isLottosExist) {
-      return this.$root.classList.add('hidden');
-    }
-    this.$root.classList.remove('hidden');
+    if (!this.state.lottos) return $hide(this.$root);
+    $show(this.$root);
     if (this.state.isShowNumber) {
-      this.$lottoIcons.classList.add('flex-col');
+      $addClass(this.$lottoIcons, 'flex-col');
     } else {
-      this.$lottoIcons.classList.remove('flex-col');
+      $removeClass(this.$lottoIcons, 'flex-col');
     }
-    if (!this.state.isComponentsMounted) this.mountComponents();
-  }
+    this.mountComponents();
+
+    // if (!this.state.isComponentsMounted) this.mountComponents();
+  };
 
   this.handleCheckBox = () => {
-    this.setState({...this.state, isShowNumber: !this.state.isShowNumber });
-  }
+    // this.setState({ ...this.state, isShowNumber: !this.state.isShowNumber });
+  };
 
   // TODO: checkBox에 대한 이벤트를 등록해야한다.
   $on(this.$checkBox, 'change', this.handleCheckBox);
 
   // NOTE: Construction
-  this.setState({ ...this.state})
+  // this.setState({ ...this.state });
 }
 
 export default Lottos;
