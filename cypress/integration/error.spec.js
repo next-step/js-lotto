81e2ca767cad3319b3c64633375000e458f57880
Sanghyeok Nam <sanam@c3r3s7.42seoul.kr>
2021-06-12 12:14:29 +0900

    test: 잘못된 winning num 에 대한 alert 테스트

--- cypress/integration/error.spec.js
+++ cypress/integration/error.spec.js
@@ -1,26 +1,49 @@
 import { ERROR, LOTTO_COUNT, VALID_PRICE } from '../utils/constants';
 import setAliase from '../utils/setAlise';
 
+const expectAlert = () => {
+  let i = 0;
+  const stub = cy.stub();
+  cy.on('window:alert', stub);
+  return function (msg) {
+    expect(stub.getCall(i++)).to.be.calledWith(msg);
+  }
+}
+
 describe('에러 테스트', () => {
   beforeEach(() => {
     cy.visit('/');
     setAliase();
+    cy.wrap({ checkAlert: expectAlert() }).as('al');
   })
 
-  context('로또 구입', () => {
+  context('정상적이지 않은 가격으로 로또 구입', () => {
     it('빈 값 입력', () => {
-      const stub = cy.stub();
-      cy.on('window:alert', stub);
       cy.typeToTarget('@pfInput', '{enter}').then(() => {
-        expect(stub.getCall(0)).to.be.calledWith(ERROR.NOT_EMPTY);
+        cy.get('@al').invoke('checkAlert', ERROR.NOT_EMPTY);
       })
     })
     it('1000원 단위가 아닌 값 입력', () => {
-      const stub = cy.stub();
-      cy.on('window:alert', stub);
       cy.typeToTarget('@pfInput', 123).type('{enter}').then(() => {
-        expect(stub.getCall(0)).to.be.calledWith(ERROR.THOUSAND);
+        cy.get('@al').invoke('checkAlert', ERROR.THOUSAND);
       })
     })
   })
+  
+  context('로또 구입 성공 후 당첨 번호 입력', () => {
+    beforeEach(() => {
+      cy.typeToTarget('@pfInput', 5000).type('{enter}')
+    })
+    it('정상 범위 밖의 숫자를 입력하면 alert나 나온다.', () => {
+      cy.get('@lottoList').find('.lotto-detail').each(($lotto) => {
+        let num = 45;
+        cy.get('@wfInputs').each(($input, index) => {
+          $input.val(num++);
+        })
+      })
+      cy.clickTarget('@wfBtn')
+      cy.get('@al').invoke('checkAlert', ERROR.NOT_VALID_NUM);
+    })
+  })
+
 })
--- cypress/utils/constants.js
+++ cypress/utils/constants.js
@@ -2,7 +2,7 @@ const ERROR = {
   NOT_EMPTY: '비어있을 수 없음',
   THOUSAND: '1000으로 나누어 떨어져야함',
   DUPLICATE: '중복된 값 노노노',
-  NOT_VALID_NUM: '0부터 45사이의 수만 가능',
+  NOT_VALID_NUM: '0 부터 45까지의 수만 괜찮음',
 }
 
 const VALID_PRICE = 5000;
