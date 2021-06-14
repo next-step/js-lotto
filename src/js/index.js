3980f43c0192ceab6e819e2a503e5ee3df69968b
inhyuck <dlsgur1447@gmail.com>
2021-06-14 23:35:58 +0900

    refactor: 주석처리된 코드 정리

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,3 @@
-// const $showResultButton = document.querySelector('.open-result-modal-button')
-// const $modalClose = document.querySelector('.modal-close')
-// const $modal = document.querySelector('.modal')
-// const $lottoNumbersToggleButton = document.querySelector(
-//   '.lotto-numbers-toggle-button'
-// )
-//
-// const onModalShow = () => {
-//   $modal.classList.add('open')
-// }
-//
-// const onModalClose = () => {
-//   $modal.classList.remove('open')
-// }
-//
-// $showResultButton.addEventListener('click', onModalShow)
-// $modalClose.addEventListener('click', onModalClose)
 import {$} from './utils/selector.js';
 import {LottoApp} from './lottoApp.js';
 
