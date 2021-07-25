0b5185612688488e5aaf057bf0599928a26f7644
sun.kist <sun.kist@kakaocorm.com>
2021-07-25 17:53:48 +0900

    feature: index수정

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,51 @@
-const $showResultButton = document.querySelector('.open-result-modal-button')
-const $modalClose = document.querySelector('.modal-close')
-const $modal = document.querySelector('.modal')
-const $lottoNumbersToggleButton = document.querySelector(
-  '.lotto-numbers-toggle-button'
-)
+// import app from "./app.js"
 
-const onModalShow = () => {
-  $modal.classList.add('open')
-}
+// const $showResultButton = document.querySelector('.open-result-modal-button')
+// const $modalClose = document.querySelector('.modal-close')
+// const $modal = document.querySelector('.modal')
+// const $lottoNumbersToggleButton = document.querySelector(
+//   '.lotto-numbers-toggle-button'
+// )
 
-const onModalClose = () => {
-  $modal.classList.remove('open')
-}
+// const onModalShow = () => {
+//   $modal.classList.add('open')
+// }
 
-$showResultButton.addEventListener('click', onModalShow)
-$modalClose.addEventListener('click', onModalClose)
+// const onModalClose = () => {
+//   $modal.classList.remove('open')
+// }
+
+// // $showResultButton.addEventListener('click', onModalShow)
+// // $modalClose.addEventListener('click', onModalClose)
+
+import App from "./app.js";
+import Subject from "./core/Subject.js";
+import Modal from "./pages/modal.js";
+import { STEP_NUMBER } from "./utils/constants.js"
+import { section } from "./utils/htmlElementCreator.js"
+
+
+
+const subject = new Subject({
+  store: {
+    lottoArr : [],
+    step: STEP_NUMBER.FIRST,
+    lastWinningLottoNum : [],
+    purchaseAmount:0,
+    checked : false
+  },
+  initFn: (modal) => {
+    modal.store.lottoArr = []
+    modal.store.lastWinningLottoNum = []
+    modal.store.purchaseAmount = 0
+    modal.store.step = STEP_NUMBER.FIRST
+  }
+})
+
+const app = new App({component : document.querySelector("#app")}, subject)
+const modal = new Modal({component: section({id: "modal-component", class: "modal", role: "dialog", ariaModal: "true", ariaLabelledby:"title-dialog"})}, subject)
+
+subject.subscribe({observer: app, state: [""]})
+subject.subscribe({observer: modal, state: ["lottoArr", "lastWinningLottoNum", "step"]})
+
+document.querySelector("#app").appendChild(modal.$component)
