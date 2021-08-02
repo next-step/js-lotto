7484bb972122788d5c6c0f67c950340cb6381606
soso01 <6125024@gmail.com>
2021-08-03 08:11:18 +0900

    안쓰는 코드 삭제

--- src/js/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as MoneyInput } from "./MoneyInput.js";
-export { default as LottoList } from "./LottoList.js";
-export { default as WinnerInput } from "./WinnerInput.js";
-export { default as Result } from "./Result.js";
