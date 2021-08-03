6d7674689e36ed03a6359738dc7fd946633e5356
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-08-03 17:50:53 +0900

    feat: constants 파일

---
+++ src/js/Constants/lottoNum.js
@@ -0,0 +1,4 @@
+const MINIMUM_NUMBER = 1000;
+const UNIT_NUMBER = 1000;
+
+export { MINIMUM_NUMBER, UNIT_NUMBER };
---
+++ src/js/Constants/message.js
@@ -0,0 +1,3 @@
+export const ERROR = {
+  DUPLICATE_LOTTO_NUMBER: "로또 번호에는 중복된 숫자를 입력할 수 없습니다.",
+};
