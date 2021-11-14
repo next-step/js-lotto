f99be72d823eca6f72c1eb61e17f7ccbda954df0
Jay <cho2304@naver.com>
2021-11-14 13:54:49 +0900

    chore: parcel 포트 변경 및 refresh 적용

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,26 @@
-const $showResultButton = document.querySelector('.open-result-modal-button')
-const $modalClose = document.querySelector('.modal-close')
-const $modal = document.querySelector('.modal')
-const $lottoNumbersToggleButton = document.querySelector(
-  '.lotto-numbers-toggle-button'
-)
+import App from './App'
 
-const onModalShow = () => {
-  $modal.classList.add('open')
+// const $showResultButton = document.querySelector('.open-result-modal-button')
+// const $modalClose = document.querySelector('.modal-close')
+// const $modal = document.querySelector('.modal')
+// const $lottoNumbersToggleButton = document.querySelector(
+//   '.lotto-numbers-toggle-button'
+// )
+
+// const onModalShow = () => {
+//   $modal.classList.add('open')
+// }
+
+// const onModalClose = () => {
+//   $modal.classList.remove('open')
+// }
+
+// $showResultButton.addEventListener('click', onModalShow)
+// $modalClose.addEventListener('click', onModalClose)
+new App()
+
+if (module.hot) {
+  module.hot.dispose(function () {
+    window.location.reload()
+  })
 }
-
-const onModalClose = () => {
-  $modal.classList.remove('open')
-}
-
-$showResultButton.addEventListener('click', onModalShow)
-$modalClose.addEventListener('click', onModalClose)
