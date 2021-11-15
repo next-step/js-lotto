bf36ea1dfb6d9916903466d224629c64aaa1ba71
inhyuck <dlsgur1447@gmail.com>
2021-11-15 20:41:33 +0900

    refactor: 금액입력모듈, 구매한로또티켓모듈 컴포넌트 단위로 분리

---
+++ src/js/components/Tickets.js
@@ -0,0 +1,27 @@
+export function Tickets($el) {
+
+    function render() {
+        $el.innerHTML = `
+            <section class="mt-9">
+                <div class="d-flex">
+                  <label class="flex-auto my-0">총 5개를 구매하였습니다.</label>
+                  <div class="flex-auto d-flex justify-end pr-1">
+                    <label class="switch">
+                      <input type="checkbox" class="lotto-numbers-toggle-button" />
+                      <span class="text-base font-normal">번호보기</span>
+                    </label>
+                  </div>
+                </div>
+                <div class="d-flex flex-wrap">
+                  <span class="mx-1 text-4xl">🎟️ </span>
+                  <span class="mx-1 text-4xl">🎟️ </span>
+                  <span class="mx-1 text-4xl">🎟️ </span>
+                  <span class="mx-1 text-4xl">🎟️ </span>
+                  <span class="mx-1 text-4xl">🎟️ </span>
+                </div>
+            </section>
+        `;
+    }
+
+    render();
+}
