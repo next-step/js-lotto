c05fc4a92451db316006de447543c3238f808a08
Tanney <dhxodms89@gmail.com>
2021-11-16 14:05:34 +0900

    feat: 로또 금액 입력 시 자동 구매하는 기능 구현

---
+++ src/js/components/LottoTickets.js
@@ -0,0 +1,71 @@
+import { STATUS_MESSAGE } from "../constants/messages.js";
+import { CLASS_NAME } from "../constants/selectors.js";
+import lottoStore, { LOTTO_STORE_KEY } from "../lottoStore.js";
+
+class LottoTickets {
+  constructor($ticketContainer, $lottoPurchaseStatus, $toggleButton) {
+    this.$ticketContainer = $ticketContainer;
+    this.$lottoPurchaseStatus = $lottoPurchaseStatus;
+    this.$toggleButton = $toggleButton;
+  }
+
+  init() {
+    lottoStore.subscribeState(
+      [LOTTO_STORE_KEY.TICKET_COUNT, LOTTO_STORE_KEY.PAYMENT_CHANGES, LOTTO_STORE_KEY.LOTTO_NUMBERS],
+      this.onChangePayment.bind(this)
+    );
+
+    this.$toggleButton.addEventListener("change", this.onToggle.bind(this));
+  }
+
+  onChangePayment() {
+    this.showPurchaseStatus();
+    this.showTickets();
+  }
+
+  onToggle(event) {
+    if (event.target.checked) {
+      this.showLottoNumbers();
+    } else {
+      this.hideLottoNumbers();
+    }
+  }
+
+  showPurchaseStatus() {
+    const { ticketCount, paymentChanges } = lottoStore.selectState();
+
+    this.$lottoPurchaseStatus.textContent = STATUS_MESSAGE.LOTTO_PURCHASE(ticketCount, paymentChanges);
+  }
+
+  showTickets() {
+    const { lottoNumbers } = lottoStore.selectState();
+
+    this.$toggleButton.disabled = false;
+    this.$ticketContainer.innerHTML = lottoNumbers
+      .map(
+        (lotto) => `
+          <div class="${CLASS_NAME.TICKET.slice(1)} d-flex align-center">
+            <span class="text-3xl mr-2">🎟️ </span>
+            <span class="${CLASS_NAME.LOTTO_NUMBERS.slice(1)} text-xl d-none">${lotto.join(", ")}</span>
+          </div>
+        `
+      )
+      .join("");
+  }
+
+  showLottoNumbers() {
+    this.$ticketContainer.classList.remove("d-flex");
+    document
+      .querySelectorAll(CLASS_NAME.LOTTO_NUMBERS)
+      .forEach(($lottoNumbers) => $lottoNumbers.classList.remove("d-none"));
+  }
+
+  hideLottoNumbers() {
+    this.$ticketContainer.classList.add("d-flex");
+    document
+      .querySelectorAll(CLASS_NAME.LOTTO_NUMBERS)
+      .forEach(($lottoNumbers) => $lottoNumbers.classList.add("d-none"));
+  }
+}
+
+export default LottoTickets;
