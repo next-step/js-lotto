0500250892fb82798ff24d671cc77620d5c63935
Tanney <dhxodms89@gmail.com>
2021-11-16 14:04:48 +0900

    feat: 상태관리 객체 추상화

---
+++ src/js/store/store.js
@@ -0,0 +1,60 @@
+export const createStore = (state) => {
+  return {
+    state: Object.keys(state).reduce((stateCollection, stateKey) => {
+      return {
+        ...stateCollection,
+        [stateKey]: { value: state[stateKey], effects: [] },
+      };
+    }, {}),
+
+    setState(stateKey, value) {
+      if (!(stateKey in this.state)) {
+        throw Error(`존재하지 않는 state입니다. (stateKey: ${stateKey})`);
+      }
+
+      this.state[stateKey] = {
+        ...this.state[stateKey],
+        value,
+      };
+
+      this.state[stateKey].effects.forEach((effect) => effect());
+    },
+
+    selectState(stateKey) {
+      if (!stateKey) {
+        return Object.keys(this.state).reduce((acc, key) => {
+          return {
+            ...acc,
+            [key]: this.state[key].value,
+          };
+        }, {});
+      }
+
+      if (!(stateKey in this.state)) {
+        throw Error(`존재하지 않는 state입니다. (stateKey: ${stateKey})`);
+      }
+
+      return this.state[stateKey].value;
+    },
+
+    subscribeState(stateKey, effect) {
+      if (typeof effect !== "function") {
+        throw Error("store의 state 구독시 effect는 함수만 허용됩니다.");
+      }
+
+      const _subscribeState = (stateKey, effect) => {
+        if (!(stateKey in this.state)) {
+          throw Error(`존재하지 않는 state입니다. (stateKey: ${stateKey})`);
+        }
+
+        this.state[stateKey].effects.push(effect);
+      };
+
+      if (Array.isArray(stateKey)) {
+        stateKey.forEach((key) => _subscribeState(key, effect));
+      } else {
+        _subscribeState(stateKey, effect);
+      }
+    },
+  };
+};
