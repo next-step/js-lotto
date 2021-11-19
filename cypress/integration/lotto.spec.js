c4d2665c604193655149cf5311d255cb3000f563
roy-jung <power4ce@gmail.com>
2021-11-19 18:32:50 +0900

    step - 2

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -1,10 +1,10 @@
 // @ts-nocheck
-import { ErrorMsgs } from '../../dist/constants.js'
+import { ErrorMsgs, UNIT_PRICE } from '../../dist/constants.js'
 before(() => {
   cy.visit('http://localhost:3000/')
 })
 
-describe('lotto', () => {
+describe('lotto step-1', () => {
   it('초기에는 입력창만 보여야 함', () => {
     cy.get('purchased-info').should('not.be.visible')
     cy.get('form-winning').should('not.be.visible')
@@ -12,23 +12,23 @@ describe('lotto', () => {
   describe('입력값에 따른 에러', () => {
     it('금액 입력하지 않고 버튼 클릭시 에러', () => {
       cy.get('form-price form').submit()
-      cy.on('window.alert', text => expect(text).to.be(ErrorMsgs))
+      cy.on('window.alert', text => expect(text).to.be(ErrorMsgs.MIN_PRICE))
     })
-    it('1000원 이하 입력시 에러', () => {
-      cy.inputPrice(500)
-      cy.on('window.alert', text => expect(text).to.be(ErrorMsgs))
+    it(`${UNIT_PRICE}원 이하 입력시 에러`, () => {
+      cy.inputPrice(UNIT_PRICE / 2)
+      cy.on('window.alert', text => expect(text).to.be(ErrorMsgs.MIN_PRICE))
     })
   })
   describe('입력값에 따른 정상동작', () => {
-    it('1000원 초과 입력시 구매한 로또 수량 및 아이콘 표시', () => {
-      cy.inputPrice(3000)
+    it(`${UNIT_PRICE}원 초과 입력시 구매한 로또 수량 및 아이콘 표시`, () => {
+      cy.inputPrice(UNIT_PRICE * 3)
       cy.get('purchased-info').should('be.visible')
       cy.get('form-winning').should('be.visible')
       cy.pickedItems().should('have.length', 3)
       cy.get('label[data-cy="amount-label"]').contains(3)
     })
-    it('100원단위 이하는 버림한다.', () => {
-      cy.inputPrice(7700)
+    it('단위금액 이하는 버림한다.', () => {
+      cy.inputPrice(UNIT_PRICE * 7.7)
       cy.pickedItems().should('have.length', 7)
       cy.get('label[data-cy="amount-label"]').contains(7)
     })
@@ -46,5 +46,34 @@ describe('lotto', () => {
         })
       })
     })
+  })
+})
+
+describe('lotto step-2', () => {
+  it('당첨번호 중복 에러', () => {
+    cy.fillWinningNumbers([1, 2, 3, 4, 5, 6, 6])
+    cy.on('window.alert', text => expect(text).to.be(ErrorMsgs.DUPLICATED))
+  })
+  it('당첨번호 정상시 모달 노출', () => {
+    cy.fillWinningNumbers([
+      '{backspace}1',
+      '{backspace}2',
+      '{backspace}3',
+      '{backspace}4',
+      '{backspace}5',
+      '{backspace}6',
+      '{backspace}7',
+    ])
+    cy.get('modal-stats').should('be.visible')
+  })
+  it('x 버튼 클릭시 모달 닫기', () => {
+    cy.get('modal-stats .modal-close').click()
+    cy.get('modal-stats').should('not.be.visible')
+  })
+  it('다시시작하기 클릭시 전체 리셋', () => {
+    cy.get('form-winning form').submit()
+    cy.get('modal-stats .btn-reset').click()
+    cy.get('purchased-info').should('not.be.visible')
+    cy.get('form-winning').should('not.be.visible')
   })
 })
