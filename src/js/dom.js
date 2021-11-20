215bc942700266674dc72329bbe64783a6bd3cc4
g1moon <gmoon.cho@gmail.com>
2021-11-20 18:06:12 +0900

    feat: DOM 제어 관련 함수 추가.

---
+++ src/js/dom.js
@@ -0,0 +1,22 @@
+const template = document.createElement('div');
+
+const createElem = (elem) => {
+  if (elem instanceof HTMLElement) return elem;
+  template.replaceChildren();
+  template.insertAdjacentHTML('afterbegin', elem);
+  return template.firstElementChild;
+}
+
+const el = (parent, children = []) => {
+  const parentElem = createElem(parent);
+  if (children.length) {
+    const frag = document.createDocumentFragment();
+    children.forEach(elem => {
+      frag.appendChild(createElem(elem))
+    });
+    parentElem.replaceChildren(frag);
+  }
+  return parentElem;
+}
+
+export default el;
