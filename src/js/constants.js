508aab5d404f0585419764cdf9974dc133854b69
g1moon <gmoon.cho@gmail.com>
2021-11-20 18:05:28 +0900

    feat: 상수 추가.

---
+++ src/js/constants.js
@@ -0,0 +1,21 @@
+export const DOM_ID = {
+  PRICE_FORM: '#priceForm',
+  TOGGLE_BUTTON: '#toggleButton',
+  PURCHASE_BUTTON: '#purchaseButton',
+  PURCHASE_MESSAGE: '#purchaseMessage',
+  MONEY_INPUT: '#moneyInput',
+  LOTTO_LIST: '#lottoList',
+  LOTTO_DETAIL: '#lottoDetail',
+  LOTTO_INPUT_FORM: '#lottoInputForm',
+  LOTTO_INFO_SECTION: "#lottoInputSection",
+};
+
+export const ERROR_MESSAGE = {
+  MIN_PRICE: '최소 금액은 1000원 입니다.',
+  UNIT_PRICE: '1000원 단위로만 입력 가능합니다.',
+  NO_INPUT: '값을 입력해주세요.',
+}
+
+export const LOTTO_PRICE = 1000;
+export const LOTTO_LENGTH = 7;
+export const LOTTO_MAX_NUMBER = 45;
