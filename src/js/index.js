eec7c4f5606510f3ea8be623c919d2bc06301d33
jho2301 <jho2301@gmail.com>
2021-11-20 15:37:52 +0900

    feat: 복권 번호는 번호보기 토글 버튼을 클릭하면, 숫자를 확인할 수 있어야 한다.

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,51 @@
 const $paymentForm = document.querySelector('.js-payment-form');
 const $lottoCount = document.querySelector('.js-lotto-count');
 const $lottoContainer = document.querySelector('.js-lotto-container');
+const $lottoToggle = document.querySelector('.js-toggle');
 
 const LOTTO_PRICE = 1000;
 
-const lottoTemplate = '<span class="mx-1 text-4xl js-lotto-ticket">🎟️ </span>';
+const lottoTemplate = (lottoNumbers = []) => `
+  <div>
+    <span class="mx-1 text-4xl js-lotto-ticket">🎟️ </span>
+    <span class="js-lotto-numbers">${lottoNumbers.join(', ')}</span>
+  </div>
+`;
 
 const getAmount = (price) => Math.floor(price / LOTTO_PRICE);
+
+const getLottoNumber = () => Math.floor(Math.random() * 45) + 1;
+
+// 로또 한 장을 발행한다.
+const issueLotto = () => {
+  const lottoNumbers = [];
+
+  for (let i = 0; i < 6; i++) {
+    const lottoNumber = getLottoNumber();
+    lottoNumbers.push(lottoNumber);
+  }
+  return lottoNumbers;
+};
+
+// 로또를 전달받은 개수만큼 발행한다.
+const issueLottos = (amount) => {
+  const lottos = [];
+
+  for (let i = 0; i < amount; i++) {
+    const lotto = issueLotto();
+    lottos.push(lotto);
+  }
+
+  return lottos;
+};
 
 const buyLotto = (price) => {
   const amount = getAmount(price);
 
+  const lottos = issueLottos(amount);
+
   $lottoCount.textContent = amount;
-  $lottoContainer.innerHTML = lottoTemplate.repeat(amount);
+  $lottoContainer.innerHTML = lottos.map(lottoTemplate).join('');
 };
 
 const handlePayment = (event) => {
@@ -24,3 +57,13 @@ const handlePayment = (event) => {
 };
 
 $paymentForm.addEventListener('submit', handlePayment);
+
+$lottoToggle.addEventListener('change', (event) => {
+  const lottoNumberHidden = 'lotto-number-hidden';
+
+  if (event.target.checked) {
+    $lottoContainer.classList.remove(lottoNumberHidden);
+  } else {
+    $lottoContainer.classList.add(lottoNumberHidden);
+  }
+});
