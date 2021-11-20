40e3044265660f6c9faaa2cba1a183c626282a24
g1moon <gmoon.cho@gmail.com>
2021-11-20 18:05:10 +0900

    feat: view 추가.

---
+++ src/js/view/LottoInfo.js
@@ -0,0 +1,32 @@
+import View from "./view.js";
+import {$, $$} from "../utils.js";
+import {DOM_ID} from "../constants.js";
+import el from "../dom.js";
+
+export default class LottoInfo extends View {
+  constructor() {
+    super();
+    this.$lottoInfoSection = $(DOM_ID.LOTTO_INFO_SECTION);
+    this.$lottoList = $(DOM_ID.LOTTO_LIST);
+    this.$toggleButton = $(DOM_ID.TOGGLE_BUTTON);
+    this.$$lottoDetail = $$(DOM_ID.LOTTO_DETAIL);
+  }
+
+  updateLottoList(lottos) {
+    el(this.$lottoList,
+      lottos.map(lotto =>
+        el(`<li class="mx-1 text-4xl lotto-wrapper">`, [
+          `<span class='lotto-icon'>🎟️</span>`,
+          `<span id='lottoDetail' class="lotto-detail" style="display: none;">${lotto}</span>`
+        ])
+      ),
+    );
+    this.$$lottoDetail = $$(DOM_ID.LOTTO_DETAIL);
+  }
+
+  bindOnClickToggleButton(handler) {
+    this.$toggleButton.addEventListener('click', event => {
+      handler(event);
+    });
+  }
+};
