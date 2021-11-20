6b4e0c7ff21d91edcc4b68bae845b78bac291618
jho2301 <jho2301@gmail.com>
2021-11-20 13:49:55 +0900

    test: 로또 구입 금액을 입력하면, 금액에 해당하는 로또를 구입해야 한다.

---
+++ cypress/integration/lotto.spec.js
@@ -0,0 +1,13 @@
+const BASE_URL = 'http://127.0.0.1:5500';
+
+before(() => {
+  cy.visit(BASE_URL);
+});
+
+describe('로또 게임', () => {
+  it('로또 구입 금액을 입력하면, 금액에 해당하는 로또를 구입해야 한다.', () => {
+    cy.get('.js-payment-input').type(5000);
+    cy.get('.js-confirm-button').click();
+    cy.get('.js-lotto-count').should('have.text', '5');
+  });
+});
