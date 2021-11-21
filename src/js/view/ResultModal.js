30dc7947e74648f43f4f25871b08279d4013d945
g1moon <gmoon.cho@gmail.com>
2021-11-21 20:51:23 +0900

    feat: ResultModal(결과 확인) view 추가.

---
+++ src/js/view/ResultModal.js
@@ -0,0 +1,32 @@
+import View from "./view.js";
+import {$, $$} from "../utils.js";
+import {DOM_ID} from "../constants.js";
+
+export default class ResultModal extends View {
+  constructor() {
+    super();
+    this.$resultModal = $(DOM_ID.RESULT_MODAL);
+    this.$resultCountList = $$(DOM_ID.RESULT_COUNT_LIST);
+    this.$profitMessage = $(DOM_ID.PROFIT_MESSAGE);
+  }
+
+  show() {
+    this.$resultModal.classList.add('open');
+  }
+
+  hide() {
+    this.$resultModal.classList.remove('open');
+  }
+
+  bindOnClickBlackout(handler) {
+    addEventListener('click', (event) => {
+      handler(event, this.$resultModal);
+    });
+  }
+
+  updateModal(result) {
+    Object.values(result).forEach((count, index) => {
+      this.$resultCountList[index].innerText = `${count}개`;
+    });
+  }
+};
