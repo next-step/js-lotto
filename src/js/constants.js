f775afb70fbdb17c00df728579ae64ff83d55561
g1moon <gmoon.cho@gmail.com>
2021-11-21 20:47:07 +0900

    add: 상수추가.

--- src/js/constants.js
+++ src/js/constants.js
@@ -8,16 +8,42 @@ export const DOM_ID = {
   LOTTO_DETAIL: '#lottoDetail',
   LOTTO_INPUT_FORM: '#lottoInputForm',
   LOTTO_INFO_SECTION: "#lottoInputSection",
+  RESULT_MODAL: '.modal',
+  RESULT_COUNT_LIST: '.modal tr .count',
+  GET_RESULT_BUTTON: '#getResultButton',
+  WINNING_NUMBERS: '#resultInputContainer input',
+  BONUS_NUMBER: '#bonusNumber',
+  PROFIT_MESSAGE: '#profitMessage',
 };
 
 export const ERROR_MESSAGE = {
   MIN_PRICE: '최소 금액은 1000원 입니다.',
   UNIT_PRICE: '1000원 단위로만 입력 가능합니다.',
   NO_INPUT: '값을 입력해주세요.',
+  MAX_NUMBER: '로또 번호는 45를 초과 할 수 없습니다.',
+  MIN_NUMBER: '로또 번호는 0 이상이어야 합니다.',
+  DUPLICATED_NUMBER: '중복되지 않게 로또 번호를 입력하세요.',
+  NONE_VALUE: '보너스 번호를 포함한 7개의 숫자를 입력하세요.',
+};
+
+export const PRIZE_TITLE = {
+  FIRST: 'first',
+  SECOND: 'second',
+  THIRD: 'third',
+  FOURTH: 'fourth',
+  FIFTH: 'fifth',
+}
+
+export const PRIZE = {
+  FIRST: 2000000000,
+  SECOND: 30000000,
+  THIRD: 1500000,
+  FOURTH: 50000,
+  FIFTH: 5000,
 }
 
 export const LOTTO_PRICE = 1000;
-export const LOTTO_LENGTH = 7;
+export const LOTTO_LENGTH = 6;
 export const LOTTO_MAX_NUMBER = 45;
 export const WINNING_NUMBERS_LENGTH = 7;
 export const MAX_LOTTO_NUMBER = 45;
