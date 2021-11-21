9bd6119bba2f6a875eed4b9e438766ca8f0c82b8
g1moon <gmoon.cho@gmail.com>
2021-11-21 20:51:51 +0900

    feat: 두개의 Array 일치 개수 계산하는 유틸 함수 추가.

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,15 @@
+import {LOTTO_LENGTH} from "./constants.js";
+
 export const $ = (selector, el = document) => el.querySelector(selector);
 
 export const $$ = (selector, el = document) => el.querySelectorAll(selector);
 
-export const getFormDataValue = (formData, key) => formData.get(key);
+export const countSameNumbers = (arr1, arr2) => {
+  let result = 0;
+  for (let i = 0; i < LOTTO_LENGTH; i++) {
+    if (arr1.includes(arr2[i])) {
+      result++;
+    }
+  }
+  return result;
+};
