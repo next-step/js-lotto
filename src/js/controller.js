fc2486410c956dd8e7af287515d6351672e585e5
g1moon <gmoon.cho@gmail.com>
2021-11-21 20:46:53 +0900

    feat: 결과 확인 눌렀을 경우와 결과창 배경화면 클릭했을 때 이벤트 추가.

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,10 @@
-import {ERROR_MESSAGE} from "./constants.js";
+import {
+  ERROR_MESSAGE,
+} from "./constants.js";
 import LottoForm from "./view/LottoForm.js";
 import LottoInfo from "./view/LottoInfo.js";
 import PriceForm from "./view/PriceForm.js";
+import ResultModal from "./view/ResultModal.js";
 
 export default class Controller {
   constructor(model, view) {
@@ -11,14 +14,16 @@ export default class Controller {
     this.LottoForm = new LottoForm();
     this.LottoInfo = new LottoInfo();
     this.PriceForm = new PriceForm();
+    this.ResultModal = new ResultModal();
 
     this.LottoInfo.bindOnClickToggleButton(this.onClickToggleButton);
     this.PriceForm.bindOnClickPurchaseButton(this.onClickPurchaseButton);
+    this.LottoForm.bindOnClickGetResultButton(this.onClickResultButton);
+    this.ResultModal.bindOnClickBlackout(this.onClickBlackout);
   }
 
   onClickPurchaseButton = (event) => {
     event.preventDefault();
-    this.model.initData();
     const price = this.PriceForm.$moneyInput.value;
     if (price.length === 0) throw Error(ERROR_MESSAGE.NO_INPUT);
 
@@ -30,12 +35,27 @@ export default class Controller {
 
     this.view.show(this.LottoForm.$lottoInputForm);
     this.view.show(this.LottoInfo.$lottoInfoSection);
-  }
+  };
 
   onClickToggleButton = () => {
     this.LottoInfo.$$lottoDetail.forEach(item => {
       item.style.display = item.style.display === 'none' ? 'inline' : 'none';
     });
+  };
+
+  onClickResultButton = (winningNumbers, bonusNumber) => {
+    this.LottoForm.checkLottoNumber(winningNumbers, bonusNumber);
+    this.ResultModal.show();
+    this.model.setResult(winningNumbers, bonusNumber);
+    this.model.setProfit();
+    this.model.setProfitMessage(this.ResultModal.$profitMessage);
+    console.log(this.model.data.result);
+    this.ResultModal.updateModal(this.model.data.result);
+  };
+
+  onClickBlackout = (event, $resultModal) => {
+    if (event.target === $resultModal
+      && $resultModal.classList.contains('open')) this.ResultModal.hide();
   };
 }
 
