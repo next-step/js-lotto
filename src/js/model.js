306d876d3562d6fada54dcb7eb1d5bf634c90e49
g1moon <gmoon.cho@gmail.com>
2021-11-21 20:50:50 +0900

    feat: 결과에 대한 model setter 추가.

--- src/js/model.js
+++ src/js/model.js
@@ -1,12 +1,30 @@
-import {ERROR_MESSAGE, LOTTO_LENGTH, LOTTO_MAX_NUMBER, LOTTO_PRICE} from './constants.js';
+import {
+  ERROR_MESSAGE,
+  LOTTO_LENGTH,
+  LOTTO_MAX_NUMBER,
+  LOTTO_PRICE,
+  PRIZE,
+  PRIZE_TITLE
+} from './constants.js';
+import {countSameNumbers} from "./utils.js";
 
 export default class Model {
   constructor() {
-    this.initData();
-  }
+    this.data = {
+      amount: 0,
+      totalPrize: 0,
+      profit: 0,
+      lottos: [],
+      result: {
+        [PRIZE_TITLE.FIRST]: 0,
+        [PRIZE_TITLE.SECOND]: 0,
+        [PRIZE_TITLE.THIRD]: 0,
+        [PRIZE_TITLE.FOURTH]: 0,
+        [PRIZE_TITLE.FIFTH]: 0,
+      }
+    };
 
-  initData() {
-    this.data = { amount: 0, lottos: [] };
+    console.log(this.data.result);;
   }
 
   setAmount(money) {
@@ -22,5 +40,41 @@ export default class Model {
 
   createOneLotto() {
     return Array.from(Array(LOTTO_LENGTH)).map(x => Math.floor(Math.random() * LOTTO_MAX_NUMBER));
+  }
+
+  setResult(winningNumbers, bonusNumber) {
+    this.data.lottos.forEach(lotto => {
+      switch (countSameNumbers(lotto, winningNumbers)) {
+        case 6:
+          this.data.result[PRIZE_TITLE.FIRST]++;
+          this.data.totalPrize += PRIZE.FIRST;
+          break;
+        case 5:
+          if (lotto.includes(bonusNumber)) {
+            this.data.result[PRIZE_TITLE.SECOND]++;
+            this.data.totalPrize += PRIZE.SECOND;
+            return;
+          }
+          this.data.result[PRIZE_TITLE.THIRD]++;
+          this.data.totalPrize += PRIZE.THIRD;
+          break;
+        case 4:
+          this.data.result[PRIZE_TITLE.FOURTH]++;
+          this.data.totalPrize += PRIZE.FOURTH;
+          break;
+        case 3:
+          this.data.result[PRIZE_TITLE.FIFTH]++;
+          this.data.totalPrize += PRIZE.FIFTH;
+          break;
+      }
+    });
+  }
+
+  setProfit() {
+    this.data.profit = Math.floor(((this.data.totalPrize - (this.data.amount * LOTTO_PRICE)) / (this.data.amount * LOTTO_PRICE)) * 100);
+  }
+
+  setProfitMessage($profitMessage) {
+    $profitMessage.innerText = `당신의 총 수익률은 ${this.data.profit}% 입니다.`;
   }
 };
