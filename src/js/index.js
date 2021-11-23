c0edc4c44fd6b2fe2aa78cfc8b4bca4c2eace7f3
정민희 <50939886+alsgml1640@users.noreply.github.com>
2021-11-23 15:07:44 +0900

    [클린코드 1기 정민희] 로또 미션 STEP1 (#48)
    * chore: 개발 환경 세팅
    
    Co-authored-by: 정민희 <alsgml1640@users.noreply.github.com>
    
    * test: 로또 구입 금액을 입력하면, 금액에 해당하는 로또를 구입해야 한다.
    
    * feat: 로또 구입 금액을 입력하면, 금액에 해당하는 로또를 구입해야 한다.
    
    * test: 복권 번호는 번호보기 토글버틀을 클릭하면, 숫자를 확인할수 있어야 한다.
    
    * feat: 복권 번호는 번호보기 토글 버튼을 클릭하면, 숫자를 확인할 수 있어야 한다.
    
    * 변경 요청 일부 수정
    
    Co-authored-by: jho2301 <jho2301@gmail.com>
    Co-authored-by: 정민희 <alsgml1640@users.noreply.github.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,73 @@
-const $showResultButton = document.querySelector('.open-result-modal-button')
-const $modalClose = document.querySelector('.modal-close')
-const $modal = document.querySelector('.modal')
-const $lottoNumbersToggleButton = document.querySelector(
-  '.lotto-numbers-toggle-button'
-)
+const $paymentForm = document.querySelector('.js-payment-form');
+const $lottoCount = document.querySelector('.js-lotto-count');
+const $lottoContainer = document.querySelector('.js-lotto-container');
+const $lottoToggle = document.querySelector('.js-toggle');
 
-const onModalShow = () => {
-  $modal.classList.add('open')
-}
+const LOTTO_PRICE = 1000;
 
-const onModalClose = () => {
-  $modal.classList.remove('open')
-}
+const lottoTemplate = (lottoNumbers = []) => `
+  <div>
+    <span class="mx-1 text-4xl js-lotto-ticket">🎟️ </span>
+    <span class="js-lotto-numbers">${lottoNumbers.join(', ')}</span>
+  </div>
+`;
 
-$showResultButton.addEventListener('click', onModalShow)
-$modalClose.addEventListener('click', onModalClose)
+
+const getAmount = (price) => Math.floor(price / LOTTO_PRICE);
+
+
+const getLottoNumber = () => Math.floor(Math.random() * 45) + 1;
+
+// 로또 한 장을 발행한다.
+const setLotto = () => {
+  const lottoNumbers = [];
+  
+  for (let i = 0; i < 6; i++) {
+    const lottoNumber = getLottoNumber();
+    lottoNumbers.push(lottoNumber);
+  }
+  return lottoNumbers;
+};
+
+// 로또를 전달받은 개수만큼 발행한다.
+const setLottos = (amount) => {
+  const lottos = [];
+
+  for (let i = 0; i < amount; i++) {
+    const lotto = setLotto();
+    lottos.push(lotto);
+  }
+
+  return lottos;
+};
+
+const handlePayment = (event) => {
+  event.preventDefault();
+
+  const price = event.target.elements['price'].value;
+  
+  if(price < 1000) {
+    alert('1000원 이상 구매 가능합니다!')
+    return;
+  }
+
+  buyLotto(price);
+};
+
+const buyLotto = (price) => {
+  const amount = getAmount(price);
+  const lottos = setLottos(amount);
+
+  $lottoCount.textContent = amount;
+  $lottoContainer.innerHTML = lottos.map(lottoTemplate).join('');
+};
+
+
+$paymentForm.addEventListener('submit', handlePayment);
+
+$lottoToggle.addEventListener('change', (event) => {
+  const lottoNumberHidden = 'lotto-number-hidden';
+
+  event.target.checked == true ? $lottoContainer.classList.remove(lottoNumberHidden) :  $lottoContainer.classList.add(lottoNumberHidden);
+  
+});
