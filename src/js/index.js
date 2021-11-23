45e5c23be5e1235ca9921ccb3fbb6c8a54af84f6
ganeodolu <ganeodolu@gmail.com>
2021-11-23 16:59:48 +0900

    Revert "add"
    This reverts commit 9f390e01b80f55a1c376fc1798638296c7fc2ba7.

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,17 @@
-import Model from './model.js';
-import View from './view.js';
-import Controller from "./controller.js";
-
-// document.addEventListener('DOMContentLoaded', (event) => {
-//   console.log('DOM fully loaded and parsed');
-//   console.log('DOM fully loaded and parsed');
-//   console.log('DOM fully loaded and parsed');
-//   console.log('DOM fully loaded and parsed');
-// });
-
 const $showResultButton = document.querySelector('.open-result-modal-button')
 const $modalClose = document.querySelector('.modal-close')
 const $modal = document.querySelector('.modal')
-
 const $lottoNumbersToggleButton = document.querySelector(
   '.lotto-numbers-toggle-button'
 )
 
 const onModalShow = () => {
-  console.log('dddssd');
   $modal.classList.add('open')
 }
 
 const onModalClose = () => {
-  console.log('dddssd');
   $modal.classList.remove('open')
 }
 
-
 $showResultButton.addEventListener('click', onModalShow)
 $modalClose.addEventListener('click', onModalClose)
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Controller(new Model(), new View());
-  console.log('hello');
-});
--- src/js/utils.js
+++
@@ -1,6 +0,0 @@
-export const $ = (selector, elements = document) => elements.querySelector(selector);
-
-export const $$ = (selector, elements = document) => elements.querySelectorAll(selector);
-
-export const getFormDataValue = (formData, key) => formData.get(key);
-
