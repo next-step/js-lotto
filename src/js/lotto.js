2a2df2e446ebce76288a284a679aa8d0ad8e9e21
che-ri <rnjs1135@gmail.com>
2021-11-24 18:23:40 +0900

    feat : 로또 6자리 자동숫자 만들기

--- src/js/lotto.js
+++ src/js/lotto.js
@@ -1,5 +1,7 @@
 const initialState = {
   lottoCnt: 0,
+  lottoNumerList: [],
+  isChecked: false,
 };
 
 export default class Lotto {
@@ -38,23 +40,65 @@ export default class Lotto {
   }
 
   onClickToggleBtn(e) {
+    //번호보기버튼 스위치
+    this.clearBoard();
+
     const isChecked = e.target.checked;
+    if (isChecked) {
+      console.log("번호 보여주자!");
+      this.state.isChecked = true;
+      return;
+    }
+    this.state.isChecked = false;
   }
 
   onConfirmBtnClick() {
+    //지불액에 따라 로또를 구입하는 함수
     const payment = Number(this.$payment.value);
     const lottoPrice = 1000;
     const numberOfPurchase = Math.trunc(payment / lottoPrice);
     this.state.lottoCnt = numberOfPurchase;
+    this.setLottoNum();
     this.renderLotto();
+  }
+
+  setLottoNum() {
+    //state에 로또숫자들을 저장시키는 함수
+    this.state.lottoNumerList = []; //초기화
+
+    const cnt = this.state.lottoCnt;
+    for (let i = 0; i < cnt; i++) {
+      const autoNumArr = this.makeAutoLottoNum();
+      this.state.lottoNumerList.push(autoNumArr);
+    }
+  }
+
+  makeAutoLottoNum() {
+    //로또 한 장에 대한 자동으로 숫자 6자리를 만들어내는 함수
+    let result = [];
+    let randomCnt = 0; //<-랜덤카운팅이 6까지 일때 while문을 돌릴 것이다.
+    while (randomCnt < 6) {
+      //한장 당 만들 수 있는 랜덤숫자갯수가 6개까지이므로 6까지 만들 것임.
+      const random_1_45 = Math.ceil(Math.random() * 45);
+      if (result.includes(random_1_45)) continue;
+      result.push(random_1_45);
+      randomCnt++;
+    }
+    return result;
+  }
+
+  clearBoard() {
+    //보드를 모두 지워주는 함수
+    this.$renderLotto.innerHTML = "";
   }
 
   renderLotto() {
     // <span class="mx-1 text-4xl">🎟️ </span>
-    const cnt = this.state.lottoCnt;
-    this.$renderLotto.innerHTML = ""; //초기화
+    this.clearBoard();
 
-    //로또를 구입한 갯수만큼 node 추가
+    const cnt = this.state.lottoCnt;
+
+    //로또를 구입한 갯수만큼 node 추가하여 화면에 렌더링
     for (let i = 0; i < cnt; i++) {
       const newSpan = document.createElement("span");
       newSpan.classList.add("mx-1", "text-4xl");
