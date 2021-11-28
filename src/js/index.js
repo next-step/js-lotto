5bc27b23115ab2bfe9a6eb46748f54cc95394211
g1moon <gmoon.cho@gmail.com>
2021-11-28 22:09:46 +0900

    clear cache

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,9 @@ import Controller from './controller.js';
 import Model from './model.js';
 import View from './view/view.js';
 
-// window.onerror = function (msg) {
-//   alert(msg);
-//   return true;
-// };
+window.onerror = function (msg) {
+  alert(msg);
+  return true;
+};
 
 new Controller(new Model(), new View());
