df88c023dcd01d62b418df54239550ed0a655752
jho2301 <jho2301@gmail.com>
2021-11-29 18:48:41 +0900

    feat: 로또를 구매하지 않으면 결과를 확인할 수 없다.

--- src/js/eventHandler/answer.js
+++ src/js/eventHandler/answer.js
@@ -3,6 +3,8 @@ import lottoManager from '../model/lotto.js';
 import { focusPaymentInput, resetMainView } from '../view/main.js';
 import { closeModal, openModal, updateResultView } from '../view/resultModal.js';
 
+const isLottoEmpty = () => !lottoManager.lottos.length;
+
 const isAnswerValid = (answer) => {
   return Object.values(answer).every((inputNumber) => inputNumber || inputNumber >= 1);
 };
@@ -18,6 +20,11 @@ export const handleAnswer = (event) => {
   event.preventDefault();
 
   const answer = filterAnswer(event.target.elements);
+
+  if (isLottoEmpty()) {
+    alert('로또를 구매하지 않았습니다. 로또를 구매한 후 결과를 확인해 주세요.');
+    return;
+  }
 
   if (!isAnswerValid(answer)) {
     alert('빈 칸이 있습니다. 모두 입력 해주세요.');
