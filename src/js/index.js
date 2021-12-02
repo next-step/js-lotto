ea8d60e990b072b110298cef45fd1f0a48287a1a
ganeodolu <ganeodolu@gmail.com>
2021-12-03 02:41:40 +0900

    fix: 토글 체크 상태에서 로또 구매시 번호 보일 수 있게 변경

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,6 @@ import App from './App.js';
 const $showResultButton = document.querySelector('.open-result-modal-button')
 const $modalClose = document.querySelector('.modal-close')
 const $modal = document.querySelector('.modal')
-const $lottoNumbersToggleButton = document.querySelector(
-  '.lotto-numbers-toggle-button'
-)
 
 const onModalShow = () => {
   $modal.classList.add('open')
