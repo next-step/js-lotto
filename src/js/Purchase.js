fbf67d5cee63ad4018589d98d2db07accf3dcce9
ganeodolu <ganeodolu@gmail.com>
2021-12-03 02:41:40 +0900

    fix: 토글 체크 상태에서 로또 구매시 번호 보일 수 있게 변경

--- src/js/Purchase.js
+++ src/js/Purchase.js
@@ -17,20 +17,31 @@ export default class Purchase {
 		this.$targetPurchaseToggle = $targetPurchaseToggle;
 		this.$targetLottoInfo = $targetLottoInfo;
 
-		const clickToggleEvent = (e) => {
-			this.$targetLottoInfo.classList.toggle("flex-col");
+		const clickToggleEvent = () => {
 			const lottoNumbersNodeList = this.$targetLottoInfo.querySelectorAll(
 				".lotto-numbers"
 			);
-			lottoNumbersNodeList.forEach((node) => node.classList.toggle("hidden"));
+			if (this.$targetPurchaseToggle.checked) {
+				this.$targetLottoInfo.classList.add("flex-col");
+				lottoNumbersNodeList.forEach((node) => node.classList.remove("hidden"));
+			} else {
+				this.$targetLottoInfo.classList.remove("flex-col");
+				lottoNumbersNodeList.forEach((node) => node.classList.add("hidden"));
+			}
 		};
+
 		this.$targetPurchaseToggle.addEventListener("click", clickToggleEvent);
 	}
 	render() {
-		this.$targetPurchaseInfo.classList.remove("hidden");
-		this.$targetPurchaseCount.innerText = countLottoTemplate(this.count);
-		this.purchasedLottos = makeLottoNumbers(this.count);
-		this.$targetLottoInfo.innerHTML = fullLottoTemplate(this.purchasedLottos);
+		if (this.count >= 1) {
+			this.$targetPurchaseInfo.classList.remove("hidden");
+			this.$targetPurchaseCount.innerText = countLottoTemplate(this.count);
+			this.purchasedLottos = makeLottoNumbers(this.count);
+			this.$targetLottoInfo.innerHTML = fullLottoTemplate(
+				this.purchasedLottos,
+				this.$targetPurchaseToggle.checked
+			);
+		}
 	}
 	setState(nextCount) {
 		this.count = nextCount;
--- src/js/utils/templates.js
+++ src/js/utils/templates.js
@@ -1,13 +1,13 @@
 export const countLottoTemplate = (countLotto) =>
 	`총 ${countLotto}개를 구매하였습니다.`;
 
-export const fullLottoTemplate = (array) =>
+export const fullLottoTemplate = (array, isChecked) =>
 	array
 		.map(
-			(subArray) => /* html */ `
+			(subArray) => /*html*/`
         <span class="mx-1 text-4xl">
           <span class="lotto-icon">🎟️</span>
-          <span class="lotto-numbers hidden">${subArray
+          <span class="lotto-numbers ${isChecked ? '' : 'hidden'}">${subArray
 						.map((val) =>
 							String(val).length === 2 ? String(val) : "&nbsp" + String(val)
 						)
