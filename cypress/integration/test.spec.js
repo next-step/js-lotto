13a54653900d7c5f1e42d1c79b6c5bc13c9a215a
ganeodolu <ganeodolu@gmail.com>
2021-12-05 01:44:01 +0900

    fix: 필요없는 비동기 삭제

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -6,44 +6,32 @@ describe("lotto test", () => {
 	it("Step1 - 1000원 미만 구매시 로또를 살 수 없고, 경고창을 표시한다.", () => {
 		const alertStub = cy.stub();
 		cy.on("window:alert", alertStub);
-		cy.get(".payment-input")
-			.type("500")
+		cy.get(".payment-input").type("500");
+		cy.get(".payment-submit")
+			.click()
 			.then(() => {
-				cy.get(".payment-submit")
-					.click()
-					.then(() => {
-						expect(alertStub.getCall(0)).to.be.calledWith(
-							"1,000원 이상 입력해주세요"
-						);
-					});
-			});
-	});
-
-	it("Step1 - 1000원 이상 구매시 로또를 살 수 있다", () => {
-		cy.get(".payment-input")
-			.type("3000")
-			.then(() => {
-				cy.get(".payment-submit").click();
-				cy.get(".purchase-count").should(
-					"have.text",
-					"총 3개를 구매하였습니다."
+				expect(alertStub.getCall(0)).to.be.calledWith(
+					"1,000원 이상 입력해주세요"
 				);
 			});
 	});
 
+	it("Step1 - 1000원 이상 구매시 로또를 살 수 있다", () => {
+		cy.get(".payment-input").type("3000");
+		cy.get(".payment-submit").click();
+		cy.get(".purchase-count").should("have.text", "총 3개를 구매하였습니다.");
+	});
+
 	it("Step1 - 로또 구매 후 토글버튼을 누르면 번호를 확인할 수 있다", () => {
-		cy.get(".payment-input")
-			.type("2000")
-			.then(() => {
-				cy.get(".payment-submit").click();
-				cy.get(".text-base").click();
-				cy.get(".lotto-numbers")
-					.first()
-					.should(($target) => {
-						const text = $target.text();
-						console.log(text);
-						expect(text.split(",").length).to.eq(6);
-					});
+		cy.get(".payment-input").type("2000");
+		cy.get(".payment-submit").click();
+		cy.get(".text-base").click();
+		cy.get(".lotto-numbers")
+			.first()
+			.should(($target) => {
+				const text = $target.text();
+				console.log(text);
+				expect(text.split(",").length).to.eq(6);
 			});
 	});
 });
