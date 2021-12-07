93b5abe3ea36477a63e06262fda15755f6ba343a
ganeodolu <ganeodolu@gmail.com>
2021-12-07 20:23:15 +0900

    fix: 리뷰 반영

--- src/js/Payment.js
+++ src/js/Payment.js
@@ -2,18 +2,18 @@ import { calculatePayment } from "./utils/functions.js";
 // import { countLottoTemplate } from "./utils/templates.js";
 
 export default class Payment {
-	constructor({ $payment, onSubmit }) {
-		this.$payment = $payment;
-		this.$paymentInput = document.querySelector(".payment-input");
+	constructor({ $paymentForm, $paymentFormInput, setPurchaseCount }) {
+		this.$paymentForm = $paymentForm;
+		this.$paymentFormInput = $paymentFormInput;
 
 		const clickSubmitEvent = (e) => {
 			e.preventDefault();
 			if (e.target.classList.contains("payment-submit")) {
-				const countLotto = calculatePayment(this.$paymentInput.valueAsNumber);
-				if (countLotto > 0) onSubmit(countLotto);
+				const countLotto = calculatePayment(this.$paymentFormInput.valueAsNumber);
+				if (countLotto > 0) setPurchaseCount(countLotto);
 			}
 		};
-		this.$paymentInput.addEventListener("click", (e) => (e.target.value = ""));
-		this.$payment.addEventListener("click", clickSubmitEvent);
+		this.$paymentFormInput.addEventListener("click", (e) => (e.target.value = ""));
+		this.$paymentForm.addEventListener("click", clickSubmitEvent);
 	}
 }
