3e30758522ab6656757f54085159447d895f3922
ganeodolu <ganeodolu@gmail.com>
2021-12-09 03:27:00 +0900

    feat: 당첨통계 및 수익률 구현

--- src/js/ResultModal.js
+++ src/js/ResultModal.js
@@ -1,10 +1,26 @@
+import { countLottoRank, calculateProfitRate } from "./utils/functions.js";
+import { resultLottoTemplate, profitRateTemplate } from "./utils/templates.js";
+
 export default class ResultModal {
-	constructor({ $showResultButton, $modal, $modalClose }) {
+	constructor({
+		purchasedLottos,
+		winningNumberArray,
+		$showResultButton,
+		$modal,
+		$modalClose,
+		$resultTable,
+		$resultProfitRate,
+	}) {
+		this.purchasedLottos = purchasedLottos;
+		this.winningNumberArray = winningNumberArray;
 		this.$showResultButton = $showResultButton;
 		this.$modal = $modal;
 		this.$modalClose = $modalClose;
+		this.$resultTable = $resultTable;
+		this.$resultProfitRate = $resultProfitRate;
 
 		const onModalShow = () => {
+			if(this.purchasedLottos.length < 1) return
 			this.$modal.classList.add("open");
 		};
 
@@ -12,7 +28,27 @@ export default class ResultModal {
 			this.$modal.classList.remove("open");
 		};
 
-		// this.$showResultButton.addEventListener("click", onModalShow);
+		this.$showResultButton.addEventListener("click", onModalShow);
 		this.$modalClose.addEventListener("click", onModalClose);
+	}
+
+	render() {
+		const lottoRank = countLottoRank(
+			this.purchasedLottos,
+			this.winningNumberArray
+		);
+		console.log(lottoRank);
+		this.$resultTable.innerHTML = resultLottoTemplate(lottoRank);
+		const profitRate = calculateProfitRate(
+			lottoRank,
+			this.purchasedLottos.length
+		);
+		this.$resultProfitRate.innerText = profitRateTemplate(profitRate);
+	}
+
+	setLottoRank(nextPurchasedLottos, nextWinningNumberArray) {
+		this.purchasedLottos = nextPurchasedLottos;
+		this.winningNumberArray = nextWinningNumberArray;
+		this.render();
 	}
 }
