010b26d00d8f48cfd22bd3b7e35dbe2c4cbf2a33
Soeun/Sona Lee <mojosoeun@gmail.com>
2021-12-13 12:28:42 +0900

    [클린코드 1기 이소은] 로또 미션 STEP 2 (#69)
    안녕하세요 기능상 깔끔하게 구현해주신 코드들 잘 봤습니다
    일정상 일단 Approve 하도록 하겠습니다
    피드백은 아래에 기재해 드리니 참고하실만한 부분이 있다면 추후에 참고하시면 될 것 같습니다
    
    기능상으로 아주 큰 금액이 들어갈 경우 시스템이 정지합니다. 예외상황(예: 아주 큰 값, 잘못된 값 등)에 대한 처리가 추가된다면 보다 탄탄한 코드가 될 것 같습니다
    아주 큰 금액이 들어가서 로또의 갯수가 많아지면 화면영역이 너무 길어지게 됩니다. 로또를 표시하는 영역의 크기를 제한하면 보다 좋은 UI가 될 것 같습니다
    python으로 서버를 띄우고 계시는데 정적자원일 경우 따로 서버가 필요하지 않습니다
    테스트 코드가 다소 단순하게 구현되어 있습니다. 성공하는 경우에 대한 극히 일부분의 테스트만 작성되어 있어서 보다 복잡한 기능상의 오류는 발견하기 힘들 것 같습니다. 테스트를 좀 더 고도화 해보시길 권해 드립니다

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,28 +1,75 @@
 import { DEFAULT_PRICE, EVENT } from "../utils/const.js";
-import { generateLotteries } from "../utils/lottery.js";
+import { generateLotteries, checkWinningNumbers, calculateProfit } from "../utils/lottery.js";
 
 const event = new Event("stateChange");
 
-export const Store = {
-  state: {
-    price: 0,
-    amount: 0,
-    lottoNumbers: [],
-    checked: false,
+const result = {
+  "lotto-3": {
+    count: 0,
+    price: 5000,
   },
+  "lotto-4": {
+    count: 0,
+    price: 50000,
+  },
+  "lotto-5": {
+    count: 0,
+    price: 1500000,
+  },
+  "lotto-5-bonus": {
+    count: 0,
+    price: 30000000,
+  },
+  "lotto-6": {
+    count: 0,
+    price: 2000000000,
+  },
+};
+
+export const initialState = {
+  price: 0,
+  amount: 0,
+  lottoNumbers: [],
+  checked: false,
+  showModal: false,
+  winningNumbers: [],
+  bonusNumber: "",
+  profit: 0,
+  result,
+};
+
+export const Store = {
+  state: initialState,
 };
 
 export function setState(key, state) {
   switch (key) {
     case EVENT.INPUT_PRICE:
-      let newState = {};
-      if (state.price > DEFAULT_PRICE) {
+      if (state.price >= DEFAULT_PRICE) {
         const amount = Math.trunc(state.price / DEFAULT_PRICE);
-        newState = { amount, lottoNumbers: generateLotteries(amount) };
+        Store.state = { ...Store.state, ...state, amount, lottoNumbers: generateLotteries(amount) };
       } else {
-        newState = { amount: 0 };
+        Store.state = { ...Store.state, amount: 0 };
       }
-      Store.state = { ...Store.state, ...newState };
+      break;
+    case EVENT.SHOW_RESULT_MODAL:
+      const winningResult = checkWinningNumbers({
+        winningNumbers: state.winningNumbers,
+        bonusNumber: state.bonusNumber,
+        lottoNumbers: Store.state.lottoNumbers,
+        result: Store.state.result,
+      });
+      const profit = calculateProfit(winningResult, Store.state.price);
+
+      Store.state = {
+        ...Store.state,
+        ...state,
+        result: winningResult,
+        profit,
+      };
+      break;
+    case EVENT.RETRY:
+      Store.state = { ...initialState };
       break;
     default:
       Store.state = { ...Store.state, ...state };
