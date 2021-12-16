e2b3abb4c411c320b33f5f0d522a60c8b231fb1c
jho2301 <jho2301@gmail.com>
2021-12-16 12:40:00 +0900

    refactor: getErrorMessage 함수 위치 변경

--- src/js/service/lotto.js
+++ src/js/service/lotto.js
@@ -1,4 +1,6 @@
+import ERROR_MESSAGE from '../constant/errorMessage.js';
 import { LOTTO } from '../constant/lotto.js';
+import lottoManager from '../model/lotto.js';
 import { getRandomNumber } from '../util/random.js';
 
 const generateLottoNumber = () => getRandomNumber(LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER);
@@ -14,4 +16,18 @@ export const isValidLottoInput = (answer) => {
         inputNumber && inputNumber >= LOTTO.MIN_NUMBER && inputNumber <= LOTTO.MAX_NUMBER
     ) && [...new Set(answerValues)].length === answerValues.length
   );
+};
+
+export const getAnswerErrorMessage = (answer) => {
+  if (!lottoManager.amount) {
+    return ERROR_MESSAGE.EMPTY_LOTTO;
+  }
+
+  if (!isValidLottoInput(answer)) {
+    return ERROR_MESSAGE.INVALID_ANSWER_INPUT;
+  }
+
+  if (lottoManager.hasLeft()) {
+    return ERROR_MESSAGE.LOTTO_LEFT;
+  }
 };
