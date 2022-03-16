25526e927507f3a95ab389833f287aaf39030a21
정현수 <jung660317@naver.com>
2022-03-16 16:43:30 +0900

    chore: Dom, Message 상수 객체 수정

--- src/js/components/PurchaseSection.js
+++ src/js/components/PurchaseSection.js
@@ -5,17 +5,15 @@ class PurchaseSection {
     return `
 			<section class="mt-9">
 				<div class="d-flex">
-					<label class="mb-2 d-inline-block">
-						구입할 금액을 입력해주세요.
-					</label>
+					<label id="${DOM.purchaseSectionLabel}" class="mb-2 d-inline-block">총 5개를 구매하였습니다.</label>
 					<div class="flex-auto d-flex justify-end pr-1">
 						<label class="switch">
-							<input type="checkbox" id="${DOM.lottoNumbersToggleButton}" class="${DOM.lottoNumbersToggleButton}" />
+							<input type="checkbox" id="${DOM.purchaseSectionLottoNumbersToggleButton}" class="lotto-numbers-toggle-button" />
 							<span class="text-base font-normal">번호보기</span>
 						</label>
 					</div>
 				</div>
-				<div id="${DOM.lottoNumbersFlexBox}" class="d-flex flex-wrap">
+				<div id="${DOM.purchaseSectionLottoNumbersFlexBox}" class="d-flex flex-wrap">
 					<span class="mx-1 text-4xl">🎟️ </span>
 					<span class="mx-1 text-4xl">🎟️ </span>
 					<span class="mx-1 text-4xl">🎟️ </span>
--- src/js/constants.js
+++ src/js/constants.js
@@ -1,6 +1,11 @@
 export const DOM = {
   purchaseFormInput: 'purchase-form-input',
   purchaseFormButton: 'purchase-form-button',
-  lottoNumbersToggleButton: 'lotto-numbers-toggle-button',
-  lottoNumbersFlexBox: 'lotto-numbers-flex-box',
+  purchaseSectionLabel: 'purchase-section-label',
+  purchaseSectionLottoNumbersToggleButton: 'purchase-section-lotto-numbers-toggle-button',
+  purchaseSectionLottoNumbersFlexBox: 'purchase-section-lotto-numbers-flex-box',
+};
+
+export const MESSAGE = {
+  pleaseEnterlottoPurchasePriceInUnitsOf1000: '로또 구입 금액을 1,000원 단위로 입력해 주세요.',
 };
