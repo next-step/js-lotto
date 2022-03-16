f3dff5fecb0432afa5132d2dcb85662c9b3bf015
정현수 <jung660317@naver.com>
2022-03-16 14:25:58 +0900

    test: 구매 기능 테스트 임시 작성

---
+++ cypress/integration/purchase.spec.js
@@ -0,0 +1,17 @@
+describe('구매 기능', () => {
+  it('로또 구입 금액을 입력하면, 금액에 해당하는 로또를 발급해야 한다.', () => {
+    expect(false).to.equal(true);
+  });
+
+  it('로또 1장의 가격은 1,000원이다.', () => {
+    expect(false).to.equal(true);
+  });
+
+  it('소비자는 자동 구매를 할 수 있어야 한다.', () => {
+    expect(false).to.equal(true);
+  });
+
+  it('복권 번호는 번호보기 토글 버튼을 클릭하면, 볼 수 있어야 한다.', () => {
+    expect(false).to.equal(true);
+  });
+});
--- cypress/integration/test.spec.js
+++
@@ -1,5 +0,0 @@
-describe('첫번째 테스트', () => {
-  it('테스트입니다.', () => {
-    expect(true).to.equal(true);
-  });
-});
