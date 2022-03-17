96b80a13a653b2cd6508387990358d45ec4661ea
circlegiven <circlegiven@gmail.com>
2022-03-17 23:02:58 +0900

    [로또 구매] 로또 구매 이벤트 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,28 @@
-const $showResultButton = document.querySelector('.open-result-modal-button')
-const $modalClose = document.querySelector('.modal-close')
-const $modal = document.querySelector('.modal')
-const $lottoNumbersToggleButton = document.querySelector(
-  '.lotto-numbers-toggle-button'
-)
+import { LottoVendingMachine } from './lottoVendingMachine.js';
+
+const $showResultButton = document.querySelector('.open-result-modal-button');
+const $modalClose = document.querySelector('.modal-close');
+const $modal = document.querySelector('.modal');
+// const $lottoNumbersToggleButton = document.querySelector(
+//   '.lotto-numbers-toggle-button'
+// );
+const $purchaseLotto = document.querySelector('form.mt-5');
+const $lottoAmount = document.querySelector('section.mt-9 label.my-0 span');
 
 const onModalShow = () => {
-  $modal.classList.add('open')
-}
+  $modal.classList.add('open');
+};
 
 const onModalClose = () => {
-  $modal.classList.remove('open')
-}
+  $modal.classList.remove('open');
+};
 
-$showResultButton.addEventListener('click', onModalShow)
-$modalClose.addEventListener('click', onModalClose)
+$showResultButton.addEventListener('click', onModalShow);
+$modalClose.addEventListener('click', onModalClose);
+
+$purchaseLotto.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const money = new FormData(event.currentTarget).get('money');
+  const lottos = LottoVendingMachine.purchaseLotto(money);
+  $lottoAmount.innerText = lottos.length;
+});
