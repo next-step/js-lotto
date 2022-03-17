d7480fd09553648152fc07d2d297a0107b906d92
hdpyo <tempest1390@gmail.com>
2022-03-17 23:17:03 +0900

    feat: [확인] 버튼 클릭 이벤트 추가 - [확인] 버튼 클릭 시,  로또를 구매하는 함수를 실행한다.

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,30 @@
-const $showResultButton = document.querySelector('.open-result-modal-button')
-const $modalClose = document.querySelector('.modal-close')
-const $modal = document.querySelector('.modal')
-const $lottoNumbersToggleButton = document.querySelector(
-  '.lotto-numbers-toggle-button'
-)
+import { createLottoTickets } from "./LottoApp.js";
+
+const $showResultButton = document.querySelector(".open-result-modal-button");
+const $modalClose = document.querySelector(".modal-close");
+const $modal = document.querySelector(".modal");
+const $lottoNumbersToggleButton = document.querySelector(".lotto-numbers-toggle-button");
 
 const onModalShow = () => {
-  $modal.classList.add('open')
-}
+  $modal.classList.add("open");
+};
 
 const onModalClose = () => {
-  $modal.classList.remove('open')
-}
+  $modal.classList.remove("open");
+};
 
-$showResultButton.addEventListener('click', onModalShow)
-$modalClose.addEventListener('click', onModalClose)
+$showResultButton.addEventListener("click", onModalShow);
+$modalClose.addEventListener("click", onModalClose);
+
+// * [확인] 버튼
+const $purchaseButton = document.querySelector("#purchaseButton");
+
+const purchaseLottoTickets = () => {
+  const $purchasePrice = Number(document.querySelector("#purchasePrice").value);
+  const numberOfLottoTickets = Math.floor($purchasePrice / 1000);
+
+  createLottoTickets(numberOfLottoTickets);
+};
+
+// * [확인] 버튼 - 이벤트 리스너 등록
+$purchaseButton.addEventListener("click", purchaseLottoTickets);
