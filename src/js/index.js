d20d854838ea4c2aed47c8dde98a6d0526a1fbb5
hdpyo <tempest1390@gmail.com>
2022-03-17 23:44:04 +0900

    feat: 초기 화면은 구입 금액 입력 화면만 보여준다. - 초기 화면에서 구매 결과와 당첨번호 입력 영역을 숨기도록 css 제어

--- src/js/index.js
+++ src/js/index.js
@@ -19,6 +19,7 @@ $modalClose.addEventListener("click", onModalClose);
 // * [확인] 버튼
 const $purchaseButton = document.querySelector("#purchaseButton");
 
+// * [확인] 버튼 클릭 시, 구매 금액만큼의 로또 티켓들을 생성한다.
 const purchaseLottoTickets = () => {
   const $purchasePrice = Number(document.querySelector("#purchasePrice").value);
   const numberOfLottoTickets = Math.floor($purchasePrice / 1000);
@@ -28,3 +29,11 @@ const purchaseLottoTickets = () => {
 
 // * [확인] 버튼 - 이벤트 리스너 등록
 $purchaseButton.addEventListener("click", purchaseLottoTickets);
+
+//! 페이지 첫 로딩 시에는, 사용자에게 구입 금액을 입력하는 화면만 보여줘야 한다.
+//! -> [확인] 버튼을 클릭했을 때 아래 화면이 나타나야 한다.
+const $purchaseResult = document.querySelector("#purchaseResult");
+$purchaseResult.style.display = "none";
+
+const $confirmWinningNumbers = document.querySelector("#confirmWinningNumbers");
+$confirmWinningNumbers.style.display = "none";
