7877a9e0a00d45d4f9fa51edde5f5c1cf401f044
dahye1013 <alrean@naver.com>
2022-03-17 13:08:54 +0900

    fix: 구입 금액 validation 오류 수정

--- src/js/model/PriceModel.js
+++ src/js/model/PriceModel.js
@@ -1,5 +1,10 @@
+// class
 const PRICE_FORM__INPUT = 'price-form__input';
+// unit
+const LOTTO_PAY_UNIT = 1000;
+// message
 import { ERR_MESSAGE } from '../constants/alertMessage.js';
+
 import LottoModel from './LottoModel.js';
 export default class PriceModel {
   #price;
@@ -10,17 +15,19 @@ export default class PriceModel {
 
   eventHandler = {
     PURCHASE: () => {
-      const inputPrice = document.querySelector('.price-form__input').value;
+      const inputPrice = Number(
+        document.querySelector('.price-form__input').value
+      );
       if (inputPrice === '') {
         return alert(ERR_MESSAGE.NONE_PRICE);
       }
-      if (inputPrice < 1000) {
+      if (inputPrice < LOTTO_PAY_UNIT) {
         return alert(ERR_MESSAGE.LESS_THAN_ENOUGH);
       }
       if (inputPrice > 100000) {
         return alert(ERR_MESSAGE.GREATER_THAN_ENOUGH);
       }
-      if (!!inputPrice % 10000) {
+      if (!!(inputPrice % LOTTO_PAY_UNIT)) {
         return alert(ERR_MESSAGE.NOT_VALID_PRICE);
       }
       this.generateLotto();
@@ -28,7 +35,7 @@ export default class PriceModel {
   };
 
   generateLotto() {
-    const quantity = this.#price / 10000;
+    const quantity = this.#price / LOTTO_PAY_UNIT;
     return new LottoModel(quantity);
   }
 
