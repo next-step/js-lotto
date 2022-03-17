90d13b85d2c608348ad4d086a2ce84e948727196
정현수 <jung660317@naver.com>
2022-03-17 15:29:14 +0900

    feat: 로또 구매 시, 금액만큼 로또 그림 반환 구현
    - createLottoWithLottoCount 메소드를 만들어서 들어온 lottoCount만큼 그림 생성

--- src/js/components/PurchaseSection.js
+++ src/js/components/PurchaseSection.js
@@ -5,22 +5,28 @@ class PurchaseSection extends Component {
   template() {
     return `
 			<div class="d-flex">
-				<label id="${DOM.purchaseSectionLabel}" class="mb-2 d-inline-block">총 ${this.props.lottoCount}개를 구매하였습니다.</label>
+				<label id="${DOM.purchaseSectionLabel}" class="mb-2 d-inline-block">총 ${
+      this.props.lottoCount
+    }개를 구매하였습니다.</label>
 				<div class="flex-auto d-flex justify-end pr-1">
 					<label class="switch">
-						<input type="checkbox" id="${DOM.purchaseSectionLottoNumbersToggleButton}" class="lotto-numbers-toggle-button" />
+						<input type="checkbox" id="${
+              DOM.purchaseSectionLottoNumbersToggleButton
+            }" class="lotto-numbers-toggle-button" />
 						<span class="text-base font-normal">번호보기</span>
 					</label>
 				</div>
 			</div>
 			<div id="${DOM.purchaseSectionLottoNumbersFlexBox}" class="d-flex flex-wrap">
-				<span class="mx-1 text-4xl">🎟️ </span>
-				<span class="mx-1 text-4xl">🎟️ </span>
-				<span class="mx-1 text-4xl">🎟️ </span>
-				<span class="mx-1 text-4xl">🎟️ </span>
-				<span class="mx-1 text-4xl">🎟️ </span>
+				${this.createLottoWithLottoCount(this.props.lottoCount)}
 			</div>
 		`;
+  }
+
+  createLottoWithLottoCount(lottoCount) {
+    return Array.from({ length: lottoCount })
+      .map(() => '<span class="mx-1 text-4xl">🎟️ </span>')
+      .join('');
   }
 }
 
