35ae003b3a1a2d49564dfb9602d682f963d41f19
circlegiven <circlegiven@gmail.com>
2022-03-17 23:01:54 +0900

    [로또] 로또 구현 및 관련 상수 추가
    - 자동 숫자 부여 기능 추가
    - 최대 로또 당첨번호 체크 로직 추가

---
+++ src/js/lotto.js
@@ -0,0 +1,35 @@
+import {
+  LOTTO_MAX_NUM,
+  LOTTO_MAX_SIZE,
+  LOTTO_MIN_NUM,
+} from './constants/lotto.js';
+
+export class Lotto {
+  #values;
+
+  constructor() {
+    this.#values = new Set();
+
+    while (true) {
+      this.#values.add(Lotto.#getRandomNumber());
+
+      if (Lotto.#isMaxCreateLottoNumber(this.#values)) {
+        break;
+      }
+    }
+  }
+
+  get value() {
+    return [...this.#values];
+  }
+
+  static #isMaxCreateLottoNumber(values) {
+    return values.size >= LOTTO_MAX_SIZE;
+  }
+
+  static #getRandomNumber() {
+    return Math.floor(
+      Math.random() * (LOTTO_MAX_NUM - LOTTO_MIN_NUM + 1) + LOTTO_MIN_NUM
+    );
+  }
+}
