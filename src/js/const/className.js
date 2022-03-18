074078a24ce42149126bfaa673b6fed6dd343759
bomee.yoon <bomee.yoon@godo.co.kr>
2022-03-19 07:54:01 +0900

    💬  chore: 클래스 이름 매핑

---
+++ src/js/const/className.js
@@ -0,0 +1,8 @@
+export const CLASS = {
+  MONEY_FORM: 'js-money-form',
+  MONEY: 'js-money',
+  LOTTO_DETAIL: 'js-lotto-detail',
+  LOTTO_DETAIL_HEADER: 'js-lotto-detail-header',
+  LOTTO_DETAIL_LIST: 'js-lotto-detail-list',
+  WINNING_FORM: 'js-winning-form',
+};
