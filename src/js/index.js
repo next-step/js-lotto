fe3b8c57be4b572084677e7e0f51ece191d0d22f
circlegiven <circlegiven@gmail.com>
2022-03-18 20:18:17 +0900

    [로또 구매] 구매한 로또 내역 보기 토글 이벤트 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,70 @@
 import { LottoVendingMachine } from './lottoVendingMachine.js';
+import { Wallet } from './wallet.js';
 
 const $showResultButton = document.querySelector('.open-result-modal-button');
 const $modalClose = document.querySelector('.modal-close');
 const $modal = document.querySelector('.modal');
-// const $lottoNumbersToggleButton = document.querySelector(
-//   '.lotto-numbers-toggle-button'
-// );
+const $lottoNumbersToggleButton = document.querySelector(
+  '.lotto-numbers-toggle-button'
+);
 const $purchaseLotto = document.querySelector('form.mt-5');
 const $purchaseResult = document.querySelector('section.mt-9');
-const $lottoAmount = $purchaseResult.querySelector('label.my-0 span');
+const $purchasedLottoAmount = $purchaseResult.querySelector('label.my-0 span');
+const $purchasedLottoList = $purchaseResult.querySelector(
+  'div.d-flex.flex-wrap'
+);
 const $lottoWinningResult = document.querySelector('form.mt-9');
+const myWallet = new Wallet();
+
+function isShowPurchasedLottoNumber() {
+  return $lottoNumbersToggleButton.checked;
+}
 
 function hidePurchaseResult() {
   $purchaseResult.style.visibility = 'hidden';
-}
-
-function hideLottoWinningResult() {
-  $lottoWinningResult.style.visibility = 'hidden';
 }
 
 function showPurchaseResult() {
   $purchaseResult.style.visibility = 'visible';
 }
 
-function changePurchaseAmount(amount) {
-  $lottoAmount.innerText = amount;
+function hideLottoWinningResult() {
+  $lottoWinningResult.style.visibility = 'hidden';
+}
+
+function initPurchasedLottoList() {
+  $purchasedLottoList.replaceChildren();
+}
+
+function getLottoElement(lotto) {
+  const $lotto = document.createElement('span');
+  $lotto.classList.add('mx-1', 'text-4xl');
+  $lotto.innerHTML = '🎟️ ';
+  if (isShowPurchasedLottoNumber()) {
+    $lotto.innerHTML += lotto.value;
+  }
+
+  return $lotto;
+}
+
+function addLottoToPurchasedLottoList(lotto) {
+  $purchasedLottoList.appendChild(getLottoElement(lotto));
+}
+
+function changePurchasedAmount(amount) {
+  $purchasedLottoAmount.innerText = amount;
+}
+
+function changePurchasedLottos(lottos) {
+  lottos.forEach((lotto) => {
+    addLottoToPurchasedLottoList(lotto);
+  });
+}
+
+function changePurchaseResult(lottos = []) {
+  initPurchasedLottoList();
+  changePurchasedAmount(lottos.length);
+  changePurchasedLottos(lottos);
 }
 
 function openPurchasedLottoWinningResultHandler() {
@@ -39,8 +79,12 @@ function purchaseHandler(event) {
   event.preventDefault();
   showPurchaseResult();
   const money = new FormData(event.currentTarget).get('money');
-  const purchasedLottoList = LottoVendingMachine.purchaseLotto(money);
-  changePurchaseAmount(purchasedLottoList.length);
+  myWallet.addLottos(LottoVendingMachine.purchaseLotto(money));
+  changePurchaseResult(myWallet.lottos);
+}
+
+function togglePurchasedLottoNumbersHandler() {
+  changePurchaseResult(myWallet.lottos);
 }
 
 $showResultButton.addEventListener(
@@ -49,6 +93,11 @@ $showResultButton.addEventListener(
 );
 $modalClose.addEventListener('click', closePurchasedLottoWinningResultHandler);
 $purchaseLotto.addEventListener('submit', purchaseHandler);
+$lottoNumbersToggleButton.addEventListener(
+  'click',
+  togglePurchasedLottoNumbersHandler
+);
 
 hidePurchaseResult();
 hideLottoWinningResult();
+initPurchasedLottoList();
