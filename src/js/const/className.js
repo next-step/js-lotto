27219e47644d1b620779d9f1d4e13ccbd4c77929
bomee.yoon <bomee.yoon@godo.co.kr>
2022-03-20 00:53:18 +0900

    💬  add: 클래스 이름 상수정의

--- src/js/const/className.js
+++ src/js/const/className.js
@@ -1,8 +1,11 @@
 export const CLASS = {
-  MONEY_FORM: 'js-money-form',
-  MONEY: 'js-money',
-  LOTTO_DETAIL: 'js-lotto-detail',
-  LOTTO_DETAIL_HEADER: 'js-lotto-detail-header',
-  LOTTO_DETAIL_LIST: 'js-lotto-detail-list',
-  WINNING_FORM: 'js-winning-form',
+  APP: '.js-app',
+  MONEY_FORM: '.js-money-form',
+  MONEY: '.js-money',
+  LOTTO_DETAIL: '.js-lotto-detail',
+  LOTTO_DETAIL_HEADER: '.js-lotto-detail-header',
+  LOTTO_COUNT: '.js-lotto-count',
+  TOGGLE_NUMBERS: '.js-toggle-numbers',
+  LOTTO_DETAIL_LIST: '.js-lotto-detail-list',
+  WINNING_FORM: '.js-winning-form',
 };
