c7110ae90cad42c4220044eb467b3954e6f0eb9e
aawlgus <aawlgus@jaranda.kr>
2022-03-19 23:48:57 +0900

    refactor: convert index.js properties to App class properties

---
+++ src/js/App.js
@@ -0,0 +1,27 @@
+class App {
+  #showResultButton = document.querySelector(".open-result-modal-button");
+  #modalClose = document.querySelector(".modal-close");
+  #modal = document.querySelector(".modal");
+  #lottoNumbersToggleButton = document.querySelector(
+    ".lotto-numbers-toggle-button"
+  );
+
+  #onModalShow = () => {
+    this.#modal.classList.add("open");
+  };
+
+  #onModalClose = () => {
+    this.#modal.classList.remove("open");
+  };
+
+  #setEvents = () => {
+    this.#showResultButton.addEventListener("click", this.#onModalShow);
+    this.#modalClose.addEventListener("click", this.#onModalClose);
+  };
+
+  constructor() {
+    this.#setEvents();
+  }
+}
+
+new App();
