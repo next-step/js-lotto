1da43c0fe9fca6ed40061ddc830a8a0b7567d82a
bomee.yoon <bomee.yoon@godo.co.kr>
2022-03-20 03:17:36 +0900

    ♻️  refactor: 로또 리스트 템플릿 분리

--- src/js/views/LottoDetailList.js
+++ src/js/views/LottoDetailList.js
@@ -1,19 +1,17 @@
 import { toggleClassName, replaceChild } from '../dom/index.js';
 
+const getLottoTemplate = (lotto) => `
+  <li>
+    <span class="mx-1 text-4xl lotto-icon">🎟️ </span>
+    <span class="lotto-detail">${lotto}</span>
+  </li>`;
+
 const render = ($el, { numbers }) => {
   const $clonedList = $el.cloneNode();
 
   $clonedList.insertAdjacentHTML(
     'afterBegin',
-    numbers
-      .map(
-        (lotto) => `
-          <li>
-            <span class="mx-1 text-4xl lotto-icon">🎟️ </span>
-            <span class="lotto-detail">${lotto}</span>
-          </li>`
-      )
-      .join('')
+    numbers.map(getLottoTemplate).join('')
   );
 
   toggleClassName($clonedList, 'remove', 'flex-column');
