99766a41379bca8ff5185d343bec6251387e090f
Dami Kim <dami.k.dev@gmail.com>
2022-03-19 17:06:00 +0900

    feat: lotto qunatity 만큼 lotto를 생성하는 lottoTicket 구현

--- src/js/models/LottoTicketList/LottoTicketList.js
+++ src/js/models/LottoTicketList/LottoTicketList.js
@@ -3,6 +3,22 @@ import LottoTicket from "./LottoTicket.js";
 class LottoTicketList {
   constructor({ quantity }) {
     this.quantity = quantity;
+    this.lottoTicketList = [];
+    this.#generateLottoTicket();
+  }
+
+  #generateLottoTicket() {
+    let quantity = this.quantity;
+
+    while (quantity > 0) {
+      const lottoTicket = new LottoTicket();
+      this.lottoTicketList.push(lottoTicket);
+      quantity--;
+    }
+  }
+
+  getLottoTicketList() {
+    return this.lottoTicketList;
   }
 }
 
