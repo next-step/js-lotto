5821483c53fc7c1b0612864455a71006393a353d
Dami Kim <dami.k.dev@gmail.com>
2022-03-19 16:46:46 +0900

    feat: LottoController에 getLottoQuantityByTotalAmount 적용

--- src/js/controllers/LottoController.js
+++ src/js/controllers/LottoController.js
@@ -1,9 +1,10 @@
 import LottoTicketList from "../models/LottoTicketList/index.js";
-
+import { getLottoQuantityByTotalAmount } from "../utils/calculator.js";
 class LottoController {
   constructor() {
     this.init();
   }
+
   init() {
     this.addEvent();
   }
@@ -14,8 +15,9 @@ class LottoController {
   }
 
   handleBuyButtonClick() {
-    const $inputMoney = document.querySelector("#input-money");
-    const inputMoney = $inputMoney.value;
+    const $totalAmount = document.querySelector("#total-amount");
+    const quantity = getLottoQuantityByTotalAmount(Number($totalAmount.value));
+    const lottoTicketList = new LottoTicketList({ quantity: quantity });
   }
 }
 
