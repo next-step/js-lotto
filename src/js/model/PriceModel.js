d575e36a76165f8d3d1d773093db48ae8d942654
dahye1013 <alrean@naver.com>
2022-03-19 13:56:31 +0900

    refactor: 금액 validation 응집도 높이기 위해 priceModel로 이동

--- src/js/model/PriceModel.js
+++ src/js/model/PriceModel.js
@@ -1,18 +1,39 @@
+import { LOTTO_PAY_UNIT } from '../constants/unit.js ';
+import { ERR_MESSAGE } from '../constants/alertMessage.js';
+
 import { PRICE_FORM__INPUT } from '../constants/selectTarget.js';
 export default class PriceModel {
   #price;
 
   constructor() {
-    this.resetPrice();
+    this.initPrice();
   }
+
+  initPrice() {
+    this.#price = '';
+  }
+
+  static validators = {
+    isValidPrice: (price) => {
+      if (price === 0) {
+        return alert(ERR_MESSAGE.NONE_PRICE);
+      }
+      if (price < LOTTO_PAY_UNIT) {
+        return alert(ERR_MESSAGE.LESS_THAN_ENOUGH);
+      }
+      if (price > 100000) {
+        return alert(ERR_MESSAGE.GREATER_THAN_ENOUGH);
+      }
+      if (!!(price % LOTTO_PAY_UNIT)) {
+        return alert(ERR_MESSAGE.NOT_VALID_PRICE);
+      }
+      return true;
+    },
+  };
 
   updatePrice(newPrice) {
     this.#price = newPrice;
     document.querySelector(`.${PRICE_FORM__INPUT}`).value = '';
-  }
-
-  resetPrice() {
-    this.#price = '';
   }
 
   getPrice() {
--- src/js/model/State.js
+++ src/js/model/State.js
@@ -1,12 +1,8 @@
 import PriceModel from './PriceModel.js';
 import LottoModel from './LottoModel.js';
 
-import { PRICE_FORM, PRICE_FORM__INPUT, PRICE_FORM__BUTTON } from '../constants/selectTarget.js';
-
-// unit
-const LOTTO_PAY_UNIT = 1000;
-// message
-import { ERR_MESSAGE } from '../constants/alertMessage.js';
+import { LOTTO_PAY_UNIT } from '../constants/unit.js';
+import { PRICE_FORM__INPUT } from '../constants/selectTarget.js';
 
 export default class State {
   #priceModel;
@@ -20,19 +16,7 @@ export default class State {
     PURCHASE: (e) => {
       e.preventDefault();
       const inputPrice = Number(document.querySelector(`.${PRICE_FORM__INPUT}`).value);
-      if (inputPrice === 0) {
-        return alert(ERR_MESSAGE.NONE_PRICE);
-      }
-      if (inputPrice < LOTTO_PAY_UNIT) {
-        return alert(ERR_MESSAGE.LESS_THAN_ENOUGH);
-      }
-      if (inputPrice > 100000) {
-        return alert(ERR_MESSAGE.GREATER_THAN_ENOUGH);
-      }
-      if (!!(inputPrice % LOTTO_PAY_UNIT)) {
-        return alert(ERR_MESSAGE.NOT_VALID_PRICE);
-      }
-
+      if (!PriceModel.validators.isValidPrice(inputPrice)) return;
       this.#priceModel.updatePrice(inputPrice);
       this.generateLotto(inputPrice);
     },
