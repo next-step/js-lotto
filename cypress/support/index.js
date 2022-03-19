3a85b4b172e95fe650f8a15a4880eee3eeb11d9b
dahye1013 <alrean@naver.com>
2022-03-19 21:54:34 +0900

    remove: 중복 import 제거

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,7 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands';
 
-import './commands';
 Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false here prevents Cypress from failing the test
   return false;
