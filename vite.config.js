83b2c148daead54a6da45d4353ba30078dbae32b
tolluset <dlqud19@gmail.com>
2022-03-19 21:41:39 +0900

    chore: add source map in build bundle

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import eslintPlugin from 'vite-plugin-eslint';
 
 export default defineConfig({
   base: './',
+  build: {
+    sourcemap: true,
+  },
   plyarnugins: [
     eslintPlugin({
       cache: false,
