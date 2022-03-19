4241fe109451d7701a2f0845b1c672535f5ca559
Dami Kim <dami.k.dev@gmail.com>
2022-03-20 01:22:34 +0900

    feat: LottoTicketView render 구현

--- src/js/controllers/LottoController.js
+++ src/js/controllers/LottoController.js
@@ -1,35 +1,68 @@
 import LottoTicketList from "../models/LottoTicketList/index.js";
-import { getQuantityByTotalAmount } from "../utils/calculator.js";
+import LottoTicketView from "../views/LottoTicketView.js";
 import LottoQuantityView from "../views/LottoQuantityView.js";
+import { getQuantityByTotalAmount } from "../utils/calculator.js";
+
 class LottoController {
+  #quantity = null;
   constructor() {
     this.$toggleButton = document.querySelector(".lotto-numbers-toggle-button");
     this.$buyButton = document.querySelector("#buy-button");
     this.$summaryContainer = document.querySelector("#summary-container");
     this.$quantity = document.querySelector("#lotto-quantity");
-    this.init();
+    this.$lottoIconsContainer = document.querySelector("#lotto-icons");
+
+    this.#init();
   }
 
-  init() {
-    this.addEvent();
+  #init() {
+    this.#addEvent();
   }
 
-  addEvent() {
+  #addEvent() {
     this.$buyButton.addEventListener(
       "click",
-      this.handleBuyButtonClick.bind(this)
+      this.#handleBuyButtonClick.bind(this)
+    );
+
+    this.$toggleButton.addEventListener(
+      "click",
+      this.#handleToggleButtonClick.bind(this)
     );
   }
 
-  handleBuyButtonClick() {
-    const $totalAmount = document.querySelector("#total-amount");
-    const quantity = getQuantityByTotalAmount(Number($totalAmount.value));
-    const lottoTicketList = new LottoTicketList({ quantity: quantity });
-    this.renderLottoQuantity(quantity);
+  #setQuantity(quantity) {
+    this.#quantity = quantity;
   }
 
-  renderLottoQuantity(quantity) {
-    new LottoQuantityView({ $target: this.$quantity, quantity });
+  #handleBuyButtonClick() {
+    const $totalAmount = document.querySelector("#total-amount");
+    const quantity = getQuantityByTotalAmount(Number($totalAmount.value));
+
+    this.#setQuantity(quantity);
+
+    this.#renderLottoQuantity();
+    this.#renderLottoTicket();
+  }
+
+  #handleToggleButtonClick() {}
+
+  #renderLottoQuantity() {
+    new LottoQuantityView({
+      $target: this.$summaryContainer,
+      quantity: this.#quantity,
+    });
+  }
+
+  #renderLottoTicket() {
+    const lottoTicketList = new LottoTicketList({
+      quantity: this.#quantity,
+    }).getLottoTicketList();
+
+    new LottoTicketView({
+      $target: this.$lottoIconsContainer,
+      lottoTicketList,
+    });
   }
 }
 
