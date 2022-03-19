b5565f2b03e23b04ab37ef10cf892e77bda062a2
Minsiki <pro.mskim0215@gmail.com>
2022-03-19 15:22:37 +0900

    cypress 설치 및 불필요한 test 파일 삭제
    cypress 설치 및 불필요한 test 파일 삭제

--- cypress/integration/examples/lotto.spec.js
+++ cypress/integration/examples/lotto.spec.js
@@ -1,11 +1,16 @@
+//import {MESSAGE} from "./src/js/constant/index.js";
+
 describe("test", () => {
     beforeEach(() => {
         cy.visit("http://localhost:5500/");
     });
 
-    it("5+7=12", () => {
-        cy.get("[data-test=purchase-price-input]").invoke("val", 200);
+    it("구입 금액을 1000단위로 입력하지 않았을때 alert", () => {
+        cy.get("[data-test=purchase-price-input]").type(1100);
+        cy.get("[data-test=purchase-button]").click();
 
-        //cy.get("[data-test=total]").contains(12);
+        cy.on("window:alert", (text) => {
+            expect(text).to.contains("금액은 1000단위로 입력해야 합니다.");
+        })
     });
 });
