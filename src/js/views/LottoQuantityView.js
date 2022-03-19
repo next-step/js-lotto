7ca5815d0f9319f043ba94fb219ea578b124bfdf
Dami Kim <dami.k.dev@gmail.com>
2022-03-20 01:18:49 +0900

    feat: LottoQuantityView에서 구매 결과, 토글 버튼 렌더하도록 수정

--- src/js/views/LottoQuantityView.js
+++ src/js/views/LottoQuantityView.js
@@ -9,7 +9,11 @@ class LottoQuantityView {
   }
 
   template() {
-    return `<label class="flex-auto my-0">총 ${this.quantity}개를 구매하였습니다.</label>`;
+    return `<label class="flex-auto my-0">총 ${this.quantity}개를 구매하였습니다.</label> <div class="flex-auto d-flex justify-end pr-1">
+    <label class="switch">
+      <input type="checkbox" class="lotto-numbers-toggle-button" />
+      <span class="text-base font-normal">번호보기</span>
+    </label>`;
   }
 }
 
