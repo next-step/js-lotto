32386383bfdb31f57933b90817163c2ff470d20e
Dami Kim <dami.k.dev@gmail.com>
2022-03-20 01:19:57 +0900

    feat: lottoTicket 출력 기능 구현(LottoTicketView)

--- src/js/views/LottoTicketView.js
+++ src/js/views/LottoTicketView.js
@@ -1,5 +1,31 @@
 class LottoTicketView {
-  constructor() {}
+  constructor({ $target, lottoTicketList }) {
+    this.$target = $target;
+    this.lottoTicketList = lottoTicketList;
+    this.init();
+  }
+  init() {
+    this.render();
+  }
+
+  render() {
+    this.$target.innerHTML = this.makeTemplate();
+  }
+
+  makeTemplate() {
+    const template = this.lottoTicketList
+      .map(ticket => ticket.getNumbers())
+      .reduce((acc, cur) => {
+        return (
+          acc +
+          `<li><span class="mx-1 text-4xl">🎟️ </span><span class="lotto-detail">${cur.join(
+            ", "
+          )}</span></li>`
+        );
+      }, "");
+
+    return template;
+  }
 }
 
 export default LottoTicketView;
