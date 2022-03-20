89e30345b9dd26fabaeb2bef718002946ebe41c3
wanho1108 <wanho1108@gmail.com>
2022-03-20 19:19:15 +0900

    test: 로또 구매 금액이 1,000원 단위가 아닐경우 사용자 경고 에러 메세지 출력 테스트 작성

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -10,4 +10,12 @@ describe('로또 구매 시', () => {
   it('구매 전에는 결과 확인하기 입력 폼 영역이 노출되지 않는다.', () => {
     cy.get('.lotto-result-confirm-form').should('not.visible');
   });
+
+  it('구매 금액이 1,000원 단위가 아닌 경우 사용자 경고 메세지를 출력한다.', () => {
+    cy.get('.lotto-buy-form-input').type('1108');
+    cy.get('.lotto-buy-form-submit').click();
+    cy.on('window:alert', (text) => {
+      expect(text).to.contains('로또 구입 금액은 1,000원 단위로 입력해주세요.');
+    });
+  });
 });
