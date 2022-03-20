905dda4ae3022030822950328798ed8fd4f1364d
정현수 <jung660317@naver.com>
2022-03-20 16:05:13 +0900

    test: 당첨 결과 기능 테스트 작성
    - 로또 구매 이후 당첨번호와 보너스 번호를 입력하는 form 테스트 작성
    - 결과 확인하기 버튼 클릭 시 입력 값들 검증 테스트 작성

---
+++ cypress/integration/result.spec.js
@@ -0,0 +1,37 @@
+describe('당첨 결과 기능', () => {
+  context('로또 구매 이후 당첨번호와 보너스 번호를 입력하는 form이 나타납니다.', () => {
+    it('당첨 번호 DOM 확인', () => {
+      expect(true).to.equal(false);
+    });
+
+    it('보너스 번호 DOM 확인', () => {
+      expect(true).to.equal(false);
+    });
+
+    it('결과 확인 버튼 DOM 확인', () => {
+      expect(true).to.equal(false);
+    });
+  });
+
+  context('결과 확인하기 버튼 클릭 시 입력 값들을 검증합니다.', () => {
+    it('당첨 번호는 필수값입니다.', () => {
+      expect(true).to.equal(false);
+    });
+
+    it('당첨 번호는 1과 45사이의 값들입니다.', () => {
+      expect(true).to.equal(false);
+    });
+
+    it('당첨 번호는 겹치지 않는 6개의 숫자입니다.', () => {
+      expect(true).to.equal(false);
+    });
+
+    it('보너스 번호는 필수값입니다.', () => {
+      expect(true).to.equal(false);
+    });
+
+    it('보너스 번호는 1과 45사이의 값 입니다.', () => {
+      expect(true).to.equal(false);
+    });
+  });
+});
