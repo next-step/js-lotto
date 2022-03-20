0a3c4e38c41879ba9b9ff641d3b8761fd1771956
bomee.yoon <bomee.yoon@godo.co.kr>
2022-03-21 07:09:02 +0900

    ♻️ alertMessage 를 MESSAGE 로 변경

--- src/js/const/constant.js
+++ src/js/const/constant.js
@@ -5,7 +5,7 @@ export const RANGE_FOR_RANDOM_NUMBERS = 45;
 export const ZERO = 0;
 export const ONE = 1;
 
-export const alertMessage = {
+export const MESSAGE = {
   PLZ_INSERT_MONEY: '금액을 입력하세요.',
   PLZ_CHECK_AMOUNT: '금액 단위를 확인하세요.',
   ALERT_MIN_AMOUNT_TO_ISSUE_LOTTO: '복권 최소 금액은 1000원 입니다.',
--- src/js/service/lotto.js
+++ src/js/service/lotto.js
@@ -4,16 +4,15 @@ import {
   MIN_MONEY_UNIT,
   PICKED_LOTTO_NUMBER_COUNT,
   RANGE_FOR_RANDOM_NUMBERS,
-  alertMessage,
+  MESSAGE,
 } from '../const/constant.js';
 
 const validateMoney = (money) => {
-  if (!money) throw alertMessage.PLZ_INSERT_MONEY;
+  if (!money) throw MESSAGE.PLZ_INSERT_MONEY;
 
-  if (money < MIN_MONEY_UNIT)
-    throw alertMessage.ALERT_MIN_AMOUNT_TO_ISSUE_LOTTO;
+  if (money < MIN_MONEY_UNIT) throw MESSAGE.ALERT_MIN_AMOUNT_TO_ISSUE_LOTTO;
 
-  if (money % MIN_MONEY_UNIT) throw alertMessage.PLZ_CHECK_AMOUNT;
+  if (money % MIN_MONEY_UNIT) throw MESSAGE.PLZ_CHECK_AMOUNT;
 
   return money;
 };
