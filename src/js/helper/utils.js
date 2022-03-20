4fe08c65576b53304861bae01650da2faf54ce47
goflvhxj2547 <goflvhxj2547@gmail.com>
2022-03-20 23:54:10 +0900

    🗃Refactor: 중복되는 조건이 없도록 수정하고 상수화 된 에러메세지를 import 한다.

--- src/js/helper/utils.js
+++ src/js/helper/utils.js
@@ -1,10 +1,10 @@
-import { isSpecial } from './valid.js';
+import { ERROR_MESSAGE, AMOUNT_UNIT } from '../constants.js';
+import { isEmpty } from './valid.js';
 
 export const validCount = amount => {
-  if (isNaN(amount) || isSpecial(amount)) throw new Error('숫자를 입력해주세요.');
-  if (Number(amount) < 1000) throw new Error('값은 1000 이상이어야 합니다.');
-  const count = amount / 1000;
-  if (!Number.isInteger(amount / 1000))
-    throw new Error('로또 구입 금액을 1,000원 단위로 입력해 주세요.');
-  return Math.trunc(count);
+  const count = Number(amount / AMOUNT_UNIT);
+  if (isEmpty(amount) || isNaN(count)) throw new Error(ERROR_MESSAGE.REQUIRED_DIGIT);
+  if (amount < AMOUNT_UNIT) throw new Error(ERROR_MESSAGE.MUST_MORE_THAN);
+  if (!Number.isInteger(count)) throw new Error(ERROR_MESSAGE.MUST_REQUIRED_AMOUNT_UNIT);
+  return count;
 };
