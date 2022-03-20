ebd422b58d4b44505eea43a5cb2abe17c688a37c
Dami Kim <dami.k.dev@gmail.com>
2022-03-20 12:51:39 +0900

    feat: test 추가 구현 (금액 입력시 ticket 렌더)

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -3,20 +3,26 @@ const BASE_URL = "http://localhost:5500";
 const payMoney = money => cy.get("#total-amount").type(money);
 const clickBuyButton = () => cy.get("#buy-button").click();
 
-const checkQuantity = (money, quantity) => {
+const buyLottoTicket = (money, quantity) => {
   payMoney(money);
   clickBuyButton();
   cy.get("#quantity-text").should("have.text", quantity);
+
+  cy.get(".ticket-list").should($list => {
+    expect($list).to.have.length(quantity);
+  });
 };
+
+const checkTicketRender = quantity => {};
 
 describe("로또 테스트", () => {
   beforeEach(() => {
     cy.visit(BASE_URL);
   });
 
-  describe("로또 금액에 따른 로또 수량 출력 테스트", () => {
-    it("1000원을 입력하면 1개를 구입한다", () => {
-      checkQuantity(1000, 1);
+  describe("로또 수량 출력 테스트", () => {
+    it("1000원을 입력하면 1개의 아이콘이 렌더된다", () => {
+      buyLottoTicket(1000, 1);
     });
   });
 });
