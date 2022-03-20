c822822e4b7e0aaa8fede65855a8f1f5d7b2f668
tolluset <dlqud19@gmail.com>
2022-03-20 23:32:12 +0900

    feat: add lotto pay

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,14 @@ import Lotto from './models/lotto';
 
 const lotto = new Lotto();
 
-const $lottoPayForm = document.querySelector('#lotto-pay-form');
+const $lottoPayForm = document.querySelector('[data-target="lotto-pay-form"]');
 const $lottoPayInput = $lottoPayForm.querySelector('input');
+
+const $lottoPaidSection = document.querySelector('[data-target="lotto-paid-section"]');
+const $lottoPaidCounts = document.querySelector('[data-target="lotto-paid-counts"]');
+const $lottoPaidSwitch = document.querySelector('[data-target="lotto-paid-switch"]');
+const $lottoPaidCheckBox = $lottoPaidSwitch.querySelector('input');
+const $lottoPaidCards = document.querySelector('[data-target="lotto-paid-cards"]');
 
 const $showResultButton = document.querySelector('.open-result-modal-button');
 const $modalClose = document.querySelector('.modal-close');
@@ -13,9 +19,53 @@ $lottoPayForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const lottos = Lotto.pay($lottoPayInput.value);
 
-  console.log('lottos', lottos);
   if (lottos === undefined) $lottoPayForm.reset();
+  else {
+    renderPaidSectionItems(lottos);
+    resetPaidSection();
+  }
 });
+
+const renderPaidSectionItems = (lottos) => {
+  $lottoPaidSection.style.visibility = 'visible';
+  $lottoPaidCounts.innerHTML = lottos.length;
+  $lottoPaidCards.innerHTML = lottos
+    .map((lotto) => {
+      return `<div class="d-flex items-center mx-1 text-4xl" data-target="lotto-paid-card">
+                <div>🎟️</div> 
+                <span class="d-none text-xl ml-3" data-target="lotto-paid-card-numbers">${lotto.join()}</span>
+              </div>`;
+    })
+    .join('');
+};
+
+$lottoPaidSwitch.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  $lottoPaidCheckBox.checked = !$lottoPaidCheckBox.checked;
+  toggleLottoPaidCards();
+});
+
+const toggleLottoPaidCards = () => {
+  const $lottoPaidCardNumbers = document.querySelectorAll(
+    '[data-target="lotto-paid-card-numbers"]',
+  );
+
+  toggleLottoPaidCardsLayout();
+  $lottoPaidCardNumbers.forEach((elm) => elm.classList.toggle('d-none'));
+};
+
+const toggleLottoPaidCardsLayout = () => {
+  $lottoPaidCards.classList.toggle('flex-col');
+  $lottoPaidCards.classList.toggle('flex-row');
+};
+
+const resetPaidSection = () => {
+  if ($lottoPaidCheckBox.checked) {
+    $lottoPaidCheckBox.checked = false;
+    toggleLottoPaidCardsLayout();
+  }
+};
 
 const onModalShow = () => {
   $modal.classList.add('open');
--- src/js/models/lotto/index.js
+++ src/js/models/lotto/index.js
@@ -1,9 +1,7 @@
-import LOTTO_PRICE from './constants';
+import { LOTTO_PRICE, LOTTO_NUMBERS_LIMIT, LOTTO_COUNTS } from './constants';
 import NOT_ALLOWED_PAY_UNIT from './messageConstants';
 
 class Lotto {
-  #paidPrice = 0;
-
   static pay(paidPrice) {
     console.log(paidPrice);
     if (!Lotto.#isValidatePaidPrice(paidPrice)) {
@@ -11,15 +9,36 @@ class Lotto {
       return;
     }
 
-    return paidPrice / LOTTO_PRICE;
+    const counts = paidPrice / LOTTO_PRICE;
+
+    return this.#makeLottoCards(counts);
   }
 
   static #isValidatePaidPrice(paidPrice) {
     return paidPrice % LOTTO_PRICE === 0 && paidPrice > 0;
   }
 
-  static #purchaseLottoAmount(paidPrice) {
-    return paidPrice / LOTTO_PRICE;
+  static #makeLottoCards(counts) {
+    const result = [];
+
+    while (result.length < counts) {
+      result.push(Lotto.#makeRandomLottoNumbers());
+    }
+
+    return result;
+  }
+
+  static #makeRandomLottoNumbers() {
+    const result = [];
+
+    while (result.length < LOTTO_COUNTS) {
+      const randomNumber = Math.floor(Math.random() * LOTTO_NUMBERS_LIMIT + 1);
+
+      if (result.includes(randomNumber)) continue;
+      result.push(randomNumber);
+    }
+
+    return result.sort((a, b) => a - b);
   }
 }
 
