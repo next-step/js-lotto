3c9d033604b536370948f3bc50075455b3314ab8
goflvhxj2547 <goflvhxj2547@gmail.com>
2022-03-20 23:54:46 +0900

    ✒️Feat: 렌더링과 이벤트 바인딩을 담당할 함수를 커링을 활용하여 추가한다.

--- src/js/helper/dom.js
+++ src/js/helper/dom.js
@@ -1,9 +1,26 @@
+import { isNil } from './valid.js';
+
 export const $ = (selector, target) => {
-  if (target === undefined) return document.querySelector(selector);
+  if (isNil(target)) return document.querySelector(selector);
   return target.querySelector(selector);
 };
 
-export const $all = selector => document.querySelectorAll(selector);
+export const $all = (selector, target) => {
+  if (isNil(target)) return document.querySelectorAll(selector);
+  return target.querySelectorAll(selector);
+};
 
 export const $addEvent = ($element, eventType, listenser) =>
   $element.addEventListener(eventType, listenser);
+
+export const $renderWithEventBind = getElement => args => {
+  try {
+    const [$element, $events = []] = getElement(args);
+    if (!($element instanceof Element)) throw new Error('첫 번째 인자로 DOM 요소를 선언해주세요.');
+    $events.forEach(({ type, callback }) => $addEvent($element, type, callback));
+    return $element;
+  } catch (error) {
+    alert(error.message);
+    return null;
+  }
+};
