8e04745f89b925f5d31830c4560e4a65ed0af63c
taeyoungs <xoxodudwkd@gmail.com>
2022-03-20 20:07:19 +0900

    add: 상수값 정의 파일 추가

---
+++ src/js/utils/constants.js
@@ -0,0 +1,21 @@
+export const CLASS_NAME = {
+    PURCHASE_FORM: 'purchase-form',
+    LOTTO_FORM: 'lotto-form',
+    SECTION_TITLE_LABEL: 'section-title',
+    LOTTO_LIST: 'lottos',
+    TOGGLE_BUTTON: 'lotto-numbers-toggle-button',
+    LOTTO_NUMBERS: 'lotto-numbers',
+};
+
+export const NUM = {
+    MAX_RANDOM: 45,
+    MIN_RANDOM: 1,
+    MAX_LOTTO_LENGTH: 6,
+    LOTTO_PRICE: 1000,
+};
+
+export const MESSAGE = {
+    IS_REQUIRED: '이 입력란을 작성하세요.',
+    MIN_VALUE: '값은 1000 이상이어야 합니다.',
+    MAX_VALUE: '값은 10000 이하여야 합니다.',
+};
