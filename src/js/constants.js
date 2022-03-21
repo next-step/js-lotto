6680f9e7ce5625df3ed7eea7619be3151039df64
wanho1108 <wanho1108@gmail.com>
2022-03-21 21:54:45 +0900

    fix: 로또 번호 발급 6개 → 7개로 변경

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,4 +1,4 @@
 export const LOTTO_PRICE_UNIT = 1000;
 export const LOTTO_PRICE_UNIT_NOT_MATCH_MESSAGE = '로또 구입 금액은 1,000원 단위로 입력해주세요.';
 export const LOTTO_NUMBER_RANGE = 45;
-export const LOTTO_NUMBER_COUNT = 6;
+export const LOTTO_NUMBER_COUNT = 7;
