1aadc99416d98a85e1b4c90c1dd6bfd0bf2875ba
aawlgus <aawlgus@jaranda.kr>
2022-03-21 23:21:15 +0900

    test: add first page render test case

--- cypress/integration/step1/buy.spec.js
+++ cypress/integration/step1/buy.spec.js
@@ -10,6 +10,22 @@ describe("Calculator Test", function () {
     cy.get(".confirm").click();
   };
 
+  it("최초 랜더 시, 로또 금액을 입력할 수 있는 인풋 필드가 존재한다. ", function () {
+    cy.get(".price-input").should("be.visible");
+  });
+
+  it("구입 금액 입력창에 아무 값이 없을 경우 적절한 placeholder를 보여준다. ", function () {
+    cy.get(".price-input").clear();
+    cy.get(".price-input").should("have.attr", "placeholder", "구입 금액");
+  });
+
+  it("구입할 금액을 입력하기 전까지는 아래 구매내역, 번호 입력창을 보여주지 않는다. ", function () {
+    // 노출 여부 판단
+    cy.get("#purchase-detail").should("not.be.visible");
+    typePriceInput("10000");
+    cy.get("#purchase-detail").should("be.visible");
+  });
+
   it("로또 금액을 입력하면 적절한 개수의 로또가 발급된다. ", function () {
     typePriceInput("10000");
     cy.get("#count-text").should("have.text", "총 10개를 구매하였습니다. ");
@@ -25,18 +41,6 @@ describe("Calculator Test", function () {
     cy.on("window:alert", (text) => {
       expect(text).to.contains(INVALID_INPUT_ALERT_STRING);
     });
-  });
-
-  it("구입 금액 입력창에 아무 값이 없을 경우 적절한 placeholder를 보여준다. ", function () {
-    cy.get(".price-input").clear();
-    cy.get(".price-input").should("have.attr", "placeholder", "구입 금액");
-  });
-
-  it("구입할 금액을 입력하기 전까지는 아래 구매내역, 번호 입력창을 보여주지 않는다. ", function () {
-    // 노출 여부 판단
-    cy.get("#purchase-detail").should("not.be.visible");
-    typePriceInput("10000");
-    cy.get("#purchase-detail").should("be.visible");
   });
 
   it("번호보기 토글 버튼을 클릭하면 복권 번호를 보여준다. ", function () {
