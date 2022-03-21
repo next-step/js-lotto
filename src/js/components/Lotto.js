bec0428cd63df09326d63740ff243746b2150132
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-03-21 21:43:39 +0900

    feat: Lotto Components 추가

---
+++ src/js/components/Lotto.js
@@ -0,0 +1,20 @@
+function Lotto(lotto) {
+  const $lotto = document.createElement('li');
+  $lotto.classList.add('mx-1', 'text-4xl', 'lotto-wrapper');
+
+  const $icon = document.createElement('span');
+  $icon.textContent = '🎟️';
+
+  const $lottoNumbers = document.createElement('span');
+  $lottoNumbers.textContent = lotto.join(', ');
+  $lottoNumbers.classList.add('lotto-detail');
+  $lottoNumbers.classList.add('display-none');
+  $lottoNumbers.style.display = 'inline';
+
+  $lotto.appendChild($icon);
+  $lotto.appendChild($lottoNumbers);
+
+  return $lotto;
+}
+
+export default Lotto;
