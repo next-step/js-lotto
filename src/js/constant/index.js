28c2ba004c6b8605becf9c316e8b68dd809cdfcd
Dami Kim <dami.k.dev@gmail.com>
2022-03-21 23:41:04 +0900

    feat: test 추가(구매 금액이 1000원 미만인 경우 alert)

--- src/js/constant/index.js
+++ src/js/constant/index.js
@@ -1,3 +1,5 @@
 export const LOTTO_PRICE = 1000;
 
 export const LOTTO_NUMBER_COUNT = 6;
+
+export const ERROR_MESSAGE_MIN_TOTAL_AMOUNT = "값은 1000원 이상이어야 합니다.";
--- src/js/models/Validator/Validator.js
+++ src/js/models/Validator/Validator.js
@@ -1,10 +1,13 @@
-import { LOTTO_PRICE } from "../../constant/index.js";
+import {
+  LOTTO_PRICE,
+  ERROR_MESSAGE_MIN_TOTAL_AMOUNT,
+} from "../../constant/index.js";
 
 class Validator {
   validateTotalAmount(money) {
     try {
       if (money < LOTTO_PRICE) {
-        throw Error("값은 1000원 이상이어야 합니다.");
+        throw Error(ERROR_MESSAGE_MIN_TOTAL_AMOUNT);
       }
     } catch (error) {
       alert(error.message);
