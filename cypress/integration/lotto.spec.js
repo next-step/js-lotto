59b9f9e4879afd80effa2e293c9faed2219e6fed
Younho Choo <yo+github@younho9.com>
2022-03-21 21:17:56 +0900

    feat: 구입 금액 입력 폼이 표시된다

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -3,7 +3,10 @@ beforeEach(() => {
 });
 
 describe('구입 금액 입력 폼', () => {
-	it('구입 금액 입력 폼이 표시된다.', () => {});
+	it('구입 금액 입력 폼이 표시된다.', () => {
+		cy.get('form[name="purchaseForm"]').should('be.visible');
+		cy.get('input[name="purchaseAmountInput"]').should('be.visible');
+	});
 	it('1000원 단위가 아닌 금액을 제출하면, 얼럿 메시지를 표시한다.', () => {});
 	it('1000원 미만의 금액을 제출하면, 유효성 검사 메시지를 표시한다.', () => {});
 });
