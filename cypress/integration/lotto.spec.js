41e1fe55d70b58ee5e70fd5d031ad3171c31d0f9
hhxdragon <gygud98@gmail.com>
2022-03-21 22:23:36 +0900

    refactor(cypress): 테스트 케이스이름을 좀 더 구체적으로 명시

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -13,35 +13,35 @@ describe('로또 테스트', () => {
     cy.visit('http://localhost:5500/');
   });
 
-  it('초기 화면 노출', () => {
+  it('첫화면 렌더링 시에, 로또리스트, 당첨번호 form 비노출', () => {
     cy.get('#lotto-list').should('not.be.visible');
     cy.get('#form-winning').should('not.be.visible');
   });
 
-  describe('금액 입력 에러', () => {
-    it('금액 입력 없을 경우', () => {
+  context('로또 금액 입력이 잘못된 경우에 따른 경고창 노출', () => {
+    it('로또 금액 입력 없이 확인 버튼 클릭할 때 경고창 노출', () => {
       cy.get('#form-price input[type=button]').click();
       cy.on('window.alert', (text) => expect(text).to.be(ERROR_MESSAGES.EMPTY));
     });
-    it('1000원 이하 입력하는 경우', () => {
+    it('로또 금액을 1,000원 이하로 구매하는 경우 경고창 노출', () => {
       handleInputPrice(500);
       cy.get('#form-price input[type=button]').click();
       cy.on('window.alert', (text) => expect(text).to.be(ERROR_MESSAGES.MIN_PRICE));
     });
-    it('100000원 초과 입력하는 경우', () => {
+    it('로또 금액을 100,000원 초과로 구매하는 경우 경고창 노출', () => {
       handleInputPrice(500);
       cy.get('#form-price input[type=button]').click();
       cy.on('window.alert', (text) => expect(text).to.be(ERROR_MESSAGES.MAX_PRICE));
     });
-    it('금액단위가 안맞는 경우', () => {
+    it('로또 금액단위가 맞지않는 경우 경고창 노출', () => {
       handleInputPrice(2022);
       cy.get('#form-price input[type=button]').click();
       cy.on('window.alert', (text) => expect(text).to.be(ERROR_MESSAGES.BUY_UNIT));
     });
   });
 
-  describe('정상금액 입력', () => {
-    it('구매 성공시 수량 및 아이콘 표시', () => {
+  context('로또 금액이 정상적으로 입력된 경우', () => {
+    it('로또 구매 성공시 로또 수량 및 로또 아이콘 표시', () => {
       handleInputPrice(3000);
       cy.get('#lotto-list').should('be.visible');
       cy.get('#form-winning').should('be.visible');
@@ -50,8 +50,8 @@ describe('로또 테스트', () => {
     });
   });
 
-  describe('번호보기 토글 클릭시', () => {
-    it('6개숫자, 중복되지 않음', () => {
+  context('로또 구매 이후 번호보기 토글버튼 클릭한 경우', () => {
+    it('6개의 로또번호 숫자가 중복되지 않는지 확인', () => {
       cy.get('.lotto-numbers-toggle-button').click();
 
       cy.get('li.lotto-list-item').each(($item) => {
