bc322addc7780942015d2c5a4238d16257829c49
wanho1108 <wanho1108@gmail.com>
2022-03-21 20:32:37 +0900

    feat: 로또 발급 기능 추가

--- src/js/app.js
+++ src/js/app.js
@@ -60,6 +60,33 @@ class App {
     const $lottoBuyFormInput = this.#element.querySelector('.lotto-buy-price-input');
     const price = parseInt($lottoBuyFormInput.value, 10);
     this.#lotto.buy(price);
+
+    if (this.#lotto.data.length) {
+      this.#renderLottoListContainer();
+      this.#renderLottoList();
+    }
+  }
+
+  #renderLottoListContainer() {
+    const $lottoListContainer = this.#element.querySelector('.lotto-list-container');
+    $lottoListContainer.innerHTML = `
+      <div class="d-flex">
+        <label class="flex-auto my-0">
+          총 <span class="lotto-list-count">${this.#lotto.data.length}</span>개를 구매하였습니다.
+        </label>
+        <div class="flex-auto d-flex justify-end pr-1">
+          <label class="switch">
+            <input type="checkbox" class="lotto-list-toggle-button" />
+            <span class="text-base font-normal">번호보기</span>
+          </label>
+        </div>
+      </div>
+      <div class="d-flex flex-wrap lotto-list"></div>`;
+  }
+
+  #renderLottoList() {
+    const $lottoList = this.#element.querySelector('.lotto-list');
+    $lottoList.innerHTML = this.#lotto.data.map(() => `<span class="mx-1 text-4xl lotto-item">🎟️</span>`).join('');
   }
 }
 
--- src/js/lotto.js
+++ src/js/lotto.js
@@ -1,16 +1,46 @@
-import { LOTTO_PRICE_UNIT, LOTTO_PRICE_UNIT_NOT_MATCH_MESSAGE } from './constants.js';
+import {
+  LOTTO_PRICE_UNIT,
+  LOTTO_PRICE_UNIT_NOT_MATCH_MESSAGE,
+  LOTTO_NUMBER_RANGE,
+  LOTTO_NUMBER_COUNT,
+} from './constants.js';
+import { shuffleArray } from './utils.js';
 
 class Lotto {
-  lottos = [];
+  /** @type {[number[]]} */
+  data = [];
 
   /**
    * @param {number} price
    */
-  // eslint-disable-next-line class-methods-use-this
   buy(price) {
+    const lottoBuyLength = price / LOTTO_PRICE_UNIT;
+
     if (price % LOTTO_PRICE_UNIT !== 0) {
       alert(LOTTO_PRICE_UNIT_NOT_MATCH_MESSAGE);
+      return;
     }
+
+    for (let i = 0; i < lottoBuyLength; i += 1) {
+      this.data.push(this.create());
+    }
+  }
+
+  create() {
+    const lotto = [];
+    const lottoRange = [...new Array(LOTTO_NUMBER_RANGE)].map((_, index) => index + 1);
+
+    shuffleArray(lottoRange);
+
+    for (let i = 0; i < LOTTO_NUMBER_COUNT; i += 1) {
+      lotto.push(lottoRange.splice(0, 1)[0]);
+    }
+
+    return lotto;
+  }
+
+  init() {
+    this.data = [];
   }
 }
 
