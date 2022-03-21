e6d1884e4a7a5c1d111753d1c3b4cff0d337367a
정현수 <jung660317@naver.com>
2022-03-21 14:43:10 +0900

    refactor: PurchaseForm 이벤트 하나로 병합
    - button의 type을 submit으로 변경 후, form의 onsubmit 이벤트에만 구매 이벤트를 등록

--- src/js/components/PurchaseForm.js
+++ src/js/components/PurchaseForm.js
@@ -13,7 +13,7 @@ class PurchaseForm extends Component {
           class="w-100 mr-2 pl-2"
           placeholder="구입 금액"
         />
-        <button type="button" id="${DOM.PURCHASE_FORM_BUTTON}" class="btn btn-cyan">확인</button>
+        <button type="submit" id="${DOM.PURCHASE_FORM_BUTTON}" class="btn btn-cyan">확인</button>
       </div>
     `;
   }
@@ -24,12 +24,11 @@ class PurchaseForm extends Component {
 
   setEvent() {
     this.$target.onsubmit = this.onSubmitPurchaseForm.bind(this);
-    $(`#${DOM.PURCHASE_FORM_BUTTON}`).onclick = this.onSubmitPurchaseForm.bind(this);
   }
 
   onSubmitPurchaseForm(e) {
-    const $purchaseFormInput = $(`#${DOM.PURCHASE_FORM_INPUT}`);
     e.preventDefault();
+    const $purchaseFormInput = e.target[0];
 
     if (!this.validateFormInput($purchaseFormInput.value)) {
       $purchaseFormInput.value = '';
