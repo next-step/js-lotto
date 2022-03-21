06accd90961be20510a4ff71d670d840fcb666fb
Hyuna Park <hyuna_park@adenasoft.com>
2022-03-21 22:34:48 +0900

    [:hammer: refactor] 구매 내역 노출 로직 함수 분할

--- src/js/components/displays/PurchaseDisplay.js
+++ src/js/components/displays/PurchaseDisplay.js
@@ -1,8 +1,9 @@
 export default class PurchaseDisplay {
-  constructor({isConfirmBtnClicked, purchasedVal}) {
+  constructor({isConfirmBtnClicked, purchasedVal, onToggle}) {
     this.isVisible = null;
     this.purchasedVal = null;
     this.$purchaseDisplayBox = null;
+    this.handleToggle = onToggle;
     this.setState(isConfirmBtnClicked, purchasedVal);
   }
 
@@ -12,26 +13,37 @@ export default class PurchaseDisplay {
     this.render();
   }
 
-  render() {
+  showPurchaseDisplayInfo() {
     const $purchaseDisplayBox = document.querySelector('section');
-    // this.$purchaseDisplayBox = $purchaseDisplayBox.cloneNode(true);
 
     if (!this.isVisible) {
       $purchaseDisplayBox.style.display = 'none';
     } else {
       $purchaseDisplayBox.style.display = 'block';
-
+  
       const $purchasedResultText = document.getElementById('purchased-result-text');
       const $purchasedTicketImgBox = document.getElementById('ticket-img-box');
-
-
+  
       $purchasedResultText.textContent = `총 ${this.purchasedVal}개를 구매하였습니다.`;
-
+  
       let ticketImages = '';
       for (let i = 0; i < this.purchasedVal; i++) {
         ticketImages += `<span class="mx-1 text-4xl">🎟️ </span>`;
       }
       $purchasedTicketImgBox.innerHTML = ticketImages;
     }
+  }
+
+  render() {
+    this.showPurchaseDisplayInfo();
+
+    const $lottoNumbersToggleBtn = document.querySelector('.lotto-numbers-toggle-button');
+    // this.$purchaseDisplayBox = $purchaseDisplayBox.cloneNode(true);
+
+    this.addEvent($lottoNumbersToggleBtn, 'input', this.handleToggle);
+  }
+
+  addEvent($el, event, callbackFn) {
+    $el.addEventListener(event, callbackFn);
   }
 }
