c40dab03b63c0ea35962e9ef5640c994b7799549
Tene <tene@nextunicorn.kr>
2022-03-23 00:27:42 +0900

    ✒️Feat: 3주차 클린코드 내용을 반영한다.
    - 유틸 함수는 도메인에 상관이 없어야 하므로 도메인 로직으로 분리한다.

--- src/js/services/lotto.js
+++ src/js/services/lotto.js
@@ -1,3 +1,6 @@
+import { AMOUNT_UNIT, ERROR_MESSAGE } from './constants.js';
+import { isEmpty } from '../helper/index.js';
+
 const useLottoService = () => {
   const getLottoNumbers = () => {
     const numbers = new Set();
@@ -11,7 +14,15 @@ const useLottoService = () => {
     return Array.from({ length: number }).map(() => getLottoNumbers());
   };
 
-  return { purchasesLotto };
+  const validCount = amount => {
+    const count = Number(amount / AMOUNT_UNIT);
+    if (isEmpty(amount) || isNaN(count)) throw new Error(ERROR_MESSAGE.REQUIRED_DIGIT);
+    if (amount < AMOUNT_UNIT) throw new Error(ERROR_MESSAGE.MUST_MORE_THAN);
+    if (!Number.isInteger(count)) throw new Error(ERROR_MESSAGE.MUST_REQUIRED_AMOUNT_UNIT);
+    return count;
+  };
+
+  return { purchasesLotto, validCount };
 };
 
 export default useLottoService;
