192cd6faa0d605f2e3b6fb4bbf35e89b3f65cecb
bomee.yoon <bomee.yoon@godo.co.kr>
2022-03-22 19:36:34 +0900

    🐛  안내 메시지  참조  오류  수정

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -1,5 +1,4 @@
-import { CLASS } from '../../src/js/const/className.js';
-import { alertMessage, MIN_MONEY_UNIT } from '../../src/js/const/constant.js';
+import { MESSAGE } from '../../src/js/const/constant.js';
 
 // const BASE_URL = cy.config('baseUrl');
 before(() => cy.visit('http://127.0.0.1:5500/index.html'));
@@ -7,8 +6,8 @@ before(() => cy.visit('http://127.0.0.1:5500/index.html'));
 describe('로또 어플리케이션 테스트', () => {
   describe('스텝 1 : 구매하기', () => {
     it('최소 구매금액은 1000원이다.', () => {
-      cy.submitMoney(500).alert(alertMessage.ALERT_MIN_AMOUNT_TO_ISSUE_LOTTO);
-      cy.submitMoney(800).alert(alertMessage.ALERT_MIN_AMOUNT_TO_ISSUE_LOTTO);
+      cy.submitMoney(500).alert(MESSAGE.ALERT_MIN_AMOUNT_TO_ISSUE_LOTTO);
+      cy.submitMoney(800).alert(MESSAGE.ALERT_MIN_AMOUNT_TO_ISSUE_LOTTO);
     });
 
     it('금액을 입력하면, 해당 금액만큼 로또를 발급한다.', () => {
