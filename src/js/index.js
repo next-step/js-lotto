6fea32d4c6d8a255a9dd5eae44403a75fcc0ebf2
yaag <yaag@yaagui-MacBookPro.local>
2022-03-22 18:21:51 +0900

    feat: 금액 입력 후 확인 버튼 클릭 이벤트 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,18 @@
-const $showResultButton = document.querySelector('.open-result-modal-button')
-const $modalClose = document.querySelector('.modal-close')
-const $modal = document.querySelector('.modal')
-const $lottoNumbersToggleButton = document.querySelector(
-  '.lotto-numbers-toggle-button'
-)
+import { purchaseLotto } from './app.js';
+
+const $showResultButton = document.querySelector('.open-result-modal-button');
+const $modalClose = document.querySelector('.modal-close');
+const $modal = document.querySelector('.modal');
+const $purchaseForm = document.querySelector('.input-purchase-form');
 
 const onModalShow = () => {
-  $modal.classList.add('open')
-}
+  $modal.classList.add('open');
+};
 
 const onModalClose = () => {
-  $modal.classList.remove('open')
-}
+  $modal.classList.remove('open');
+};
 
-$showResultButton.addEventListener('click', onModalShow)
-$modalClose.addEventListener('click', onModalClose)
+$purchaseForm.addEventListener('submit', purchaseLotto);
+$showResultButton.addEventListener('click', onModalShow);
+$modalClose.addEventListener('click', onModalClose);
