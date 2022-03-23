ffa66ea72f15e3c0cebd64ad0b9efc95d8e7a7a7
정현수 <jung660317@naver.com>
2022-03-23 15:18:47 +0900

    chore: lotto 관련 함수들 폴더 변경
    - utils에서 services로 변경
    - utils는 조금 더 범용성있는 함수로 쓰일 때 사용

--- src/js/utils/lotto.js
+++
@@ -1,9 +0,0 @@
-export const getWinningResult = (winningLottoNumbers, purchaseLottoNumbers) => {
-  console.log(winningLottoNumbers);
-  console.log(purchaseLottoNumbers);
-};
-
-export const getYield = (purchasePrice, winningResult) => {
-  console.log(purchasePrice);
-  console.log(winningResult);
-};
