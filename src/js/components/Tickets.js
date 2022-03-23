744adfc3ed1774969cd1b6167d52099e651a8928
Jin Hyung Park <hophfg@yahoo.co.kr>
2022-03-24 04:02:26 +0900

    refactor: Tickets to render with replaceRender

--- src/js/components/Tickets.js
+++ src/js/components/Tickets.js
@@ -1,4 +1,5 @@
 import { $ } from "../utils/document.js";
+import { replaceRender } from "../utils/replaceRender.js";
 import renderLottoNumbers from "./renderLottoNumbers.js";
 
 export const Tickets = ($el, tickets) => {
@@ -13,24 +14,27 @@ export const Tickets = ($el, tickets) => {
   };
 
   function render() {
-    $el.insertAdjacentHTML('beforeEnd', `
-            <section class="mt-9">
-                <div class="d-flex">
-                  <label class="flex-auto my-0" data-test="tickets-count">
-                  총 ${tickets.length}개를 구매하였습니다.
-                  </label>
+    $el = replaceRender({
+      $originEl: $el,
+      replaceHTML: `
+          <section class="mt-9">
+              <div class="d-flex">
+                  <label class="flex-auto my-0" data-test="tickets-count">총 ${tickets.length}개를 구매하였습니다.</label>
                   <div class="flex-auto d-flex justify-end pr-1">
-                    <label class="switch" data-test="number-detail-switch">
-                      <input type="checkbox" class="lotto-numbers-toggle-button" data-test="amount-input"/>
-                      <span class="text-base font-normal">번호보기</span>
-                    </label>
+                      <label class="switch" data-test="number-detail-switch">
+                          <input type="checkbox" class="lotto-numbers-toggle-button" data-test="amount-input"/>
+                          <span class="text-base font-normal">번호보기</span>
+                      </label>
                   </div>
-                </div>
-                <div data-component="lotto-numbers"><div>
-            </section>
-        `);
-
-    $('.switch', $el).addEventListener('change', switchShowNumbers);
+              </div>
+              <div data-component="lotto-numbers"><div>
+          </section>
+      `,
+      bindEvents: [
+        ($el) => $('.switch', $el)
+          .addEventListener('change', switchShowNumbers),
+      ],
+    });
   }
 
   render();
