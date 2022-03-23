8ac631fa248b833e264e5e57596a0bdb4f2ec2e2
hdpyo <hudongpyo.dev@gmail.com>
2022-03-23 22:52:14 +0900

    chore: .prettierrc.js 파일 추가
    - 기존에 settings.json 에 작성하여 전역으로 관리하던 prettier 설정을 분리.

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "singleQuote": false,     //* 홑따옴표를 쓸 건지?
+  "semi": true,             //* 세미콜론 작동작성 여부
+  "tabWidth": 2,            //* 탭을 눌렀을 때 몇칸 띄울 것인지?
+  "printWidth": 80,         //* 한줄 내에서 코드를 맞추기. 한 줄이 이 글자수를 넘게 되면 줄바꿈 된다.
+  "trailingComma": "all",   //* 객체, 배열, 함수 등의 후행에 쉼표를 찍을지? (es5, none, all)
+  "arrowParens": "avoid",   //* 단독 화살표 함수의 매개변수 주위에 괄호를 붙임
+  "proseWrap": "preserve",
+  "quoteProps": "preserve", //* Syntax에 맞다면 개발자가 입력한대로 따옴표 사용하기
+  "bracketSpacing": true,   //* 객체 리터럴 내부의 공백 제어 여부
+}
