d61ddd5d39a85f26dd6dd09d496946c035ad109b
circlegiven <circlegiven@gmail.com>
2022-03-23 22:22:23 +0900

    [공통] lint 에러 수정

--- src/js/view/lottoVendingView.js
+++ src/js/view/lottoVendingView.js
@@ -1,40 +1,38 @@
 import purchasedHistoryView from './purchasedHistoryView.js';
-import {LottoVendingMachine} from '../lottoVendingMachine.js';
+import { LottoVendingMachine } from '../lottoVendingMachine.js';
 
 export default (function () {
+  const $purchaseLotto = document.querySelector('form.mt-5');
+  const $purchaseMoney = $purchaseLotto.querySelector('input');
+  const $winingLottoHistory = document.querySelector('form.mt-9');
 
-    const $purchaseLotto = document.querySelector('form.mt-5');
-    const $purchaseMoney = $purchaseLotto.querySelector('input');
-    const $winingLottoHistory = document.querySelector('form.mt-9');
+  function handlePurchaseLotto(event) {
+    const money = new FormData(event.currentTarget).get('money');
+    event.preventDefault();
+    purchasedHistoryView.changePurchasedHistory(
+      LottoVendingMachine.purchaseLotto(money)
+    );
+  }
 
-    function handlePurchaseLotto(event) {
-        const money = new FormData(event.currentTarget).get('money');
-        event.preventDefault();
-        purchasedHistoryView.changePurchasedHistory(LottoVendingMachine.purchaseLotto(money));
-    }
+  function initialMoney() {
+    $purchaseMoney.value = '';
+  }
 
-    function initialMoney() {
-        $purchaseMoney.value = '';
-    }
+  function initialWinningHistory() {
+    $winingLottoHistory.style.display = 'none';
+  }
 
-    function initialWinningHistory() {
-        $winingLottoHistory.style.display = 'none';
-    }
+  function eventBindings() {
+    $purchaseLotto.addEventListener('submit', handlePurchaseLotto);
+  }
 
-    function eventBindings() {
-        $purchaseLotto.addEventListener(
-            'submit',
-            handlePurchaseLotto
-        );
-    }
+  function initial() {
+    initialMoney();
+    initialWinningHistory();
+    purchasedHistoryView.initial();
+  }
 
-    function initial() {
-        initialMoney();
-        initialWinningHistory();
-        purchasedHistoryView.initial();
-    }
+  eventBindings();
 
-    eventBindings();
-
-    return { initial };
+  return { initial };
 })();
--- src/js/view/purchasedHistoryView.js
+++ src/js/view/purchasedHistoryView.js
@@ -1,82 +1,91 @@
-import {Wallet} from '../data/wallet.js';
+import { Wallet } from '../data/wallet.js';
 
 export default (function () {
+  const $purchaseHistory = document.querySelector('section.mt-9');
+  const $purchasedLottoAmount =
+    $purchaseHistory.querySelector('label.my-0 span');
+  const $showLottoNumbersToggleButton = document.querySelector(
+    '.lotto-numbers-toggle-button'
+  );
+  const $purchasedLottoTickets = $purchaseHistory.querySelector(
+    'div.d-flex.flex-wrap'
+  );
+  const wallet = new Wallet();
 
-    const $purchaseHistory = document.querySelector('section.mt-9');
-    const $purchasedLottoAmount = $purchaseHistory.querySelector('label.my-0 span');
-    const $showLottoNumbersToggleButton = document.querySelector(
-        '.lotto-numbers-toggle-button'
+  function isShowLottoNumbers() {
+    return $showLottoNumbersToggleButton.checked;
+  }
+
+  function getLottoTicket(lotto) {
+    const $lotto = document.createElement('span');
+    $lotto.classList.add('mx-1', 'text-4xl');
+    $lotto.innerHTML = `🎟️ <span>${lotto.value}</span>`;
+    return $lotto;
+  }
+
+  function changePurchaseLottoTickets(lottoList) {
+    const $lottos = document.createDocumentFragment();
+    lottoList.forEach((lotto) => {
+      $lottos.appendChild(getLottoTicket(lotto));
+    });
+
+    $purchasedLottoTickets.replaceChildren($lottos);
+  }
+
+  function showPurchaseHistory() {
+    $purchaseHistory.style.visibility = 'visible';
+  }
+
+  function hidePurchaseHistory() {
+    $purchaseHistory.style.visibility = 'hidden';
+  }
+
+  function showPurchasedLottoNumbers() {
+    $purchasedLottoTickets
+      .querySelectorAll('span > span')
+      .forEach(($ticketNumber) => $ticketNumber.classList.remove('hide'));
+  }
+
+  function hidePurchasedLottoNumbers() {
+    $purchasedLottoTickets
+      .querySelectorAll('span > span')
+      .forEach(($ticketNumber) => $ticketNumber.classList.add('hide'));
+  }
+
+  function changeShowLottoNumbersByToggleStatus() {
+    if (isShowLottoNumbers()) {
+      showPurchasedLottoNumbers();
+      return;
+    }
+
+    hidePurchasedLottoNumbers();
+  }
+
+  function eventBindings() {
+    $showLottoNumbersToggleButton.addEventListener(
+      'click',
+      changeShowLottoNumbersByToggleStatus
     );
-    const $purchasedLottoTickets = $purchaseHistory.querySelector('div.d-flex.flex-wrap');
-    let wallet = new Wallet();
+  }
 
-    function isShowLottoNumbers() {
-        return $showLottoNumbersToggleButton.checked;
-    }
+  function changePurchasedLottoAmount(amount) {
+    $purchasedLottoAmount.innerText = amount;
+  }
 
-    function getLottoTicket(lotto) {
-        const $lotto = document.createElement('span');
-        $lotto.classList.add('mx-1', 'text-4xl');
-        $lotto.innerHTML = `🎟️ <span>${lotto.value}</span>`;
-        return $lotto;
-    }
+  function changePurchasedHistory(lottoList = []) {
+    wallet.setLottos(lottoList);
+    changePurchasedLottoAmount(lottoList.length);
+    changePurchaseLottoTickets(lottoList);
+    showPurchaseHistory();
+    changeShowLottoNumbersByToggleStatus();
+  }
 
-    function changePurchaseLottoTickets(lottoList) {
-        const $lottos = document.createDocumentFragment();
-        lottoList.forEach((lotto) => {
-            $lottos.appendChild(getLottoTicket(lotto));
-        });
+  function initial() {
+    wallet.removeAllLottos();
+    hidePurchaseHistory();
+  }
 
-        $purchasedLottoTickets.replaceChildren($lottos);
-    }
+  eventBindings();
 
-    function showPurchaseHistory() {
-        $purchaseHistory.style.visibility = 'visible';
-    }
-
-    function hidePurchaseHistory() {
-        $purchaseHistory.style.visibility = 'hidden';
-    }
-
-    function showPurchasedLottoNumbers() {
-        $purchasedLottoTickets.querySelectorAll('span > span').forEach($ticketNumber => $ticketNumber.style.display = 'inline');
-    }
-
-    function hidePurchasedLottoNumbers() {
-        $purchasedLottoTickets.querySelectorAll('span > span').forEach($ticketNumber => $ticketNumber.style.display = 'none');
-    }
-
-    function changeShowLottoNumbersByToggleStatus() {
-        if (isShowLottoNumbers()) {
-            showPurchasedLottoNumbers();
-            return;
-        }
-
-        hidePurchasedLottoNumbers();
-    }
-
-    function eventBindings() {
-        $showLottoNumbersToggleButton.addEventListener('click', changeShowLottoNumbersByToggleStatus)
-    }
-
-    function changePurchasedLottoAmount(amount) {
-      $purchasedLottoAmount.innerText = amount;
-    }
-
-    function changePurchasedHistory(lottoList = []) {
-        wallet.setLottos(lottoList);
-        changePurchasedLottoAmount(lottoList.length);
-        changePurchaseLottoTickets(lottoList);
-        showPurchaseHistory();
-        changeShowLottoNumbersByToggleStatus();
-    }
-
-    function initial() {
-        wallet.removeAllLottos();
-        hidePurchaseHistory();
-    }
-
-    eventBindings();
-
-    return { initial, changePurchasedHistory };
+  return { initial, changePurchasedHistory };
 })();
