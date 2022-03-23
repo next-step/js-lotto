2442fd2a47498a1665d6b3258bf2916710346ce6
yaag <yaag@yaagui-MacBookPro.local>
2022-03-23 19:05:38 +0900

    modify: 기존에 구매한 로또가 있는 상태에서 로또를 재구매 했을 경우에 대한 테스트 코드 작성

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -13,7 +13,7 @@ describe('Lotto Cypress', () => {
   });
 
   describe('STEP 1', () => {
-    describe('로또 구입 금액이 유효한 값일 경우', () => {
+    context('로또 구입 금액이 유효한 값일 경우', () => {
       it('1,000원 당 로또 한 장을 구입할수 있다.', () => {
         cy.get('#input-price').type(4000);
         cy.get('.input-purchase-form')
@@ -36,7 +36,7 @@ describe('Lotto Cypress', () => {
       });
     });
 
-    describe('로또 구입 금액이 유효하지 않은 값일 경우', () => {
+    context('로또 구입 금액이 유효하지 않은 값일 경우', () => {
       it('로또 구입 금액은 1000 단위로 입력 되어야 한다', () => {
         const alertStub = cy.stub();
         cy.on('window:alert', alertStub);
@@ -46,6 +46,54 @@ describe('Lotto Cypress', () => {
           .submit()
           .then(() => {
             expect(alertStub).to.be.calledWith(ERROR_INPUT_PRICE_UNIT);
+          });
+      });
+    });
+
+    context('로또를 구입한 상태에서 새롭게 로또를 구입했을 경우', () => {
+      // it('기존 로또 티켓과 숫자들은 화면에서 지워져야 한다.', () => {
+      //   cy.get('#input-price').type(4000);
+      //   cy.get('.input-purchase-form')
+      //     .submit()
+      //     .then(() => {
+      //       cy.get('#input-price').clear();
+      //       cy.get('#input-price').type(7000);
+      //       cy.get('.input-purchase-form')
+      //         .submit()
+      //         .then(() => {
+      //           cy.get('.lotto-content').should('not.be.visible');
+      //         });
+      //     });
+      // });
+
+      it('번호 보기가 비활성화(toggle off) 되어 있어야 한다', () => {
+        cy.get('#input-price').type(4000);
+        cy.get('.input-purchase-form')
+          .submit()
+          .then(() => {
+            cy.get('#input-price').clear();
+            cy.get('#input-price').type(7000);
+            cy.get('.input-purchase-form')
+              .submit()
+              .then(() => {
+                cy.get('#lotto-numbers-toggle').should('not.be.checked');
+                cy.get('.lotto-numbers').should('not.be.visible');
+              });
+          });
+      });
+
+      it('새로 구입한 로또는 이전 티켓과 중복되어 렌더링 되지 않는다', () => {
+        cy.get('#input-price').type(4000);
+        cy.get('.input-purchase-form')
+          .submit()
+          .then(() => {
+            cy.get('#input-price').clear();
+            cy.get('#input-price').type(7000);
+            cy.get('.input-purchase-form')
+              .submit()
+              .then(() => {
+                cy.get('.lotto-content').should('have.length', 7);
+              });
           });
       });
     });
