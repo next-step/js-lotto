ead169d7e879f2184d0534359e063ca70aa8a90e
정현수 <jung660317@naver.com>
2022-03-23 14:14:07 +0900

    test: 당첨 결과 기능 함수 테스트 생성
    - getWinningResult은 알맞은 등수가 나오는지 테스트
    - getYield은 알맞은 수익률을 계산하는지 테스트

---
+++ cypress/integration/lottoResult.spec.js
@@ -0,0 +1,149 @@
+import { getWinningResult, getYield } from '../../src/js/utils/lotto.js';
+
+describe('당첨 결과 기능', () => {
+  beforeEach(() => {
+    cy.reload();
+  });
+
+  context('당첨 통계에는 알맞게 등수가 표시되어야 합니다.', () => {
+    const result = {
+      first: 0,
+      second: 0,
+      third: 0,
+      fourth: 0,
+      fifth: 0,
+    };
+
+    it('3개가 같다 = 5등', () => {
+      const winningLottoNumbers = [1, 2, 3, 4, 5, 6, 7];
+      const purchaseLottoNumbers = [[1, 2, 3, 10, 11, 12, 13]];
+
+      const expectResult = {
+        ...result,
+        fifth: 1,
+      };
+
+      expect(getWinningResult(winningLottoNumbers, purchaseLottoNumbers)).to.deep.equal(
+        expectResult,
+      );
+    });
+
+    it('4개가 같다 = 4등', () => {
+      const winningLottoNumbers = [1, 2, 3, 4, 5, 6, 7];
+      const purchaseLottoNumbers = [[1, 2, 3, 4, 11, 12, 13]];
+
+      const expectResult = {
+        first: 0,
+        second: 0,
+        third: 0,
+        fourth: 1,
+        fifth: 0,
+      };
+
+      expect(getWinningResult(winningLottoNumbers, purchaseLottoNumbers)).to.deep.equal(
+        expectResult,
+      );
+    });
+
+    it('5개가 같다 = 3등', () => {
+      const winningLottoNumbers = [1, 2, 3, 4, 5, 6, 7];
+      const purchaseLottoNumbers = [[1, 2, 3, 4, 5, 12, 13]];
+
+      const expectResult = {
+        first: 0,
+        second: 0,
+        third: 1,
+        fourth: 0,
+        fifth: 0,
+      };
+
+      expect(getWinningResult(winningLottoNumbers, purchaseLottoNumbers)).to.deep.equal(
+        expectResult,
+      );
+    });
+
+    it('5개가 같고, 보너스 번호가 같다 = 2등', () => {
+      const winningLottoNumbers = [1, 2, 3, 4, 5, 6, 7];
+      const purchaseLottoNumbers = [[1, 2, 3, 4, 5, 12, 7]];
+
+      const expectResult = {
+        first: 0,
+        second: 1,
+        third: 0,
+        fourth: 0,
+        fifth: 0,
+      };
+
+      expect(getWinningResult(winningLottoNumbers, purchaseLottoNumbers)).to.deep.equal(
+        expectResult,
+      );
+    });
+
+    it('6개가 같다 = 1등', () => {
+      const winningLottoNumbers = [1, 2, 3, 4, 5, 6, 7];
+      const purchaseLottoNumbers = [[1, 2, 3, 4, 5, 6, 13]];
+
+      const expectResult = {
+        first: 1,
+        second: 0,
+        third: 0,
+        fourth: 0,
+        fifth: 0,
+      };
+
+      expect(getWinningResult(winningLottoNumbers, purchaseLottoNumbers)).to.deep.equal(
+        expectResult,
+      );
+    });
+  });
+
+  context('당첨 통계에는 알맞은 수익률이 표기되어야 합니다.', () => {
+    it('5000원 구매, 5등 1개 = 0%', () => {
+      const purchasePrice = 5000;
+
+      const winningResult = {
+        first: 0,
+        second: 0,
+        third: 0,
+        fourth: 0,
+        fifth: 1,
+      };
+
+      const expectResult = 0;
+
+      expect(getWinningResult(purchasePrice, winningResult)).to.deep.equal(expectResult);
+    });
+
+    it('5000원 구매, 4등 1개 = 900%', () => {
+      const purchasePrice = 5000;
+
+      const winningResult = {
+        first: 0,
+        second: 0,
+        third: 0,
+        fourth: 1,
+        fifth: 0,
+      };
+
+      const expectResult = 900;
+
+      expect(getWinningResult(purchasePrice, winningResult)).to.deep.equal(expectResult);
+    });
+
+    it('5000원 구매, 1등 1개 = 39999900%', () => {
+      const purchasePrice = 5000;
+
+      const winningResult = {
+        first: 1,
+        second: 0,
+        third: 0,
+        fourth: 0,
+        fifth: 0,
+      };
+
+      const expectResult = 39999900;
+
+      expect(getWinningResult(purchasePrice, winningResult)).to.deep.equal(expectResult);
+    });
+  });
+});
---
+++ src/js/utils/lotto.js
@@ -0,0 +1,9 @@
+export const getWinningResult = (winningLottoNumbers, purchaseLottoNumbers) => {
+  console.log(winningLottoNumbers);
+  console.log(purchaseLottoNumbers);
+};
+
+export const getYield = (purchasePrice, winningResult) => {
+  console.log(purchasePrice);
+  console.log(winningResult);
+};
