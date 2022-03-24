d24b08beb71f2ec332956f7c9f3a28e89eaf6d70
hdpyo <hudongpyo.dev@gmail.com>
2022-03-24 12:07:40 +0900

    chore: ESLint 설정 추가 및 포맷팅 변경
    - js 파일 import 시, ESLint에서  Unexpected use of file extension "js" for "파일명.js" 발생
      ㄴ .eslintrc.js 파일 내 rules 에 옵션 추가
    
    - 포맷팅 변경 by prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,18 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-  ],
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    "html"
-  ],
+  plugins: ["react", "html"],
   rules: {
+    // * import 시 확장자명을 입력해도 에러로 판단하지 않는다.
+    "import/prefer-default-export": "off",
+    "import/extensions": ["off"],
   },
 };
