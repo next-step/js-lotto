a3742399ab7f44c15ac4cbc44b2c364c29c1d61d
karl <elrion018@gmail.com>
2022-03-26 00:50:19 +0900

    chore : 개발 환경 셋팅

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,17 @@
-const $showResultButton = document.querySelector('.open-result-modal-button')
-const $modalClose = document.querySelector('.modal-close')
-const $modal = document.querySelector('.modal')
+const $showResultButton = document.querySelector('.open-result-modal-button');
+const $modalClose = document.querySelector('.modal-close');
+const $modal = document.querySelector('.modal');
 const $lottoNumbersToggleButton = document.querySelector(
   '.lotto-numbers-toggle-button'
-)
+);
 
 const onModalShow = () => {
-  $modal.classList.add('open')
-}
+  $modal.classList.add('open');
+};
 
 const onModalClose = () => {
-  $modal.classList.remove('open')
-}
+  $modal.classList.remove('open');
+};
 
-$showResultButton.addEventListener('click', onModalShow)
-$modalClose.addEventListener('click', onModalClose)
+$showResultButton.addEventListener('click', onModalShow);
+$modalClose.addEventListener('click', onModalClose);
