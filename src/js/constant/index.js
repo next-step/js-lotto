f972dee650db88c4716897053667636d1b92ea12
Minsiki <43837214+Minsiki@users.noreply.github.com>
2022-03-25 23:22:43 +0900

    불필요한 SELECTOR 제거

--- src/js/constant/index.js
+++ src/js/constant/index.js
@@ -1,38 +1,7 @@
-export const SELECTOR = {
-    ID: {
-        APP: "#app",
-        PURCHASE_PRICE_INPUT: "#purchase-price-input",
-        PURCHASE_BUTTON: "#purchase-button",
-        LOTTO_WINNING_AREA: "#lotto-winning-area",
-        LOTTO_PURCHASE_AREA: "#lotto-purchase-area",
-        LOTTO_AMOUNT_AREA: "#lotto-amount-area",
-        LOTTO_TICKET_AREA: "#lotto-ticket-area",
-        LOTTO_WINNING_AREA: "#lotto-winning-area",
-        OPEN_RESULT_MODAL_BUTTON: "#open-result-modal-button",
-        RESULT_MODAL: "#result_modal",
-        RESULT_MODAL_CLOSE: "#result_modal_close"
-    },
-    CLASS: {
-        LOTTO: ".lotto",
-        LOTTO_NUMBER_TOGGLE_BUTTON: ".lotto-numbers-toggle-button",
-        LOTTO_DETAIL: ".lotto-detail",
-        LOTTO_TICKET: ".lotto-ticket",
-        SWITCH: ".switch",
-        FLEX_COL: ".flex-col",
-        DISPLAY_NONE: ".d-none",
-        OPEN: ".open",
-        WINNING_NUMBER: ".winning-number",
-        BONUS_NUMBER: ".bonus-number"
-    },
-};
-
-export const LOTTO_MIN_NUMBER = 1;
 export const LOTTO_MAX_NUMBER = 45;
-export const LOTTO_TICKET_LENGTH = 6;
 export const LOTTO_UNIT = 1000;
 export const MIN_PURCHASE_PRICE = 1000;
 export const MAX_PURCHASE_PRICE = 100000;
-export const MAX_PURCHASE_AMOUNT = MAX_PURCHASE_PRICE / LOTTO_UNIT;
 
 export const MESSAGE = {
     ERROR: {
