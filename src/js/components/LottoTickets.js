79759c0a241f685a36acc6cde48bcb7765673cd8
Minsiki <43837214+Minsiki@users.noreply.github.com>
2022-03-25 23:25:12 +0900

    class filed에 변수 전언 및 private 메소드로 수정

--- src/js/components/LottoTickets.js
+++ src/js/components/LottoTickets.js
@@ -1,56 +1,56 @@
-import { SELECTOR } from "../constant/index.js";
-import { $, $$ } from "../utils/selector.js";
-
 export class LottoTickets {
-    constructor($amountArea, $ticketArea) {
-        this.$amountArea = $amountArea;
-        this.$ticketArea = $ticketArea;
-        this.amount = 0;
-        this.isChecked = false;
+    isChecked = false;
+    amount = 0;
+    tickets = null;
+
+    $switch = null;
+    $amountArea = null;
+    $ticketArea = null;
+    $lottoTickets = null;
+
+    constructor($amountAreaElement, $ticketAreaElement) {
+        this.$amountArea = $amountAreaElement;
+        this.$ticketArea = $ticketAreaElement;
     }
 
     render() {
-        this.$amountArea.innerHTML = this.getLottoAmountTemplate();
-        this.$ticketArea.innerHTML = this.getLottoTicketsTemplate();
+        this.$amountArea.innerHTML = this.#getLottoAmountTemplate();
+        this.$ticketArea.innerHTML = this.#getLottoTicketsTemplate();
     }
 
     mounted() {
-        this.$switch = $(SELECTOR.CLASS.SWITCH);
-        this.$lottoTickets = $$(SELECTOR.CLASS.LOTTO_TICKET);
+        this.$switch = document.querySelector(".switch");
+        this.$lottoTickets = document.querySelectorAll(".lotto-ticket");
     }
 
     setEvent() {
-        this.$switch.addEventListener("click", (event) => this.onSwitchClick(event));
+        this.$switch.addEventListener("click", (event) => this.#onSwitchClick(event));
     }
 
     getAmount() {
         return this.amount;
     }
 
-    setAmount(amount) {
-        this.amount = amount;
+    setAmount(amountValue) {
+        this.amount = amountValue;
     }
 
-    setTickets(tickets) {
-        this.tickets = tickets;
+    setTickets(ticketList) {
+        this.tickets = ticketList;
     }
 
-    onSwitchClick(event) {
-        if (
-            event.target.classList.contains(SELECTOR.CLASS.LOTTO_NUMBER_TOGGLE_BUTTON.substring(1))
-        ) {
+    #onSwitchClick(event) {
+        if (event.target.classList.contains("lotto-numbers-toggle-button")) {
             this.isChecked = !this.isChecked;
-            this.$ticketArea.classList.toggle(SELECTOR.CLASS.FLEX_COL.substring(1));
+            this.$ticketArea.classList.toggle("flex-col");
             this.$ticketArea.classList.toggle("flex-row");
             Array.from(this.$lottoTickets).forEach((ticket) => {
-                ticket
-                    .querySelector(SELECTOR.CLASS.LOTTO_DETAIL)
-                    .classList.toggle(SELECTOR.CLASS.DISPLAY_NONE.substring(1));
+                ticket.querySelector(".lotto-detail").classList.toggle("d-none");
             });
         }
     }
 
-    getLottoAmountTemplate() {
+    #getLottoAmountTemplate() {
         return `    
         <label class="flex-auto my-0">총 <span data-test="lotto-amount">${this.getAmount()}</span>개를 구매하였습니다.</label>
         <div class="flex-auto d-flex justify-end pr-1">
@@ -64,19 +64,18 @@ export class LottoTickets {
         `;
     }
 
-    getLottoTicketsTemplate() {
-        let template = "";
-
-        this.tickets.forEach((ticket) => {
-            template += `
-            <li class="mx-1 text-4xl lotto-wrapper d-block p-0 lotto-ticket" data-test="lotto-ticket">
+    #getLottoTicketsTemplate() {
+        return this.tickets
+            .map(
+                (ticket) =>
+                    `<li class="mx-1 text-4xl lotto-wrapper d-block p-0 lotto-ticket" data-test="lotto-ticket">
                 <span class="lotto-icon">🎟️ </span>
-                <span class="lotto-detail text-xl ${this.isChecked ? "" : "d-none"}" data-test="lotto-detail">
+                <span class="lotto-detail text-xl 
+                ${this.isChecked ? "" : "d-none"}" data-test="lotto-detail">
                 ${ticket.join(", ")}
                 </span>
-            </li>`;
-        });
-
-        return template;
+            </li>`
+            )
+            .join("");
     }
 }
