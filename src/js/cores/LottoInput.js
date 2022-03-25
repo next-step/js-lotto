a188c98be1c671d063a764c5a721cd6a38156d0c
karl <elrion018@gmail.com>
2022-03-26 03:13:52 +0900

    feature : lottoView 추가

--- src/js/cores/LottoInput.js
+++ src/js/cores/LottoInput.js
@@ -8,14 +8,16 @@ export class LottoInput {
   }
 
   inputMoney(money) {
-    if (!this.lottoValidator.isDivisibleMoneyByThousand(money))
-      return alert('로또 구입 금액을 1,000 단위로 입력해 주세요.');
-
     if (!this.lottoValidator.isLowerThanUpperLimit(money)) {
+      return;
     }
 
     if (!this.lottoValidator.isUpperThanLowerLimit(money)) {
+      return;
     }
+
+    if (!this.lottoValidator.isDivisibleMoneyByThousand(money))
+      return alert('로또 구입 금액을 1,000 단위로 입력해 주세요.');
 
     this.lottoShop.inputMoney(money);
   }
