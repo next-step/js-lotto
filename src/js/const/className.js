8058dad15db53b3dfbf646f9d76a1ecdfe0ec001
bomee.yoon <bomee.yoon@godo.co.kr>
2022-03-26 10:01:44 +0900

    🔥  불필요한 클래스 삭제

--- src/js/const/className.js
+++ src/js/const/className.js
@@ -1,8 +1,6 @@
 export const CLASS = {
   APP: '.js-app',
   MONEY_FORM: '.js-money-form',
-  MONEY: '.js-money',
-  LOTTO_DETAIL: '.js-lotto-detail',
   LOTTO_HEADER: '.js-lotto-detail-header',
   LOTTO_COUNT: '.js-lotto-count',
   TOGGLE_NUMBERS: '.js-toggle-numbers',
