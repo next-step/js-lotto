d172815776d86dc57dee408606e38d12be2f8d05
bomee.yoon <bomee.yoon@godo.co.kr>
2022-03-26 09:59:12 +0900

    ♻️  클래스 이름 정리

--- src/js/const/className.js
+++ src/js/const/className.js
@@ -3,9 +3,9 @@ export const CLASS = {
   MONEY_FORM: '.js-money-form',
   MONEY: '.js-money',
   LOTTO_DETAIL: '.js-lotto-detail',
-  LOTTO_DETAIL_HEADER: '.js-lotto-detail-header',
+  LOTTO_HEADER: '.js-lotto-detail-header',
   LOTTO_COUNT: '.js-lotto-count',
   TOGGLE_NUMBERS: '.js-toggle-numbers',
-  LOTTO_DETAIL_LIST: '.js-lotto-detail-list',
+  LOTTO_LIST: '.js-lotto-detail-list',
   WINNING_FORM: '.js-winning-form',
 };
