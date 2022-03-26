2ed1441a2cf30acb4cea686243c898de5f055fb9
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-03-26 16:00:30 +0900

    refactor: 함수 분리

--- src/js/view/ShowLottoNumberSection.js
+++ src/js/view/ShowLottoNumberSection.js
@@ -1,2 +1,22 @@
     this.$lottoContainer = $('.lotto-container', this.$target);
     this.isShow = false;
+  createOneLotto = () => {
+    const lotto = [];
+    while (lotto.length !== 6) {
+      const randomNumber = this.getLottoNumber();
+      if (lotto.includes(randomNumber)) {
+        continue;
+      }
+
+      lotto.push(randomNumber);
+    }
+
+    return lotto;
+  };
+
+  getLottoNumber = () => {
+    return parseInt(
+      Math.random() * (LOTTO_NUMBER_MAX - LOTTO_NUMBER_MIN) + LOTTO_NUMBER_MIN
+    );
+  };
+
