29dee920dbf1102dc6582f8b4836ddc1cf78e2ac
bomee.yoon <bomee.yoon@godo.co.kr>
2022-03-26 09:58:36 +0900

    🔥  불필요한 코드 삭제

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -2,14 +2,3 @@ export const range = (number, callback = null) =>
   Array.from({ length: number }, (_, i) => (callback ? callback(i) : i));
 
 export const size = (array = []) => array.length;
-
-export const validateCurry =
-  (validate, prevState) =>
-  (success, fail = alert) => {
-    try {
-      return success(validate());
-    } catch (message) {
-      fail(message);
-      return prevState;
-    }
-  };
