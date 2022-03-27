a4ef16f865f02d9d037e8bff7d8b8a668bdf88ee
aawlgus <aawlgus@jaranda.kr>
2022-03-27 23:45:37 +0900

    refactor: delete unused util function

--- src/js/util.js
+++ src/js/util.js
@@ -1,26 +1,3 @@
-import { RANDOM_INTEGER_CONDITION, LOTTO_LENGTH } from "./constants.js";
-
-export const createLotto = () => {
-  const lottoArray = Array(LOTTO_LENGTH)
-    .fill(0)
-    .map(() =>
-      getRandomInteger(
-        RANDOM_INTEGER_CONDITION.START,
-        RANDOM_INTEGER_CONDITION.END
-      )
-    );
-  const lottoSet = new Set(lottoArray);
-  while (lottoSet.size < LOTTO_LENGTH) {
-    lottoSet.add(
-      getRandomInteger(
-        RANDOM_INTEGER_CONDITION.START,
-        RANDOM_INTEGER_CONDITION.END
-      )
-    );
-  }
-  return [...lottoSet];
-};
-
 export const getRandomInteger = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
