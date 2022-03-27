86b60599d77838ca069205ad2e776826fedf7a89
Younho Choo <yo+github@younho9.com>
2022-03-28 00:19:04 +0900

    refactor: format 함수 정규식 변수 네이밍

--- src/js/lib/format.js
+++ src/js/lib/format.js
@@ -1,9 +1,7 @@
 export const format = (source, ...replacements) => {
 	for (const [index, replacement] of replacements.entries()) {
-		source = source.replace(
-			new RegExp('\\{' + index + '\\}', 'g'),
-			replacement,
-		);
+		const placeholderRegExp = new RegExp('\\{' + index + '\\}', 'g');
+		source = source.replace(placeholderRegExp, replacement);
 	}
 
 	return source;
