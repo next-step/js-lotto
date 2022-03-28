5c47bd7e4f634005370e5966e2d222813d8de62e
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-03-28 21:21:07 +0900

    feat: constants 함수 추가

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -2,5 +2,16 @@ export const INVALID_LOTTO_PRICE = `로또 구입 금액을 1,000원 단위로 �
 export const INVALID_LOTTO_MINIMUM_PRICE = `로또 구입 금액은 1,000원 이상으로 구매해 주세요.`;
 export const INVALID_LOTTO_MAXIMUM_PRICE = `로또 구입 금액은 100,000원 이하로 구매해 주세요.`;
 
-export const LOTTO_NUMBER_MAX = 45;
-export const LOTTO_NUMBER_MIN = 1;
+export const LOTTO_PRICE_MAXIMUM = 100000;
+export const LOTTO_PRICE_MINIMUM = 1000;
+export const LOTTO_NUMBER_MAXIMUM = 45;
+export const LOTTO_NUMBER_MINIMUM = 1;
+
+export const INVALID_LOTTO_NUMBER = `모든 로또 번호를 채워주세요.`;
+export const INVALID_MINIMUM_LOTTO_NUMBER = `값은 1이상이여야 합니다.`;
+export const INVALID_MAXIMUM_LOTTO_NUMBER = `값은 45이하여야 합니다.`;
+export const INVALID_OVERLAP_LOTTO_NUMBER = `로또 번호는 중복될 수 없습니다.`;
+
+export const LOTTO_WIN_MONEY = [
+  0, 0, 0, 5000, 50000, 1500000, 30000000, 2000000000,
+];
