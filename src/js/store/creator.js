8fb00790b50e81ed5dccf7819e40630ec333b0e0
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-03-28 21:20:23 +0900

    feat: store 함수

--- src/js/store/creator.js
+++ src/js/store/creator.js
@@ -1,6 +1,25 @@
 import CreateAction from './createAction.js';
-import { SET_PRICE } from './actions.js';
+import {
+  SET_PRICE,
+  SET_PURCHASED_LOTTO_NUMBERS,
+  SET_PURCHASED_LOTTO_BONUS_NUMBER,
+  SET_WIN_LOTTO_NUMBER,
+  RESTART,
+} from './actions.js';
 
 const setPrice = (price) => CreateAction(SET_PRICE, { price });
+const setPurchasedLottoNumbers = (purchasedLottoNumbers) =>
+  CreateAction(SET_PURCHASED_LOTTO_NUMBERS, { purchasedLottoNumbers });
+const setWinLottoNumber = (winLottoNumber) =>
+  CreateAction(SET_WIN_LOTTO_NUMBER, { winLottoNumber });
+const setPurchasedLottoBonusNumber = (bonusNumber) =>
+  CreateAction(SET_PURCHASED_LOTTO_BONUS_NUMBER, { bonusNumber });
+const restart = () => CreateAction(RESTART);
 
-export { setPrice };
+export {
+  setPrice,
+  setPurchasedLottoNumbers,
+  setPurchasedLottoBonusNumber,
+  setWinLottoNumber,
+  restart,
+};
