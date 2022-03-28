6345ad52e7c8707a7d7409dcf63fd578c517a930
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-03-28 21:23:51 +0900

    refactor: 가격 string -> number 변경

--- cypress/integration/examples/Lotto.spec.js
+++ cypress/integration/examples/Lotto.spec.js
@@ -20,7 +20,7 @@ describe('Lotto', () => {
 
   describe('step 1: 구입 기능', () => {
     it('로또 구입 금액을 입력하면, 금액에 해당하는 로또를 발급해야 한다.', () => {
-      buyLotto('3000');
+      buyLotto(3000);
       cy.get('#purchased-lotto').should(
         'have.text',
         '총 3개를 구매하였습니다.'
@@ -28,33 +28,32 @@ describe('Lotto', () => {
     });
 
     it('1000원 단위로 로또를 구매 할 수 있다.', () => {
-      buyLotto('3200', () =>
+      buyLotto(3200, () =>
         expect(alertStub).to.be.calledWith(INVALID_LOTTO_PRICE)
       );
     });
 
     it('최소 1,000원 미만의 금액을 입력하면 오류를 발생한다.', () => {
-      buyLotto('300', () =>
+      buyLotto(300, () =>
         expect(alertStub).to.be.calledWith(INVALID_LOTTO_MINIMUM_PRICE)
       );
     });
 
     it('최대 100,000원이 넘는 금액을 입력하면 오류를 발생한다.', () => {
-      buyLotto('3000000', () =>
+      buyLotto(3000000, () =>
         expect(alertStub).to.be.calledWith(INVALID_LOTTO_MAXIMUM_PRICE)
       );
     });
 
     it('번호보기가 토글(해제)되면, 복권 번호를 볼 수 없다.', () => {
-      buyLotto('3000');
-
+      buyLotto(3000);
       cy.get('#purcharsed-lottos .lotto-detail').each((lottoNumber) => {
         expect(lottoNumber).not.to.be.visible;
       });
     });
 
     it('번호보기가 토글(체크)되면, 복권 번호를 볼 수 있어야 한다.', () => {
-      buyLotto('3000');
+      buyLotto(3000);
       cy.get('.lotto-numbers-toggle-button').check({ force: true });
 
       cy.get('#purcharsed-lottos').each((lottoNumber) => {
