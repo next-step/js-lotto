c3f8963039cd3470333ab832dd544a6252ed1813
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-03-28 21:24:52 +0900

    feat: lotto step2 test case 추가

--- cypress/integration/examples/Lotto.spec.js
+++ cypress/integration/examples/Lotto.spec.js
@@ -2,13 +2,27 @@ import {
   INVALID_LOTTO_PRICE,
   INVALID_LOTTO_MINIMUM_PRICE,
   INVALID_LOTTO_MAXIMUM_PRICE,
+  INVALID_LOTTO_NUMBER,
+  INVALID_MINIMUM_LOTTO_NUMBER,
+  INVALID_MAXIMUM_LOTTO_NUMBER,
+  INVALID_OVERLAP_LOTTO_NUMBER,
 } from '../../../src/js/utils/constants.js';
+import { $ } from '../../../src/js/utils/dom.js';
 
 const BASE_URL = '../../index.html';
 
 const buyLotto = (money, clickEvent) => {
   cy.get('#input-price').type(money);
   cy.get('#input-price-btn')
+    .click()
+    .then(() => clickEvent);
+};
+
+const setWinLottoNumber = (lottoNumber, clickEvent) => {
+  cy.get('.winning-number').each((winNumber, index) => {
+    cy.get(winNumber).type(lottoNumber[index]);
+  });
+  cy.get('#show-result-btn')
     .click()
     .then(() => clickEvent);
 };
@@ -58,6 +72,50 @@ describe('Lotto', () => {
 
       cy.get('#purcharsed-lottos').each((lottoNumber) => {
         expect(lottoNumber).to.be.visible;
+      });
+    });
+  });
+
+  describe('step 2: 1) 당첨 결과 기능', () => {
+    it('결과 확인하기 버튼을 누르면 당첨 통계, 수익률을 모달로 확인할 수 있다.', () => {
+      buyLotto(2000);
+      setWinLottoNumber([1, 2, 3, 4, 5, 6, 7]);
+    });
+
+    it('당첨 번호는 1이상 적어야한다.', () => {
+      buyLotto(2000);
+      setWinLottoNumber([0, 2, 3, 4, 5, 6, 7], () =>
+        expect(alertStub).to.be.calledWith(INVALID_MINIMUM_LOTTO_NUMBER)
+      );
+    });
+
+    it('당첨 번호는 45이하 적어야한다.', () => {
+      buyLotto(2000);
+      setWinLottoNumber([47, 2, 3, 4, 5, 6, 7], () =>
+        expect(alertStub).to.be.calledWith(INVALID_MAXIMUM_LOTTO_NUMBER)
+      );
+    });
+
+    it('당첨 번호는 중복 될 수 없다.', () => {
+      buyLotto(2000);
+      setWinLottoNumber([1, 2, 3, 4, 5, 5, 7], () =>
+        expect(alertStub).to.be.calledWith(INVALID_OVERLAP_LOTTO_NUMBER)
+      );
+    });
+
+    it('당첨 번호는 모두 입력 되어야 한다.', () => {
+      buyLotto(2000);
+      setWinLottoNumber([1, ' ', 3, 4, 5, 6, 7], () =>
+        expect(alertStub).to.be.calledWith(INVALID_LOTTO_NUMBER)
+      );
+    });
+
+    it('다시 시작하기 버튼을 누르면 초기화 되서 다시 구매를 시작할 수 있다.', () => {
+      buyLotto(2000);
+      setWinLottoNumber([1, 2, 3, 4, 5, 6, 7]);
+      cy.get('#restart_btn').click();
+      cy.get('#lotto-num-form').each((lottoNumForm) => {
+        expect(lottoNumForm).not.to.be.visible;
       });
     });
   });
