8f02bb0bad1048d6c82ee9ed6c0b0a5957f02cb2
circlegiven <circlegiven@gmail.com>
2022-03-29 15:55:44 +0900

    [테스트] 로또 구매 결과보기 창 보여주기 까지 테스트

--- cypress/integration/lottoVendingMachine.spec.js
+++ cypress/integration/lottoVendingMachine.spec.js
@@ -18,9 +18,18 @@ Cypress.Commands.add('isEqualLottoAmount', (value) => {
 
 Cypress.Commands.add('showWinningResult', () => cy.get('form.mt-9').submit());
 
+Cypress.Commands.add('isShowPurchaseHistory', () =>
+  cy.get('section.mt-9').should('be.visible')
+);
+
+Cypress.Commands.add('isHidePurchaseHistory', () =>
+  cy.get('section.mt-9').should('not.be.visible')
+);
+
 Cypress.Commands.add('isShowWinningResultModal', () =>
   cy.get('div.modal').should('have.class', 'open')
 );
+
 Cypress.Commands.add('isHideWinningResultModal', () =>
   cy.get('div.modal').should('not.have.class', 'open')
 );
@@ -45,19 +54,62 @@ describe('로또 자판기', () => {
   describe('로또 구매', () => {
     it('금액 지불전 구매한 로또 갯수는 0', () => {
       cy.isEqualLottoAmount(0);
+      cy.isHidePurchaseHistory();
     });
 
     it('금액 지불후 구매한 로또 갯수', () => {
       cy.inputMoney(3000);
       cy.purchase();
       cy.isEqualLottoAmount(3);
+      cy.isShowPurchaseHistory();
     });
 
     it('로또 구매후 당첨 결과를 입력하지 않은채 결과보기시 화면이 나타나지 않음', () => {
       cy.inputMoney(3000);
       cy.purchase();
+      cy.isShowPurchaseHistory();
       cy.showWinningResult();
       cy.isHideWinningResultModal();
+    });
+
+    it('로또 구매후 당첨 결과를 모두 입력하지 않은 경우 결과보기시 화면이 나타나지 않음', () => {
+      cy.inputMoney(3000);
+      cy.purchase();
+      cy.isShowPurchaseHistory();
+      cy.get('input.winning-number').then((els) => {
+        [...els].forEach((el) => {
+          cy.wrap(el).type('1');
+        });
+        cy.showWinningResult();
+        cy.isHideWinningResultModal();
+      });
+    });
+
+    it('로또 구매후 당첨 결과를 중복되게 입력된 경우 결과보기시 화면이 나타나지 않음', () => {
+      cy.inputMoney(3000);
+      cy.purchase();
+      cy.isShowPurchaseHistory();
+      cy.get('input.winning-number, input.bonus-number').then((els) => {
+        [...els].forEach((el) => {
+          cy.wrap(el).type('1');
+        });
+        cy.showWinningResult();
+        cy.isHideWinningResultModal();
+      });
+    });
+
+    it('로또 구매후 당첨 결과를 중복되게 입력된 경우 결과보기시 화면이 나타나지 않음', () => {
+      cy.inputMoney(3000);
+      cy.purchase();
+      cy.isShowPurchaseHistory();
+      cy.get('input.winning-number, input.bonus-number').then((els) => {
+        let value = 1;
+        [...els].forEach((el) => {
+          cy.wrap(el).type(value++);
+        });
+        cy.showWinningResult();
+        cy.isShowWinningResultModal();
+      });
     });
   });
 });
