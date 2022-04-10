485cbcc4134e8426b29e3ce453d7600bc7d5895b
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-04-10 16:30:46 +0900

    refactor: validation, predicate, handleError 로 나누어 관심사 분리

--- src/js/validation.js
+++ src/js/validation.js
@@ -16,82 +16,106 @@ const showErrorInput = (element) => {
   element.classList.add('error-input');
 };
 
-const resetInputErrorStyle = (selector) => {
-  selector.forEach((input) => input.classList.remove('error-input'));
+const hasEmptyInput = (selector) =>
+  Array.from(selector).find(($input) => $input.value === '');
+
+const hasUnderMinimumNumber = (selector) =>
+  Array.from(selector).find(($input) => $input.value < LOTTO_NUMBER_MINIMUM);
+
+const hasUnderMaximumNumber = (selector) =>
+  Array.from(selector).find(($input) => LOTTO_NUMBER_MAXIMUM < $input.value);
+
+const validate = (predicate, handleError) => {
+  if (!predicate) {
+    return false;
+  }
+  handleError();
+  return true;
 };
 
 const validation = {
-  lottoPrice: (money) => {
-    if (money % LOTTO_PRICE_MINIMUM !== 0) {
-      alert(INVALID_LOTTO_PRICE);
-      return true;
-    }
-    return false;
-  },
-  lottomMinimumPrice: (money) => {
-    if (money < LOTTO_PRICE_MINIMUM) {
-      alert(INVALID_LOTTO_MINIMUM_PRICE);
-      return true;
-    }
-    return false;
-  },
-  lottomMaximumPrice: (money) => {
-    if (LOTTO_PRICE_MAXIMUM < money) {
-      alert(INVALID_LOTTO_MAXIMUM_PRICE);
-      return true;
-    }
-    return false;
-  },
-  emptyLottoNumber: () => {
-    for (let lottoInput of $$('.winning-number')) {
-    resetInputErrorStyle(winningNumberElement);
-      if (lottoInput.value.length === 0) {
-        showErrorInput(lottoInput);
-        alert(INVALID_LOTTO_NUMBER);
-        return true;
-      }
-    }
-    return false;
-  },
+  lottoPrice: (money) =>
+    validate(predicate.lottoPrice(money), handleError.lottoPrice),
+  lottomMinimumPrice: (money) =>
+    validate(
+      predicate.lottomMinimumPrice(money),
+      handleError.lottomMinimumPrice
+    ),
+  lottomMaximumPrice: (money) =>
+    validate(
+      predicate.lottomMaximumPrice(money),
+      handleError.lottomMaximumPrice
+    ),
+  emptyLottoNumber: (winningNumberElement) => {
+    const errorInput = predicate.emptyLottoNumber(winningNumberElement);
 
-  lottoNumberUnderMinimum: () => {
-    for (let lottoInput of $$('.winning-number')) {
-    resetInputErrorStyle(winningNumberElement);
-      if (lottoInput.value < LOTTO_NUMBER_MINIMUM) {
-        showErrorInput(lottoInput);
-        alert(INVALID_MINIMUM_LOTTO_NUMBER);
-        return true;
-      }
-    }
-    return false;
+    if (errorInput) showErrorInput(errorInput);
+    return validate(
+      predicate.emptyLottoNumber(winningNumberElement),
+      handleError.emptyLottoNumber
+    );
   },
+  lottoNumberUnderMinimum: (winningNumberElement) => {
+    const errorInput = predicate.lottoNumberUnderMinimum(winningNumberElement);
+    if (errorInput) showErrorInput(errorInput);
 
-  lottoNumberOverMaximum: () => {
-    for (let lottoInput of $$('.winning-number')) {
-    resetInputErrorStyle(winningNumberElement);
-      if (LOTTO_NUMBER_MAXIMUM < lottoInput.value) {
-        showErrorInput(lottoInput);
-        alert(INVALID_MAXIMUM_LOTTO_NUMBER);
-        return true;
-      }
-    }
-    return false;
+    return validate(
+      predicate.lottoNumberUnderMinimum(winningNumberElement),
+      handleError.lottoNumberUnderMinimum
+    );
   },
+  lottoNumberOverMaximum: (winningNumberElement) => {
+    const errorInput = predicate.lottoNumberOverMaximum(winningNumberElement);
+    if (errorInput)
+      showErrorInput(predicate.lottoNumberOverMaximum(winningNumberElement));
 
-  lottoNumberOverlap: () => {
-    for (let lottoInput of $$('.winning-number')) {
+    return validate(
+      predicate.lottoNumberOverMaximum(winningNumberElement),
+      handleError.lottoNumberOverMaximum
+    );
+  },
+  lottoNumberOverlap: (winningNumberElement) => {
+    return validate(
+      predicate.lottoNumberOverlap(winningNumberElement),
+      handleError.lottoNumberOverlap
+    );
+  },
+};
+
+const predicate = {
+  lottoPrice: (money) => money % LOTTO_PRICE_MINIMUM !== 0,
+  lottomMinimumPrice: (money) => money < LOTTO_PRICE_MINIMUM,
+  lottomMaximumPrice: (money) => LOTTO_PRICE_MAXIMUM < money,
+  emptyLottoNumber: (winningNumberElement) =>
+    hasEmptyInput(winningNumberElement),
+  lottoNumberUnderMinimum: (winningNumberElement) =>
+    hasUnderMinimumNumber(winningNumberElement),
+  lottoNumberOverMaximum: (winningNumberElement) =>
+    hasUnderMaximumNumber(winningNumberElement),
+  lottoNumberOverlap: (winningNumberElement) => {
     const lottoNumber = [];
-    resetInputErrorStyle(winningNumberElement);
+    for (let lottoInput of winningNumberElement) {
       const lottoNumberValue = lottoInput.value;
-      if (lottoNumber.indexOf(lottoNumberValue) > -1) {
+      const isDuplicated = lottoNumber.indexOf(lottoNumberValue) > -1;
+      if (isDuplicated) {
         showErrorInput(lottoInput);
-        alert(INVALID_OVERLAP_LOTTO_NUMBER);
         return true;
       }
       lottoNumber.push(lottoNumberValue);
     }
+
     return false;
   },
+};
+
+const handleError = {
+  lottoPrice: () => alert(INVALID_LOTTO_PRICE),
+  lottomMinimumPrice: () => alert(INVALID_LOTTO_MINIMUM_PRICE),
+  lottomMaximumPrice: () => alert(INVALID_LOTTO_MAXIMUM_PRICE),
+  emptyLottoNumber: () => alert(INVALID_LOTTO_NUMBER),
+  lottoNumberUnderMinimum: () => alert(INVALID_MINIMUM_LOTTO_NUMBER),
+  lottoNumberOverMaximum: () => alert(INVALID_MAXIMUM_LOTTO_NUMBER),
+  lottoNumberOverlap: () => alert(INVALID_OVERLAP_LOTTO_NUMBER),
 };
 
 export default validation;
