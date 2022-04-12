9bdf7e8680ae0b0ef3705c22ee74c38bfd6e4820
circlegiven <circlegiven@gmail.com>
2022-04-12 14:33:08 +0900

    [Fix] 로또 구매내역 및 당첨결과 확인 내역 UI와 도메인 분리

--- src/js/view/WinningLottoView.js
+++ src/js/view/WinningLottoView.js
@@ -1,16 +1,36 @@
 import AbstractView from './AbstractView.js';
 import WinningLotto from '../WinningLotto.js';
+import wallet from '../data/wallet.js';
+import { WINNING_RESULT_CASE } from '../constants/lotto.js';
 
 const $winingLottoForm = document.querySelector('#winning-result-form');
 const $winningAllNumbers = $winingLottoForm.querySelectorAll(
   'input.winning-number, input.bonus-number'
 );
+const $winningNumbers = $winingLottoForm.querySelectorAll(
+  'input.winning-number'
+);
+const $winningBonusNumber = $winingLottoForm.querySelector('.bonus-number');
 const $winningResultModal = document.querySelector('.modal');
 const $modalCloseButton = $winningResultModal.querySelector('.modal-close');
 const $restartButton = $winningResultModal.querySelector('.btn.btn-cyan');
+const $winningResultPanel = $winningResultModal.querySelector('tbody');
+const $resultRevenue = $winningResultModal.querySelector(
+  '.text-center.font-bold'
+);
 
 function notificationDuplicateNumber() {
   alert('중복된 당첨 숫자가 존재합니다.');
+}
+
+function winningResultPanelTemplate({ label, reward, winningNumber }) {
+  const $template = document.createElement('template');
+  $template.innerHTML = `<tr class="text-center">
+    <td class="p-3">${label}</td>
+    <td class="p-3">${new Intl.NumberFormat().format(reward)}</td>
+    <td class="p-3">${winningNumber} 개</td>
+  </tr>`;
+  return $template.content.firstChild;
 }
 
 class WinningLottoView extends AbstractView {
@@ -24,6 +44,14 @@ class WinningLottoView extends AbstractView {
     $winingLottoForm.classList.add('hide');
   }
 
+  static #winningNumbers() {
+    return [...$winningNumbers].map(($winningNumber) => $winningNumber.value);
+  }
+
+  static #winningBonusNumber() {
+    return $winningBonusNumber.value;
+  }
+
   static #initializeWinningNumbers() {
     $winningAllNumbers.forEach(($winningNumber) => {
       // eslint-disable-next-line no-param-reassign
@@ -31,8 +59,31 @@ class WinningLottoView extends AbstractView {
     });
   }
 
+  static #changeWinningResultPanel(winningResult) {
+    const $panel = document.createDocumentFragment();
+    $panel.append(
+      ...WINNING_RESULT_CASE.map((winningCase) =>
+        winningResultPanelTemplate({
+          label: winningCase.label,
+          reward: winningCase.reward,
+          winningNumber: winningResult[winningCase.key],
+        })
+      )
+    );
+    $winningResultPanel.replaceChildren($panel);
+    $resultRevenue.innerHTML = `당신의 총 수익률은 ${
+      WinningLotto.totalRevenue(winningResult) / wallet.purchasedPrice()
+    }%입니다.`;
+  }
+
   static #openModal() {
     $winningResultModal.classList.add('open');
+    const winningResult = WinningLotto.winningResult({
+      lottoList: wallet.lottos(),
+      winningNumbers: WinningLottoView.#winningNumbers(),
+      bonusNumber: WinningLottoView.#winningBonusNumber(),
+    });
+    WinningLottoView.#changeWinningResultPanel(winningResult);
   }
 
   static #closeModal() {
