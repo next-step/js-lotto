2b11039c2f28d55d8aead29c0d376802d31bfe84
taeyoungs <xoxodudwkd@gmail.com>
2022-04-17 14:40:36 +0900

    mod: App 컴포넌트 로직 분리

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,3 @@
 import App from './components/App.js';
 
-const $showResultButton = document.querySelector('.open-result-modal-button');
-const $modalClose = document.querySelector('.modal-close');
-const $modal = document.querySelector('.modal');
-
-const onModalShow = () => {
-    $modal.classList.add('open');
-};
-
-const onModalClose = () => {
-    $modal.classList.remove('open');
-};
-
-$showResultButton.addEventListener('click', onModalShow);
-$modalClose.addEventListener('click', onModalClose);
-
 new App();
