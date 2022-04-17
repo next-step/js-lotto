bda172bb3edbe534bf506be537e39e8e1f57ad17
taeyoungs <xoxodudwkd@gmail.com>
2022-04-17 14:38:42 +0900

    add: 로또 결과 확인 Form 컴포넌트 구현

---
+++ src/js/components/LottoForm.js
@@ -0,0 +1,28 @@
+import { CLASS_NAME } from '../constants/index.js';
+
+const $lottoForm = document.querySelector(`form.${CLASS_NAME.LOTTO_FORM}`);
+const $winningNumbers = document.querySelectorAll(
+    `input.${CLASS_NAME.WINNING_NUMBER}`
+);
+const $bonusNumber = document.querySelector(`input.${CLASS_NAME.BONUS_NUMBER}`);
+const $modal = document.querySelector('.modal');
+
+function LottoForm({ setRankingTable }) {
+    const handleLottoFormSubmit = (e) => {
+        e.preventDefault();
+
+        const winningNumbers = [];
+        $winningNumbers.forEach(($winningNumber) =>
+            winningNumbers.push(Number($winningNumber.value))
+        );
+        const bonusNumber = Number($bonusNumber.value);
+
+        setRankingTable({ bonusNumber, winningNumbers });
+
+        $modal.classList.add('open');
+    };
+
+    $lottoForm.addEventListener('submit', handleLottoFormSubmit);
+}
+
+export default LottoForm;
