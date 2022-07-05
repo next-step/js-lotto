1c2e5b8494a68d8e70e0b9884ae0445a45a0d0ce
junzero741 <junzero741@gmail.com>
2022-07-06 07:48:26 +0900

    [FEAT] : 기존 index.js 파일에서 쓰고 있던 문자열 선택자 상수로 선언

---
+++ src/js/constants/selectors.js
@@ -0,0 +1,4 @@
+export const OPEN_RESULT_MODAL_BUTTON = '.open-result-modal-button';
+export const CLOSE_RESULT_MODAL_BUTTON = '.modal-close';
+export const MODAL = 'modal';
+export const LOTTO_NUMBERS_TOGGLE_BUTTON = '.lotto-numbers-toggle-button';
