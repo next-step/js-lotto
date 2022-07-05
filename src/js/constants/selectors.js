c5211daa488186d90579f6aee3d204d47a29065b
junzero741 <junzero741@gmail.com>
2022-07-06 08:21:37 +0900

    [FIX] : 모달 선택자 상수에 . 추가

--- src/js/constants/selectors.js
+++ src/js/constants/selectors.js
@@ -1,4 +1,4 @@
 export const RESULT__MODAL_OPEN_BUTTON = '.open-result-modal-button';
 export const RESULT_MODAL_CLOSE_BUTTON = '.modal-close';
-export const MODAL = 'modal';
+export const MODAL = '.modal';
 export const LOTTO_NUMBERS_TOGGLE_BUTTON = '.lotto-numbers-toggle-button';
