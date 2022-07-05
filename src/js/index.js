d54b532dfc9ef9029c493db8dcfd0c6bcb98a573
junzero741 <junzero741@gmail.com>
2022-07-06 08:00:12 +0900

    [TEST] : 엘리먼트 선택 함수, 상수 선택자 정상 작동 테스트

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
+import {$, addEvent} from './util.js';
+import { OPEN_RESULT_MODAL_BUTTON, CLOSE_RESULT_MODAL_BUTTON, LOTTO_NUMBERS_TOGGLE_BUTTON } from './constants/selectors.js';
+
+
 const $showResultButton = document.querySelector('.open-result-modal-button')
 const $modalClose = document.querySelector('.modal-close')
 const $modal = document.querySelector('.modal')
@@ -15,3 +19,11 @@ const onModalClose = () => {
 
 $showResultButton.addEventListener('click', onModalShow)
 $modalClose.addEventListener('click', onModalClose)
+
+const initApp = function () {
+  addEvent($(OPEN_RESULT_MODAL_BUTTON), 'click', ()=>{console.log('modal open!')})
+  addEvent($(CLOSE_RESULT_MODAL_BUTTON), 'click', ()=>{console.log('modal close!')})
+  addEvent($(LOTTO_NUMBERS_TOGGLE_BUTTON), 'click', ()=>{console.log('lotto toggle!')})
+}
+
+initApp();
