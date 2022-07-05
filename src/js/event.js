b05b12cdb2b5714450cae6db2c9a9120d9c5efa4
junzero741 <junzero741@gmail.com>
2022-07-06 08:06:03 +0900

    [REFAC] : 사용자 이벤트 콜백 함수 모듈 분리

---
+++ src/js/event.js
@@ -0,0 +1,11 @@
+export const onClickOpenResultModalBtn = function () {
+  console.log('modal open!')
+}
+
+export const onClickCloseResultModalBtn = function () {
+  console.log('modal close!')
+}
+
+export const onClickLottoNumbersToggleBtn = function () {
+  console.log('lotto toggle!')
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import {$, addEvent} from './util.js';
 import { OPEN_RESULT_MODAL_BUTTON, CLOSE_RESULT_MODAL_BUTTON, LOTTO_NUMBERS_TOGGLE_BUTTON } from './constants/selectors.js';
-
+import { onClickOpenResultModalBtn, onClickCloseResultModalBtn, onClickLottoNumbersToggleBtn } from './event.js';
 
 const $showResultButton = document.querySelector('.open-result-modal-button')
 const $modalClose = document.querySelector('.modal-close')
@@ -21,9 +21,9 @@ $showResultButton.addEventListener('click', onModalShow)
 $modalClose.addEventListener('click', onModalClose)
 
 const initApp = function () {
-  addEvent($(OPEN_RESULT_MODAL_BUTTON), 'click', ()=>{console.log('modal open!')})
-  addEvent($(CLOSE_RESULT_MODAL_BUTTON), 'click', ()=>{console.log('modal close!')})
-  addEvent($(LOTTO_NUMBERS_TOGGLE_BUTTON), 'click', ()=>{console.log('lotto toggle!')})
+  addEvent($(OPEN_RESULT_MODAL_BUTTON), 'click', onClickOpenResultModalBtn)
+  addEvent($(CLOSE_RESULT_MODAL_BUTTON), 'click', onClickCloseResultModalBtn)
+  addEvent($(LOTTO_NUMBERS_TOGGLE_BUTTON), 'click', onClickLottoNumbersToggleBtn)
 }
 
 initApp();
