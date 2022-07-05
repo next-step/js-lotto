1b9d3565c8af73b995cf00aaee7275a4a9dce144
junzero741 <junzero741@gmail.com>
2022-07-06 07:59:35 +0900

    [FIX] : 엘리먼트 선택 함수 $, 18012에 return 문 빼먹음

--- src/js/util.js
+++ src/js/util.js
@@ -1,3 +1,9 @@
-export const $ = function (selector) {document.querySelector(selector)};
-export const $$ = function (selector) {document.querySelectorAll(selector)};
-export const addEvent = function(target, eventType, callback) {target.addEventListener(eventType, callback)};
+export const $ = function (selector) {
+  return document.querySelector(selector);
+};
+export const $$ = function (selector) {
+  return document.querySelectorAll(selector);
+};
+export const addEvent = function (target, eventType, callback) {
+  target.addEventListener(eventType, callback);
+};
