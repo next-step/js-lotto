946f51ceafe632988d869309bbe939cba1bf0bac
junzero741 <junzero741@gmail.com>
2022-07-06 09:04:30 +0900

    [FEAT] : flux 패턴을 위한 store 생성 클래스 작성

---
+++ src/js/store/createStore.js
@@ -0,0 +1,17 @@
+export class Store {
+
+  #state
+
+  constructor(initialState, reducer) {
+    this.#state = initialState;
+    this.reducer = reducer;
+  }
+  
+  getState () {
+    return this.#state
+  }
+  
+  dispatch (action) {
+    this.#state = this.reducer(this.#state, action);
+  }
+}
