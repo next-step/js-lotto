e0da3c155303e14cd44b3409f7b77b7de995f102
junzero741 <junzero741@gmail.com>
2022-07-08 07:19:47 +0900

    [TEST] : 로또 구입 금액 입력 e2e 테스트 작성

--- src/js/constants/selectors.js
+++ src/js/constants/selectors.js
@@ -1,4 +1,5 @@
-export const RESULT__MODAL_OPEN_BUTTON = '.open-result-modal-button';
-export const RESULT_MODAL_CLOSE_BUTTON = '.modal-close';
-export const MODAL = '.modal';
-export const LOTTO_NUMBERS_TOGGLE_BUTTON = '.lotto-numbers-toggle-button';
+export const RESULT__MODAL_OPEN_BUTTON = ".open-result-modal-button";
+export const RESULT_MODAL_CLOSE_BUTTON = ".modal-close";
+export const MODAL = ".modal";
+export const LOTTO_NUMBERS_TOGGLE_BUTTON = ".lotto-numbers-toggle-button";
+export const PRICE_INPUT = "#price-input";
