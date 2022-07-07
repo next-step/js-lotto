c4042f4d53cef771cdd7ea86abef1a543f9e1619
junzero741 <junzero741@gmail.com>
2022-07-08 07:40:27 +0900

    [FEAT] : 가격 입력 관련 selector 추가

--- src/js/constants/selectors.js
+++ src/js/constants/selectors.js
@@ -1,6 +1,12 @@
+/* -------------------------------------------- 로또 구매 관련 -------------------------------------------- */
+export const PRICE_INPUT = "#price-input";
+export const PURCHASED_LOTTO_COUNT_TEXT = "#lotto-purchased-count";
+export const LOTTO_PURCHASE_FORM = "#lotto-purchase-form";
+
+/* -------------------------------------------- 토글버튼 -------------------------------------------- */
+export const LOTTO_NUMBERS_TOGGLE_BUTTON = ".lotto-numbers-toggle-button";
+
+/* -------------------------------------------- 모달 관련 -------------------------------------------- */
 export const RESULT__MODAL_OPEN_BUTTON = ".open-result-modal-button";
 export const RESULT_MODAL_CLOSE_BUTTON = ".modal-close";
 export const MODAL = ".modal";
-export const LOTTO_NUMBERS_TOGGLE_BUTTON = ".lotto-numbers-toggle-button";
-export const PRICE_INPUT = "#price-input";
-export const PURCHASED_LOTTO_COUNT_TEXT = "#lotto-purchased-count";
