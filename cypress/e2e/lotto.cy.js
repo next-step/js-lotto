0419def7ffd8b7ceb1a8a04a0729c917a59a71a5
doinki <doinki94@gmail.com>
2022-07-07 23:10:08 +0900

    test: 로또 테스트 추가

--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -1,5 +1,49 @@
+import { LOTTO_UNIT_PRICE, errorMessages } from '../../src/js/constants';
+
 describe('lotto', () => {
-  it('visit', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
+
+  it('로또 구입 금액을 입력하면, 금액에 해당하는 로또를 발급해야 한다.', () => {
+    const won = 5000;
+    const totalLotto = won / LOTTO_UNIT_PRICE;
+
+    cy.pay(won);
+
+    cy.findByRole('alert').should('contain', `${totalLotto}개`);
+    cy.findAllByRole('listitem').should('have.lengthOf', totalLotto);
+  });
+
+  it('로또 1장의 가격은 1,000원이다.', () => {
+    cy.on('window:alert', (message) => {
+      expect(message).to.equal(errorMessages.LOTTO_UNIT_PRICE_ERROR);
+    });
+
+    cy.findByPlaceholderText('구입 금액').type('5100');
+    cy.findByRole('button', { name: '확인' }).click();
+  });
+
+  it('소비자는 자동 구매를 할 수 있어야 한다.', () => {
+    cy.pay(5000);
+    cy.get('.lotto-detail').each((el) => {
+      const numbers = el.text().split(', ').map(Number);
+
+      expect(numbers).to.have.lengthOf(6);
+      numbers.forEach((number) => {
+        expect(number).to.above(0);
+        expect(number).to.below(46);
+      });
+    });
+  });
+
+  it('복권 번호는 번호보기 토글 버튼을 클릭하면, 볼 수 있어야 한다.', () => {
+    cy.pay(5000);
+
+    cy.findByText('번호보기').click();
+    cy.get('.lotto-detail').should('be.visible');
+
+    cy.findByText('번호보기').click();
+    cy.get('.lotto-detail').should('not.be.visible');
   });
 });
