baf5b2f2e99d090a19a1b18631eb5b1ad3227685
doinki <doinki94@gmail.com>
2022-07-07 23:09:18 +0900

    feat: custom command 추가

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,6 @@
 import '@testing-library/cypress/add-commands';
+
+Cypress.Commands.add('pay', (won) => {
+  cy.findByPlaceholderText('구입 금액').type(won);
+  cy.findByRole('button', { name: '확인' }).click();
+});
