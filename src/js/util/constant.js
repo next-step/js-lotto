1f0d9ed595a8429fdb1803aaf257c8693bde72a4
YunSeong Park <qkrdbstjd95@naver.com>
2022-07-08 19:08:25 +0900

    [New Feature] 로또 금액이 맞지 않을 경우 Error constant 추가

--- src/js/util/constant.js
+++ src/js/util/constant.js
@@ -1,3 +1,7 @@
 export const LOTTO_PRICE = 1000;
 export const LOTTO_NUMBER_LENGHT = 6;
 export const LOTTO_MAX_VALUE = 45;
+
+export const ERROR_MESSAGE = {
+  NOT_MATCHED_LOTTO_PRICE: `로또 구입 금액을 ${LOTTO_PRICE.toLocaleString()}원 단위로 입력해주세요.`,
+};
