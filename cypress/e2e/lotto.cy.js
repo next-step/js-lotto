0e763f2deeacbd7def124f0f6f591b6108aa048d
mingdy <alswl1500@naver.com>
2022-07-09 22:15:55 +0900

    feat: "복권 구입에 성공하면 구입한 금액에 맞는 개수의 복권리스트를 보여준다." 테스트 케이스 작성

--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -7,13 +7,17 @@ describe("lotto", () => {
     cy.on("window:alert", alertStub);
 
     cy.get("#input-price").type(300);
-    cy.get(".btn-cyan")
-      .contains("확인")
+    cy.get("#buy-lotto-button")
       .click()
       .then(() => {
         expect(alertStub).to.be.calledWith(
           "로또 구입 금액을 1,000원 단위로 입력해 주세요."
         );
       });
+  });
+  it("복권 구입에 성공하면 구입한 금액에 맞는 개수의 복권리스트를 보여준다.", () => {
+    cy.get("#input-price").type(3000);
+    cy.get("#buy-lotto-button").click();
+    cy.get(".lotto-count").should("have.text", "총 3개를 구매하였습니다.");
   });
 });
