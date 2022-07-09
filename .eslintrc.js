b6c482b50372291117cff3f7201999cdb4327917
sujin lee <jmakeaz@gmail.com>
2022-07-09 15:28:28 +0900

    프로젝트 환경 설정
    - prettier 추가
    - eslint 추가
    - http-server 추가
    - gitignore 디렉토리 추가

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+};
