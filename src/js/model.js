13050f2131d1174fcd1cdc6d66598c9d2a28b242
ag502 <ag502@naver.com>
2022-07-09 23:35:45 +0900

    add: 로또 번호 자동 생성 및 로또 구매 모델 추가

---
+++ src/js/model.js
@@ -0,0 +1,28 @@
+import { PRICE_OF_LOTTO, NUM_OF_LOTTO_NUMBERS, MIN_LOTTO_NUM, MAX_LOTTO_NUM } from "../js/util/constants.js";
+
+class Model {
+  constructor() {
+    this.curLotties = [];
+  }
+
+  buyLotties(paidPrice) {
+    const numOfLotties = Math.floor(paidPrice / PRICE_OF_LOTTO);
+
+    for (let _ = 0; _ < numOfLotties; _++) {
+      this.curLotties.push(this._autoGenerateNumber());
+    }
+  }
+
+  _autoGenerateNumber() {
+    const lottoNumbers = new Array(NUM_OF_LOTTO_NUMBERS);
+
+    lottoNumbers.forEach((_, idx) => {
+      const randomLottoNumber = Math.random() * (MAX_LOTTO_NUM - MIN_LOTTO_NUM + 1);
+      lottoNumbers[idx] = randomLottoNumber;
+    });
+
+    return lottoNumbers;
+  }
+}
+
+export default new Model();
