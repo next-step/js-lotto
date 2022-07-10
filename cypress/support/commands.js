eaf971279901881190f3ec4596c59feaf696ac76
wondonghwi <donghwi1289@gmail.com>
2022-07-10 15:37:40 +0900

    ♻️ Cypress 공통되는 내용 custom cummands로 변경

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,11 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('priceInputAndClick', (price) => {
+  cy.get('input[name="purchasePrice"]').type(price, { force: true }).get('#purchaseForm > .d-flex > .btn').click();
+});
+
+Cypress.Commands.add('showToggleButtonClick', () => {
+  cy.get('.text-base').click();
+});
+
+Cypress.Commands.add('onOccurAlert', (text) => {
+  cy.on('window:alert', (alert) => expect(alert).to.contains(text));
+});
