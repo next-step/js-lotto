d7dc52594bc13fd44367a3a42ebabecd393829cc
정대윤 <sensecodevalue@gmail.com>
2022-07-11 20:42:02 +0900

    refactor: lotto util의 magic number 변경

--- src/js/utils/lotto.js
+++ src/js/utils/lotto.js
@@ -1,6 +1,10 @@
+const LOTTO_MAX_NUMBER = 45;
+const LOTTO_MIN_NUMBER = 1;
+const LOTTO_LENGTH = 6;
+
 function makeLotto() {
-  const balls = new Array(45).fill(null).map((value, index) => index + 1);
-  const lotto = new Array(6).fill(null).map(() => {
+  const balls = new Array(LOTTO_MAX_NUMBER).fill(null).map((value, index) => index + 1);
+  const lotto = new Array(LOTTO_LENGTH).fill(null).map(() => {
     const ballIndex = Math.floor(Math.random() * (balls.length - 1));
     return balls.splice(ballIndex, 1)[0];
   });
@@ -8,12 +12,14 @@ function makeLotto() {
 }
 
 function validateLotto(numbers) {
-  if (numbers.length !== 6) throw new Error('Lotto should have 6 numbers');
+  if (numbers.length !== LOTTO_LENGTH) throw new Error(`Lotto should have ${LOTTO_LENGTH} numbers`);
 
-  const isOutOfRangeNumber = numbers.filter(number => number < 1 || number > 45).length > 0;
-  if (isOutOfRangeNumber) throw new Error('Lottos number should be between 1 and 45.');
+  const isOutOfRangeNumber =
+    numbers.filter(number => number < LOTTO_MIN_NUMBER || number > LOTTO_MAX_NUMBER).length > 0;
+  if (isOutOfRangeNumber)
+    throw new Error(`Lottos number should be between ${LOTTO_MIN_NUMBER} and ${LOTTO_MAX_NUMBER}.`);
 
-  const hasDuplicateNumber = new Set(numbers).size !== 6;
+  const hasDuplicateNumber = new Set(numbers).size !== LOTTO_LENGTH;
   if (hasDuplicateNumber) throw new Error('Lotto should not have duplicate numbers');
 }
 
