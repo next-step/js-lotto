b17174b0050fa47394d191db1a172b165ca71840
doinki <doinki94@gmail.com>
2022-07-11 10:25:12 +0900

    refactor: custom command 수정

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 import '@testing-library/cypress/add-commands';
 
 Cypress.Commands.add('pay', (won) => {
-  cy.findByPlaceholderText('구입 금액').type(won);
-  cy.findByRole('button', { name: '확인' }).click();
+  cy.get('input[name=won]').type(won);
+  cy.findByRole('button').click();
 });
