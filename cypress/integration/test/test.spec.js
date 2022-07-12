8a6e88d7b9eac9f3e13dad21016f38e71aec3b4b
jisoo-log <jisoolee.dev@gmail.com>
2022-07-12 19:48:56 +0900

    test: 구매 인터랙션 및 토글 관련 테스트코드 추가

--- cypress/integration/test/test.spec.js
+++ cypress/integration/test/test.spec.js
@@ -1,8 +1,14 @@
-import { ERR_MSG } from '../../../src/js/constants';
+import { ERR_MSG, LOTTO_PRICE } from '../../../src/js/constants';
 
 describe('행운의 로또 테스트', () => {
   const clickBuyBtn = () => cy.get('.buy-lotto-button').click();
   const typeMoneyInput = (money) => cy.get('.money-input').type(money);
+  const buyLotto = (n) => {
+    typeMoneyInput(n * LOTTO_PRICE);
+    clickBuyBtn();
+  };
+  const clickToggle = () =>
+    cy.get('.lotto-numbers-toggle-button').click({ force: true });
 
   beforeEach(() => {
     cy.visit('http://127.0.0.1:5500/index.html');
@@ -37,6 +43,37 @@ describe('행운의 로또 테스트', () => {
       clickBuyBtn().then(() => {
         expect(stub.getCall(0)).to.be.calledWith(ERR_MSG.NOT_MULTIPLE_OF_1000);
       });
+    });
+  });
+
+  describe('로또를 구매하면', () => {
+    it('구매한 개수가 아래에 표시된다.', () => {
+      buyLotto(3);
+      cy.get('.number-label').should('have.text', '총 3개를 구매하였습니다.');
+    });
+    it('구매한만큼 로또 아이콘이 생긴다.', () => {
+      buyLotto(3);
+      cy.get('.lotto-icon').each((item, index, list) =>
+        expect(list).to.have.length(3)
+      );
+    });
+  });
+
+  describe('토글을', () => {
+    it('건드리지 않으면 숫자가 안보인다.', () => {
+      buyLotto(1);
+      cy.get('.lotto-numbers').should('not.be.visible');
+    });
+    it('켜면 숫자가 표시된다.', () => {
+      buyLotto(1);
+      clickToggle();
+      cy.get('.lotto-numbers').should('be.visible');
+    });
+    it('끄면 숫자가 안보인다', () => {
+      buyLotto(1);
+      clickToggle();
+      clickToggle();
+      cy.get('.lotto-numbers').should('not.be.visible');
     });
   });
 });
