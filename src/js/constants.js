eedb5c4801d99f8d27634c48e892cff06c67d727
Jaemin Ahn <jae7415@gmail.com>
2022-07-12 22:14:03 +0900

    [클린코드 3기 안재민] 로또 미션 STEP 1 (#124)
    * setup cypress
    
    * buy 1000 won lotto
    
    * use selectors from constants.js
    
    * purchase lotteries
    
    * toggle lotto numbers
    
    * remove magic numbers
    
    * set default type attribute of button in form tag
    
    * use state as function parameter in render method
    
    * auto focusing on charge input when user set invalid value
    
    * throw error when $app is not instance of HTMLElement

---
+++ src/js/constants.js
@@ -0,0 +1,14 @@
+export const PRICE_PER_LOTTO = 1000;
+export const LOTTO_ICON = "🎟️";
+
+export const SELECTORS = {
+  APP: "#app",
+  CHARGE_FORM: "#charge",
+  CHARGE_INPUT: "#charge input[type=number]",
+  CHARGE_BUTTON: "#charge button",
+  NUMBER_OF_LOTTOS: "#num-of-lottos",
+  LOTTOS: "#lottos",
+  NUMBER_VISIBILITY_TOGGLE: "#visibility-toggle",
+};
+
+export const LOTTO_NUMBER_REGEXP = /((\d{1,2},?)\s?){6}/;
