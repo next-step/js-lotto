fef8f3510f4282d75dfb327f6f66d4f489b74ade
minji kim <38098157+MINGDY98@users.noreply.github.com>
2022-07-12 21:24:26 +0900

    [클린코드 3기 김민지] 로또 미션 STEP 1 (#128)
    * feat: input에 대한 상세 요구사항 작성 및 구입 결과 section html, css 추가
    
    * feat: cypress 설치
    
    * feat: lotto 금액 1000원 미만일 경우 alert 띄우는 테스트 코드 작성  및 구현
    
    * feat: "복권 구입에 성공하면 구입한 금액에 맞는 개수의 복권리스트를 보여준다." 테스트 케이스 작성
    
    * feat: 로또 구입에 성공시 금액에 해당하는 로또 발급
    
    * feat: lotto list span-> li 태그로 변경
    
    * feat: 로또 숫자 랜덤 발급
    
    * feat: '복권 번호는 번호보기 토글 버튼을 클릭하면, 볼 수 있어야 한다.' 테스트 코드 작성
    
    * feat: toggle click에 따라 복권 번호 보여주기/보여주지않기 적용
    
    * feat: lotteries detail flex-col css 적용
    
    * feat: '6개의 복권 번호는 중복이 있으면 안된다.' 테스트코드 작성
    
    * feat: 로또 한장에 중복 숫자 없게끔 적용
    
    * refactor: let-> const 변경, 로또 티켓&번호 생성&html적용 함수 리팩토링
    
    * refactor: tickets detail toggle 리팩토링
    
    * feat: 숫자 상수 선언
    
    * fix: 복권번호토글 로직 변경에 따른 테스트 코드 변경

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,89 @@
-const $showResultButton = document.querySelector('.open-result-modal-button')
-const $modalClose = document.querySelector('.modal-close')
-const $modal = document.querySelector('.modal')
-const $lottoNumbersToggleButton = document.querySelector(
-  '.lotto-numbers-toggle-button'
-)
+import { $ } from "./utils/dom.js";
+import { LOTTO_PRICE, MAX_LOTTO_NUM, MIN_LOTTO_NUM } from "./utils/constant.js";
 
-const onModalShow = () => {
-  $modal.classList.add('open')
+function App() {
+  this.init = () => {
+    initEventListeners();
+  };
 }
 
-const onModalClose = () => {
-  $modal.classList.remove('open')
-}
+const initEventListeners = () => {
+  const onModalShow = () => {
+    $(".modal").classList.add("open");
+  };
 
-$showResultButton.addEventListener('click', onModalShow)
-$modalClose.addEventListener('click', onModalClose)
+  const onModalClose = () => {
+    $(".modal").classList.remove("open");
+  };
+
+  const onToggleLotteryDetail = () => {
+    const toggleChecked = $(".lotto-numbers-toggle-button").checked;
+    const lotteryTickets = $("#lottery-tickets");
+
+    if (toggleChecked) {
+      lotteryTickets.classList.add("visible-number", "flex-col");
+    } else {
+      lotteryTickets.classList.remove("visible-number", "flex-col");
+    }
+  };
+
+  const randomNumberGenerator = () => {
+    return Math.floor(Math.random() * MAX_LOTTO_NUM + MIN_LOTTO_NUM);
+  };
+
+  const lottoTemplateGenerator = (lotteryNumbers) => {
+    return `<li class="mx-1 text-4xl">🎟️<span id=lottery-${0} class="lottery-number">${[
+      ...lotteryNumbers,
+    ].join(",")}</span></li>`;
+  };
+
+  const lottoNumberGenerator = () => {
+    const lotteryNumbers = new Set();
+
+    while (lotteryNumbers.size < 6) {
+      lotteryNumbers.add(randomNumberGenerator());
+    }
+
+    return [...lotteryNumbers];
+  };
+
+  const lottoGenerator = (count) => {
+    return Array.from({ length: count })
+      .map(() => lottoTemplateGenerator(lottoNumberGenerator()))
+      .join("");
+  };
+
+  const getLotto = (purchaseValue) => {
+    const lottoCount = purchaseValue / LOTTO_PRICE;
+
+    $(".lotto-count").innerText = `총 ${lottoCount}개를 구매하였습니다.`;
+    $("#lottery-tickets").innerHTML = lottoGenerator(lottoCount);
+  };
+
+  const onBuyLotto = (event) => {
+    event.preventDefault();
+
+    const purchaseValue = event.target[0].valueAsNumber;
+
+    if (purchaseValue % LOTTO_PRICE !== 0 || purchaseValue <= 0) {
+      alert("로또 구입 금액을 1,000원 단위로 입력해 주세요.");
+      return;
+    }
+
+    getLotto(purchaseValue);
+
+    $("#after-purchase").classList.remove("invisible");
+  };
+
+  $(".open-result-modal-button").addEventListener("click", onModalShow);
+  $(".modal-close").addEventListener("click", onModalClose);
+
+  $("#lotto-purchase-form").addEventListener("submit", onBuyLotto);
+
+  $(".lotto-numbers-toggle-button").addEventListener(
+    "click",
+    onToggleLotteryDetail
+  );
+};
+const app = new App();
+app.init();
