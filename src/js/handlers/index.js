106f5129384e6af2e30f1f3d94e5b7871f70ea61
channy107 <43908861+channy107@users.noreply.github.com>
2022-07-14 02:49:57 +0900

    [클린코드 3기 임헌찬] 로또 미션 STEP 1 (#121)
    * 로또 미션 1단계 구현
    
    * 로또 뽑기 로직 변경
    
    Co-authored-by: kakao_ent <luis.me>

---
+++ src/js/handlers/index.js
@@ -0,0 +1,62 @@
+import { LOTTO_NUMBER_COUNT, MAX_LOTTO_NUMBER, MIN_PRICE } from "../constants/index.js";
+import { STORE } from "../store/index.js";
+import { validatePrice } from "../validates/index.js";
+import { $modal, $purchaseInputValue, addLottoCountLabel, addLottoTicket, showPurchaseViewSection } from "../view/index.js"
+
+export const onModalShow = () => {
+  $modal.classList.add('open')
+}
+
+export const onModalClose = () => {
+  $modal.classList.remove('open')
+}
+
+const getLottoNumber = (count) => {
+  let lottos = [];
+  for (let i = 0; i < count; i++) {
+    let lotto = []
+    while (lotto.length < LOTTO_NUMBER_COUNT) {
+      const number = parseInt(Math.random() * MAX_LOTTO_NUMBER) + 1;
+      if (!lotto.includes(number)) {
+        lotto.push(number);
+      }
+    }
+    lottos.push(lotto);
+  }
+  return lottos
+};
+
+export const handlePurchaseButtonClick = () => {
+  const inputValue = $purchaseInputValue.value;
+  const lottoCount = inputValue / MIN_PRICE;
+  try {
+    validatePrice(inputValue);
+  } catch (error) {
+    alert(error);
+    return
+  }
+
+  showPurchaseViewSection();
+
+  addLottoCountLabel(lottoCount);
+
+  addLottoTicket(lottoCount);
+  const lottos = getLottoNumber(lottoCount);
+  STORE.lottos = lottos;
+}
+
+export const handleLottoNumbersToggleButtonClick = (e) => {
+  const isChecked = e.target.checked;
+  const lottoNumbers = document.querySelectorAll('span.mx-1');
+
+  if (isChecked) {
+   lottoNumbers.forEach((section, index) => {
+    section.innerText += STORE.lottos[index].join(', ') 
+   })
+  } else {
+    lottoNumbers.forEach((section) => {
+      section.innerText = '🎟️ '
+     })
+
+  }
+}
