602cd573e53c1da873c593f86d29b566ab0f1d31
정대윤 <sensecodevalue@gmail.com>
2022-07-16 04:59:08 +0900

    refactor: fragment로 LottoList 랜더 줄이기

--- src/js/components/LottoList.js
+++ src/js/components/LottoList.js
@@ -5,21 +5,14 @@ import LottoItem from './LottoItem.js';
 export default function LottoList() {
   const $lottoList = document.querySelector('#lotto-list');
 
-  const clearElementChildren = element => {
-    while (element.firstChild) {
-      element.removeChild(element.firstChild);
-    }
-  };
-
   const render = () => {
-    const $lottoListClone = $lottoList.cloneNode(true);
-    clearElementChildren($lottoListClone);
+    const lottoListFragment = new DocumentFragment();
 
     store.state.lottoList.forEach(lotto => {
-      $lottoListClone.appendChild(LottoItem({ lotto, visible: store.state.isVisibleLottos }));
+      lottoListFragment.appendChild(LottoItem({ lotto, visible: store.state.isVisibleLottos }));
     });
 
-    $lottoList.replaceChildren(...$lottoListClone.childNodes);
+    $lottoList.replaceChildren(lottoListFragment);
   };
 
   registeReactiveRender(render);
