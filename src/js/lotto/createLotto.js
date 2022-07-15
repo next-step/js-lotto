49ed259484a934f9cd733e02890c4c4afa6918c8
정대윤 <sensecodevalue@gmail.com>
2022-07-16 05:45:34 +0900

    refactor: lotto 만들때 balls 만들기 for문 줄이기

--- src/js/lotto/createLotto.js
+++ src/js/lotto/createLotto.js
@@ -1,7 +1,9 @@
 import { LOTTO_MAX_NUMBER, LOTTO_LENGTH } from '../constants/lotto.js';
 
+const BALLS = new Array(LOTTO_MAX_NUMBER).fill(null).map((value, index) => index + 1);
+
 export default function createLotto() {
-  const balls = new Array(LOTTO_MAX_NUMBER).fill(null).map((value, index) => index + 1);
+  const balls = [...BALLS];
   const lotto = new Array(LOTTO_LENGTH).fill(null).map(() => {
     const ballIndex = Math.floor(Math.random() * (balls.length - 1));
     return balls.splice(ballIndex, 1)[0];
