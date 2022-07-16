c8a971206def7271db9800b92f2248b140dfde96
sujin lee <jmakeaz@gmail.com>
2022-07-16 19:00:13 +0900

    로또티켓 당첨 결과 메소드 추가

--- src/js/constant/index.js
+++ src/js/constant/index.js
@@ -2,3 +2,11 @@ export const LOTTO_NUMBER_TYPE = {
   WINNING: 'winning',
   BONUS: 'bonus',
 };
+
+export const RANK = {
+  FIRST: '1',
+  SECOND: '2',
+  THIRD: '3',
+  FOURTH: '4',
+  FIFTH: '5',
+};
--- src/js/model/lotto-ticket.js
+++ src/js/model/lotto-ticket.js
@@ -1,3 +1,5 @@
+import { LOTTO_NUMBER_TYPE, RANK } from '../constant/index.js';
+
 export default class LottoTicket {
   #numbers;
 
@@ -7,5 +9,34 @@ export default class LottoTicket {
 
   get numbers() {
     return this.#numbers;
+  }
+
+  #getNumberOfWinning(winningNumberMap) {
+    return this.#numbers.reduce(
+      (acc, cur) => {
+        const key = winningNumberMap.get(String(cur));
+        if (key) {
+          acc[key] += 1;
+        }
+        return acc;
+      },
+      { [LOTTO_NUMBER_TYPE.WINNING]: 0, [LOTTO_NUMBER_TYPE.BONUS]: 0 }
+    );
+  }
+
+  getRank(winningNumberMap) {
+    const { winning, bonus } = this.#getNumberOfWinning(winningNumberMap);
+    switch (winning) {
+      case 6:
+        return RANK.FIRST;
+      case 5:
+        return bonus ? RANK.SECOND : RANK.THIRD;
+      case 4:
+        return RANK.FOURTH;
+      case 3:
+        return RANK.FIFTH;
+      default:
+        return null;
+    }
   }
 }
