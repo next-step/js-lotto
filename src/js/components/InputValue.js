7e3344e146c953cc05d93d04d61a04555f9262d0
won junho <wnsgh1993@gmail.com>
2022-07-17 13:48:22 +0900

    inputvalue 껍데기 생성

--- src/js/components/InputValue.js
+++ src/js/components/InputValue.js
@@ -2,7 +2,12 @@ import Component from "../core/Component.js";
 
 export default class InputValue extends Component {
 	setEvent() {
-		const {inputValue} = this.$props;
-		
+		// const {inputValue} = this.$props;
+		// // const $inputMoneyForm = this.$target.querySelector('#i/nput-money-form')
+		// this.addEvent('submit', '#input-money-form', ({event}) => {
+		// 	event.preventDefault();
+		// 	const $inputValue = event.target['inputMoney'].value
+		// 	inputValue(Number($inputValue))
+		// })	
 	}
 }
