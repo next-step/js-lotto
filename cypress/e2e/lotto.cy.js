d1c281e8dc13278b27eaec045ec752ae2c23f65e
wondonghwi <donghwi1289@gmail.com>
2022-07-19 12:29:12 +0900

    🚧 constants 상수 지정

--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -1,4 +1,4 @@
-import { LOTTO_UNIT } from '../../src/js/constants.js';
+import { LOTTO } from '../../src/js/constants.js';
 
 describe('Lotto 테스트', () => {
   beforeEach(() => {
@@ -6,13 +6,13 @@ describe('Lotto 테스트', () => {
   });
 
   describe('input에 구입금액을 입력하고 확인버튼을 누른 경우', () => {
-    it(`value값이 ${LOTTO_UNIT}원으로 나누어떨어지지 않는다면 경고를표시하고 초기화한다.`, () => {
+    it(`value값이 ${LOTTO.UNIT}원으로 나누어떨어지지 않는다면 경고를표시하고 초기화한다.`, () => {
       cy.priceInputAndClick(1350);
-      cy.onOccurAlert(`로또 구입 금액을 ${LOTTO_UNIT}원 단위로 입력해 주세요.`);
+      cy.onOccurAlert(`로또 구입 금액을 ${LOTTO.UNIT}원 단위로 입력해 주세요.`);
       cy.get('.mr-2').should('have.value', '');
     });
 
-    describe(`value값이 ${LOTTO_UNIT}원으로 나누어떨어진다면`, () => {
+    describe(`value값이 ${LOTTO.UNIT}원으로 나누어떨어진다면`, () => {
       it(`value값이 반환된다.`, () => {
         cy.priceInputAndClick(5000);
         cy.get('.mr-2').should('have.value', '5000');
@@ -27,7 +27,7 @@ describe('Lotto 테스트', () => {
       cy.get('#purchase-section').should('be.visible');
     });
 
-    it(`로또 1장의 가격은 ${LOTTO_UNIT}원 이고 3000원 구매시 3개가 구매된다.`, () => {
+    it(`로또 1장의 가격은 ${LOTTO.UNIT}원 이고 3000원 구매시 3개가 구매된다.`, () => {
       cy.priceInputAndClick(3000);
       cy.get('.my-0').contains(`총 3개를 구매하였습니다.`);
     });
--- src/js/constants.js
+++ src/js/constants.js
@@ -1,13 +1,68 @@
-const LOTTO_UNIT = 1000;
-const LOTTO_SIZE = 6;
+const LOTTO = {
+  BONUS_NUMBER_SCORE: 1,
+  WINNING_NUMBER_SCORE: 2,
+  MAX_INPUT_LENGTH: 2,
+  SIZE: 6,
+  WINNING_INPUT_SIZE: 7,
+  UNIT: 1000,
+};
+
+const SCORE = {
+  FIRST_PLACE_SCORE: 12,
+  SECOND_PLACE_BONUS_SCORE: 11,
+  THIRD_PLACE_SCORE: 10,
+  FOURTH_PLACE_BONUS_SCORE: 9,
+  FOURTH_PLACE_SCORE: 8,
+  FIFTH_PLACE_BONUS_SCORE: 7,
+  FIFTH_PLACE_SCORE: 6,
+};
 
 const LOTTO_NUMBER_UNIT = {
   min: 1,
   max: 45,
 };
 
-const ALERT_MESSAGES = {
-  LOTTO_UNIT_ERROR: `로또 구입 금액을 ${LOTTO_UNIT}원 단위로 입력해 주세요.`,
+const INPUT_NAME = {
+  WINNING_NUMBER: 'winning-number',
+  BONUS_NUMBER: 'bonus-number',
 };
 
-export { LOTTO_UNIT, LOTTO_NUMBER_UNIT, ALERT_MESSAGES, LOTTO_SIZE };
+const ALERT_MESSAGES = {
+  LOTTO_UNIT_ERROR: `로또 구입 금액을 ${LOTTO.UNIT}원 단위로 입력해 주세요.`,
+  DUPLICATE_WINNING_NUMBER_ERROR: '로또 번호에는 중복된 숫자를 입력할 수 없습니다.',
+};
+
+const INITIAL_PURCHASE_TEXT_LABEL = '총 0개를 구매하였습니다.';
+
+const RANKING = {
+  [SCORE.FIRST_PLACE_SCORE]: {
+    place: 1,
+    price: 2_000_000_000,
+  },
+  [SCORE.SECOND_PLACE_BONUS_SCORE]: {
+    place: 2,
+    price: 30_000_000,
+  },
+  [SCORE.THIRD_PLACE_SCORE]: {
+    place: 3,
+    price: 1_5000_000,
+  },
+  [SCORE.FOURTH_PLACE_BONUS_SCORE]: {
+    place: 4,
+    price: 50_000,
+  },
+  [SCORE.FOURTH_PLACE_SCORE]: {
+    place: 4,
+    price: 50_000,
+  },
+  [SCORE.FIFTH_PLACE_BONUS_SCORE]: {
+    place: 5,
+    price: 5000,
+  },
+  [SCORE.FIFTH_PLACE_SCORE]: {
+    place: 5,
+    price: 5000,
+  },
+};
+
+export { LOTTO, LOTTO_NUMBER_UNIT, ALERT_MESSAGES, INITIAL_PURCHASE_TEXT_LABEL, INPUT_NAME, RANKING };
