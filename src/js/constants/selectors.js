7975789f00a563f21e7ba8c847c865c4b1541e10
junzero741 <junzero741@gmail.com>
2022-07-20 11:58:39 +0900

    [REFAC] : 사용하지 않는 주석 코드 삭제

--- src/js/constants/selectors.js
+++ src/js/constants/selectors.js
@@ -30,16 +30,3 @@ export const spanSelector = {
   PURCHASED_LOTTO_COUNT_TEXT: "#lotto-purchased-count",
   LOTTO_NUMBERS_DETAIL: ".lotto-detail",
 };
-
-// export const LOTTO_PURCHASE_FORM = "#lotto-purchase-form";
-// export const PRICE_INPUT = "#price-input";
-// export const PRICE_SUBMIT = "#price-submit-btn";
-
-/* -------------------------------------------- 로또 티켓 관련 -------------------------------------------- */
-// export const LOTTO_NUMBERS_TOGGLE_BUTTON = ".switch";
-// export const LOTTO_TICKETS_WRAPPER = "#lotto-tickets-wrapper";
-
-/* -------------------------------------------- 모달 관련 -------------------------------------------- */
-// export const RESULT__MODAL_OPEN_BUTTON = ".open-result-modal-button";
-// export const RESULT_MODAL_CLOSE_BUTTON = ".modal-close";
-// export const MODAL = ".modal";
