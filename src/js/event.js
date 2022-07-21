3d85996bd70e5ae8ea5abb20418dfaacd084854c
junzero741 <junzero741@gmail.com>
2022-07-21 23:24:16 +0900

    [FEAT] : 로또 결과 번호 입력값 파싱

--- src/js/event.js
+++ src/js/event.js
@@ -1,16 +1,8 @@
-import { modalSelector, inputSelector, formSelector } from './constants/selectors.js';
+import { formSelector } from './constants/selectors.js';
 import { $ } from './util.js';
-import { closeModal, showModal, showLottoDetailNumbers, hideLottoDetailNumbers, showElement } from './view.js';
+import { showLottoDetailNumbers, hideLottoDetailNumbers, showElement } from './view.js';
 import { resetPriceInputValue, savePriceInputValueToStore, resetLottoList } from './model.js';
 import { validateInputMoney } from './validate.js';
-
-export const onClickOpenResultModalBtn = function () {
-	showModal($(modalSelector.LOTTO_RESULT_MODAL));
-};
-
-export const onClickCloseResultModalBtn = function () {
-	closeModal($(modalSelector.LOTTO_RESULT_MODAL));
-};
 
 export const onCheckLottoNumbersToggleBtn = function ({ target: { checked } }) {
 	if (checked) {
@@ -22,11 +14,23 @@ export const onCheckLottoNumbersToggleBtn = function ({ target: { checked } }) {
 
 export const onSubmitLottoPurchaseForm = function (ev) {
 	ev.preventDefault();
-	const priceInputVal = ev.srcElement[0].value;
+	const priceInputVal = ev.srcElement[0].valueAsNumber;
 	resetPriceInputValue();
 	resetLottoList();
 	if (validateInputMoney(priceInputVal)) {
 		savePriceInputValueToStore(priceInputVal);
 		showElement($(formSelector.LOTTO_ANSWER_FORM));
 	}
+};
+
+export const onSubmitLottoAnswerForm = function (ev) {
+	ev.preventDefault();
+	const { elements } = ev.srcElement;
+	const answerValues = Array.from(elements).reduce((acc, cur) => {
+		const { valueAsNumber } = cur;
+		if (valueAsNumber) {
+			return [...acc, cur.valueAsNumber];
+		}
+		return acc;
+	}, []);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,11 @@
 import { $, addEvent } from './util.js';
 import { buttonSelector, formSelector } from './constants/selectors.js';
-import { onClickOpenResultModalBtn, onClickCloseResultModalBtn, onCheckLottoNumbersToggleBtn, onSubmitLottoPurchaseForm } from './event.js';
+import { onCheckLottoNumbersToggleBtn, onSubmitLottoPurchaseForm, onSubmitLottoAnswerForm } from './event.js';
 import { subscribeViewsToStore } from './view.js';
 
 const initApp = function () {
 	subscribeViewsToStore();
-	addEvent($(buttonSelector.LOTTO_RESULT_MODAL_OPEN), 'click', onClickOpenResultModalBtn);
-	addEvent($(buttonSelector.LOTTO_RESULT_MODAL_CLOSE), 'click', onClickCloseResultModalBtn);
+	addEvent($(formSelector.LOTTO_ANSWER_FORM), 'submit', onSubmitLottoAnswerForm);
 	addEvent($(buttonSelector.LOTTO_NUMBERS_DETAIL_TOGGLE), 'change', onCheckLottoNumbersToggleBtn);
 	addEvent($(formSelector.LOTTO_PURCHASE_FORM), 'submit', onSubmitLottoPurchaseForm);
 };
