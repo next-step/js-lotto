9f5fd891f0c69ad7f9c00dcabeeeebfdfdcca609
junzero741 <junzero741@gmail.com>
2022-07-22 19:59:27 +0900

    [WIP] : 2단계 로또 테스트코드 작성중

--- cypress/e2e/lotto-answer.cy.js
+++ cypress/e2e/lotto-answer.cy.js
@@ -4,6 +4,7 @@ import {
   spanSelector,
   formSelector,
   liSelector,
+  modalSelector,
 } from "../../src/js/constants/selectors";
 
 describe("로또 당첨 결과 기능 테스트", () => {
@@ -29,9 +30,39 @@ describe("로또 당첨 결과 기능 테스트", () => {
   });
 
   context("로또 결과 테스트", () => {
-    it("결과 확인하기 버튼이 화면에 보인다.", () => {});
-    it("결과 확인하기 버튼을 누르면 결과 모달이 화면에 보인다.", () => {});
-    it("로또 당첨 번호에 중복된 숫자가 입력되어 있으면 결과를 확인할 수 없다.", () => {});
+    it("결과 확인하기 버튼이 화면에 보인다.", () => {
+      cy.get(buttonSelector.LOTTO_RESULT_MODAL_OPEN).should("be.visible");
+    });
+    it("결과 확인하기 버튼을 누르면 결과 모달이 화면에 보인다.", () => {
+      cy.get(inputSelector.LOTTO_ANSWER_NUMBER_INPUT).each((input, idx) => {
+        cy.wrap(input)
+          .type(idx + 1)
+          .should("have.value", idx + 1);
+      });
+      cy.get(inputSelector.LOTTO_BONUS_NUMBER_INPUT).type(7);
+      cy.get(formSelector.LOTTO_ANSWER_FORM)
+        .submit()
+        .then(() => {
+          cy.get(modalSelector.LOTTO_RESULT_MODAL).should("be.visible");
+        });
+    });
+    it("로또 당첨 번호에 중복된 숫자가 입력하고 결과를 보려하면 에러창이 뜬다.", () => {
+      const stub = cy.stub();
+      cy.on("window:alert", stub);
+      cy.get(inputSelector.LOTTO_ANSWER_NUMBER_INPUT).each((input) => {
+        cy.wrap(input).type(1).should("have.value", 1);
+      });
+      cy.get(inputSelector.LOTTO_BONUS_NUMBER_INPUT)
+        .type(1)
+        .should("have.value", 1);
+      cy.get(buttonSelector.LOTTO_RESULT_MODAL_OPEN)
+        .click()
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith(
+            "Error: 로또 번호에는 중복된 숫자를 입력할 수 없습니다."
+          );
+        });
+    });
     it("결과 모달에 수익률이 숫자로 표시된다.");
   });
 
