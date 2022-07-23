936a19cc69be4609e8217549ec76c5e5d46cf70a
blockodyssey-sckim <chan@blockodyssey.io>
2022-07-23 20:30:02 +0900

    mod: 기존 lotto, toggle 코드 제거

--- src/js/constants/index.js
+++
@@ -1,3 +0,0 @@
-export const PRICE_UNIT = 1000;
-export const LOTTO_MAX_NUMBER = 45;
-export const LOTTO_DIGITS = 6;
--- src/js/validation/index.js
+++
@@ -1,6 +0,0 @@
-import { PRICE_UNIT } from '../constants/index.js';
-
-export const isPriceUnitCheck = (input) => {
-  const price = input.value;
-  return price % PRICE_UNIT === 0;
-};
