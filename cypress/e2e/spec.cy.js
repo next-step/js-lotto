926ef34b0eeab251fd33323196fe42bc90e66316
정대윤 <sensecodevalue@gmail.com>
2022-07-23 20:32:28 +0900

    review: 각테스트가 독립적으로 될 수 있도록 변경

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,52 +1,53 @@
-describe('로또 테스트', () => {
-  before(() => {
-    cy.visit('/', { timeout: 1000 });
+import { PAYMENT_UNIT } from '../../src/js/constants/common.js';
+
+describe('로또 생성 테스트', () => {
+  beforeEach(() => {
+    cy.visit('/');
   });
 
   it('최초 접속했을 경우, 금액 입력창이 비워져 있다.', () => {
-    cy.get('#form-amount').within(() => {
-      cy.get('input').should('have.value', '');
-    });
+    cy.get('#input-amount').should('have.value', '');
   });
 
   it('금액 입력창에 1000원 단위가 아닌 금액을 입력 후 구매한다.', () => {
     const inputAmount = 7777;
 
-    cy.get('#form-amount').within(() => {
-      cy.root().then($form => {
-        $form.on('submit', e => {
-          e.preventDefault();
-        });
-      });
-      cy.get('input').type(inputAmount);
-      cy.root().submit();
-    });
+    cy.get('#input-amount').type(inputAmount);
+    cy.get('#form-amount').submit();
 
     cy.alert('로또 구입 금액을 1,000원 단위로 입력해 주세요.');
-    cy.get('#form-amount').within(() => {
-      cy.get('input').should('have.value', '');
-    });
+    cy.get('#input-amount').should('have.value', '');
   });
 
   it('1000원 단위 금액 입력 후 구매한다.', () => {
     const inputAmount = 7000;
+    const lottoCount = inputAmount / PAYMENT_UNIT;
 
     cy.get('#form-amount').within(() => {
       cy.get('input').type(inputAmount);
       cy.root().submit();
     });
 
-    cy.get('#lotto-amount').should('have.text', '총 7개를 구매하였습니다.');
-    cy.get('.lotto-number').should('have.length', 7);
+    cy.get('#lotto-amount').should('have.text', `총 ${lottoCount}개를 구매하였습니다.`);
+    cy.get('.lotto-number').should('have.length', lottoCount);
   });
 
-  it('번호보기 라디오 버튼을 On으로 토글한다.', () => {
+  it('번호보기 라디오 버튼을 On으로 토글하면, 구매한 로또의 번호를 확인할 수 있다.', () => {
+    const inputAmount = 7000;
+    cy.get('#input-amount').type(inputAmount);
+    cy.get('#form-amount').submit();
+
     cy.get('.switch').click();
 
     cy.get('.lotto-number--visible').should('have.length', 7);
   });
 
-  it('번호보기 라디오 버튼을 Off으로 토글한다.', () => {
+  it('번호보기 라디오 버튼을 Off로 토글하면, 구매한 로또의 번호를 확인할 수 없다.', () => {
+    const inputAmount = 7000;
+    cy.get('#input-amount').type(inputAmount);
+    cy.get('#form-amount').submit();
+    cy.get('.switch').click();
+
     cy.get('.switch').click();
 
     cy.get('lotto-number--visible').should('have.length', 0);
