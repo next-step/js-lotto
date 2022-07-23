3add72996c4d0af2f8e834fa05317e256aff6646
정대윤 <sensecodevalue@gmail.com>
2022-07-23 18:10:41 +0900

    feat: 결과보기 및 다시시작하기 로직 추가

---
+++ src/js/controller/ButtonRestart.js
@@ -0,0 +1,27 @@
+import useModal from '../hooks/useModal.js';
+import { DISPATCH } from '../store/constants.js';
+import store from '../store/index.js';
+
+export default function ButtonRestart() {
+  const { $modal, close: closeModal } = useModal();
+  const $restartButton = $modal.querySelector('button');
+  const $inputAmount = document.querySelector('#input-amount');
+  const $inputWinningNumbers = document.querySelectorAll('.winning-number');
+  const $inputBonusNumber = document.querySelector('.bonus-number');
+
+  const clearInputs = () => {
+    $inputWinningNumbers.forEach($el => {
+      $el.value = '';
+    });
+    $inputBonusNumber.value = '';
+    $inputAmount.value = '';
+  };
+
+  const restart = () => {
+    store.dispatch(DISPATCH.RESTART_LOTTO);
+    clearInputs();
+    closeModal();
+  };
+
+  $restartButton.addEventListener('click', restart);
+}
