9985081493600a2461cd84c9d4487be6e274ff3a
정대윤 <sensecodevalue@gmail.com>
2022-07-26 15:53:09 +0900

    review: getLottoRank switch/case if 문 줄여서 보기

--- src/js/domain/lotto/getLottoRank.js
+++ src/js/domain/lotto/getLottoRank.js
@@ -1,21 +1,21 @@
-import { RANK, RANK_CONDITION } from './constants.js';
-
 const FAIL = 'FAIL';
 
+const NO_BONUS = 'NO_BONUS';
+const BONUS = 'BONUS';
+
+const RANK_CONDITION_MAP = {
+  NO_BONUS_3: 'FIFTH',
+  NO_BONUS_4: 'FOURTH',
+  NO_BONUS_5: 'THRID',
+  BONUS_5: 'SECOND',
+  NO_BONUS_6: 'FIRST',
+};
+
+const makeRankCondtionMapKey = ({ count, hasBonusNumber }) =>
+  `${hasBonusNumber ? BONUS : NO_BONUS}_${count}`;
+
 export default function getLottoRank({ count, hasBonusNumber }) {
-  switch (count) {
-    case RANK_CONDITION.FIFTH.matchConditionCount:
-      return RANK.FIFTH;
-    case RANK_CONDITION.FOURTH.matchConditionCount:
-      return RANK.FOURTH;
-    case RANK_CONDITION.SECOND.matchConditionCount:
-      if (hasBonusNumber) {
-        return RANK.SECOND;
-      }
-      return RANK.THRID;
-    case RANK_CONDITION.FIRST.matchConditionCount:
-      return RANK.FIRST;
-    default:
-      return FAIL;
-  }
+  const rankCondtionMapKey = makeRankCondtionMapKey({ count, hasBonusNumber });
+
+  return RANK_CONDITION_MAP[rankCondtionMapKey] || FAIL;
 }
