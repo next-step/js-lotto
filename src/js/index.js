ba0783f063121d5d99a731c93f409b6fa3d20440
nara04040 <nara04040@naver.com>
2022-11-09 22:23:51 +0900

    feat : 로또 구입 금액 입력하는 input을 제어하는 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,20 @@
-const $showResultButton = document.querySelector('.open-result-modal-button')
-const $modalClose = document.querySelector('.modal-close')
-const $modal = document.querySelector('.modal')
-const $lottoNumbersToggleButton = document.querySelector(
-  '.lotto-numbers-toggle-button'
-)
+const $showResultButton = document.querySelector(".open-result-modal-button");
+const $modalClose = document.querySelector(".modal-close");
+const $modal = document.querySelector(".modal");
+const $lottoNumbersToggleButton = document.querySelector(".lotto-numbers-toggle-button");
+const $paymentCost = document.querySelector("#payment-cost");
+const $paymentButton = document.querySelector("#payment-button");
 
 const onModalShow = () => {
-  $modal.classList.add('open')
-}
+  $modal.classList.add("open");
+};
 
 const onModalClose = () => {
-  $modal.classList.remove('open')
-}
-
-$showResultButton.addEventListener('click', onModalShow)
-$modalClose.addEventListener('click', onModalClose)
+  $modal.classList.remove("open");
+};
+const paymentCost = (e) => {
+  console.log($paymentCost.value);
+};
+$showResultButton.addEventListener("click", onModalShow);
+$modalClose.addEventListener("click", onModalClose);
+$paymentButton.addEventListener("click", paymentCost);
