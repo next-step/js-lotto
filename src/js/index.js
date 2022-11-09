f5668e2d4d61cc4c0e244b82c028070a76adadf6
nara04040 <nara04040@naver.com>
2022-11-09 23:37:04 +0900

    feat : 로또 구입 금액 input submit 이벤트 제어

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ const $modal = document.querySelector(".modal");
 const $lottoNumbersToggleButton = document.querySelector(".lotto-numbers-toggle-button");
 const $paymentCost = document.querySelector("#payment-cost");
 const $paymentButton = document.querySelector("#payment-button");
+const $paymentForm = document.querySelector(".payment-form");
 
 const onModalShow = () => {
   $modal.classList.add("open");
@@ -12,9 +13,12 @@ const onModalShow = () => {
 const onModalClose = () => {
   $modal.classList.remove("open");
 };
-const paymentCost = (e) => {
+function paymentCost() {
   console.log($paymentCost.value);
-};
+  $paymentCost.focus();
+}
 $showResultButton.addEventListener("click", onModalShow);
 $modalClose.addEventListener("click", onModalClose);
 $paymentButton.addEventListener("click", paymentCost);
+// submit이벤트여도 값이 넘어가야한다.
+$paymentForm.addEventListener("submit", paymentCost);
