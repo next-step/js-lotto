5ac1fe54d90108bf9a09eff4ab9c14ff1ffc16a9
Seung-wan <tmddhks0104@gmail.com>
2022-11-11 22:44:29 +0900

    Feat: cypress 셋업

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable global-require */
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+    baseUrl: 'http://localhost:5500',
+  },
+});
