9eac7f5dfb73c0c4d770f1c61078aea53c0c0be0
cyjin-jani <jinjanic91@gmail.com>
2022-11-12 23:07:37 +0900

    c: 로또구매 확인 버튼 온클릭 이벤트 함수 일부 구현 (잘못된 입력에 대한 처리 부분)

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
+import { isValidForNoAmount, isValidForExactAmount } from './validators.js';
+
 const $showResultButton = document.querySelector('.open-result-modal-button');
 const $modalClose = document.querySelector('.modal-close');
 const $modal = document.querySelector('.modal');
 const $lottoNumbersToggleButton = document.querySelector('.lotto-numbers-toggle-button');
+const $purchaseAmountInput = document.querySelector('#purchaseAmount');
+const $purchaseButton = document.querySelector('#purchaseButton');
 
 const onModalShow = () => {
   $modal.classList.add('open');
@@ -11,5 +15,18 @@ const onModalClose = () => {
   $modal.classList.remove('open');
 };
 
+const onPurchaseClick = () => {
+  const purchasedAmount = $purchaseAmountInput.value;
+  if (!isValidForNoAmount(purchasedAmount)) {
+    alert('반드시 값을 입력해주세요!');
+    return;
+  }
+  if (!isValidForExactAmount(purchasedAmount)) {
+    alert('로또 한 장의 단위는 1000원 입니다.');
+    return;
+  }
+};
+
 $showResultButton.addEventListener('click', onModalShow);
 $modalClose.addEventListener('click', onModalClose);
+$purchaseButton.addEventListener('click', onPurchaseClick);
