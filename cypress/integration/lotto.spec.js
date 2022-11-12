462c491b29291185fa9930327104e8987480a2b4
nara04040 <nara04040@naver.com>
2022-11-13 00:15:07 +0900

    test : input테스트, 토글버튼 테스트

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -7,15 +7,35 @@ describe("로또게임 페이지 테스트", () => {
   });
 });
 
+describe("input button 테스트", () => {
+  it("input 버튼을 누른다.", () => {
+    cy.get("#payment-button").click();
+  });
+});
+
 describe("input 테스트", () => {
   it("input에 숫자를 넣는 테스트", () => {
     cy.get("#payment-cost-input").click();
     cy.get("#payment-cost-input").type("1000");
   });
+  it("로또가격을 1000원 단위가 아닌경우", () => {
+    cy.visit("/");
+    cy.get("#payment-cost-input").click();
+    cy.get("#payment-cost-input").type("1200");
+    cy.get("#payment-button").click();
+  });
+  cy.on("window:alert", (text) => {
+    expect(text).to.equal("로또 구입 금액을 1,000원 단위로 입력해 주세요.");
+  });
+  cy.on("window:confirm", () => true);
 });
 
-describe("input button 테스트", () => {
-  it("input 버튼을 누른다.", () => {
+describe("번호보기 토글버튼 테스트", () => {
+  it("토글버튼 클릭했을때 ", () => {
+    cy.visit("/");
+    cy.get("#payment-cost-input").click();
+    cy.get("#payment-cost-input").type("1000");
     cy.get("#payment-button").click();
+    cy.get(".lotto-numbers-toggle-button").click();
   });
 });
