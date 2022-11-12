eb23ae8bf52b4c7b0864c69abeb68f2e9d3e1ced
Hojoong Choi <inegg.apps@gmail.com>
2022-11-12 19:55:08 +0900

    feat: 당첨 번호 조회 기능에 대한 추가 테스트 케이스를 구상한다

--- cypress/e2e/dom.cy.js
+++ cypress/e2e/dom.cy.js
@@ -8,14 +8,28 @@ describe('로또 요구사항을 테스트한다', () => {
     labelPurchasingStatus: getSelector('purchasing-status'),
     lottoIcons: getSelector('lotto-icons'),
     toggleVisibleNumbers: getSelector('toggle-visible-numbers'),
+    inputWinningNumber: getSelector('input-winning-number'),
+    inputBonusNumber: getSelector('input-bonus-number'),
+    btnWinning: getSelector('btn-result'),
   };
 
   const MESSAGE = {
     INVALID_AMOUNT_UNIT: '구매 금액은 1,000원 단위로만 입력이 가능합니다',
+    INVALID_WINNING_MODAL: '당첨 번호와 보너스 번호를 모두 입력하세요',
+    INVALID_WINNING_NUMBER_RANGE: '당첨 번호와 보너스 번호는 1~45까지만 입력할 수 있습니다',
+    INVALID_WINNING_NUMBER_DUPLICATED: '당첨 번호와 보너스 번호는 서로 겹칠 수 없습니다',
   };
 
   const messageFunction = {
     getPurchasingStatus: (amount = 0) => `총 ${amount}개를 구매하였습니다.`,
+  };
+
+  const getRandomWinningNumbers = (length = 1) => {
+    const set = new Set();
+    while (set.size < length) {
+      set.add(Math.floor(Math.random() * 45) + 1);
+    }
+    return [...set];
   };
 
   beforeEach(() => {
@@ -73,9 +87,65 @@ describe('로또 요구사항을 테스트한다', () => {
     });
   });
 
-  describe.only('복권 번호는 번호 보기 토글 버튼을 클릭하면 볼 수 있어야 한다', () => {
+  describe('복권 번호는 번호 보기 토글 버튼을 클릭하면 볼 수 있어야 한다', () => {
     it('번호 보기용 토글이 있다', () => {
       cy.get(dom.toggleVisibleNumbers);
+    });
+  });
+
+  describe('당첨 결과를 입력하고 결과 확인을 누르면 모달 창이 떠야 한다', () => {
+    it('결과 확인 버튼을 누를 수 있어야 한다', () => {
+      cy.get(dom.btnWinning).click();
+    });
+
+    it('당첨 번호를 입력할 수 있는 입력 상자가 6개 있다', () => {
+      cy.get(dom.inputWinningNumber).its('length').should('eq', 6);
+    });
+
+    it('당첨 번호와 보너스 번호 필드가 모두 입력되어야 결과를 확인할 수 있다', () => {
+      const stub = cy.stub();
+      cy.on('window:alert', stub);
+      cy.get(dom.btnWinning)
+        .click()
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith(MESSAGE.INVALID_WINNING_MODAL);
+        });
+    });
+    it('당첨 번호와 보너스 번호에 입력할 수 있는 숫자의 범위는 1~45까지이다', () => {
+      const invalidNumbers = [-1, 49, 100, 0];
+      const stub = cy.stub();
+      cy.on('window:alert', stub);
+
+      for (const invalidNumber of invalidNumbers) {
+        const numbers = getRandomWinningNumbers(6);
+        const myWinnings = [invalidNumber, ...numbers.slice(0, 6)];
+        const bonus = numbers.slice(5)[0];
+        cy.get(dom.inputWinningNumber).each(($el, index) => {
+          cy.wrap($el).clear().type(myWinnings[index]);
+        });
+        cy.get(dom.inputBonusNumber).clear().type(bonus);
+        cy.get(dom.btnWinning)
+          .click()
+          .then(() => {
+            expect(stub.getCall(0)).to.be.calledWith(MESSAGE.INVALID_WINNING_NUMBER_RANGE);
+          });
+      }
+    });
+
+    it('당첨 번호와 보너스 번호의 필드는 서로 겹치지 않아야 한다', () => {
+      const numbers = [1, 1, 2, 3, 4, 5];
+      const stub = cy.stub();
+      cy.on('window:alert', stub);
+
+      cy.get(dom.inputWinningNumber).each(($el, index) => {
+        cy.wrap($el).clear().type(numbers[index]);
+      });
+      cy.get(dom.inputBonusNumber).clear().type(2);
+      cy.get(dom.btnWinning)
+        .click()
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith(MESSAGE.INVALID_WINNING_NUMBER_DUPLICATED);
+        });
     });
   });
 });
