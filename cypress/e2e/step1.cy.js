faa0ae12b61c8eed925797861bfb2e2547daccf3
cyjin-jani <jinjanic91@gmail.com>
2022-11-12 23:06:16 +0900

    c: 구매금액의 잘못된 입력 관련된 테스트 케이스 작성

--- cypress/e2e/step1.cy.js
+++ cypress/e2e/step1.cy.js
@@ -4,6 +4,7 @@ describe('로또 어플리케이션 단계1', () => {
   });
 
   const $getPurchaseAmount = () => cy.get('[data-cy="purchase-amount"]');
+  const $getPurchaseButton = () => cy.get('[data-cy="purchase-button"]');
 
   describe('로또 구입 금액을 입력한다.', () => {
     it('입력할 input 태그가 존재한다.', () => {
@@ -19,6 +20,46 @@ describe('로또 어플리케이션 단계1', () => {
       $getPurchaseAmount().type('1000a').should('have.value', '1000');
       $getPurchaseAmount().clear();
       $getPurchaseAmount().type('abc').should('have.value', '');
+    });
+  });
+
+  describe('금액에 해당하는 로또를 발급해야 하며, 로또는 한 장에 1000원이다.', () => {
+    it(`구매에 필요한 '확인'버튼이 존재한다.`, () => {
+      $getPurchaseButton().should('exist');
+    });
+
+    it(`'확인'버튼을 클릭할 수 있어야 한다.`, () => {
+      $getPurchaseButton().click();
+    });
+
+    it(`구매금액 미입력 후 '확인'버튼을 누르면, alert로 '반드시 값을 입력해주세요!' 라는 메세지가 나타난다.`, () => {
+      const alertStub = cy.stub();
+      cy.on('window:alert', alertStub);
+      $getPurchaseButton()
+        .click()
+        .then(() => {
+          const actualMessage = alertStub.getCall(0).lastArg;
+          expect(actualMessage).to.equal('반드시 값을 입력해주세요!');
+        });
+    });
+
+    it(`구매금액에 1000단위가 아닌 값을 입력후 '확인'버튼을 누르면, alert로 '로또 한 장의 단위는 1000원 입니다.' 라는 메세지가 나타난다.`, () => {
+      const alertStub = cy.stub();
+      cy.on('window:alert', alertStub);
+
+      const invalidInputValues = ['999', '100', '1001', '-1000'];
+      invalidInputValues.forEach((value, idx) => {
+        $getPurchaseAmount().type(value);
+        $getPurchaseButton()
+          .click()
+          .then(() => {
+            const actualMessage = alertStub.getCall(0).lastArg;
+            expect(actualMessage).to.equal('로또 한 장의 단위는 1000원 입니다.');
+            if (idx < invalidInputValues.length) {
+              $getPurchaseAmount().clear();
+            }
+          });
+      });
     });
   });
 });
