6a375a855fb1d56761949760c0800441460de916
Seung-wan <tmddhks0104@gmail.com>
2022-11-14 00:50:15 +0900

    Fix: 테스트케이스 추가

--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -1,5 +1,6 @@
-const { ERROR_MESSAGE } = require('../../src/js/constants/index.js');
+const { LOTTO, ERROR_MESSAGE } = require('../../src/js/constants/index.js');
 const { SELECTOR } = require('../constants/selector.js');
+const { generateRandomNumbersToArray } = require('../../src/js/utils/index.js');
 
 beforeEach(() => {
   cy.visit('index.html');
@@ -101,5 +102,30 @@ describe('로또 어플리케이션을 테스트한다.', () => {
     cy.getByDataset(SELECTOR.LOTTO_NUMBERS_TOGGLE_BUTTON).uncheck({ force: true });
 
     cy.getByDataset(SELECTOR.LOTTO_DETAIL_NUMBER).should('have.class', 'none');
+  });
+});
+
+describe('로또 번호를 생성하는 함수를 테스트한다.', () => {
+  it('로또 번호는 1~45의 범위안에 존재해야 한다.', () => {
+    const MIN_INDEX = 0;
+    const MAX_INDEX = 5;
+    const randomNumbersArray = Array.from({ length: 100 }, () =>
+      generateRandomNumbersToArray(LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER, LOTTO.NUMBER_AMOUNT),
+    );
+
+    cy.wrap(randomNumbersArray).each((elem) => {
+      expect(elem[MIN_INDEX]).to.gte(LOTTO.MIN_NUMBER);
+      expect(elem[MAX_INDEX]).to.lte(LOTTO.MAX_NUMBER);
+    });
+  });
+
+  it('로또 번호는 6개가 존재해야 한다.', () => {
+    const randomNumbersArray = Array.from({ length: 100 }, () =>
+      generateRandomNumbersToArray(LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER, LOTTO.NUMBER_AMOUNT),
+    );
+
+    cy.wrap(randomNumbersArray).each((elem) => {
+      expect(elem.length).to.eq(LOTTO.NUMBER_AMOUNT);
+    });
   });
 });
