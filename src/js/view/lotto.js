94a0abfcd519af0556a0149dadc7a1a1d48c740a
Seung-wan <tmddhks0104@gmail.com>
2022-11-13 19:32:55 +0900

    Feat: 로또 view 코드 작성

---
+++ src/js/view/lotto.js
@@ -0,0 +1,40 @@
+import { SELECTOR } from '../constants/lotto.js';
+import { generateLottoNumbers } from '../service/lotto.js';
+import { $, $all } from '../utils/dom.js';
+
+const lottoTemplate = (lottoNumbers) => `
+<li>
+  <span class="mx-1 text-4xl" data-cy="lotto-icon">🎟️ </span>
+  <span class="lotto-detail js-lotto-detail-number none" data-cy="lotto-detail-number">${lottoNumbers.join(', ')}</span>
+</li>
+`;
+
+export const getLottoPurchasePrice = () => $(SELECTOR.PURCHASE_PRICE_INPUT).value;
+
+export const showPurchasedLotto = () => {
+  $(SELECTOR.PURCHASED_LOTTO).classList.remove('none');
+};
+
+export const showLottoResultForm = () => {
+  $(SELECTOR.LOTTO_RESULT_FORM).classList.remove('none');
+};
+
+export const showLottoNumber = () => {
+  $(SELECTOR.LOTTO_ICON_WRAPPER).classList.toggle('flex-col');
+  $all(SELECTOR.LOTTO_DETAIL_NUMBER).forEach((elem) => {
+    elem.classList.toggle('none');
+  });
+};
+
+export const renderLottoPurchaseCountText = (count) => {
+  $(SELECTOR.LOTTO_PURCHASE_COUNT_TEXT).textContent = count;
+};
+
+export const renderLottoIcons = (count) => {
+  let renderedHtml = '';
+  for (let i = 0; i < count; i++) {
+    renderedHtml += lottoTemplate(generateLottoNumbers());
+  }
+
+  $(SELECTOR.LOTTO_ICON_WRAPPER).innerHTML = renderedHtml;
+};
