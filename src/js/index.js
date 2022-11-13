079211dcb149b33ee1b71cb71c3c3883d70ee964
cyjin-jani <jinjanic91@gmail.com>
2022-11-13 20:44:33 +0900

    c: 로또 아이콘 동적 렌더링 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import { isValidForNoAmount, isValidForExactAmount } from './validators.js';
+import LottoModel from './lotto.js';
 
 const $showResultButton = document.querySelector('.open-result-modal-button');
 const $modalClose = document.querySelector('.modal-close');
@@ -9,6 +10,8 @@ const $purchaseButton = document.querySelector('#purchaseButton');
 const $purchasedLottoList = document.querySelector('#purchasedLottoList');
 const $winningNumberInputs = document.querySelector('#winningNumberInputs');
 const $totalQuantity = document.querySelector('#totalQuantity');
+const $lottoIconList = document.querySelector('#lottoIconList');
+
 const lottoState = {
   purchasedAmount: 0,
   quantity: 0,
@@ -34,10 +37,22 @@ const onPurchaseClick = (e) => {
     alert('로또 한 장의 단위는 1000원 입니다.');
     return;
   }
+  initPurchaseLotto();
   lottoState.purchasedAmount = purchasedAmount;
   lottoState.quantity = Number(purchasedAmount) / 1000;
   displayDetails();
   $totalQuantity.innerText = lottoState.quantity;
+  generateLottos(lottoState.quantity);
+  renderLottoIcons();
+};
+
+const generateLottos = (quantity) => {
+  for (let i = 0; i < quantity; i++) {
+    const lotto = new LottoModel([`i${i}`, '2', '3', '4', '5']);
+    lottoState.lottos.push(lotto);
+  }
+};
+
 const displayDetails = () => {
   if (!lottoState.quantity) {
     $purchasedLottoList.style.display = 'none';
@@ -47,6 +62,34 @@ const displayDetails = () => {
   $purchasedLottoList.style.display = 'block';
   $winningNumberInputs.style.display = 'block';
 };
+
+const renderLottoIcons = () => {
+  if (!lottoState.quantity || !lottoState.lottos.length) return;
+  lottoState.lottos.forEach((lotto) => {
+    const li = document.createElement('li');
+    li.className = 'mx-1 text-4xl';
+    const icon = document.createElement('div');
+    icon.className = 'lotto-item';
+    icon.innerText = `🎟️`;
+    li.appendChild(icon);
+    // TODO: lotto의 번호 보여주기.
+    $lottoIconList.appendChild(li);
+  });
+};
+
+const resetDatas = () => {
+  lottoState.purchasedAmount = 0;
+  lottoState.quantity = 0;
+  lottoState.lottos = [];
+};
+
+const resetLottoIcons = () => {
+  $lottoIconList.innerHTML = '';
+};
+
+const initPurchaseLotto = () => {
+  resetDatas();
+  resetLottoIcons();
 };
 
 $showResultButton.addEventListener('click', onModalShow);
