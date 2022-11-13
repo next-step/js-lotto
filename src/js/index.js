09cdd40f05f4c7c52f8fe4d4763f647bb3c698de
Seung-wan <tmddhks0104@gmail.com>
2022-11-13 22:47:41 +0900

    Fix: 이벤트 핸들러 등록 수정

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,9 @@ import { $ } from './utils/dom.js';
 import { SELECTOR } from './constants/lotto.js';
 import { handleClickNumberToggle, handleSubmit } from './eventHandlers/lotto.js';
 
-$(SELECTOR.PAYMENT_FORM).addEventListener('submit', handleSubmit);
-$(SELECTOR.LOTTO_NUMBERS_TOGGLE_BUTTON).addEventListener('click', handleClickNumberToggle);
+const bindEvents = () => {
+  $(SELECTOR.PAYMENT_FORM).addEventListener('submit', handleSubmit);
+  $(SELECTOR.LOTTO_NUMBERS_TOGGLE_BUTTON).addEventListener('click', handleClickNumberToggle);
+};
+
+bindEvents();
