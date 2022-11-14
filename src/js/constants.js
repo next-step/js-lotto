4ef99ecd9b601b4b2541af311dd73f3064982026
cyjin-jani <jinjanic91@gmail.com>
2022-11-14 21:54:34 +0900

    c: 상수들 관리하는 별도 파일 및 상수들 추가

---
+++ src/js/constants.js
@@ -0,0 +1,4 @@
+export const LOTTO_NUMBER_RANGE = 45;
+export const LOTTO_LENGTH = 6;
+export const MESSAGE_FOR_EMPTY_VALUE = '반드시 값을 입력해주세요!';
+export const MESSAGE_FOR_INVALID_UNIT_VALUE = '로또 한 장의 단위는 1000원 입니다.';
