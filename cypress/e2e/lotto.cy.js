f44e61ab548a1ed84d3bf9daadd8aef11934ece5
박예정 <imgoing222@gmail.com>
2022-11-14 18:38:09 +0900

    test: 테스트코드 작성

--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -1,41 +1,79 @@
+import { lotto } from "../../src/js/lotto";
+
 const HTML = "../../index.html";
 
 describe("로또 구입 금액을 입력하면 금액에 해당하는 로또를 발급해야 한다.", () => {
-	beforeEach(() => {
-		cy.visit(HTML);
-	});
+  beforeEach(() => {
+    cy.visit(HTML);
+  });
 
-	it("로또 구입 금액을 입력한다", () => {
-		// - 입력할 input 태그가 있다.
-		const purchaseInput = cy.get('[data-cy="purchaseInput"]');
-		// - 로또 구입 금액을 입력하면 화면에 입력한 금액이 그대로 보여져야 한다.
-		purchaseInput.type("5000");
-		purchaseInput.should("have.value", "5000");
-		// - 금액은 숫자만 입력할 수 있다.
-		purchaseInput.type("{selectAll}");
-		purchaseInput.type("{del}");
-		purchaseInput.type("1000a");
-		purchaseInput.should("have.value", "1000");
-		// - 값은 100000 이하여야 한다.
-	});
+  it("로또 구입 금액을 입력한다", () => {
+    const $purchaseInput = cy.get('[data-cy="purchaseInput"]');
 
-	it("금액에 해당하는 로또를 발급해야 한다.", () => {
-		const stub = cy.stub();
-		cy.on("window:alert", stub);
+    $purchaseInput.type("5000");
+    $purchaseInput.should("have.value", "5000");
 
-		const purchaseInput = cy.get('[data-cy="purchaseInput"]');
+    cy.delete($purchaseInput);
 
-		// - 클릭할 확인 버튼이 있다.
-		const confirmButton = cy.get('[data-cy="confirmButton"]');
-		// - 확인 버튼을 클릭할 수 있어야 한다.
-		// - 1,000원 단위로 입력이 되지 않았을 경우 alert를 띄워준다.
-		// - 999원, 1001원, -1,500원
-		// - asbc, 가나다라, 1000a
-		purchaseInput.type("999");
-		confirmButton.click().then(() => {
-			expect(stub.getCall(0)).to.be.calledWith("1000원 단위로 입력해주세요.");
-		});
+    $purchaseInput.type("1000a");
+    $purchaseInput.should("have.value", "1000");
+  });
 
-		//  - 확인 버튼을 클릭했을 때, 입력한 금액에 맞는 로또 개수가 발급되어야 한다.
-	});
+  it("금액에 해당하는 로또를 발급해야 한다.", () => {
+    const stub = cy.stub();
+    cy.on("window:alert", stub);
+
+    const $purchaseInput = cy.get('[data-cy="purchaseInput"]');
+    const $confirmButton = cy.get('[data-cy="confirmButton"]');
+
+    $purchaseInput.type("1001");
+    $confirmButton.click().then(() => {
+      expect(stub.getCall(0)).to.be.calledWith(
+        "로또 구입 금액을 1,000원 단위로 입력해 주세요."
+      );
+    });
+
+    cy.delete($purchaseInput);
+
+    $purchaseInput.type("3000");
+    $confirmButton.click();
+    lotto.lottos.length === 3;
+  });
+
+  it("소비자는 자동 구매를 할 수 있어야 한다.", () => {
+    const $purchaseInput = cy.get('[data-cy="purchaseInput"]');
+    const $confirmButton = cy.get('[data-cy="confirmButton"]');
+    const $totalPurchaseMessage = cy.get('[data-cy="totalPurchaseMessage"]');
+
+    $purchaseInput.type("4000");
+    $confirmButton
+      .click()
+      .then(() =>
+        $totalPurchaseMessage.should("have.text", "총 4개를 구매하였습니다.")
+      );
+
+    const lottoIcons = cy.get(".lotto-icon");
+    lottoIcons.should("have.length", 4);
+  });
+
+  it("복권 번호는 번호보기 토글 버튼을 클릭하면, 볼 수 있어야 한다.", () => {
+    const $purchaseInput = cy.get('[data-cy="purchaseInput"]');
+    const $confirmButton = cy.get('[data-cy="confirmButton"]');
+    const $switch = cy.get('[data-cy="switch"]');
+
+    $purchaseInput.type("5000");
+    $confirmButton.click();
+
+    // toggle-swich on
+    $switch.click();
+
+    let lottoNumbers = cy.get(".lotto-numbers");
+    lottoNumbers.should("have.length", 5);
+
+    // toggle-swich off
+    $switch.click();
+
+    lottoNumbers = cy.get(".lotto-numbers");
+    lottoNumbers.should("have.css", "display", "none");
+  });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("delete", (element) => {
+  element.type("{selectAll}");
+  element.type("{del}");
+});
