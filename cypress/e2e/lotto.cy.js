82feee5b53d84ca79b76d08636181a9636b7127d
cameo29 <wodls0209@naver.com>
2022-11-15 00:16:50 +0900

    테스트 케이스 수정

--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -2,8 +2,8 @@ const inputSelector = "[data-cy='purchase-amount']";
 const buttonSelector = "[data-cy='purchase-button']";
 const numbersSelector = "[data-cy='purchase-numbers']";
 const iconSelector = "[data-cy='lotto-icon']";
-const toggleSelector = "lotto-numbers-toggle-button";
-const lottoNumberSelector = "[data-cy='lotto-number']";
+const toggleSelector = ".switch";
+const lottoNumberSelector = ".lotto-number";
 
 const ERROR_MESSAGE = "1,000원 단위로 입력하세요.";
 
@@ -37,10 +37,11 @@ describe("로또 어플리케이션을 테스트한다", () => {
   it("1,000원 단위로 입력하지 않은 경우 alert를 띄어준다.", () => {
     cy.get(inputSelector).type("1500");
     const stub = cy.stub();
+    cy.on("window:alert", stub);
     cy.get(buttonSelector)
       .click()
       .then(() => {
-        expect(stub.getCall(0).to.be.calledWith(ERROR_MESSAGE));
+        expect(stub.getCall(0)).to.be.calledWith(ERROR_MESSAGE);
       });
   });
 
@@ -71,16 +72,16 @@ describe("로또 어플리케이션을 테스트한다", () => {
   it("번호보기 토글을 ON하면 복권 번호가 각각 표시된다", () => {
     cy.get(inputSelector).type("3000");
     cy.get(buttonSelector).click();
-    cy.get(toggleSelector).check();
-    cy.get(lottoNumberSelector).should("exist");
+    cy.get(toggleSelector).click();
+    cy.get(lottoNumberSelector).should("be.visible");
   });
 
   it("번호보기 토글을 OFF하면 복권 번호가 사라진다", () => {
     cy.get(inputSelector).type("3000");
     cy.get(buttonSelector).click();
-    cy.get(toggleSelector).check();
-    cy.get(toggleSelector).uncheck();
+    cy.get(toggleSelector).click();
+    cy.get(toggleSelector).click();
 
-    cy.get(lottoNumberSelector).should("not.exist");
+    cy.get(lottoNumberSelector).should("not.be.visible");
   });
 });
