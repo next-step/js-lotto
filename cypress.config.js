e767841a3627be295c3924508d64b368e367ee16
Geuni <dlrmsgnl620@naver.com>
2022-11-15 00:20:02 +0900

    Test: 페어프로그래밍 내용 복습 & 테스트코드 추가

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5500",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -1,1 +1,33 @@
-cy.visit("http://localhost:5500");
+// 로또 구입 금액을 입력하면, 금액에 해당하는 로또를 발급해야 한다.
+// 로또 1장의 가격은 1,000원이다.
+// 소비자는 자동 구매를 할 수 있어야 한다.
+
+const {it} = require("mocha");
+
+// 복권 번호는 번호보기 토글 버튼을 클릭하면, 볼 수 있어야 한다.
+const inputSelector = `[data-cy="purchase-amount"]`;
+const buttonSelector = `[data-cy="purchase-button"]`;
+
+describe("로또 구매기능 테스트", () => {
+  beforeEach(() => {
+    cy.visit("../../index html");
+  });
+
+  it("로또 구입 금액을 입력할 input 태그가 존재한다.", () => {
+    cy.get(inputSelector).should("exist");
+  });
+
+  it("로또 금액을 입력하면 화면에 입력한 금액이 그대로 보여진다", () => {
+    cy.get(inputSelector).type("1000");
+    cy.get(inputSelector).should("have.value", "1000");
+  });
+
+  it("로또 금액은 숫자만 입력 가능하다.", () => {
+    cy.get(inputSelector).type("1000a");
+    cy.get(inputSelector).should("have.value", "1000");
+  });
+
+  it("클릭할 확인 버튼이 존재한다.", () => {
+    cy.get(buttonSelector).should("exist");
+  });
+});
