6367d83cf75cf287a3bcc690abbc73fca5e47cf6
publee <publizm@gmail.com>
2022-11-15 06:10:44 +0900

    docs: project initial setup

---
+++ cypress.config.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {},
+});
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,1 @@
+describe("empty spec", () => {});
