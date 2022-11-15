f7c500292b75f5831bbea718d5f899fe03169fe1
Dongmin Ahn <eamon3481@naver.com>
2022-11-15 14:36:34 +0900

    feat : add purchase submit event

---
+++ src/js/dom.js
@@ -0,0 +1,11 @@
+export const $showResultButton = document.querySelector(
+ '.open-result-modal-button'
+);
+export const $modalClose = document.querySelector('.modal-close');
+export const $modal = document.querySelector('.modal');
+export const $lottoNumbersToggleButton = document.querySelector(
+ '.lotto-numbers-toggle-button'
+);
+export const $purchaseForm = document.querySelector(
+ '[data-cy="purchase-amount-form"]'
+);
--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,36 @@
 import './../css/index.css';
+import { $modal, $modalClose, $purchaseForm, $showResultButton } from './dom';
 
-const $showResultButton = document.querySelector('.open-result-modal-button');
-const $modalClose = document.querySelector('.modal-close');
-const $modal = document.querySelector('.modal');
-const $lottoNumbersToggleButton = document.querySelector(
- '.lotto-numbers-toggle-button'
-);
+class App {
+ constructor() {
+  this.setEvent();
+ }
 
-const onModalShow = () => {
- $modal.classList.add('open');
-};
+ setEvent() {
+  $purchaseForm.addEventListener(
+   'submit',
+   this.handlePurchaseFormSubmit.bind(this)
+  );
+ }
 
-const onModalClose = () => {
- $modal.classList.remove('open');
-};
+ handlePurchaseFormSubmit(event) {
+  event.preventDefault();
+  const target = event.currentTarget;
+  if (!target) return;
+  console.log(+target.purchase.value);
+ }
+}
 
-$showResultButton.addEventListener('click', onModalShow);
-$modalClose.addEventListener('click', onModalClose);
+new App();
+
+// // TODO : modal logic 분리
+// const onModalShow = () => {
+//  $modal.classList.add('open');
+// };
+
+// const onModalClose = () => {
+//  $modal.classList.remove('open');
+// };
+
+// $showResultButton.addEventListener('click', onModalShow);
+// $modalClose.addEventListener('click', onModalClose);
