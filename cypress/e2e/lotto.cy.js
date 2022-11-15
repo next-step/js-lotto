0c5d28b792a6a1054cf506b1efa7611116e47bfd
Geuni <dlrmsgnl620@naver.com>
2022-11-15 21:29:05 +0900

    Test: 오타 수정(dot)

--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -1,16 +1,14 @@
 // 로또 구입 금액을 입력하면, 금액에 해당하는 로또를 발급해야 한다.
 // 로또 1장의 가격은 1,000원이다.
 // 소비자는 자동 구매를 할 수 있어야 한다.
-
-const {it} = require("mocha");
-
 // 복권 번호는 번호보기 토글 버튼을 클릭하면, 볼 수 있어야 한다.
+
 const inputSelector = `[data-cy="purchase-amount"]`;
 const buttonSelector = `[data-cy="purchase-button"]`;
 
 describe("로또 구매기능 테스트", () => {
   beforeEach(() => {
-    cy.visit("../../index html");
+    cy.visit("../../index.html");
   });
 
   it("로또 구입 금액을 입력할 input 태그가 존재한다.", () => {
@@ -29,5 +27,9 @@ describe("로또 구매기능 테스트", () => {
 
   it("클릭할 확인 버튼이 존재한다.", () => {
     cy.get(buttonSelector).should("exist");
+  });
+
+  it("로또 구입 금액을 입력하면, 금액에 해당하는 로또를 발급해야 한다.", () => {
+    cy.get(inputSelector).type("1000").should("have.value", "1000");
   });
 });
