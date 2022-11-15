e8d93e2facf86ed6ec3e5a2a17adf934410e3ddc
Seung-wan <tmddhks0104@gmail.com>
2022-11-15 21:49:32 +0900

    Refactor: for문 파이프라인으로 변경

--- src/js/service/lotto.js
+++ src/js/service/lotto.js
@@ -1,12 +1,12 @@
 import { LOTTO } from '../constants/index.js';
 import { generateRandomNumbersToArray } from '../utils/index.js';
 
+export const getLottoPurchaseCount = (lottoPurchasePrice) => {
+  return lottoPurchasePrice / LOTTO.PRICE;
+};
+
 export const generateLottoNumbersToArray = (count) => {
-  const array = [];
-
-  for (let i = 0; i < count; i++) {
-    array.push(generateRandomNumbersToArray(LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER, LOTTO.NUMBER_AMOUNT));
-  }
-
-  return array;
+  return [...Array(count)].map(() =>
+    generateRandomNumbersToArray(LOTTO.MIN_NUMBER, LOTTO.MAX_NUMBER, LOTTO.NUMBER_AMOUNT),
+  );
 };
