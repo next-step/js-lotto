32effb493ac1c6d60084b31dea1ccf97a72522ae
nara04040 <nara04040@naver.com>
2022-11-16 23:30:27 +0900

    refactor : test코드에 beforeEach를 사용한다.

--- cypress/integration/lotto.spec.js
+++ cypress/integration/lotto.spec.js
@@ -1,38 +1,34 @@
 describe("로또게임 페이지 테스트", () => {
-  it("should visit", () => {
+  beforeEach(() => {
     cy.visit("/");
   });
+
   it("금액 입력창이 비워져있다.", () => {
     cy.get("#payment-cost-input").should("have.value", "");
   });
-});
 
-describe("input button 테스트", () => {
   it("input 버튼을 누른다.", () => {
     cy.get("#payment-button").click();
   });
-});
 
-describe("input 테스트", () => {
   it("input에 숫자를 넣는 테스트", () => {
     cy.get("#payment-cost-input").click();
     cy.get("#payment-cost-input").type("1000");
   });
+
   it("로또가격을 1000원 단위가 아닌경우", () => {
-    cy.visit("/");
     cy.get("#payment-cost-input").click();
     cy.get("#payment-cost-input").type("1200");
     cy.get("#payment-button").click();
   });
+
   cy.on("window:alert", (text) => {
     expect(text).to.equal("로또 구입 금액을 1,000원 단위로 입력해 주세요.");
   });
-  cy.on("window:confirm", () => true);
-});
 
-describe("번호보기 토글버튼 테스트", () => {
+  cy.on("window:confirm", () => true);
+
   it("토글버튼 클릭했을때 ", () => {
-    cy.visit("/");
     cy.get("#payment-cost-input").click();
     cy.get("#payment-cost-input").type("1000");
     cy.get("#payment-button").click();
