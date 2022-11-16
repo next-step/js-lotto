b82555209d8bf8a7d3283779241aaf8d0eb4e9df
Geuni <dlrmsgnl620@naver.com>
2022-11-17 02:06:31 +0900

    Refactor: 불필요한 주석 삭제

--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -1,8 +1,3 @@
-// 로또 구입 금액을 입력하면, 금액에 해당하는 로또를 발급해야 한다.
-// 로또 1장의 가격은 1,000원이다.
-// 소비자는 자동 구매를 할 수 있어야 한다.
-// 복권 번호는 번호보기 토글 버튼을 클릭하면, 볼 수 있어야 한다.
-
 const formSelector = `[data-cy="purchase-form"]`;
 const inputSelector = `[data-cy="purchase-amount"]`;
 const buttonSelector = `[data-cy="purchase-button"]`;
