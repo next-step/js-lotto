abd0111ccfeb78de56553cccdb11da7e94b94f60
oyg0420 <oyg900420@gmail.com>
2022-11-16 21:37:10 +0900

    feat: cypress command 추가
    1. command 추가로 cypress 명령어 간소화

--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -1,3 +1,4 @@
+import { ERROR_MESSAGE } from '../../src/const';
 import Lotto from '../../src/js/lotto';
 
 const lotto = new Lotto();
@@ -9,86 +10,71 @@ describe('로또 테스트', () => {
 
   describe('로또 구입 금액을 입력한다.', () => {
     it('입력할 Input 태그가 있다.', () => {
-      cy.get('#input-price').should('be.visible');
+      cy.getInputPrice(1000).should('be.visible');
     });
 
     it('입력한 금액이 화면에 노출된다.', () => {
-      cy.get('#input-price').type(2000);
-      cy.get('#input-price').should('have.value', 2000);
+      cy.getInputPrice(2000).should('have.value', 2000);
     });
   });
 
   describe('금액에 해당하는 로또를 발급해야 한다.', () => {
     it('확인할 버튼이 있다.', () => {
-      cy.get('#input-price-btn').should('be.visible');
+      cy.clickInputPriceButton().should('be.visible');
     });
 
     it('금액은 1,000원 단위로만 입력할 수 있다.', () => {
       const stub = cy.stub();
       cy.on('window:alert', stub);
-      cy.get('#input-price').type(2500);
-      cy.get('#input-price-btn')
-        .click()
-        .then(() => {
-          expect(stub.getCall(0)).to.be.calledWith(
-            '1,000단위의 숫자를 입력해주세요.'
-          );
-        });
+      cy.getInputPrice(2500);
+      cy.clickInputPriceButton().then(() => {
+        expect(stub.getCall(0)).to.be.calledWith(ERROR_MESSAGE.UNIT);
+      });
     });
 
     it('양수만 입력할 수 있다.', () => {
       const stub = cy.stub();
       cy.on('window:alert', stub);
-      cy.get('#input-price').type(-1000);
-      cy.get('#input-price-btn')
-        .click()
-        .then(() => {
-          expect(stub.getCall(0)).to.be.calledWith(
-            '0이상의 숫자를 입력해주세요.'
-          );
-        });
+      cy.getInputPrice(-1000);
+      cy.clickInputPriceButton().then(() => {
+        expect(stub.getCall(0)).to.be.calledWith(ERROR_MESSAGE.NEGATIVE);
+      });
     });
   });
 
   describe('로또 1장의 가격은 1,000원이다.', () => {
     it('입력한 금액 / 1,000 값인 결과가 구매한 로또의 숫자가 된다.', () => {
-      cy.get('#input-price').type(2000);
-      cy.get('#input-price-btn')
-        .click()
-        .then(() => {
-          cy.get('#total-purchased').should('have.text', 2);
-        });
+      cy.getInputPrice(2000);
+      cy.clickInputPriceButton().then(() => {
+        cy.getTotalPurchased(2);
+      });
     });
   });
 
   describe('소비자는 자동 구매를 할 수 있어야 한다.', () => {
     it('구매한 로또 숫자와 동일한 로또 아이콘이 나열된다.', () => {
-      cy.get('#input-price').type(5000);
-      cy.get('#input-price-btn')
-        .click()
-        .then(() => {
-          cy.get('#total-purchased').should('have.text', 5);
-          cy.get('.lotto-icon').should('have.length', 5);
-        });
+      cy.getInputPrice(5000);
+      cy.clickInputPriceButton().then(() => {
+        cy.getTotalPurchased(5);
+        cy.getTotalLottoIcon(5);
+      });
     });
   });
 
   describe('복권 번호는 번호보기 토글 버튼을 클릭하면, 볼 수 있어야 한다.', () => {
     it('토글 버튼을 OFF → ON 변경시 구매한 로또 개수의 아이콘과 숫자가 노출된다.', () => {
-      cy.get('#input-price').type(5000);
-      cy.get('#input-price-btn').click();
-      cy.get('.switch').click();
-      const lottoDetail = cy.get('.lotto-detail');
-      lottoDetail.should('have.length', 5);
+      cy.getInputPrice(5000);
+      cy.clickInputPriceButton();
+      cy.clickSwitchButton();
+      cy.getTotalLottoNumber(5);
     });
 
     it('토글 버튼을 ON → OFF 변경시 숫자는 가려지며, 구매한 로또 개수의 아이콘만 노출된다.', () => {
-      cy.get('#input-price').type(5000);
-      cy.get('#input-price-btn').click();
-      cy.get('.switch').click();
-      cy.get('.switch').click();
-      const lottoDetail = cy.get('.lotto-detail');
-      lottoDetail.should('have.length', 0);
+      cy.getInputPrice(5000);
+      cy.clickInputPriceButton();
+      cy.clickSwitchButton();
+      cy.clickSwitchButton();
+      cy.getTotalLottoNumber(0);
     });
 
     it('숫자는 중복이 존재하지 않는다.', () => {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,23 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('getInputPrice', (price) => {
+  cy.get('#input-price').type(price);
+});
+
+Cypress.Commands.add('clickInputPriceButton', () => {
+  cy.get('#input-price-btn').click();
+});
+
+Cypress.Commands.add('clickSwitchButton', () => {
+  cy.get('.switch').click();
+});
+
+Cypress.Commands.add('getTotalPurchased', (total) => {
+  cy.get('#total-purchased').should('have.text', total);
+});
+
+Cypress.Commands.add('getTotalLottoIcon', (total) => {
+  cy.get('.lotto-icon').should('have.length', total);
+});
+
+Cypress.Commands.add('getTotalLottoNumber', (total) => {
+  cy.get('.lotto-detail').should('have.length', total);
+});
