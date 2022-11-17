707559523cc0a29c1e0a5111b043a7cd02a7bae7
= <=ykh4933@naver.com>
2022-11-17 16:01:01 +0900

    테스트 코드 수정

--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -1,4 +1,6 @@
 import { ERROR_MSSAGE, LOTTO } from '../../src/js/utils/constants.js';
+import { getLottoNumbers } from '../../src/js/utils/common.js';
+
 const $purchaseInput = '[data-cy="purchase-amount"]';
 const $purchaseButton = '[data-cy="purchase-button"]';
 
@@ -12,91 +14,74 @@ describe('TEST LOTTO APLICATION', () => {
     purchaseInputTag.should('exist');
   });
 
-  it('success When valid price of Lotto input', () => {
-    const purchaseInputTag = cy.get($purchaseInput);
-    purchaseInputTag.type(1000);
-    purchaseInputTag.should('have.value', 1000);
-  });
+  context('valid purchase amount of Lotto in Input tag', () => {
+    it('success When valid price of Lotto input', () => {
+      const purchaseInputTag = cy.get($purchaseInput);
+      purchaseInputTag.type(1000);
+      purchaseInputTag.should('have.value', 1000);
+    });
 
-  it('fails When invalid price of Lotto input', () => {
-    const purchaseInputTag = cy.get($purchaseInput);
-    purchaseInputTag.type('1000a');
-    purchaseInputTag.should('have.value', 1000);
-  });
-
-  it('check valid Price is submitted', () => {
-    const alertStub = cy.stub();
-    cy.on('window:alert', alertStub);
-
-    const purchaseInputTag = cy.get($purchaseInput);
-    const purchaseButtonTag = cy.get($purchaseButton);
-
-    purchaseInputTag.type(1234);
-
-    purchaseButtonTag.click().then(() => {
-      expect(alertStub.getCall(0)).to.be.calledWith(ERROR_MSSAGE.AMOUNT);
+    it('fails When invalid price of Lotto input', () => {
+      const purchaseInputTag = cy.get($purchaseInput);
+      purchaseInputTag.type('1000a');
+      purchaseInputTag.should('have.value', 1000);
     });
   });
 
-  it('show component when the valid price is submitted', () => {
-    const purchaseInputTag = cy.get($purchaseInput);
-    const purchaseButtonTag = cy.get($purchaseButton);
+  context('check valid Price is submitted', () => {
+    it('alert when invalid Price is submitted', () => {
+      const alertStub = cy.stub();
+      cy.on('window:alert', alertStub);
 
-    purchaseInputTag.type(1000);
-    purchaseButtonTag.click().then(() => {
-      cy.get('#purchased-lottos').should('be.visible');
-      cy.get('#input-lotto-nums').should('be.visible');
+      cy.purchaseLotto(1234).then(() => {
+        expect(alertStub.getCall(0)).to.be.calledWith(ERROR_MSSAGE.AMOUNT);
+      })
     });
-  });
 
-  it('show total count of lottos', () => {
-    const purchaseInputTag = cy.get($purchaseInput);
-    const purchaseButtonTag = cy.get($purchaseButton);
-
-    purchaseInputTag.type(7000);
-    purchaseButtonTag.click();
-    cy.get('#total-purchased').contains('7');
-  });
-
-  it('show total images of lottos', () => {
-    const purchaseInputTag = cy.get($purchaseInput);
-    purchaseInputTag.type(7000);
-    cy.get('#input-price-form').submit();
-
-    cy.get('.lotto-image').should('have.length', 7);
-  });
-
-  it('value true when toggle on', () => {
-    const purchaseInputTag = cy.get($purchaseInput);
-    purchaseInputTag.type(7000);
-    cy.get('#input-price-form').submit();
-
-    cy.get('.lotto-numbers-toggle-button')
-      .check({ force: true })
-      .should('be.checked');
-  });
-
-  it('shows the numbers of lottos when toggle on', () => {
-    const purchaseInputTag = cy.get($purchaseInput);
-    purchaseInputTag.type(7000);
-    cy.get('#input-price-form').submit();
-
-    cy.get('.lotto-numbers-toggle-button').check({ force: true });
-    cy.get('.lotto-numbers').should('be.visible');
-  });
-
-  it('lotto number should be 6 numbers', () => {
-    const purchaseInputTag = cy.get($purchaseInput);
-    purchaseInputTag.type(7000);
-    cy.get('#input-price-form').submit();
-
-    cy.get('.lotto-numbers-toggle-button').check({ force: true });
-    cy.get('.lotto-numbers')
-      .first()
-      .then(($element) => {
-        const lottoNumberText = $element.text();
-        const lottoNumbers = lottoNumberText.split(',');
-        expect(lottoNumbers.length).to.equal(LOTTO.LENGTH);
+    it('visible templates when the valid price is submitted', () => {
+      cy.purchaseLotto(7000).then(() => {
+        cy.get('#purchased-lottos').should('be.visible');
+        cy.get('#input-lotto-nums').should('be.visible');
       });
+    });
+  });
+
+  context('check result of Purchase Lottery', () => {
+    it('show total count of lottos', () => {
+      cy.purchaseLotto(7000);
+      cy.get('#total-purchased').contains('7');
+    });
+
+    it('show total images of lottos', () => {
+      cy.purchaseLotto(7000);
+      cy.get('.lotto-image').should('have.length', 7);
+    });
+  });
+
+  context('check Lotto Numbers toggle', () => {
+    it('checked when toggle on', () => {
+      cy.purchaseLotto(9000);
+      cy.onToggleLottoNumbers().should('be.checked');
+    });
+
+    it('shows the numbers of lottos when toggle on', () => {
+      cy.purchaseLotto(8000);
+      cy.onToggleLottoNumbers();
+      cy.get('.lotto-numbers').should('be.visible');
+    });
+  });
+
+  context('check that valid Numbers of Lottery is published', () => {
+    it('lotto number should be 6 numbers', () => {
+      cy.purchaseLotto(7000);
+      cy.onToggleLottoNumbers();
+      cy.get('.lotto-numbers')
+        .first()
+        .then(($element) => {
+          const lottoNumberText = $element.text();
+          const lottoNumbers = lottoNumberText.split(',');
+          expect(lottoNumbers.length).to.equal(LOTTO.LENGTH);
+        });
+    });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,11 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const $purchaseInput = '[data-cy="purchase-amount"]';
+
+Cypress.Commands.add('purchaseLotto', (amount) => {
+    const purchaseInputTag = cy.get($purchaseInput);
+    purchaseInputTag.type(amount);
+    cy.get('#input-price-form').submit();
+})
+
+Cypress.Commands.add('onToggleLottoNumbers', () => {
+    cy.get('.lotto-numbers-toggle-button').check({ force: true });
+})
