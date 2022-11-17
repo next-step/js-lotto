d6169a524189e2261da9219112a3c2cae98989c3
Seung-wan <tmddhks0104@gmail.com>
2022-11-18 00:01:18 +0900

    Feat: 이벤트 핸들러 작성

--- src/js/eventHandlers/lotto.js
+++ src/js/eventHandlers/lotto.js
@@ -1,15 +1,30 @@
-import { getLottoPurchaseCount } from '../service/lotto.js';
-import { assertLottoPurchasePrice } from '../utils/validation.js';
+import model from '../model/Model.js';
+import { getLottoWinningNumbers } from '../service/lotto.js';
+import { assertLottoPurchasePrice, assertLottoWinningNumbers } from '../utils/validation.js';
 import {
   getLottoPurchasePrice,
-  renderLottoPurchaseCountText,
-  renderLottoIcons,
-  showPurchasedLotto,
-  showLottoResultForm,
+  hideModal,
+  renderLottoResult,
+  renderResultForm,
+  resetLottoPurchasePrice,
+  resetView,
+  showModal,
   toggleLottoNumber,
 } from '../view/lotto.js';
 
-export const handleSubmit = (e) => {
+export const handleClickNumberToggle = () => {
+  toggleLottoNumber();
+};
+
+export const handleClickOpenModal = () => {
+  showModal();
+};
+
+export const handleCLickCloseModal = () => {
+  hideModal();
+};
+
+export const handleSubmitPaymentForm = (e) => {
   e.preventDefault();
 
   try {
@@ -17,19 +32,37 @@ export const handleSubmit = (e) => {
 
     assertLottoPurchasePrice(lottoPurchasePrice);
 
-    const lottoPurchaseCount = getLottoPurchaseCount(lottoPurchasePrice);
+    model.buyLotto(lottoPurchasePrice);
 
-    renderLottoPurchaseCountText(lottoPurchaseCount);
-    renderLottoIcons(lottoPurchaseCount);
+    renderLottoResult(model.lottoPurchaseCount, model.lottos);
+  } catch (error) {
+    console.error(error);
+    alert(error.message);
 
-    showPurchasedLotto();
-    showLottoResultForm();
+    resetLottoPurchasePrice();
+  }
+};
+
+export const handleSubmitResultForm = (e) => {
+  e.preventDefault();
+
+  try {
+    const { winning, bonus } = getLottoWinningNumbers();
+
+    assertLottoWinningNumbers([...winning, bonus]);
+
+    model.calculateLottoWinningResult(winning, bonus);
+    renderResultForm(model.rateOfReturn, model.lottoWinningCount);
+
+    handleClickOpenModal();
   } catch (error) {
     console.error(error);
     alert(error.message);
   }
 };
 
-export const handleClickNumberToggle = () => {
-  toggleLottoNumber();
+export const handleClickReset = () => {
+  model.reset();
+
+  resetView(model.lottoPurchaseCount, model.lottos, model.rateOfReturn, model.lottoWinningCount);
 };
