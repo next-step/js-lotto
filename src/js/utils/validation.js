cb33e6f39c14e24a0e1e22eab507ceaad8f80986
Seung-wan <tmddhks0104@gmail.com>
2022-11-18 00:02:13 +0900

    Feat: validation 추가

--- src/js/utils/validation.js
+++ src/js/utils/validation.js
@@ -12,8 +12,16 @@ const assert = (predicate, message) => {
   }
 };
 
+const isDuplicatedLottoNumber = (numbers) => {
+  return new Set(numbers).size !== LOTTO.WINNING_AMOUNT;
+};
+
 export const assertLottoPurchasePrice = (price) => {
   assert(!isZeroNumber(price), ERROR_MESSAGE.INVALID_ZERO_LOTTO_PRICE);
   assert(isPositiveNumber(price), ERROR_MESSAGE.INVALID_NEGATIVE_LOTTO_PRICE);
   assert(isRemainderZero(price, LOTTO.PRICE), ERROR_MESSAGE.INVALID_LOTTO_PRICE_UNIT);
+};
+
+export const assertLottoWinningNumbers = (numbers) => {
+  assert(!isDuplicatedLottoNumber(numbers), ERROR_MESSAGE.DUPLICATED_LOTTO_WINNING_NUMBER);
 };
