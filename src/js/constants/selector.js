cc269887abc47994a07807b88db171cab359b27f
Seung-wan <tmddhks0104@gmail.com>
2022-11-18 00:00:06 +0900

    Feat: js selector 추가

--- src/js/constants/selector.js
+++ src/js/constants/selector.js
@@ -2,9 +2,19 @@ export const SELECTOR = {
   PAYMENT_FORM: '.js-payment-form',
   PURCHASE_PRICE_INPUT: '.js-purchase-price-input',
   PURCHASED_LOTTO: '.js-purchased-lotto',
+
   LOTTO_PURCHASE_COUNT_TEXT: '.js-lotto-purchase-count-text',
   LOTTO_DETAIL_NUMBER: '.js-lotto-detail-number',
   LOTTO_NUMBERS_TOGGLE_BUTTON: '.js-lotto-numbers-toggle-button',
   LOTTO_ICON_WRAPPER: '.js-lotto-icon-wrapper',
   LOTTO_RESULT_FORM: '.js-lotto-result-form',
+
+  WINNING_NUMBER_INPUT_WRAPPER: '.js-winning-number-input-wrapper',
+  BONUS_NUMBER_INPUT: '.js-bonus-number-input',
+
+  MODAL: '.js-modal',
+  MODAL_CLOSE_ICON: '.js-modal-close-icon',
+  LOTTO_WINNING_COUNT: '.js-lotto-winning-count',
+  RATE_OF_RETURN: '.js-rate-of-return',
+  RESET_BUTTON: '.js-reset-button',
 };
