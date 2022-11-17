a3f941ff9c9ecdc3f5ab2af50790efd52d334ade
cyjin-jani <jinjanic91@gmail.com>
2022-11-18 00:02:50 +0900

    c: 로또 번호 최소값 상수화 및 구매 단위 상수화

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,4 +1,6 @@
 export const LOTTO_NUMBER_RANGE = 45;
+export const LOTTO_NUMBER_RANGE_MIN = 1;
 export const LOTTO_LENGTH = 6;
+export const LOTTO_PURCHASE_UNIT = 1000;
 export const MESSAGE_FOR_EMPTY_VALUE = '반드시 값을 입력해주세요!';
 export const MESSAGE_FOR_INVALID_UNIT_VALUE = '로또 한 장의 단위는 1000원 입니다.';
