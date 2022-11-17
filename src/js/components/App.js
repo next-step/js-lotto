ede4a7cf454edeafd2c612cf76b2b9e343ca1fa4
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2022-11-17 09:03:52 +0900

    feat : '소비자는 자동 구매를 할 수 있어야 한다. 요구사항 구현

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,8 +1,13 @@
 import Component from '../core/Component.js';
 import Header from './Header.js';
 import PurchasePrice from './PurchasePrice.js';
-import PurchaseTicket from './PurchaseTicket.js';
-import { UNIT_OF_PRICE } from '../utils/constant.js';
+import PurchaseTickets from './PurchaseTickets.js';
+import {
+  UNIT_OF_PRICE,
+  LOTTO_COUNT,
+  LOTTO_MIN_NUMBER,
+  LOTTO_MAX_NUMBER,
+} from '../utils/constant.js';
 
 export default class App extends Component {
   constructor($target, state) {
@@ -13,27 +18,49 @@ export default class App extends Component {
       this.state,
       this.issueTicket.bind(this),
     );
-    this.purchaseTicket = new PurchaseTicket(
+    this.purchaseTickets = new PurchaseTickets(
       this.$target.querySelector('#target'),
       this.state,
+      this.toggleShowNumbers.bind(this),
     );
   }
 
   setState(newState) {
     this.state = newState;
-    this.purchaseTicket.setState(this.state);
+    this.purchaseTickets.setState(this.state);
     console.log(this.state);
   }
 
-  getTicket(purchasePrice) {
-    let ticket = 0;
-    ticket = purchasePrice / UNIT_OF_PRICE;
-    return ticket;
+  getTicketCount(purchasePrice) {
+    return purchasePrice / UNIT_OF_PRICE;
+  }
+
+  getRandomNumbers() {
+    const set = new Set();
+    while (set.size < LOTTO_COUNT) {
+      const value = Math.floor(
+        Math.random() * (LOTTO_MAX_NUMBER - LOTTO_MIN_NUMBER + 1) +
+          LOTTO_MIN_NUMBER,
+      );
+      set.add(value);
+    }
+    return Array.from(set);
+  }
+
+  getLottoNumbers(ticketAmount) {
+    return new Array(ticketAmount).fill('').map(() => this.getRandomNumbers());
   }
 
   issueTicket(purchasePrice) {
-    const price = Number(purchasePrice);
-    const ticket = this.getTicket(price);
-    this.setState({ ...this.state, purchasePrice: price, ticket });
+    const ticketCount = this.getTicketCount(purchasePrice);
+    const tickets = this.getLottoNumbers(ticketCount);
+    this.setState({ ...this.state, purchasePrice, ticketCount, tickets });
+  }
+
+  toggleShowNumbers() {
+    this.setState({
+      ...this.state,
+      isNumberVisible: !this.state.isNumberVisible,
+    });
   }
 }
