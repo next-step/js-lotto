94e15dd4f6b7ea054ba08cf08c9acfec8d38d558
hyeonbang <91249212+hyeonbang@users.noreply.github.com>
2022-11-19 00:45:53 +0900

    [클린코드 4기 방현경] 로또 미션 STEP1 (#161)
    * install :: live-server
    
    * install :: cypress
    
    * feat :: 로또 기능 구현
    
    * test :: cypress 테스트 코드 구현
    
    * fix :: 기능 관련 버그 수정 (음수값에 대한 예외처리 및 에러처리)
    
    * refactor :: 코드 간결화 및 toggle button 버그 수정
    
    * refactor :: 피드백 반영 및 validator.js 모듈 분리
    
    * refactor :: 로또 번호 생성 영역 lottoNumbers.js로 분리
    
    * fix :: lottoNumbers.js 로직 메소드 인자 누락
    
    * test :: 테스트 코드 리펙토링 및 로직 변경에 따른 테스트 케이스 추가

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,97 @@
+const $purchaseInput = '[data-cy="purchase-amount"]';
+const $purchaseButton = '[data-cy="purchase-button"]';
+const $lottoTickets = '[data-cy="lotto-tickets"]';
+const $lottoNumbersToggleButton = '[data-cy="toggle-lotto-numbers"]';
+
+describe('로또 구현 테스트', () => {
+    beforeEach('로또 페이지 방문', () => {
+        cy.visit('/');
+    })
+
+    inputSpec();
+    purchaseSpec();
+
+})
+
+function inputSpec() {
+    it('금액을 입력할 input이 존재한다.', () => {
+        cy.get($purchaseInput).should('exist');
+    })
+
+    it('금액을 입력할 수 있다.', () => {
+        typePurchaseInput('1000');
+    })
+
+    it('입력한 금액을 그대로 보여준다.', () => {
+        typePurchaseInput('1000');
+        cy.get($purchaseInput).should('have.value', '1000');
+    })
+
+    it('금액의 단위는 1000으로만 입력한다. :: 틀린 값', () => {
+        typePurchaseInput('1001');
+        checkAlert(clickPurchaseButton(), '로또 구입 금액을 1,000원 단위로 입력해 주세요.');
+    })
+
+    it('1000원 이하의 금액을 입력하고 확인 버튼을 누르면 최소금액 미달 경고창이 뜬다.', () => {
+        typePurchaseInput('900');
+        checkAlert(clickPurchaseButton(), '1000원 이하로 구입할 수 없습니다.');
+    })
+
+    it('10000원 이상의 금액을 입력하고 확인 버튼을 누르면 최대금액 초과 경고창이 뜬다.', () => {
+        typePurchaseInput('11000');
+        checkAlert(clickPurchaseButton(), '10000원 이상 구입할 수 없습니다.');
+    })
+
+
+    it('숫자 이외의 텍스트를 입력하면 입력을 받지 않는다.', () => {
+        typePurchaseInput('abcd');
+        cy.get($purchaseInput).should('not.have.value');
+    })
+}
+
+function purchaseSpec() {
+    it('1000 단위의 금액을 입력하고 확인 버튼을 누르면 로또 티켓이 금액에 맞는 갯수만큼 발급된다.', () => {
+        typePurchaseInput('2000');
+        clickPurchaseButton();
+        cy.get($lottoTickets).children('li').should('have.length', 2);
+    })
+
+    it('1000 단위의 금액을 입력하고 Enter 키를 누르면 로또 티켓이 금액에 맞는 갯수만큼 발급된다.', () => {
+        typePurchaseInput('3000').type('{enter}');
+        cy.get($lottoTickets).children('li').should('have.length', 3);
+    })
+
+    it('번호보기 버튼을 누르면 자동 배정된 번호를 확인 할 수 있다.', () => {
+        typePurchaseInput('1000');
+        clickPurchaseButton();
+        cy.get($lottoNumbersToggleButton).click();
+        cy.get($lottoTickets).children().children('.lotto-numbers').should('exist');
+    })
+
+    it('번호보기 버튼을 끄면 자동 배정된 번호를 숨길 수 있다.', () => {
+        typePurchaseInput('3000');
+        clickPurchaseButton();
+        clickLottoNumbersToggleButton();
+        cy.get($lottoTickets).children().children('.lotto-numbers').should('exist');
+        clickLottoNumbersToggleButton();
+        cy.get($lottoTickets).children().children('.lotto-numbers').should('not.be.visible');
+    })
+}
+
+function typePurchaseInput(price) {
+    return cy.get($purchaseInput).type(price);
+}
+
+function clickPurchaseButton() {
+    return cy.get($purchaseButton).click();
+}
+
+function clickLottoNumbersToggleButton() {
+    return cy.get($lottoNumbersToggleButton).click();
+}
+
+function checkAlert(scenario, message) {
+    const stub = cy.stub();
+    cy.on('window:alert', stub);
+    scenario.then(() => expect(stub.getCall(0).lastArg).to.equals(message));
+}
