4f2ef45ff6da4bbb6134085ac051c05d490097bf
Woo KyungJun <natcho9010@gmail.com>
2022-11-20 21:17:13 +0900

    feat: add ResultStore

---
+++ src/js/store/ResultStore.js
@@ -0,0 +1,45 @@
+import { createStore } from '../core/Store.js';
+
+// 결과 창 통신을 위한 store
+
+// 기본적인 default store의 형태
+const initStore = {
+  purchaseCost: 0,
+  result: {
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    bonus: 0,
+  },
+  isShow: false,
+}
+
+const reducer = (actionType, payload, store) => {
+  const newStore = { ...store };
+  // store를 변환시킨 뒤 return한다.
+  switch (actionType) {
+    case ('purchase'): {
+      newStore.purchaseCost = payload;
+      break;
+    }
+    case ('showResult'): {
+      newStore.result = payload.result;
+      newStore.isShow = true;
+      break;
+    }
+    case ('closeResult'): {
+      newStore.result = initStore.result;
+      newStore.isShow = false;
+      break;
+    }
+    default: {
+      newStore.result = initStore.result;
+      newStore.isShow = false;
+    }
+  }
+
+  return newStore;
+}
+
+export const resultStore = createStore(initStore, reducer);
