490bba3fdf8fe195d3f1afa828b5367eeb7c89a1
hyeonbang <hyeonkyeong.bang815@gmail.com>
2022-11-21 00:07:58 +0900

    test :: command.js에 자주 사용하는 테스트 동작 등록

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -19,75 +19,63 @@ function inputSpec() {
     })
 
     it('금액을 입력할 수 있다.', () => {
-        typePurchaseInput('1000');
+        cy.typePurchaseInput('1000');
     })
 
     it('입력한 금액을 그대로 보여준다.', () => {
-        typePurchaseInput('1000');
+        cy.typePurchaseInput('1000');
         cy.get($purchaseInput).should('have.value', '1000');
     })
 
     it('금액의 단위는 1000으로만 입력한다. :: 틀린 값', () => {
-        typePurchaseInput('1001');
-        checkAlert(clickPurchaseButton(), '로또 구입 금액을 1,000원 단위로 입력해 주세요.');
+        cy.typePurchaseInput('1001');
+        checkAlert(cy.clickPurchaseButton(), '로또 구입 금액을 1,000원 단위로 입력해 주세요.');
     })
 
     it('1000원 이하의 금액을 입력하고 확인 버튼을 누르면 최소금액 미달 경고창이 뜬다.', () => {
-        typePurchaseInput('900');
-        checkAlert(clickPurchaseButton(), '1000원 이하로 구입할 수 없습니다.');
+        cy.typePurchaseInput('900');
+        checkAlert(cy.clickPurchaseButton(), '1000원 이하로 구입할 수 없습니다.');
     })
 
     it('10000원 이상의 금액을 입력하고 확인 버튼을 누르면 최대금액 초과 경고창이 뜬다.', () => {
-        typePurchaseInput('11000');
-        checkAlert(clickPurchaseButton(), '10000원 이상 구입할 수 없습니다.');
+        cy.typePurchaseInput('11000');
+        checkAlert(cy.clickPurchaseButton(), '10000원 이상 구입할 수 없습니다.');
     })
 
 
     it('숫자 이외의 텍스트를 입력하면 입력을 받지 않는다.', () => {
-        typePurchaseInput('abcd');
+        cy.typePurchaseInput('abcd');
         cy.get($purchaseInput).should('not.have.value');
     })
 }
 
 function purchaseSpec() {
     it('1000 단위의 금액을 입력하고 확인 버튼을 누르면 로또 티켓이 금액에 맞는 갯수만큼 발급된다.', () => {
-        typePurchaseInput('2000');
-        clickPurchaseButton();
+        cy.typePurchaseInput('2000');
+        cy.clickPurchaseButton();
         cy.get($lottoTickets).children('li').should('have.length', 2);
     })
 
     it('1000 단위의 금액을 입력하고 Enter 키를 누르면 로또 티켓이 금액에 맞는 갯수만큼 발급된다.', () => {
-        typePurchaseInput('3000').type('{enter}');
+        cy.typePurchaseInput('3000').type('{enter}');
         cy.get($lottoTickets).children('li').should('have.length', 3);
     })
 
     it('번호보기 버튼을 누르면 자동 배정된 번호를 확인 할 수 있다.', () => {
-        typePurchaseInput('1000');
-        clickPurchaseButton();
+        cy.typePurchaseInput('1000');
+        cy.clickPurchaseButton();
         cy.get($lottoNumbersToggleButton).click();
         cy.get($lottoTickets).children().children('.lotto-numbers').should('exist');
     })
 
     it('번호보기 버튼을 끄면 자동 배정된 번호를 숨길 수 있다.', () => {
-        typePurchaseInput('3000');
-        clickPurchaseButton();
-        clickLottoNumbersToggleButton();
+        cy.typePurchaseInput('3000');
+        cy.clickPurchaseButton();
+        cy.clickLottoNumbersToggleButton();
         cy.get($lottoTickets).children().children('.lotto-numbers').should('exist');
-        clickLottoNumbersToggleButton();
+        cy.clickLottoNumbersToggleButton();
         cy.get($lottoTickets).children().children('.lotto-numbers').should('not.be.visible');
     })
-}
-
-function typePurchaseInput(price) {
-    return cy.get($purchaseInput).type(price);
-}
-
-function clickPurchaseButton() {
-    return cy.get($purchaseButton).click();
-}
-
-function clickLottoNumbersToggleButton() {
-    return cy.get($lottoNumbersToggleButton).click();
 }
 
 function checkAlert(scenario, message) {
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,20 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const $purchaseInput = '[data-cy="purchase-amount"]';
+const $purchaseButton = '[data-cy="purchase-button"]';
+const $lottoTickets = '[data-cy="lotto-tickets"]';
+const $lottoNumbersToggleButton = '[data-cy="toggle-lotto-numbers"]';
+
+Cypress.Commands.add('typePurchaseInput', (price) => {
+    cy.get($purchaseInput).type(price);
+})
+
+Cypress.Commands.add('clickPurchaseButton', () => {
+    cy.get($purchaseButton).click();
+})
+
+Cypress.Commands.add('clickLottoNumbersToggleButton', () => {
+    return cy.get($lottoNumbersToggleButton).click();
+})
+
+Cypress.Commands.add('checkTicketUnit', (length) => {
+    cy.get($lottoTickets).children('li').should('have.length', length);
+})
