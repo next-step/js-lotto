72ef178e4d6d02d5aa90afb52fef41964a973e7f
Woo KyungJun <natcho9010@gmail.com>
2022-11-20 21:17:52 +0900

    feat: add checkIsSameElementInArray

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -1,3 +1,16 @@
 export function createRandomNumber(maxNumber = 1) {
   return Math.floor(Math.random() * maxNumber);
 }
+
+export function checkIsSameElementInArray(array) {
+  const set = new Set();
+
+  return array.some((el) => {
+    if (set.has(el)) {
+      return true;
+    }
+
+    set.add(el);
+    return false;
+  });
+}
