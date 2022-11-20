21b1ffb2b781e74fa04e8d33633146beff4f6028
Woo KyungJun <natcho9010@gmail.com>
2022-11-20 22:17:35 +0900

    feat: calc profit ratio

--- src/js/components/ResultModal.js
+++ src/js/components/ResultModal.js
@@ -5,13 +5,7 @@ const resultRecords = Array.from(table.getElementsByTagName('tbody')[0].children
 
 const $profit = document.getElementById('profit');
 
-const resultAndMoneyMap = {
-  3: 5000,
-  4: 50000,
-  5: 1500000,
-  bonus: 30000000,
-  6: 2000000000,
-};
+
 
 modalCloseButton.addEventListener('click', (e) => {
   e.stopPropagation();
@@ -30,4 +24,25 @@ export function ResultModal({ result, isShow, purchaseCost }) {
     const lottoCount = tr.getAttribute('data-table');
     tr.children[2].textContent = `${result[lottoCount]}개`
   });
+
+  const totalPrice = calcPrice(result);
+  const profitRatio = Math.floor(((totalPrice - purchaseCost) / purchaseCost) * 100);
+  $profit.textContent = profitRatio;
+}
+
+function calcPrice(result) {
+  let price = 0;
+  const resultAndMoneyMap = {
+    3: 5000,
+    4: 50000,
+    5: 1500000,
+    bonus: 30000000,
+    6: 2000000000,
+  };
+
+  Object.entries(result).forEach(([key, val]) => {
+    price += (resultAndMoneyMap[key] * val);
+  });
+
+  return price;
 }
