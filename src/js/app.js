c4158fcc3a6b01d718d9ea3237d143005d888e32
oyg0420 <oyg900420@gmail.com>
2022-11-20 22:04:40 +0900

    refactor: 당첨번호 입력 폼 분리
    1. winningtInputForm 으로 분리

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,14 @@
-import { $ } from '../utils.js';
 import InputPriceForm from './view/input-price-form.js';
 import PurchasedLotto from './view/purchased-lotto.js';
+import WinningInputForm from './view/winning-input-form.js';
 
 class App {
   constructor($target, lotto) {
     this.lotto = lotto;
     this.$target = $target;
-    this.$inputLottoNumbers = $('#input-lotto-numbers');
     new InputPriceForm($target, lotto);
     new PurchasedLotto($target, lotto);
-
-    this.initRender();
-  }
-
-  renderInputLottoNumbers() {
-    if (this.showPurchasedLottos) {
-      this.$inputLottoNumbers.style.display = 'block';
-      return;
-    }
-
-    this.$inputLottoNumbers.style.display = 'none';
-  }
-
-  initRender() {
-    this.renderInputLottoNumbers();
+    new WinningInputForm($target, lotto);
   }
 }
 
---
+++ src/js/view/winning-input-form.js
@@ -0,0 +1,19 @@
+import { $ } from '../../utils.js';
+import Controller from '../controller.js';
+
+class WinningInputForm extends Controller {
+  constructor($target, model) {
+    super($target, model);
+    this.$inputLottoNumbers = $('#input-lotto-numbers');
+  }
+
+  render() {
+    const { lottoCount } = this.model;
+    if (lottoCount > 0) {
+      this.$inputLottoNumbers.style.display = 'block';
+      return;
+    }
+  }
+}
+
+export default WinningInputForm;
