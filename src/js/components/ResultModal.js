6e36b3df0ef97dd6f8759fb8b5c25e260bec7680
Woo KyungJun <natcho9010@gmail.com>
2022-11-20 21:59:28 +0900

    feat: modal close button

--- src/js/components/ResultModal.js
+++ src/js/components/ResultModal.js
@@ -1,4 +1,18 @@
 const modal = document.getElementById('modal');
+const modalCloseButton = document.getElementById('modal-close');
+
+const resultAndMoneyMap = {
+  3: 5000,
+  4: 50000,
+  5: 1500000,
+  bonus: 30000000,
+  6: 2000000000,
+};
+
+modalCloseButton.addEventListener('click', (e) => {
+  e.stopPropagation();
+  modal.classList.remove('open');
+});
 
 
 export function ResultModal({ result, isShow }) {
