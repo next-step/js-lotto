686625e1b8cd3853fe2f9068b213501f1826fe89
publee <publizm@gmail.com>
2022-11-22 00:03:55 +0900

    feat: 생성된 로또 UI render 로직 추가

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,9 +1,11 @@
 // view
 class Ui {
   #lottoList;
+  #purchasedCount;
 
   constructor() {
     this.#lottoList = document.querySelector("#lotto-list");
+    this.#purchasedCount = document.querySelector(".purchased-count");
   }
 
   onToggleReadMore(checked) {
@@ -14,8 +16,34 @@ class Ui {
     this.#lottoList.classList.remove("flex-col");
   }
 
-  render = (lottos) => {
-    console.log("lottos", lottos);
+  renderLottoElements(lottos) {
+    lottos.forEach((lotto) => {
+      const newListItem = document.createElement("li");
+      newListItem.className = "d-flex mx-1 text-4xl";
+
+      const listOfLottoNumber = document.createElement("span");
+      listOfLottoNumber.className = "lotto-detail ml-1";
+      listOfLottoNumber.style.display = "none";
+      listOfLottoNumber.innerText = lotto;
+
+      const listOfLottoIcon = document.createElement("span");
+      listOfLottoIcon.className = "lotto-icon";
+      listOfLottoIcon.innerText = "🎟️ ";
+
+      newListItem.appendChild(listOfLottoIcon);
+      newListItem.appendChild(listOfLottoNumber);
+      this.#lottoList.appendChild(newListItem);
+    });
+  }
+
+  renderLottoCount(count) {
+    this.#purchasedCount.innerText = count;
+  }
+
+  render = (state) => {
+    console.log("lottos", state);
+    this.renderLottoElements(state.lottos);
+    this.renderLottoCount(state.gameCount);
   };
 }
 
