f7b142411a601804fecf303d794cd4b6d021296a
oyg0420 <oyg900420@gmail.com>
2022-11-21 22:09:47 +0900

    refactor: 구조개선
    1. controller render 에 model 정보 내려주도록 변경

--- src/js/controllers/controller.js
+++ src/js/controllers/controller.js
@@ -2,7 +2,7 @@ class Controller {
   constructor(view, model) {
     this.view = view;
     this.model = model;
-    this.view.render();
+    this.view.render(this.model);
     this.initEventHandler();
   }
 
@@ -13,11 +13,11 @@ class Controller {
   initEventHandler() {
     this.view.$target.addEventListener('click', (event) => {
       this.addClickEvent(event);
-      this.view.render();
+      this.view.render(this.model);
     });
     this.view.$target.addEventListener('change', (event) => {
       this.addChangeEvent(event);
-      this.view.render();
+      this.view.render(this.model);
     });
   }
 }
