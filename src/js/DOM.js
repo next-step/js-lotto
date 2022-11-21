371026e6740feb4c794c8f7020c93e1dba794817
nara04040 <nara04040@naver.com>
2022-11-21 17:48:32 +0900

    feat : 당첨번호 DOM요소 제어 기능구현

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,10 +1,11 @@
-import { $ } from "./utils.js";
+import { $, $$ } from "./utils.js";
 
 export const $showModalButton = $(".open-result-modal-button");
-export const $removeModalButton = $(".modal-close");
-export const $modal = $(".modal");
 export const $lottoNumbersToggleButton = $(".lotto-numbers-toggle-button");
 export const $paymentCost = $("#payment-cost-input");
 export const $paymentButton = $("#payment-button");
 export const $paymentForm = $(".payment-form");
 export const $paymentTickets = $("#payment-ticket");
+export const $modal = $(".modal");
+export const $removeModalButton = $(".modal-close");
+export const $$winningNumbers = $$(".winning-number");
