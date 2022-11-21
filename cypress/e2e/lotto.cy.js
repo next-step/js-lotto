6389d004035999d043f2a7c489583852952d203c
semin <awesummer.1021@gmail.com>
2022-11-21 17:40:43 +0900

    test : Function을 Command로 변경

--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -7,26 +7,22 @@ const $ticketList = '.ticket-list';
 const $btnViewNumber = '[data-cy="btn-toggle-numbers"]';
 const min = 1000;
 const max = 100000;
+const unit = 1000;
 
-function typeInputPurchase(value) {
-  cy.get($inputPurchase).type(value);
-}
-
-function checkToHave(element, chainer, value) {
-  cy.get(element).should(`have.${chainer}`, value);
-}
-
-function clearValue(element) {
-  cy.get(element).clear();
-}
-
-function checkVisible(element) {
-  cy.get(element).should('be.visible');
-}
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * 10) + 1;
-}
+Cypress.Commands.addAll({
+  typeInputPurchase(value) {
+    cy.get($inputPurchase).type(value);
+  },
+  checkToHave(element, chainer, value) {
+    cy.get(element).should(`have.${chainer}`, value);
+  },
+  clearValue(element) {
+    cy.get(element).clear();
+  },
+  checkVisible(element) {
+    cy.get(element).should('be.visible');
+  },
+});
 
 describe('로또 구입 금액 입력', () => {
   beforeEach(() => {
@@ -37,27 +33,27 @@ describe('로또 구입 금액 입력', () => {
     const testValue = ['1000e', '-1000'];
 
     testValue.forEach(value => {
-      typeInputPurchase(value);
-      checkToHave($inputPurchase, 'have', 1000);
-      clearValue($inputPurchase);
+      cy.typeInputPurchase(value);
+      cy.checkToHave($inputPurchase, 'have', 1000);
+      cy.clearValue($inputPurchase);
     });
   });
 
   it(`${min} 단위의 값만 허용한다`, () => {
-    typeInputPurchase(`${min + 1}`);
+    cy.typeInputPurchase(`${min + 1}`);
     cy.get($submitPurchase).click();
-    checkVisible($messagePurchase);
-    checkToHave($messagePurchase, 'text', `${min} 단위로 입력해 주세요`);
+    cy.checkVisible($messagePurchase);
+    cy.checkToHave($messagePurchase, 'text', `${unit} 단위로 입력해 주세요`);
   });
 
   it(`${min} ~ ${max} 사이의 값만 허용한다 (값 미입력)`, () => {
     const testValue = [' ', '0'];
 
     testValue.forEach(value => {
-      typeInputPurchase(value);
+      cy.typeInputPurchase(value);
       cy.get($submitPurchase).click();
-      checkVisible($messagePurchase);
-      checkToHave($messagePurchase, 'text', '값을 입력해 주세요');
+      cy.checkVisible($messagePurchase);
+      cy.checkToHave($messagePurchase, 'text', '값을 입력해 주세요');
     });
   });
 
@@ -65,37 +61,36 @@ describe('로또 구입 금액 입력', () => {
     const testValue = [`${min - 1}`, `${max + 1000}`];
 
     testValue.forEach(value => {
-      typeInputPurchase(value);
+      cy.typeInputPurchase(value);
       cy.get($submitPurchase).click();
-      checkVisible($messagePurchase);
-      checkToHave($messagePurchase, 'text', `${min} ~ ${max} 사이의 값을 입력해 주세요`);
+      cy.checkVisible($messagePurchase);
+      cy.checkToHave($messagePurchase, 'text', `${min} ~ ${max} 사이의 값을 입력해 주세요`);
     });
   });
 
   it('올바른 값 입력 시, input 내의 내용을 지운다', () => {
-    typeInputPurchase(min);
+    cy.typeInputPurchase(min);
     cy.get($submitPurchase).click();
-    checkToHave($inputPurchase, 'text', '');
+    cy.checkToHave($inputPurchase, 'text', '');
   });
 });
 
 describe('로또 구입 후 확인', () => {
-  let ticketNum;
+  const ticketNum = Math.floor(Math.random() * 100) + 1;
 
   beforeEach(() => {
     cy.visit('/');
-    ticketNum = getRandomNumber();
-    typeInputPurchase(`${ticketNum * 1000}`);
+    cy.typeInputPurchase(`${ticketNum * unit}`);
     cy.get($submitPurchase).click();
   });
 
   it('구매 문구 확인', () => {
-    checkVisible($resultWrap);
-    checkToHave($messageTotal, 'text', `총 ${ticketNum}개를 구매하였습니다.`);
+    cy.checkVisible($resultWrap);
+    cy.checkToHave($messageTotal, 'text', `총 ${ticketNum}개를 구매하였습니다.`);
   });
 
   it('발행된 티켓 갯수 확인', () => {
-    checkToHave(`${$ticketList} > li`, 'length', ticketNum);
+    cy.checkToHave(`${$ticketList} > li`, 'length', ticketNum);
   });
 
   it('번호보기 버튼 on 시, 번호를 확인할 수 있다', () => {
