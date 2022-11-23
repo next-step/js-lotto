d449d9b9c618e39c7c5a5c578181ad7cf9d210c7
Seung-wan <tmddhks0104@gmail.com>
2022-11-24 00:10:54 +0900

    Feat: 이벤트 바인딩

--- src/js/index.js
+++ src/js/index.js
@@ -6,10 +6,14 @@ import {
   handleSubmitPaymentForm,
   handleClickReset,
   handleSubmitResultForm,
+  handleSubmitSelfBuyingForm,
+  handleClickAutoBuyingButton,
 } from './eventHandlers/lotto.js';
 
 const bindEvents = () => {
   $(SELECTOR.PAYMENT_FORM).addEventListener('submit', handleSubmitPaymentForm);
+  $(SELECTOR.LOTTO_SELF_BUYING_FORM).addEventListener('submit', handleSubmitSelfBuyingForm);
+  $(SELECTOR.LOTTO_AUTO_BUYING_BUTTON).addEventListener('click', handleClickAutoBuyingButton);
   $(SELECTOR.LOTTO_NUMBERS_TOGGLE_BUTTON).addEventListener('click', handleClickNumberToggle);
   $(SELECTOR.LOTTO_RESULT_FORM).addEventListener('submit', handleSubmitResultForm);
   $(SELECTOR.MODAL_CLOSE_ICON).addEventListener('click', handleCLickCloseModal);
