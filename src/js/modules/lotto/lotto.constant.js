6845419f9ed97b4dfc97bc709095f61a7f76876a
pyuung <kyuungah@gmail.com>
2022-11-23 10:53:32 +0900

    [클린코드 4기 이경아] 로또 미션 STEP1 (#172)
    * feat: :sparkles: step1 기본 기능 구현
    
    * feat: :sparkles: cypress추가
    
    * feat: :sparkles: 기본 테스트코드 작성
    
    * refactor: :recycle: step1 피드백 반영

---
+++ src/js/modules/lotto/lotto.constant.js
@@ -0,0 +1,15 @@
+export const LOTTO_MACHINE_CONFIG = {
+  price: 1000,
+  winningNumberCnt: 6,
+  numberMin: 1,
+  numberMax: 45,
+}
+
+export const DOM_ELEMENT = {
+  lottoImg: `<span class="mx-1 text-4xl lotto-img" >🎟️ </span>`,
+}
+
+export const ERROR_TEXT = {
+  EMPTY_PURCHASE_AMOUNT: '구입 금액을 입력해주세요.',
+  INVALID_PURCHASE_AMOUNT: '원 단위로 구매 가능합니다.',
+}
