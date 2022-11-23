3126de45684195b28a43ca54d55589b760b7bb2d
Dongmin Ahn <eamon3481@naver.com>
2022-11-23 10:32:07 +0900

    refactor : move validate logic and lotteryMin

--- src/js/Lottery/Lottery.js
+++ src/js/Lottery/Lottery.js
@@ -1,12 +1,14 @@
 export default class Lottery {
  #lotteryCount;
  #lotteryMax;
+ #lotteryMin;
  #bonus;
  #numbers;
 
  constructor() {
   this.#lotteryCount = 6;
   this.#lotteryMax = 45;
+  this.#lotteryMin = 1;
   this.#bonus = null;
   this.#numbers = [];
  }
@@ -23,13 +25,6 @@ export default class Lottery {
  }
 
  setLottery(number) {
-  if (this.isIncludes(number)) {
-   throw new Error('로또 번호는 중복가능하지 않습니다. 정확히 입력해주세요');
-  }
-  if (this.#numbers.length >= this.#lotteryCount && this.#bonus) {
-   throw new Error('로또를 모두 뽑았습니다.');
-  }
-
   this.validateLottoNumber(number);
 
   if (this.#numbers.length === this.#lotteryCount) {
@@ -40,8 +35,15 @@ export default class Lottery {
  }
 
  validateLottoNumber(number) {
+  if (this.isIncludes(number)) {
+   throw new Error('로또 번호는 중복가능하지 않습니다. 정확히 입력해주세요');
+  }
+  if (this.#numbers.length >= this.#lotteryCount && this.#bonus) {
+   throw new Error('로또를 모두 뽑았습니다.');
+  }
+
   if (!Number.isInteger(number)) throw Error('로또 번호는 정수여야합니다.');
-  if (number > 45 || number < 1) {
+  if (number > this.#lotteryMax || number < this.#lotteryMin) {
    throw Error('1~45 중에 입력해주세요', number);
   }
  }
