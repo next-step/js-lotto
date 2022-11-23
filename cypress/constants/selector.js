7456d33c716ece983624b73aab80cab74920adff
Seung-wan <tmddhks0104@gmail.com>
2022-11-24 00:10:26 +0900

    Feat: 테스트코드 작성

--- cypress/constants/selector.js
+++ cypress/constants/selector.js
@@ -9,7 +9,6 @@ export const SELECTOR = {
   LOTTO_NUMBERS_TOGGLE_BUTTON: 'lotto-numbers-toggle-button',
   LOTTO_ICON_LIST: 'lotto-icon-list',
   LOTTO_RESULT_FORM: 'lotto-result-form',
-
   WINNING_NUMBER_INPUT_WRAPPER: 'winning-number-input-wrapper',
   BONUS_NUMBER_INPUT: 'bonus-number-input',
   OPEN_RESULT_MODAL_BUTTON: 'open-result-modal-button',
@@ -18,4 +17,13 @@ export const SELECTOR = {
   LOTTO_WINNING_COUNT: 'lotto-winning-count',
   RATE_OF_RETURN: 'rate-of-return',
   RESET_BUTTON: 'reset-button',
+
+  LOTTO_PURCHASE_CONTAINER: 'lotto-purchase-container',
+  LOTTO_ANNOUNCEMENT_MESSAGE: 'lotto-announcement-message',
+  LOTTO_CURRENT_PURCHASED_STATUS: 'lotto-current-purchased-status',
+  LOTTO_SELF_BUYING_FORM: 'lotto-self-buying-form',
+  LOTTO_PURCHASING_NUMBER_INPUT_WRAPPER: 'lotto-purchasing-number-input-wrapper',
+  LOTTO_PURCHASING_BONUS_NUMBER_INPUT: 'lotto-purchasing-bonus-number-input',
+  LOTTO_SELF_BUYING_BUTTON: 'lotto-self-buying-button',
+  LOTTO_AUTO_BUYING_BUTTON: 'lotto-auto-buying-button',
 };
--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -7,27 +7,52 @@ beforeEach(() => {
   cy.visit('index.html');
 });
 
-const buyLotto = (price) => {
-  cy.getByDataset(SELECTOR.PURCHASE_PRICE_INPUT).type(price);
-  cy.getByDataset(SELECTOR.PAYMENT_FORM).submit();
+const typeLottoPurchasingNumbers = () => {
+  cy.getByDataset(SELECTOR.LOTTO_PURCHASING_NUMBER_INPUT_WRAPPER)
+    .children()
+    .each(($el, idx) => {
+      cy.wrap($el).type(String(DUMMY_DATA.LOTTO_NUMBERS[idx]));
+    })
+    .then(() => {
+      cy.getByDataset(SELECTOR.LOTTO_PURCHASING_BONUS_NUMBER_INPUT).type(DUMMY_DATA.BONUS_NUMBER);
+    });
 };
 
-const typeWinningNumbers = () => {
+const typeLottoWinningNumbers = () => {
   cy.getByDataset(SELECTOR.WINNING_NUMBER_INPUT_WRAPPER)
     .children()
     .each(($el, idx) => {
-      cy.wrap($el).type(String(DUMMY_DATA.WINNING_NUMBERS[idx]));
+      cy.wrap($el).type(String(DUMMY_DATA.LOTTO_NUMBERS[idx]));
     })
     .then(() => {
       cy.getByDataset(SELECTOR.BONUS_NUMBER_INPUT).type(DUMMY_DATA.BONUS_NUMBER);
     });
 };
 
+const buyLottoSelf = (price) => {
+  cy.getByDataset(SELECTOR.PURCHASE_PRICE_INPUT).type(price);
+  cy.getByDataset(SELECTOR.PAYMENT_FORM).submit();
+
+  Cypress._.times(price / LOTTO.PRICE, () => {
+    typeLottoPurchasingNumbers();
+    cy.getByDataset(SELECTOR.LOTTO_SELF_BUYING_FORM).submit();
+  });
+};
+
+const buyLottoAuto = (price) => {
+  cy.getByDataset(SELECTOR.PURCHASE_PRICE_INPUT).type(price);
+  cy.getByDataset(SELECTOR.PAYMENT_FORM).submit();
+
+  Cypress._.times(price / LOTTO.PRICE, () => {
+    cy.getByDataset(SELECTOR.LOTTO_AUTO_BUYING_BUTTON).click();
+  });
+};
+
 const typeDuplicatedWinningNumbers = () => {
   cy.getByDataset(SELECTOR.WINNING_NUMBER_INPUT_WRAPPER)
     .children()
     .each(($el) => {
-      cy.wrap($el).type(String(DUMMY_DATA.WINNING_NUMBERS[0]));
+      cy.wrap($el).type(String(DUMMY_DATA.LOTTO_NUMBERS[0]));
     })
     .then(() => {
       cy.getByDataset(SELECTOR.BONUS_NUMBER_INPUT).type(DUMMY_DATA.BONUS_NUMBER);
@@ -41,7 +66,7 @@ const clickOpenResultModalButton = () => {
 describe('로또 어플리케이션을 테스트한다.', () => {
   describe('결과 확인하기 버튼을 누르면 당첨 통계, 수익률을 모달로 확인할 수 있다.', () => {
     beforeEach(() => {
-      buyLotto('100000');
+      buyLottoAuto('10000');
     });
 
     it('결과 확인하기 버튼이 존재한다.', () => {
@@ -62,7 +87,7 @@ describe('로또 어플리케이션을 테스트한다.', () => {
     });
 
     it('결과 확인하기 버튼을 누르면 모달창이 뜬다.', () => {
-      typeWinningNumbers();
+      typeLottoWinningNumbers();
       clickOpenResultModalButton();
 
       cy.getByDataset(SELECTOR.MODAL).should('have.class', 'open');
@@ -88,15 +113,15 @@ describe('로또 어플리케이션을 테스트한다.', () => {
       };
 
       cy.getByDataset(SELECTOR.LOTTO_DETAIL_NUMBER).each(($el) => {
-        const dummyLottoNumberArray = $el.text().split(',').map(Number);
+        const LottoNumberArray = $el.text().split(',').map(Number);
 
-        const rank = getLottoRank(dummyLottoNumberArray, DUMMY_DATA.WINNING_NUMBERS, DUMMY_DATA.BONUS_NUMBER);
+        const rank = getLottoRank(LottoNumberArray, DUMMY_DATA.LOTTO_NUMBERS, DUMMY_DATA.BONUS_NUMBER);
         if (rank < LOTTO.WINNING_MINIMUM_NUMBER) return;
 
         lottoWinningCount[rank]++;
       });
 
-      typeWinningNumbers();
+      typeLottoWinningNumbers();
       clickOpenResultModalButton();
 
       cy.getByDataset(SELECTOR.LOTTO_WINNING_COUNT)
@@ -122,16 +147,16 @@ describe('로또 어플리케이션을 테스트한다.', () => {
         .each(($el) => {
           const dummyLottoNumberArray = $el.text().split(',').map(Number);
 
-          const rank = getLottoRank(dummyLottoNumberArray, DUMMY_DATA.WINNING_NUMBERS, DUMMY_DATA.BONUS_NUMBER);
+          const rank = getLottoRank(dummyLottoNumberArray, DUMMY_DATA.LOTTO_NUMBERS, DUMMY_DATA.BONUS_NUMBER);
           if (rank < LOTTO.WINNING_MINIMUM_NUMBER) return;
 
           lottoWinningCount[rank]++;
         })
         .then(() => {
-          typeWinningNumbers();
+          typeLottoWinningNumbers();
           clickOpenResultModalButton();
 
-          const returnOfRate = getRateOfReturn(lottoWinningCount, 100000);
+          const returnOfRate = getRateOfReturn(lottoWinningCount, 10000);
 
           cy.getByDataset(SELECTOR.RATE_OF_RETURN).should('have.text', `당신의 총 수익률은 ${returnOfRate}%입니다.`);
         });
@@ -140,8 +165,8 @@ describe('로또 어플리케이션을 테스트한다.', () => {
 
   describe('다시 시작하기 버튼을 누르면 초기화되서 다시 구매를 시작할 수 있다.', () => {
     beforeEach(() => {
-      buyLotto('100000');
-      typeWinningNumbers();
+      buyLottoAuto('3000');
+      typeLottoWinningNumbers();
       clickOpenResultModalButton();
     });
 
@@ -218,15 +243,15 @@ describe('로또 어플리케이션을 테스트한다.', () => {
     });
 
     it('구입할 금액을 입력하고 확인 버튼을 클릭하면 화면에 구매 안내메시지의 개수가 구입한 로또의 개수와 일치해야 한다.', () => {
-      buyLotto('100000');
+      buyLottoAuto('3000');
 
-      cy.getByDataset(SELECTOR.LOTTO_PURCHASE_COUNT_TEXT).should('have.text', '100');
+      cy.getByDataset(SELECTOR.LOTTO_PURCHASE_COUNT_TEXT).should('have.text', '3');
     });
 
     it('구입할 금액을 입력하고 확인 버튼을 클릭하면 화면에 로또 아이콘의 개수가 구입한 로또의 개수와 일치해야 한다.', () => {
-      buyLotto('100000');
+      buyLottoAuto('3000');
 
-      cy.getByDataset(SELECTOR.LOTTO_ICON_LIST).should('have.length', '100');
+      cy.getByDataset(SELECTOR.LOTTO_ICON_LIST).should('have.length', '3');
     });
   });
 
@@ -247,7 +272,7 @@ describe('로또 어플리케이션을 테스트한다.', () => {
 
   describe('복권 번호는 번호보기 토글 버튼을 클릭하면, 볼 수 있어야 한다.', () => {
     beforeEach(() => {
-      buyLotto('100000');
+      buyLottoAuto('3000');
     });
     it('로또를 발급하고 난 후에, 화면에 번호보기 토글 버튼이 존재한다.', () => {
       cy.getByDataset(SELECTOR.LOTTO_NUMBERS_TOGGLE_BUTTON).should('exist');
@@ -264,6 +289,45 @@ describe('로또 어플리케이션을 테스트한다.', () => {
       cy.getByDataset(SELECTOR.LOTTO_NUMBERS_TOGGLE_BUTTON).uncheck({ force: true });
 
       cy.getByDataset(SELECTOR.LOTTO_DETAIL_NUMBER).should('have.class', 'none');
+    });
+  });
+
+  describe('소비자는 수동 구매를 할 수 있다.', () => {
+    it('로또 구입 금액을 입력하면 아래에 로또를 구매해달라는 메시지가 보인다.', () => {
+      cy.getByDataset(SELECTOR.PURCHASE_PRICE_INPUT).type('5000');
+      cy.getByDataset(SELECTOR.PAYMENT_FORM).submit();
+
+      cy.getByDataset(SELECTOR.LOTTO_ANNOUNCEMENT_MESSAGE).should(
+        'have.text',
+        '5000원을 입력하셨습니다, 로또를 5장 구매해주세요.',
+      );
+    });
+
+    it('로또를 수동으로 모두 구매하면 구매한 개수만큼 메시지와 아이콘이 보인다.', () => {
+      buyLottoSelf('5000');
+
+      cy.getByDataset(SELECTOR.PURCHASED_LOTTO).should('not.have.class', 'none');
+      cy.getByDataset(SELECTOR.LOTTO_PURCHASE_COUNT_TEXT).should('have.text', '5');
+      cy.getByDataset(SELECTOR.LOTTO_ICON_LIST).should('have.length', '5');
+    });
+  });
+
+  describe('수동 구매 후 남는 금액이 있다면 자동으로 구매할 수 있어야 한다.', () => {
+    it('구입할 금액에 5000원을 입력하고 2회 수동구매를 하고 3회 자동구매를 하면 구매 결과 화면으로 넘어간다.', () => {
+      cy.getByDataset(SELECTOR.PURCHASE_PRICE_INPUT).type('5000');
+      cy.getByDataset(SELECTOR.PAYMENT_FORM).submit();
+
+      Cypress._.times(2, () => {
+        typeLottoPurchasingNumbers();
+        cy.getByDataset(SELECTOR.LOTTO_SELF_BUYING_FORM).submit();
+      });
+      Cypress._.times(3, () => {
+        cy.getByDataset(SELECTOR.LOTTO_AUTO_BUYING_BUTTON).click();
+      });
+
+      cy.getByDataset(SELECTOR.PURCHASED_LOTTO).should('not.have.class', 'none');
+      cy.getByDataset(SELECTOR.LOTTO_PURCHASE_COUNT_TEXT).should('have.text', '5');
+      cy.getByDataset(SELECTOR.LOTTO_ICON_LIST).should('have.length', '5');
     });
   });
 });
