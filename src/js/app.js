40851787a61b5e8cb8631b99c4f65d5bd0d6d6ca
oyg0420 <oyg900420@gmail.com>
2022-11-26 22:16:45 +0900

    feat: 로또 결과
    1. 로또 결과 app 추가

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,21 @@
 import InputPriceFormController from './controllers/input_price_form_controller.js';
 import PurchasedLottoController from './controllers/purchase_lotto_controller.js';
 import WinningInputFormController from './controllers/winning_input_form_controller.js';
+import WinningResultModalController from './controllers/winning_result_modal_controller.js';
 import InputPriceFormView from './views/input_price_form_view.js';
 import PurchasedLottoView from './views/purchase_lotto_view.js';
 import WinningInputFormView from './views/winning_input_form_view.js';
+import WinningResultModalView from './views/winning_result_modal_view.js';
 
 class App {
   constructor($target, lotto) {
     new InputPriceFormController(new InputPriceFormView($target), lotto);
     new PurchasedLottoController(new PurchasedLottoView($target), lotto);
     new WinningInputFormController(new WinningInputFormView($target), lotto);
+    new WinningResultModalController(
+      new WinningResultModalView($target),
+      lotto
+    );
   }
 }
 
