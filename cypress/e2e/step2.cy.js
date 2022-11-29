16ac28c5d2ad46c324d7214211fc09482f981d51
yousooa <kinsk2839@naver.com>
2022-11-29 18:45:55 +0900

    test: 개행 리팩토링

--- cypress/e2e/step2.cy.js
+++ cypress/e2e/step2.cy.js
@@ -16,6 +16,13 @@ describe('당첨 번호 & 보너스 번호 입력 테스트', () => {
   });
 
   context('당첨 번호 6개와 보너스 번호 1개는 1 ~ 45 중 중복되지 않은 7개의 수이다.', () => {
+    const INVALID_CASE = {
+      EMPTY: { winningNumbers: [1, 3, 7, 5, 2], bonus: 10 },
+      INVALID_WINNING_NUMBER_WITHIN_RANGE: { winningNumbers: [1, 0, 7, 4, 5, 2], bonus: 10 },
+      INVALID_BONUS_WITHIN_RANGE: { winningNumbers: [1, 27, 7, 4, 5, 2], bonus: 47 },
+      DUPLICATED: { winningNumbers: [1, 1, 7, 4, 5, 2], bonus: 1 },
+    };
+
     const alertInvalidNumbers = (errorMessage) => {
       const alertStub = cy.stub();
 
@@ -25,13 +32,6 @@ describe('당첨 번호 & 보너스 번호 입력 테스트', () => {
         .then(() => {
           expect(alertStub.getCall(0)).to.be.calledWith(errorMessage);
         });
-    };
-
-    const INVALID_CASE = {
-      EMPTY: { winningNumbers: [1, 3, 7, 5, 2], bonus: 10 },
-      INVALID_WINNING_NUMBER_WITHIN_RANGE: { winningNumbers: [1, 0, 7, 4, 5, 2], bonus: 10 },
-      INVALID_BONUS_WITHIN_RANGE: { winningNumbers: [1, 27, 7, 4, 5, 2], bonus: 47 },
-      DUPLICATED: { winningNumbers: [1, 1, 7, 4, 5, 2], bonus: 1 },
     };
 
     it('당첨 번호와 보너스 번호 input에 하나라도 입력하지 않으면 alert 창이 뜬다.', () => {
@@ -76,7 +76,6 @@ describe('모달창 테스트', () => {
 
     cy.clickPurchaseBtn(PRICE_INPUT);
     cy.typeWinningNumbersAndBonus({ winningNumbers: WINNING_NUMBERS, bonus: BONUS });
-
     cy.get('.open-result-modal-btn').click();
   };
 
@@ -103,7 +102,6 @@ describe('모달창 테스트', () => {
   context('모달창의 다시 시작하기 버튼을 누르면 초기화된다.', () => {
     before(() => {
       openModalTest();
-
       cy.get('.restart-btn').click();
     });
 
@@ -125,7 +123,6 @@ describe('모달창 테스트', () => {
   context('모달창의 x 버튼을 누르면 모달창 뜨기 이전 화면으로 돌아간다.', () => {
     before(() => {
       openModalTest();
-
       cy.get('.modal-close').click();
     });
 
