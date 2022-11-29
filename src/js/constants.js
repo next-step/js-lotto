37340557563a6d5f64283aabca6a1cf58222c9a8
cyjin-jani <jinjanic91@gmail.com>
2022-11-29 23:13:10 +0900

    refactor: 메세지에 상수 활용

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,10 +1,10 @@
 export const LOTTO_NUMBER_RANGE_MAX = 45;
 export const LOTTO_NUMBER_RANGE_MIN = 1;
 export const LOTTO_LENGTH = 6;
-export const LOTTO_PURCHASE_UNIT = 1000;
+export const LOTTO_PURCHASE_UNIT = 1_000;
 
 export const MESSAGE_FOR_EMPTY_VALUE = '반드시 값을 입력해주세요!';
-export const MESSAGE_FOR_INVALID_UNIT_VALUE = '로또 한 장의 단위는 1000원 입니다.';
+export const MESSAGE_FOR_INVALID_UNIT_VALUE = `로또 한 장의 단위는 ${LOTTO_PURCHASE_UNIT}원 입니다.`;
 export const MESSAGE_FOR_INVALID_WINNING_NUMBERS = '중복된 번호를 입력할 수 없습니다.';
 
 export const PRIZE_FOR_PLACE = {
