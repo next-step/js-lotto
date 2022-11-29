b1c9afbe5a3e189c7e13881ee4182baca3024a02
yousooa <kinsk2839@naver.com>
2022-11-29 14:39:53 +0900

    test: 상수 리팩토링, 결과 확인하기 기능 테스트 추가

--- cypress/e2e/step2.cy.js
+++ cypress/e2e/step2.cy.js
@@ -27,49 +27,75 @@ describe('로또 결과 확인 테스트', () => {
         });
     };
 
+    const INVALID_CASE = {
+      EMPTY: { winningNumbers: [1, 3, 7, 5, 2], bonus: 10 },
+      INVALID_WINNING_NUMBER_WITHIN_RANGE: { winningNumbers: [1, 0, 7, 4, 5, 2], bonus: 10 },
+      INVALID_BONUS_WITHIN_RANGE: { winningNumbers: [1, 27, 7, 4, 5, 2], bonus: 47 },
+      DUPLICATED: { winningNumbers: [1, 1, 7, 4, 5, 2], bonus: 1 },
+    };
+
     it('당첨 번호와 보너스 번호 input에 하나라도 입력하지 않으면 alert 창이 뜬다.', () => {
-      const winningNumbers = [1, 3, 7, 5, 2];
-      const bonusNumber = 10;
+      const { winningNumbers, bonus } = INVALID_CASE.EMPTY;
 
       cy.get('.winning-number').each(($ele, idx) => {
         if (idx === 5) return;
         cy.get($ele).type(winningNumbers[idx]);
       });
-      cy.get('.bonus-number').type(bonusNumber);
+      cy.get('.bonus-number').type(bonus);
       alertInvalidNumbers(ERROR_MESSAGE.INVALID_NUMBER_WITHIN_RANGE);
     });
 
     it('당첨 번호가 1 ~ 45 사이의 수가 아니면 alert 창이 뜬다.', () => {
-      const winningNumbers = [1, 0, 7, 4, 5, 2];
-      const bonusNumber = 10;
+      const { winningNumbers, bonus } = INVALID_CASE.INVALID_WINNING_NUMBER_WITHIN_RANGE;
 
       cy.get('.winning-number').each(($ele, idx) => {
         cy.get($ele).type(winningNumbers[idx]);
       });
-      cy.get('.bonus-number').type(bonusNumber);
+      cy.get('.bonus-number').type(bonus);
       alertInvalidNumbers(ERROR_MESSAGE.INVALID_NUMBER_WITHIN_RANGE);
     });
 
     it('보너스 번호가 1 ~ 45 사이의 수가 아니면 alert 창이 뜬다.', () => {
-      const winningNumbers = [1, 27, 7, 4, 5, 2];
-      const bonusNumber = 47;
-
+      const { winningNumbers, bonus } = INVALID_CASE.INVALID_BONUS_WITHIN_RANGE;
       cy.get('.winning-number').each(($ele, idx) => {
         cy.get($ele).type(winningNumbers[idx]);
       });
-      cy.get('.bonus-number').type(bonusNumber);
+      cy.get('.bonus-number').type(bonus);
       alertInvalidNumbers(ERROR_MESSAGE.INVALID_NUMBER_WITHIN_RANGE);
     });
 
     it('당첨 번호와 보너스 번호 중 중복되는 숫자가 있으면 alert창이 뜬다', () => {
-      const winningNumbers = [1, 1, 7, 4, 5, 2];
-      const bonusNumber = 1;
+      const { winningNumbers, bonus } = INVALID_CASE.DUPLICATED;
 
       cy.get('.winning-number').each(($ele, idx) => {
         cy.get($ele).type(winningNumbers[idx]);
       });
-      cy.get('.bonus-number').type(bonusNumber);
+      cy.get('.bonus-number').type(bonus);
       alertInvalidNumbers(ERROR_MESSAGE.DUPLICATED_NUMBER);
+    });
+  });
+
+  context('결과 확인하기 버튼을 누르면 당첨 통계, 수익률 결과가 있는 모달창이 뜬다.', () => {
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const bonus = 7;
+
+    beforeEach(() => {
+      cy.get('.winning-number').each(($ele, idx) => {
+        cy.get($ele).type(winningNumbers[idx]);
+      });
+      cy.get('.bonus-number').type(bonus);
+      cy.get('.open-result-modal-button').click();
+      cy.get('.modal').should('have.class', 'open');
+    });
+
+    it('당첨 갯수를 확인할 수 있다.', () => {
+      cy.get('.winning-count').each(($ele) => {
+        cy.get($ele).invoke('text').should('match', /[0-9]/);
+      });
+    });
+
+    it('수익률을 확인할 수 있다.', () => {
+      cy.get('.profit-rate-result').invoke('text').should('match', /[0-9]/);
     });
   });
 });
