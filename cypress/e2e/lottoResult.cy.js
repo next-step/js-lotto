dff7951fbbc352bba639d90e278a1ddb296ecbbe
Geuni <dlrmsgnl620@naver.com>
2022-11-30 12:34:03 +0900

    Test: test code error 수정

--- cypress/e2e/lottoResult.cy.js
+++ cypress/e2e/lottoResult.cy.js
@@ -10,32 +10,32 @@ describe("당첨 결과기능 테스트", () => {
     cy.visit("../../index.html");
     cy.purchaseLotto(5000);
   });
-});
 
-context("번호 입력하기 테스트", () => {
-  it("로또 번호를 입력할 수 있다.", () => {
-    cy.get(inputSelector).each((input) => {
-      cy.wrap(input).type(1).should("have.value", 1);
+  context("번호 입력하기 테스트", () => {
+    it("로또 번호를 입력할 수 있다.", () => {
+      cy.get(inputSelector).each((input) => {
+        cy.wrap(input).type(1).should("have.value", 1);
+      });
+    });
+    it("보너스 번호를 입력할 수 있다.", () => {
+      cy.get(bonusInputSelector).each((input) => {
+        cy.wrap(input).type(2).should("have.value", 2);
+      });
+    });
+    it("1, 2, 3, 4, 5, 6 / 보너스 7 => 가능", () => {
+      cy.get(inputSelector).each((input, idx) => {
+        cy.wrap(input).type(idx + 1);
+      });
+      cy.get(bonusInputSelector).type(7);
+      cy.get(buttonSelector).click();
+      cy.get(modalSelector).should("have.class", "open");
     });
   });
-  it("보너스 번호를 입력할 수 있다.", () => {
-    cy.get(bonusInputSelector).each((input) => {
-      cy.wrap(input).type(2).should("have.value", 2);
-    });
-  });
-  it("1, 2, 3, 4, 5, 6 / 보너스 7 => 가능", () => {
-    cy.get(inputSelector).each((input, idx) => {
-      cy.wrap(input).type(idx + 1);
-    });
-    cy.get(bonusInputSelector).type(7);
-    cy.get(buttonSelector).click();
-    cy.get(modalSelector).should("have.class", "open");
-  });
-});
 
-context("다시하기 테스트", () => {
-  it("다시하기 버튼을 누르면 초기화 된다.", () => {
-    cy.get(resetButtonSelector).click();
-    cy.get(spanSelector).should("have.value", 0);
+  context("다시하기 테스트", () => {
+    it("다시하기 버튼을 누르면 초기화 된다.", () => {
+      cy.get(resetButtonSelector).click();
+      cy.get(spanSelector).should("have.value", 0);
+    });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,34 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
 const formSelector = `[data-cy="purchase-form"]`;
 const inputSelector = `[data-cy="purchase-amount"]`;
 const buttonSelector = `[data-cy="purchase-button"]`;
 
-Cypress.Commands.add("purchaseLotto"),
-  (money) => {
-    cy.get(inputSelector).type(money);
-    cy.get(formSelector).submit();
-  };
+Cypress.Commands.add("purchaseLotto", (money) => {
+  cy.get(inputSelector).type(money);
+  cy.get(formSelector).submit();
+});
