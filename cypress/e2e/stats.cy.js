6fdcc70fbb0c804d1032bc87330267ace3496cd2
hyeonbang <hyeonkyeong.bang815@gmail.com>
2022-12-08 21:35:52 +0900

    test :: 구조 변화에 따른 테스트 케이스 수정

--- cypress/e2e/stats.cy.js
+++ cypress/e2e/stats.cy.js
@@ -13,7 +13,8 @@ describe('로또 구현 테스트 :: 지난주 로또 번호 입력', () => {
     })
 
     beforeEach('로또 구입', () => {
-        cy.typePurchaseInput('3000').type('{enter}');
+        cy.typePriceInput('3000').type('{enter}');
+        cy.clickPurchaseButton();
     })
 
     statsSpec();
