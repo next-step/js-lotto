688463eb3867b4fdc21151dd272beda2da7e987b
박예정 <imgoing222@gmail.com>
2022-12-12 01:09:40 +0900

    refactor: switch문을 lookup table로 변경

--- src/js/service/lotto.js
+++ src/js/service/lotto.js
@@ -1,71 +1,72 @@
 import { LOTTO } from "../constants/lotto.js";
 import { createRandomNumbers } from "../utils/operate.js";
+
 class Lotto {
-	#lottos;
-	#winningNumbers;
-	#bonusNumber;
-	#result = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
-	#inputTotal = 0;
-	#earningTotal = 0;
+  #lottos;
+  #winningNumbers;
+  #bonusNumber;
+  #result = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+  #inputTotal = 0;
+  #earningTotal = 0;
 
-	issueLottos(number) {
-		const lottos = Array.from(Array(number), () => createRandomNumbers());
-		this.#lottos = lottos;
-		this.#inputTotal += number * 1000;
-		return lottos;
-	}
+  issueLottos(number) {
+    const lottos = Array.from(Array(number), () => createRandomNumbers());
+    this.#lottos = lottos;
+    this.#inputTotal += number * 1000;
+    return lottos;
+  }
 
-	setWinningOrBonusNumber(numbers) {
-		Array.isArray(numbers)
-			? (this.#winningNumbers = numbers)
-			: (this.#bonusNumber = numbers);
-	}
+  setWinningOrBonusNumber(numbers) {
+    Array.isArray(numbers)
+      ? (this.#winningNumbers = numbers)
+      : (this.#bonusNumber = numbers);
+  }
 
-	checkResult() {
-		for (const lotto of this.#lottos) {
-			const rank = this.setRank(lotto);
-			if (!rank) continue;
-			this.#result[rank] += 1;
-			this.calculateEarningTotal(rank);
-		}
-		return [this.#result, this.#earningTotal, this.#inputTotal];
-	}
+  checkResult() {
+    for (const lotto of this.#lottos) {
+      const rank = this.setRank(lotto);
+      if (!rank) continue;
+      this.#result[rank] += 1;
+      this.calculateEarningTotal(rank);
+    }
+    return [this.#result, this.#earningTotal, this.#inputTotal];
+  }
 
-	setRank(lottoNumbers) {
-		const sameNumberCount = lottoNumbers.filter((lottoNumber) =>
-			this.#winningNumbers.includes(lottoNumber)
-		).length;
+  setRank(lottoNumbers) {
+    const { length: matchingNumberCount } = lottoNumbers.filter((lottoNumber) =>
+      this.#winningNumbers.includes(lottoNumber)
+    );
 
-		switch (sameNumberCount) {
-			case 6:
-				return 1;
-			case 5:
-				return lottoNumbers.includes(this.#bonusNumber) ? 2 : 3;
-			case 4:
-				return 4;
-			case 3:
-				return 5;
-			default:
-				return 0;
-		}
-	}
+    if (matchingNumberCount < 3) return 0;
+    if (matchingNumberCount === 5 && lottoNumbers.includes(this.#bonusNumber))
+      return lottoRank["5+1"];
+    return lottoRank[matchingNumberCount];
+  }
 
-	calculateEarningTotal(rank) {
-		this.#earningTotal += LOTTO.PRIZE_MONEY[rank];
-	}
+  calculateEarningTotal(rank) {
+    this.#earningTotal += LOTTO.PRIZE_MONEY[rank];
+  }
 
-	clearResult() {
-		this.#result = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
-	}
+  clearResult() {
+    this.#result = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+  }
 
-	initialize() {
-		this.#lottos;
-		this.#winningNumbers;
-		this.#bonusNumber;
-		this.#result = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
-		this.#inputTotal = 0;
-		this.#earningTotal = 0;
-	}
+  initialize() {
+    this.#lottos;
+    this.#winningNumbers;
+    this.#bonusNumber;
+    this.#result = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+    this.#inputTotal = 0;
+    this.#earningTotal = 0;
+  }
 }
+
+const lottoRank = {
+  6: 1,
+  "5+1": 2,
+  5: 3,
+  4: 4,
+  3: 5,
+};
 
 export default new Lotto();
