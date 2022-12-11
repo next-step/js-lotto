8f60dd59e5646213d29aef0c5eff2a1c7c57349d
박예정 <imgoing222@gmail.com>
2022-12-11 18:32:00 +0900

    refactor: 숫자 구분 기호 추가

--- src/js/constants/lotto.js
+++ src/js/constants/lotto.js
@@ -1,13 +1,13 @@
 export const LOTTO = {
-	MIN_NUMBER: 1,
-	MAX_NUMBER: 45,
-	TOTAL_NUMBER_COUNT: 6,
-	LOTTO_UNIT: 1000,
-	PRIZE_MONEY: {
-		1: 2000000000,
-		2: 30000000,
-		3: 1500000,
-		4: 50000,
-		5: 5000,
-	},
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 45,
+  TOTAL_NUMBER_COUNT: 6,
+  LOTTO_UNIT: 1000,
+  PRIZE_MONEY: {
+    1: 2_000_000_000,
+    2: 30_000_000,
+    3: 1_500_000,
+    4: 50_000,
+    5: 5_000,
+  },
 };
