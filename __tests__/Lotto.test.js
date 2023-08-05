7f41cd6c236f56899772606cde8cf870c7c77c15
손진영 <sm099@naver.com>
2023-08-05 23:48:12 +0900

    test(step2): 구매 금액 만큼의 로또 생성 기능 구현에 대한 테스팅

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,25 +1,67 @@
-import { DEFAULT_LIMIT_LOTTO_COUNT } from '../src/step1/constants/lotto.js';
+import {
+  DEFAULT_LIMIT_LOTTO_COUNT,
+  MAX_LOTTO_NUMBER,
+  MIN_LOTTO_NUMBER,
+} from '../src/step1/constants/lotto.js';
 import { LottoMerchant } from '../src/step1/model/index.js';
-import { isAllLottoValidRange } from './utils/index.js';
+import { isAllLottoValidRange, isSixNumberInLotto } from './utils/index.js';
 
 describe('로또 발행 기능 testing', () => {
-  test('로또 구입 금액을 입력 시 로또 1개를 발행한다.', () => {
+  const createLottos = (amount) => {
     const lottoMerchant = new LottoMerchant();
-    const lottos = lottoMerchant.sellLotto('9000');
+    return lottoMerchant.sellLotto(amount);
+  };
+  test('로또 구입 금액을 입력 시 로또 1개를 발행한다.', () => {
+    const lottos = createLottos(1000);
     expect(lottos).toHaveLength(1);
   });
 
-  test('구매한 로또 1개의 번호는 총 6개여야 한다.', () => {
-    const lottoMerchant = new LottoMerchant();
-    const lottos = lottoMerchant.sellLotto('5000');
-    expect(
-      lottos.every((lotto) => lotto.length === DEFAULT_LIMIT_LOTTO_COUNT),
-    ).toBeTruthy();
+  test(`구매한 로또 1개의 번호는 총 ${DEFAULT_LIMIT_LOTTO_COUNT}개여야 한다.`, () => {
+    const lottos = createLottos(1000);
+    expect(lottos.every((lotto) => isSixNumberInLotto(lotto))).toBeTruthy();
   });
 
-  test('구매한 로또 번호는 1 ~ 45까지의 범위를 가진다.', () => {
-    const lottoMerchant = new LottoMerchant();
-    const lottos = lottoMerchant.sellLotto('5000');
+  test(`구매한 로또 1개의 번호는 ${MIN_LOTTO_NUMBER} ~ ${MAX_LOTTO_NUMBER}까지의 범위를 가진다.`, () => {
+    const lottos = createLottos(1000);
     expect(lottos.every((lotto) => isAllLottoValidRange(lotto))).toBeTruthy();
   });
+
+  test.each([
+    [3000, 3],
+    [4000, 4],
+    [6000, 6],
+    [2000, 2],
+    [8000, 8],
+  ])('%i원치의 로또는 총 %i개가 생성되어야 한다.', (amount, numberOfLotto) => {
+    const lottos = createLottos(amount).length;
+    expect(lottos).toEqual(numberOfLotto);
+  });
+
+  test.each([
+    [3, 3000],
+    [4, 4000],
+    [6, 6000],
+    [2, 2000],
+    [8, 8000],
+  ])(
+    `%i개의 로또 번호는 모두 ${DEFAULT_LIMIT_LOTTO_COUNT}개여야 한다.`,
+    (_, amount) => {
+      const lottos = createLottos(amount);
+      expect(lottos.every((lotto) => isSixNumberInLotto(lotto))).toBeTruthy();
+    },
+  );
+
+  test.each([
+    [3, 3000],
+    [4, 4000],
+    [6, 6000],
+    [2, 2000],
+    [8, 8000],
+  ])(
+    `%i개의 로또 번호는 모두 ${MIN_LOTTO_NUMBER} ~ ${MAX_LOTTO_NUMBER}의 범위를 가진다.`,
+    (_, amount) => {
+      const lottos = createLottos(amount);
+      expect(lottos.every((lotto) => isAllLottoValidRange(lotto))).toBeTruthy();
+    },
+  );
 });
