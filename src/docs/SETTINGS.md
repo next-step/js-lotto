b6d0e76391f03202380e539a5f3b8dd24c17992c
손진영 <sm099@naver.com>
2023-08-05 20:08:06 +0900

    chore: 프로젝트 시작 전 사전 세팅
    prettier, eslint, package.json, jest 관련 사전 세팅 진행

---
+++ src/docs/SETTINGS.md
@@ -0,0 +1,30 @@
+# 준비물
+
+1. 노드 17 버전 ⬆ (node:readline/promises이 노드 17 버전 이상 부터 지원이 됩니다 🥲)
+2. yarn (1.22.19)
+
+```
+# 1번
+// nvm 버전 확인
+nvm --version
+
+// 특정 버전의 node install
+nvm install 18.17.0
+
+// 사용할 node version
+nvm use 18.17.0
+
+# 2번
+// yarn 버전 확인
+yarn -v
+
+// yarn 설치
+yarn install
+```
+
+# 시작
+
+```
+1. `yarn` 혹은 `yarn install`로 설치 한다.
+2. `yarn start-step1`으로 앱을 실행 한다.
+```
--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,17 @@
-const $showResultButton = document.querySelector('.open-result-modal-button')
-const $modalClose = document.querySelector('.modal-close')
-const $modal = document.querySelector('.modal')
-const $lottoNumbersToggleButton = document.querySelector(
-  '.lotto-numbers-toggle-button'
-)
+// const $showResultButton = document.querySelector('.open-result-modal-button');
+// const $modalClose = document.querySelector('.modal-close');
+// const $modal = document.querySelector('.modal');
+// const $lottoNumbersToggleButton = document.querySelector(
+//   '.lotto-numbers-toggle-button',
+// );
 
-const onModalShow = () => {
-  $modal.classList.add('open')
-}
+// const onModalShow = () => {
+//   $modal.classList.add('open');
+// };
 
-const onModalClose = () => {
-  $modal.classList.remove('open')
-}
+// const onModalClose = () => {
+//   $modal.classList.remove('open');
+// };
 
-$showResultButton.addEventListener('click', onModalShow)
-$modalClose.addEventListener('click', onModalClose)
+// $showResultButton.addEventListener('click', onModalShow);
+// $modalClose.addEventListener('click', onModalClose);
