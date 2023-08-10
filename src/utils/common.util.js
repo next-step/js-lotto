89237778d8d0b94def4deda1fca2d70bf22de0e8
ding-co <pangoons@naver.com>
2023-08-10 14:21:16 +0900

    update: Expand getRandomNumber util fn for excludedNumbers

--- src/utils/common.util.js
+++ src/utils/common.util.js
@@ -2,5 +2,15 @@ export const print = (str) => {
   console.log(str);
 };
 
-export const getRandomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1)) + min;
+export const getRandomNumber = (min, max, excludedNumbers = []) => {
+  let randomNumber = null;
+
+  while (!randomNumber) {
+    const candidate = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (excludedNumbers.indexOf(candidate) === -1) {
+      randomNumber = candidate;
+    }
+  }
+
+  return randomNumber;
+};
