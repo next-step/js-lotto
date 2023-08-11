65760cf3fae42daefc2b3ee564033c5fd1eb79df
cobocho <rlaalsrb1111@naver.com>
2023-08-11 16:10:33 +0900

    Test: 테스트코드 리팩토링

--- __tests__/lotto.test.js
+++ __tests__/lotto.test.js
@@ -1,41 +1,44 @@
+import { LOTTO_PRIZE_TABLE } from '../src/js/constants/lotto-config.js';
 import Exchange from '../src/js/domain/Exchange.js';
 import Lotto from '../src/js/domain/Lotto.js';
 
+const DEFAULT_LOTTO_NUMBERS = [1, 2, 3, 4, 5, 6];
+
 describe('로또 테스트', () => {
   it('로또는 여섯개의 숫자로 이루어진 배열을 매개변수로 받으며 그 값을 필드에 소유한다.', () => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+    const lotto = new Lotto(DEFAULT_LOTTO_NUMBERS);
 
     expect(lotto.numbers.length).toBe(6);
   });
 
   it.each([
-    [[1, 2, 3, 4, 5, 6], 6],
+    [DEFAULT_LOTTO_NUMBERS, 6],
     [[1, 2, 3, 4, 5, 9], 5],
     [[1, 2, 3, 4, 9, 9], 4],
     [[1, 2, 3, 9, 9, 9], 3],
     [[1, 2, 9, 9, 9, 9], 2],
     [[1, 9, 9, 9, 9, 9], 1],
     [[9, 9, 9, 9, 9, 9], 0],
-  ])('로또는 입력받은 배열과 비교하여 몇개가 동일한지 알 수 있다.', (...input) => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+  ])('로또는 입력받은 배열과 비교하여 몇개가 동일한지 알 수 있다.', (winningNumbers, correctCount) => {
+    const lotto = new Lotto(DEFAULT_LOTTO_NUMBERS);
 
-    expect(lotto.compare([...input][0])).toBe([...input][1]);
+    expect(lotto.compare(winningNumbers)).toBe(correctCount);
   });
 
-  it.each([1, 2, 3, 4, 5, 6])('로또는 보너스 번호가 자신의 번호에 포함되는지 알 수 있다.', (input) => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+  it.each(DEFAULT_LOTTO_NUMBERS)('로또는 보너스 번호가 자신의 번호에 포함되는지 알 수 있다.', (bonus) => {
+    const lotto = new Lotto(DEFAULT_LOTTO_NUMBERS);
 
-    expect(lotto.hasBonus(input)).toBeTruthy();
+    expect(lotto.hasBonus(bonus)).toBeTruthy();
   });
 
-  it.each([7, 8, 9])('로또는 보너스 번호가 자신의 번호에 포함되지 않는지 알 수 있다.', (input) => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+  it.each([7, 8, 9])('로또는 보너스 번호가 자신의 번호에 포함되지 않는지 알 수 있다.', (bonus) => {
+    const lotto = new Lotto(DEFAULT_LOTTO_NUMBERS);
 
-    expect(lotto.hasBonus(input)).toBeFalsy();
+    expect(lotto.hasBonus(bonus)).toBeFalsy();
   });
 
   it.each([
-    [[1, 2, 3, 4, 5, 6], 6, 1],
+    [DEFAULT_LOTTO_NUMBERS, 6, 1],
     [[1, 2, 3, 4, 5, 9], 6, 2],
     [[1, 2, 3, 4, 5, 9], 7, 3],
     [[1, 2, 3, 4, 9, 9], 6, 4],
@@ -43,11 +46,8 @@ describe('로또 테스트', () => {
     [[1, 2, 9, 9, 9, 9], 6, 6],
     [[1, 9, 9, 9, 9, 9], 6, 7],
     [[9, 9, 9, 9, 9, 9], 6, 8],
-  ])('로또는 당첨번호와 보너스 번호를 입력받으면 자신의 등수를 알 수 있다.', (...input) => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
-    const winningNumbers = [...input][0];
-    const bonus = [...input][1];
-    const rank = [...input][2];
+  ])('로또는 당첨번호와 보너스 번호를 입력받으면 자신의 등수를 알 수 있다.', (winningNumbers, bonus, rank) => {
+    const lotto = new Lotto(DEFAULT_LOTTO_NUMBERS);
 
     lotto.setRank(winningNumbers, bonus);
 
@@ -55,19 +55,16 @@ describe('로또 테스트', () => {
   });
 
   it.each([
-    [[1, 2, 3, 4, 5, 6], 6, 2_000_000_000],
-    [[1, 2, 3, 4, 5, 9], 6, 30_000_000],
-    [[1, 2, 3, 4, 5, 9], 7, 1_500_000],
-    [[1, 2, 3, 4, 9, 9], 6, 50_000],
-    [[1, 2, 3, 9, 9, 9], 6, 5_000],
+    [DEFAULT_LOTTO_NUMBERS, 6, LOTTO_PRIZE_TABLE['1']],
+    [[1, 2, 3, 4, 5, 9], 6, LOTTO_PRIZE_TABLE['2']],
+    [[1, 2, 3, 4, 5, 9], 7, LOTTO_PRIZE_TABLE['3']],
+    [[1, 2, 3, 4, 9, 9], 6, LOTTO_PRIZE_TABLE['4']],
+    [[1, 2, 3, 9, 9, 9], 6, LOTTO_PRIZE_TABLE['5']],
     [[1, 2, 9, 9, 9, 9], 6, 0],
     [[1, 9, 9, 9, 9, 9], 6, 0],
     [[9, 9, 9, 9, 9, 9], 6, 0],
-  ])('거래소에 로또를 제출할 시 등수에 맞는 당첨금을 받는다', (...input) => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
-    const winningNumbers = [...input][0];
-    const bonus = [...input][1];
-    const prize = [...input][2];
+  ])('거래소에 로또를 제출할 시 등수에 맞는 당첨금을 받는다', (winningNumbers, bonus, prize) => {
+    const lotto = new Lotto(DEFAULT_LOTTO_NUMBERS);
 
     lotto.setRank(winningNumbers, bonus);
 
