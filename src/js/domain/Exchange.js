8590781807c1c1e44a6a627d6c828dd864c08953
cobocho <rlaalsrb1111@naver.com>
2023-08-11 23:13:22 +0900

    Feat: 수익률 계산 기능 구현

--- src/js/domain/Exchange.js
+++ src/js/domain/Exchange.js
@@ -4,6 +4,10 @@ class Exchange {
   getLottoPrize({ rank }) {
     return LOTTO_PRIZE_TABLE[rank] ? LOTTO_PRIZE_TABLE[rank] : 0;
   }
+
+  static calculateRateOfReturn(investment, proceeds) {
+    return (proceeds / investment) * 100;
+  }
 }
 
 export default Exchange;
