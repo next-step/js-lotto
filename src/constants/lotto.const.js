677bf002c2e0f54ba97d5aaf182fbde459bc05aa
ding-co <pangoons@naver.com>
2023-08-11 12:00:30 +0900

    refactor: Constants for PROMPT_PREFIX_SYMBOL (>)

--- src/constants/lotto.const.js
+++ src/constants/lotto.const.js
@@ -1,7 +1,9 @@
-export const QUESTION_PURCHASE_AMOUNT = '> 구입금액을 입력해 주세요.';
-export const QUESTION_LOTTO_ANSWER = '> 당첨 번호를 입력해 주세요. ';
-export const QUESTION_LOTTO_BONUS = '> 보너스 번호를 입력해 주세요. ';
-export const QUESTION_RESTART = '> 다시 시작하시겠습니까? (y/n) ';
+import { PROMPT_PREFIX_SYMBOL } from './prompt.const.js';
+
+export const QUESTION_PURCHASE_AMOUNT = `${PROMPT_PREFIX_SYMBOL} 구입금액을 입력해 주세요.`;
+export const QUESTION_LOTTO_ANSWER = `${PROMPT_PREFIX_SYMBOL} 당첨 번호를 입력해 주세요. `;
+export const QUESTION_LOTTO_BONUS = `${PROMPT_PREFIX_SYMBOL} 보너스 번호를 입력해 주세요. `;
+export const QUESTION_RESTART = `${PROMPT_PREFIX_SYMBOL} 다시 시작하시겠습니까? (y/n) `;
 
 export const LOTTO_AMOUNT_UNIT = 1_000;
 
---
+++ src/constants/prompt.const.js
@@ -0,0 +1,1 @@
+export const PROMPT_PREFIX_SYMBOL = '>';
