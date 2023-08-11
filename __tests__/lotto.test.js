278d7380a8286f2caa0e0208ab1c9523be533fb0
cobocho <rlaalsrb1111@naver.com>
2023-08-11 14:59:47 +0900

    Feat: `Lotto` 클래스 작성

--- __tests__/lotto.test.js
+++ __tests__/lotto.test.js
@@ -1,3 +1,5 @@
+import Lotto from '../src/js/domain/Lotto.js';
+
 describe('로또 테스트', () => {
   it('로또는 여섯개의 숫자로 이루어진 배열을 매개변수로 받으며 그 값을 필드에 소유한다.', () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
