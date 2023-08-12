8f127943b464c53f87641238f56fb03b46cb9eef
손진영 <sm099@naver.com>
2023-08-12 15:33:35 +0900

    feat(step6): 예외 처리를 위한 에러 메시지 추가
    1. lotto 도메인과 관련된 상수 추가
    
    2. 에러 메시지 관련 상수 추가

--- src/step1/constants/lotto.js
+++ src/step1/constants/lotto.js
@@ -3,3 +3,5 @@ export const MIN_LOTTO_NUMBER = 1;
 export const DEFAULT_LIMIT_LOTTO_COUNT = 6;
 export const EXIT_GENERATE_LOTTO_COUNT = 0;
 export const PRICE_PER_LOTTO = 1000;
+export const MAX_WINNING_COUNT = 6;
+export const MIN_WINNING_COUNT = 0;
--- src/step1/constants/message.js
+++ src/step1/constants/message.js
@@ -1,3 +1,12 @@
+import {
+  DEFAULT_LIMIT_LOTTO_COUNT,
+  MAX_LOTTO_NUMBER,
+  MAX_WINNING_COUNT,
+  MIN_LOTTO_NUMBER,
+  MIN_WINNING_COUNT,
+  PRICE_PER_LOTTO,
+} from './lotto.js';
+
 export const INPUT_MESSAGE = Object.freeze({
   BUY_AMOUNT: '> 구입금액을 입력해 주세요.',
   BONUS_NUMBER: '\n> 보너스 번호를 입력해 주세요. ',
@@ -18,4 +27,18 @@ export const OUTPUT_MESSAGE = Object.freeze({
     Object.entries(result)
       .map((element) => `${element.join(' - ')}개`)
       .join('\n'),
+});
+
+export const ERROR_MESSAGE = Object.freeze({
+  TYPE_OF_NUMBER: '숫자만 입력해주세요.',
+  GREATER_THEN_PRICE_PER_LOTTO: `${PRICE_PER_LOTTO}원 이상 입력해주세요.`,
+  NO_CHANGES: `현재 앱에서는 잔돈이 존재할 수 없습니다. ${PRICE_PER_LOTTO}원 단위로 입력해주세요.`,
+  RESET_WITH_ERROR: (error) => `${error.message} 게임을 다시 시작합니다.`,
+  DUPLICATE_LOTTO_NUMBERS: '중복되는 로또 번호가 존재합니다.',
+  NOT_DEFAULT_LIMIT_LOTTO_COUNT: `로또 번호가 ${DEFAULT_LIMIT_LOTTO_COUNT}개가 아닙니다.`,
+  INVALID_LOTTO_NUMBER_RANGE: `로또 번호가 ${MIN_LOTTO_NUMBER}에서 ${MAX_LOTTO_NUMBER}사이의 숫자가 아닙니다.`,
+  INVALID_BONUS_NUMBER_RANGE: `보너스 번호가 ${MIN_LOTTO_NUMBER}에서 ${MAX_LOTTO_NUMBER}사이의 숫자가 아닙니다.`,
+  DUPLICATE_BONUS_NUMBER: '보너스 번호가 당첨 번호와 중복됩니다.',
+  INVALID_AMOUNT: '유효하지 않은 금액 입니다.',
+  INVALID_WINNING_COUNT: `로또의 정답 갯수는 ${MIN_WINNING_COUNT}~${MAX_WINNING_COUNT}개 사이 입니다.`,
 });
