de4f62c97054bd9843ea955c4b1e7fe7c91c65bc
gusrb3164 <gusrb3164@gmail.com>
2023-08-12 20:42:48 +0900

    feat: 로또 번호와 당첨 번호와 보너스 번호를 비교하여 금액을 구한다.

---
+++ src/domain/LottoPrize.js
@@ -0,0 +1,38 @@
+export class LottoPrize {
+  #winningNumbers;
+  #bonusNumber;
+
+  constructor({winningNumbers, bonusNumber}) {
+    this.#winningNumbers = winningNumbers;
+    this.#bonusNumber = bonusNumber;
+  }
+
+  #getMatchedCount(lottery) {
+    return lottery.filter(number => this.#winningNumbers.includes(number)).length;
+  }
+  #isBonusMatched(lottery) {
+    return lottery.includes(this.#bonusNumber);
+  }
+
+  getLottoPrize(lottery) {
+    const matchedCount = this.#getMatchedCount(lottery);
+    const isBonusMatched = this.#isBonusMatched(lottery);
+
+    if (matchedCount === 6) {
+      return 'FIRST';
+    }
+    if (matchedCount === 5 && isBonusMatched) {
+      return 'SECOND';
+    }
+    if (matchedCount === 5) {
+      return 'THIRD';
+    }
+    if (matchedCount === 4) {
+      return 'FOURTH';
+    }
+    if (matchedCount === 3) {
+      return 'FIFTH';
+    }
+    return 'LOSS';
+  }
+}
---
+++ src/domain/constants/prizeMap.js
@@ -0,0 +1,8 @@
+export const PRIZE_MAP = {
+  FIRST: 2000000000,
+  SECOND: 30000000,
+  THIRD: 1500000,
+  FOURTH: 50000,
+  FIFTH: 5000,
+  LOSS: 0,
+};
