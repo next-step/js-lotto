75f112c95a833f1177a62fe82af415a6ddb4345e
손진영 <sm099@naver.com>
2023-08-12 15:51:58 +0900

    feat(step6): LottoCalculator에 대한 예외 처리 구현

--- src/step1/model/LottoCalculator.js
+++ src/step1/model/LottoCalculator.js
@@ -1,11 +1,22 @@
+import { ERROR_MESSAGE } from '../constants/message.js';
+import LottoValidator from '../utils/validate/validator/LottoValidator.js';
+
 export default class LottoCalculator {
   #winningAmount;
 
   #investmentAmount;
 
   constructor(winningAmount, investmentAmount) {
+    this.#validate(winningAmount, investmentAmount);
     this.#winningAmount = winningAmount;
     this.#investmentAmount = investmentAmount;
+  }
+
+  #validate(winningAmount, investmentAmount) {
+    LottoValidator.validateTypeOfNumbers([winningAmount, investmentAmount]);
+    if (investmentAmount === 0) {
+      throw TypeError(ERROR_MESSAGE.INVALID_AMOUNT);
+    }
   }
 
   static #convertToRateOfReturn(rateOfReturn) {
