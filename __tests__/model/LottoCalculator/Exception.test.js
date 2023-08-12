41928217f7a723f6d6382dadf97b49254e32e34c
손진영 <sm099@naver.com>
2023-08-12 15:52:16 +0900

    test(step6): LottoCalculator에 대한 테스팅 구현
    1. 예외처리 테스팅 구현
    
    2. 기능 관련 테스팅 구현

---
+++ __tests__/model/LottoCalculator/Exception.test.js
@@ -0,0 +1,53 @@
+import { ERROR_MESSAGE } from '../../../src/step1/constants/message';
+import { LottoError } from '../../../src/step1/errors';
+import { LottoCalculator } from '../../../src/step1/model';
+
+describe('LottoCalculator 예외 관련 테스트', () => {
+  test.each([
+    {
+      winningAmount: 'string',
+      investmentAmount: 200,
+      expectedErrorType: LottoError,
+      expectedErrorMessage: ERROR_MESSAGE.TYPE_OF_NUMBER,
+    },
+    {
+      winningAmount: undefined,
+      investmentAmount: 200,
+      expectedErrorType: LottoError,
+      expectedErrorMessage: ERROR_MESSAGE.TYPE_OF_NUMBER,
+    },
+    {
+      winningAmount: 100,
+      investmentAmount: 'string',
+      expectedErrorType: LottoError,
+      expectedErrorMessage: ERROR_MESSAGE.TYPE_OF_NUMBER,
+    },
+    {
+      winningAmount: 100,
+      investmentAmount: null,
+      expectedErrorType: LottoError,
+      expectedErrorMessage: ERROR_MESSAGE.TYPE_OF_NUMBER,
+    },
+    {
+      winningAmount: 100,
+      investmentAmount: undefined,
+      expectedErrorType: LottoError,
+      expectedErrorMessage: ERROR_MESSAGE.TYPE_OF_NUMBER,
+    },
+    {
+      winningAmount: 100,
+      investmentAmount: 0,
+      expectedErrorType: TypeError,
+      expectedErrorMessage: ERROR_MESSAGE.INVALID_AMOUNT,
+    },
+  ])(
+    '당첨 금액이 $winningAmount의 형태이고 투자 금액이 $investmentAmount의 형태일때, 에러가 발생한다.',
+    ({ winningAmount, investmentAmount, expectedErrorType, expectedErrorMessage }) => {
+      // given - when
+      const createLottoCalculator = () => new LottoCalculator(winningAmount, investmentAmount);
+      // then
+      expect(() => createLottoCalculator()).toThrow(expectedErrorType);
+      expect(() => createLottoCalculator()).toThrow(expectedErrorMessage);
+    },
+  );
+});
---
+++ __tests__/model/LottoCalculator/Feature.test.js
@@ -0,0 +1,49 @@
+import { LottoCalculator } from '../../../src/step1/model';
+
+describe('LottoCalculator 기능 관련 테스트', () => {
+  test.each([
+    {
+      input: {
+        winningAmount: 2_031_555_000,
+        investmentAmount: 5_000,
+      },
+      output: {
+        rateOfReturn: '40631100%',
+      },
+    },
+    {
+      input: {
+        winningAmount: 5_000,
+        investmentAmount: 5_000,
+      },
+      output: {
+        rateOfReturn: '100%',
+      },
+    },
+    {
+      input: {
+        winningAmount: 5_000,
+        investmentAmount: 8_000,
+      },
+      output: {
+        rateOfReturn: '62.5%',
+      },
+    },
+    {
+      input: {
+        winningAmount: 60_000,
+        investmentAmount: 7_000,
+      },
+      output: {
+        rateOfReturn: '857.1%',
+      },
+    },
+  ])(
+    '투자 금액이 $input.investmentAmount원이고, 당첨 금액이 $input.winningAmount일 때 수익율은 $output.rateOfReturn이다.',
+    ({ input: { investmentAmount, winningAmount }, output }) => {
+      const lottoCalculator = LottoCalculator.fromLottoCalculator(winningAmount, investmentAmount);
+      const rateOfReturn = lottoCalculator.calculateRateOfReturn();
+      expect(rateOfReturn).toStrictEqual(output.rateOfReturn);
+    },
+  );
+});
