8260052598054089cb01bea42d563c8cd4d9634b
손진영 <sm099@naver.com>
2023-08-12 15:41:28 +0900

    feat(step6): Lotto에 대한 예외 처리 구현

--- src/step1/model/Lotto.js
+++ src/step1/model/Lotto.js
@@ -1,11 +1,20 @@
 import { DEFAULT_LIMIT_LOTTO_COUNT, MAX_LOTTO_NUMBER, MIN_LOTTO_NUMBER } from '../constants/lotto.js';
 import { RandomNumberGenerator } from '../utils/generator/index.js';
+import LottoValidator from '../utils/validate/validator/LottoValidator.js';
 
 export default class Lotto {
   #lottoNumbers;
 
   constructor(lottoNumbers) {
+    this.#validate(lottoNumbers);
     this.#lottoNumbers = lottoNumbers;
+  }
+
+  #validate(lottoNumbers) {
+    LottoValidator.validateTypeOfNumbers(lottoNumbers);
+    LottoValidator.validateDefaultLottoCount(lottoNumbers);
+    LottoValidator.validateDuplicateLottoNumbers(lottoNumbers);
+    LottoValidator.validateLottoNumbersInRange(lottoNumbers);
   }
 
   static fromLottoByRandomNumber(
