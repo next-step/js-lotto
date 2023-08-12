a20ba036b8e73d07a50db7d4db0671728dee0445
sangwon <sangwon@sangwonui-MacBookPro.local>
2023-08-13 02:59:41 +0900

    FEAT: 로또번호 생성 테스트 코드 작성

---
+++ src/js/LottoMachine.test.js
@@ -0,0 +1,24 @@
+import { LottoMachine } from './LottoMachine.js';
+
+describe('LottoMachine', () => {
+  describe('로또 번호 생성', () => {
+    it('각 로또 번호는 45 이하의 자연수이다.', () => {
+      const lottoMachine = new LottoMachine();
+
+      const lottoNumbers = lottoMachine.generateLottoNumbers();
+
+      lottoNumbers.forEach(number => {
+        expect(number).toBeGreaterThanOrEqual(1);
+        expect(number).toBeLessThanOrEqual(45);
+      });
+    });
+
+    it('생성된 로또 번호는 중복되지않는다.', () => {
+      const lottoMachine = new LottoMachine();
+
+      const lottoNumbers = lottoMachine.generateLottoNumbers();
+
+      expect(lottoNumbers.length).toBe([...new Set(lottoNumbers)].length);
+    });
+  });
+});
