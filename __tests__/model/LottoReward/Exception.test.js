c97ad96adb3d166ddfbf399767d5aa2d2ccfaf35
손진영 <sm099@naver.com>
2023-08-14 00:19:46 +0900

    fix(test): LottoReward 예외 테스트 내 테스트 케이스 수정
    lottoResults 객체가 아닌 lottoMatchingInfo로 변경

--- __tests__/model/LottoReward/Exception.test.js
+++ __tests__/model/LottoReward/Exception.test.js
@@ -5,43 +5,43 @@ import { LottoReward } from '../../../src/step1/model';
 describe('LottoReward 예외 관련 테스트', () => {
   test.each([
     {
-      lottoResults: [
-        [-1, false],
-        [5, true],
-        [3, false],
-        [6, true],
-        [4, false],
+      lottoMatchingInfo: [
+        { winningCount: -1, hasBonusNumber: false },
+        { winningCount: 5, hasBonusNumber: false },
+        { winningCount: 3, hasBonusNumber: false },
+        { winningCount: 6, hasBonusNumber: true },
+        { winningCount: 4, hasBonusNumber: false },
       ],
       expectedErrorType: LottoError,
       expectedErrorMessage: ERROR_MESSAGE.INVALID_WINNING_COUNT,
     },
     {
-      lottoResults: [
-        [7, true],
-        [2, true],
-        [3, false],
-        [5, true],
-        [6, false],
+      lottoMatchingInfo: [
+        { winningCount: 7, hasBonusNumber: false },
+        { winningCount: 2, hasBonusNumber: false },
+        { winningCount: 3, hasBonusNumber: false },
+        { winningCount: 5, hasBonusNumber: true },
+        { winningCount: 6, hasBonusNumber: false },
       ],
       expectedErrorType: LottoError,
       expectedErrorMessage: ERROR_MESSAGE.INVALID_WINNING_COUNT,
     },
     {
-      lottoResults: [
-        ['five', true],
-        [4, false],
-        [2, false],
-        [6, true],
-        [5, true],
+      lottoMatchingInfo: [
+        { winningCount: 'five', hasBonusNumber: false },
+        { winningCount: 2, hasBonusNumber: false },
+        { winningCount: 3, hasBonusNumber: false },
+        { winningCount: 5, hasBonusNumber: true },
+        { winningCount: 6, hasBonusNumber: false },
       ],
       expectedErrorType: LottoError,
       expectedErrorMessage: ERROR_MESSAGE.INVALID_WINNING_COUNT,
     },
   ])(
     'TestCase %#번에서 lottoResults 내 winningCount가 문제가 있기 때문에 에러가 발생한다.',
-    ({ lottoResults, expectedErrorType, expectedErrorMessage }) => {
+    ({ lottoMatchingInfo, expectedErrorType, expectedErrorMessage }) => {
       // given - when
-      const createLottoReward = () => LottoReward.from(lottoResults);
+      const createLottoReward = () => LottoReward.from(lottoMatchingInfo);
       // then
       expect(() => createLottoReward()).toThrow(expectedErrorType);
       expect(() => createLottoReward()).toThrow(expectedErrorMessage);
