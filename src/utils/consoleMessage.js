ea652ccce451b868ad0872e8c8dfe6afbb939d18
bxxmi <bmi6638@gmail.com>
2023-08-13 17:46:52 +0900

    [fix] 구매한 로또 갯수 메세지 함수 return 추가

---
+++ src/utils/consoleMessage.js
@@ -0,0 +1,9 @@
+export const inputLottoPriceMessage = "구입 금액을 입력해 주세요.\n";
+
+export const showBuyedLottoCountMessage = (lottoCount) => {
+  return `${lottoCount}개를 구매했습니다.`;
+};
+
+export const inputLottoNumberMessage = "당첨 번호를 입력해 주세요.";
+
+export const inputBonusNumberMessage = "보너스 번호를 입력해 주세요.";
