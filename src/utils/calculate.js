690d5c59b88f9f036121a52dd55d77906a0ce396
bxxmi <bmi6638@gmail.com>
2023-08-13 18:26:11 +0900

    [feat] 랜덤 번호 생성 로직 추가

--- src/utils/calculate.js
+++ src/utils/calculate.js
@@ -4,6 +4,7 @@ export const calcLottoCount = (price) => {
   return price / SINGLE_LOTTO_PRICE;
 };
 
+<<<<<<< HEAD
 export const createRandomLottoNumber = () => {
   return Math.floor(Math.random() * 45) + 1;
 };
@@ -11,3 +12,9 @@ export const createRandomLottoNumber = () => {
 export const priceWithCommas = (price) => {
   return price.toLocaleString();
 };
+=======
+// 1부터 45까지의 랜덤 숫자 생성
+export const createRandomLottoNumber = () => {
+  return Math.floor(Math.random() * 45) + 1;
+};
+>>>>>>> b3ae1c8 ([feat] 랜덤 번호 생성 로직 추가)
