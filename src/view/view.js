bbef9b964ba6f1a76a9c648d75fd7eb85f1dfd42
bxxmi <bmi6638@gmail.com>
2023-08-13 18:25:30 +0900

    [feat] console 출력을 위한 view 생성

---
+++ src/view/view.js
@@ -0,0 +1,7 @@
+export const displayAvailableLottoCount = (lottoCount) => {
+  return console.log(lottoCount);
+};
+
+export const displayLottoNumbers = (lottoNumbers) => {
+  lottoNumbers.forEach((lotto) => console.log(lotto));
+};
