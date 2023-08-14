201b648aa639100c28c511301f2347f13cfd36cb
cobocho <rlaalsrb1111@naver.com>
2023-08-14 20:31:11 +0900

    Test: 2,3,4,5,낙첨 테스트 작성

--- __tests__/LottoChecker.test.js
+++ __tests__/LottoChecker.test.js
@@ -21,4 +21,93 @@ describe('로또 등수 확인 테스트', () => {
       [LOTTO_REWARD_CODE.FIFTH]: 0,
     });
   });
+
+  it('2등 테스트.', () => {
+    const lottoChecker = new LottoChecker();
+
+    const lotto = new Lotto(DEFAULT_LOTTO_NUMBERS);
+    lotto.check([1, 2, 3, 4, 5, 8], 6);
+
+    const lottoRewardBoard = lottoChecker.getLottoRewardBoard([lotto]);
+
+    expect(lottoRewardBoard).toEqual({
+      [LOTTO_REWARD_CODE.FIRST]: 0,
+      [LOTTO_REWARD_CODE.SECOND]: 1,
+      [LOTTO_REWARD_CODE.THIRD]: 0,
+      [LOTTO_REWARD_CODE.FOURTH]: 0,
+      [LOTTO_REWARD_CODE.FIFTH]: 0,
+    });
+  });
+
+  it('3등 테스트.', () => {
+    const lottoChecker = new LottoChecker();
+
+    const lotto = new Lotto(DEFAULT_LOTTO_NUMBERS);
+    lotto.check([1, 2, 3, 4, 5, 0], 0);
+
+    const lottoRewardBoard = lottoChecker.getLottoRewardBoard([lotto]);
+
+    expect(lottoRewardBoard).toEqual({
+      [LOTTO_REWARD_CODE.FIRST]: 0,
+      [LOTTO_REWARD_CODE.SECOND]: 0,
+      [LOTTO_REWARD_CODE.THIRD]: 1,
+      [LOTTO_REWARD_CODE.FOURTH]: 0,
+      [LOTTO_REWARD_CODE.FIFTH]: 0,
+    });
+  });
+
+  it('4등 테스트.', () => {
+    const lottoChecker = new LottoChecker();
+
+    const lotto = new Lotto(DEFAULT_LOTTO_NUMBERS);
+    lotto.check([1, 2, 3, 4, 0, 0], 0);
+
+    const lottoRewardBoard = lottoChecker.getLottoRewardBoard([lotto]);
+
+    expect(lottoRewardBoard).toEqual({
+      [LOTTO_REWARD_CODE.FIRST]: 0,
+      [LOTTO_REWARD_CODE.SECOND]: 0,
+      [LOTTO_REWARD_CODE.THIRD]: 0,
+      [LOTTO_REWARD_CODE.FOURTH]: 1,
+      [LOTTO_REWARD_CODE.FIFTH]: 0,
+    });
+  });
+
+  it('5등 테스트.', () => {
+    const lottoChecker = new LottoChecker();
+
+    const lotto = new Lotto(DEFAULT_LOTTO_NUMBERS);
+    lotto.check([1, 2, 3, 0, 0, 0], 0);
+
+    const lottoRewardBoard = lottoChecker.getLottoRewardBoard([lotto]);
+
+    expect(lottoRewardBoard).toEqual({
+      [LOTTO_REWARD_CODE.FIRST]: 0,
+      [LOTTO_REWARD_CODE.SECOND]: 0,
+      [LOTTO_REWARD_CODE.THIRD]: 0,
+      [LOTTO_REWARD_CODE.FOURTH]: 0,
+      [LOTTO_REWARD_CODE.FIFTH]: 1,
+    });
+  });
+
+  it.each([
+    [1, 2, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+  ])('낙첨 테스트.', (...winningNumber) => {
+    const lottoChecker = new LottoChecker();
+
+    const lotto = new Lotto(DEFAULT_LOTTO_NUMBERS);
+    lotto.check(winningNumber, 0);
+
+    const lottoRewardBoard = lottoChecker.getLottoRewardBoard([lotto]);
+
+    expect(lottoRewardBoard).toEqual({
+      [LOTTO_REWARD_CODE.FIRST]: 0,
+      [LOTTO_REWARD_CODE.SECOND]: 0,
+      [LOTTO_REWARD_CODE.THIRD]: 0,
+      [LOTTO_REWARD_CODE.FOURTH]: 0,
+      [LOTTO_REWARD_CODE.FIFTH]: 0,
+    });
+  });
 });
