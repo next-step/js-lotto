bfb250596770c00e4e38e248d93a47eb790474cd
froggy1014 <clcl6084@gmail.com>
2023-08-15 02:48:20 +0900

    feat: seperate responsibility models

--- src/domain/model/LottoMachine.js
+++ src/domain/model/LottoMachine.js
@@ -1,56 +1,20 @@
-import { LottoView } from "../view/LottoView.js";
+import { LottoView } from '../view/LottoView.js';
 
 export class LottoMachine {
-  lottos = [];
-  results
-  view;
-
-
   constructor(count) {
-    this.view = LottoView
-    this.results = Array.from({ length: 5 }).fill(0);
-    this.#insertMoney(count);
+    this.view = LottoView;
+    this.lottos = Array.from({ length: count }, () => this.issueLotto());
   }
 
-  #insertMoney (count) {
-    this.lottos = Array.from({length: count}).map((_) => this.#issueLotto());
-  }
-
-  #issueLotto () {
+  issueLotto() {
     let lottoNumbers = [];
     while (lottoNumbers.length < 6) {
-    let number = Math.floor(Math.random() * 45) + 1;
+      let number = Math.floor(Math.random() * 45) + 1;
       if (!lottoNumbers.includes(number)) {
         lottoNumbers.push(number);
       }
     }
-    this.view.LOTTO_NUMBERS(lottoNumbers)
+    this.view.LOTTO_NUMBERS(lottoNumbers);
     return lottoNumbers;
-  }
-
-  recordResult (count) {
-    switch (count) {
-      case 3:
-        this.machine.results[0]++;
-        break;
-      case 4:
-        this.machine.results[1]++;
-        break;
-      case 5:
-        this.machine.results[2]++;
-        break;
-      case 6:
-        this.machine.results[3]++;
-        break;
-      case 7:
-        this.machine.results[4]++;
-        break;
-      default:
-        break;
-    }
-  }
-
-  get lottos () {
-    return this.lottos;
   }
 }
