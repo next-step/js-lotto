6c50b25641c13294673bc951ffd236f3f0431a9e
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-08-15 16:34:43 +0900

    feat: 로또 구입 금액을 입력하면 구입 금액에 해당하는 만큼의 로또를 발행한다

--- src/Controller/GameController.js
+++ src/Controller/GameController.js
@@ -1,15 +1,20 @@
+import { Store } from '../Model';
+import { PRODUCTS, PRODUCTS_NAME } from '../constants';
+
 export class GameController {
   #view;
+  #store;
 
   constructor(view) {
     this.#view = view;
+    this.#store = new Store(PRODUCTS);
   }
 
-  configStore() {
-    this.#visitStore();
-  }
-
-  async #visitStore() {
+  async buyTickets() {
     const purchaseAmount = await this.#view.readPurchaseAmount();
+    const tickets = this.#store.sell(
+      PRODUCTS_NAME.LOTTO_TICKET,
+      purchaseAmount
+    );
   }
 }
--- src/constants/products.js
+++ src/constants/products.js
@@ -1,5 +1,9 @@
 import { LottoTicket } from '../Model';
 
+export const PRODUCTS_NAME = Object.freeze({
+  LOTTO_TICKET: 'LottoTicket',
+});
+
 export const PRODUCTS = Object.freeze({
   LottoTicket: {
     product: LottoTicket,
