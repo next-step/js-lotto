b8d3c8bb01b58ba1f6cd5209000a1bd52d54d2db
ericagong <48196721+ericagong@users.noreply.github.com>
2023-08-15 20:40:09 +0900

    refactor: 테스트 코드 beforeEach로 중복 제거

--- __tests__/LotteryMachine.test.js
+++ __tests__/LotteryMachine.test.js
@@ -1,23 +1,23 @@
 import LotteryMachine from "../src/js/LotteryMachine";
 
 describe("로또 발행 테스트", () => {
+  let lotto;
+  beforeEach(() => {
+    lotto = LotteryMachine.issueLotto();
+  });
   it("6개의 숫자를 뽑는다.", () => {
-    const lotto = LotteryMachine.issueLotto();
     expect(lotto.length).toBe(6);
   });
 
   it("로또 6개 숫자는 중복이 없다.", () => {
-    const lotto = LotteryMachine.issueLotto();
     expect(new Set(lotto).size).toBe(6);
   });
 
   it("로또 6개 숫자는 [1, 45] 사이의 범위에 존재한다.", () => {
-    const lotto = LotteryMachine.issueLotto();
     lotto.forEach((num) => expect(num >= 1 && num <= 45).toBe(true));
   });
 
-  it("로또는 6개 숫자 배열 형태로 반환된다.", () => {
-    const lotto = LotteryMachine.issueLotto();
+  it("로또는 6개 숫자 배열로 반환된다.", () => {
     expect(lotto).toBeInstanceOf(Array);
     lotto.forEach((num) => expect(typeof num).toBe("number"));
   });
