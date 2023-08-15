5272366056dddfc152375d14dfc053a0c983548a
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-08-15 19:17:48 +0900

    reafactor: store 내의 하드코딩 된 값 제거

--- src/constants/message.js
+++ src/constants/message.js
@@ -1,6 +1,8 @@
 const ERROR = Object.freeze({
   EMPTY_STRING: '빈 공백은 입력이 불가능합니다.',
   IS_NOT_POSITIVE_NUMBER: '0 이상의 정수만 입력이 가능합니다.',
+  PRODUCT_NOT_FOUND: '제품을 찾을 수 없습니다.',
+  INSUFFICIENT_PURCHASE_AMOUNT: '구입 금액보다 상품의 가격이 높습니다.',
 });
 
 const READ = Object.freeze({
--- src/utils/Validator/Store.js
+++ src/utils/Validator/Store.js
@@ -1,11 +1,14 @@
+import { MESSAGE } from '../../constants';
+
 export const Store = {
   /* Buy Product */
   validateProductName(products, productName) {
-    if (!products.has(productName)) throw new Error('제품을 찾을 수 없습니다.');
+    if (!products.has(productName))
+      throw new Error(MESSAGE.ERROR.PRODUCT_NOT_FOUND);
   },
 
   validatePurchaseAmount(purchaseAmount, productPrice) {
     if (purchaseAmount < productPrice)
-      throw new Error('구입 금액보다 상품의 가격이 높습니다.');
+      throw new Error(MESSAGE.ERROR.INSUFFICIENT_PURCHASE_AMOUNT);
   },
 };
