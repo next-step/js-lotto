ebf477030c695569d9897e6159489521674c948a
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-08-15 15:21:50 +0900

    test: lottoTicket model 테스트코드 추가

--- __tests__/unit/Models/LottoTicket.test.js
+++ __tests__/unit/Models/LottoTicket.test.js
@@ -1,16 +1,46 @@
 import { LottoTicket } from '../../../src/Model';
+import { NUMBER } from '../../../src/constants';
 
 /**
  * 로또 티켓의 번호는 1-43 사이의 숫자들로 구성된다.
- * 당첨 번호는 5개이다.
+ * 당첨 번호는 6개이다.
  * 보너스 번호는 1개이다.
- * 로또는 가격을 생성자에서 주입받는다.
  */
 
 describe('LottoTicket', () => {
-  test('로또 티켓이 정상적으로 생성되는지 확인한다.', () => {});
-  test('로또 티켓의 번호는 1-43 사이의 숫자들로 구성된다.', () => {});
-  test('로또 티켓의 당첨 번호는 5개이다.', () => {});
-  test('로또 티켓의 보너스 번호는 1개이다.', () => {});
-  test('로또 티켓의 가격은 생성자에서 주입받는다.', () => {});
+  test('로또 티켓이 정상적으로 생성되는지 확인한다.', () => {
+    const ticket = new LottoTicket();
+
+    expect(ticket).toBeInstanceOf(LottoTicket);
+  });
+
+  test('로또 티켓의 번호는 1-43 사이의 숫자들로 구성된다.', () => {
+    const ticket = new LottoTicket();
+    const { numbers, bonusNumber } = ticket.getTicketNumbers();
+    const ticketNumbers = [...numbers, bonusNumber];
+    const validTicketNumbers = ticketNumbers.every(
+      (number) =>
+        NUMBER.LOTTO_TICKET.MIN_RANGE <= number &&
+        number <= NUMBER.LOTTO_TICKET.MAX_RANGE
+    );
+
+    expect(validTicketNumbers).toBe(true);
+  });
+
+  test('로또 티켓의 당첨 번호는 6개이다.', () => {
+    const ticket = new LottoTicket();
+    const { numbers } = ticket.getTicketNumbers();
+
+    expect(numbers.length).toBe(6);
+  });
+
+  test('로또 티켓의 보너스 번호는 1개이다.', () => {
+    const ticket = new LottoTicket();
+    const { bonusNumber } = ticket.getTicketNumbers();
+    const validBonusNumber =
+      NUMBER.LOTTO_TICKET.MIN_RANGE <= bonusNumber &&
+      bonusNumber <= NUMBER.LOTTO_TICKET.MAX_RANGE;
+
+    expect(validBonusNumber).toBe(true);
+  });
 });
