a1cd02a882ec17458e49fa0eba98db431ab5b234
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-08-15 11:49:01 +0900

    test: 난수 생성 함수 테스트코드 추가

---
+++ __tests__/unit/utils/getRandomIntInRange.test.js
@@ -0,0 +1,10 @@
+import { getRandomIntInRange } from '../../../src/utils';
+
+/**
+ * 주어진 범위 내의 난수를 생성한다.
+ * 로또 class 내부의 번호를 결정하는 private method에 사용된다.
+ */
+
+describe('난수 생성 함수', () => {
+  test('정해진 범위 내의 숫자를 생성한다.', () => {});
+});
