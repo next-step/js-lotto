86b4b9c3e629789aabf577345e57a88911d619d3
cobocho <rlaalsrb1111@naver.com>
2023-08-15 13:08:56 +0900

    Test: 교환소 테스트코드 작성

--- __tests__/exchange.test.js
+++ __tests__/exchange.test.js
@@ -1,19 +1,23 @@
-import { LOTTO_PRIZE_TABLE } from '../src/js/constants/lotto-config.js';
+import { LOTTO_REWARD_CODE, LOTTO_REWARD_CONDITION } from '../src/js/constants/lotto-config.js';
 import Exchange from '../src/js/domain/Exchange.js';
 import Lotto from '../src/js/domain/Lotto.js';
+import LottoChecker from '../src/js/domain/LottoChecker.js';
+import { DEFAULT_LOTTO_NUMBERS, LOTTO_REWARD_DUMMY, MATCHED_BONUS } from './constants/lotto.js';
 
 describe('교환소 테스트', () => {
-  it.skip('로또의 등수에 따라 당첨금을 반환한다', () => {
-    const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
-    const winningNumbers = [1, 2, 3, 4, 5, 7];
-    const bonus = 6;
+  it('로또의 등수에 따라 당첨금을 반환한다', () => {
+    const lotto = new Lotto(DEFAULT_LOTTO_NUMBERS);
+    const lottoChecker = new LottoChecker();
 
-    lotto.check(winningNumbers, bonus);
+    lotto.check(LOTTO_REWARD_DUMMY.SECOND, MATCHED_BONUS);
+
+    const lottoRewardBoard = lottoChecker.getLottoRewardBoard([lotto]);
 
     const exchange = new Exchange();
-    const prize = exchange.getLottoPrize(lotto);
 
-    expect(prize).toBe(LOTTO_PRIZE_TABLE['2']);
+    const prize = exchange.getLottoPrize(lottoRewardBoard);
+
+    expect(prize).toBe(LOTTO_REWARD_CONDITION[LOTTO_REWARD_CODE.SECOND].prize);
   });
 
   it('수익률을 계산한다', () => {
