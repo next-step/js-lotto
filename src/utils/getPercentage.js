872e63fce0eaa6bead6d42efffae080923d6422b
gusrb3164 <gusrb3164@gmail.com>
2023-08-16 22:48:54 +0900

    refactor: percent 곱 상수화

--- src/utils/getPercentage.js
+++ src/utils/getPercentage.js
@@ -1,7 +1,9 @@
+const PERCENT_MULTIPLY = 100;
+
 export const getPercentage = ({value, total}, fixedNumber = 0) => {
   if (total === 0) {
     throw new Error('total은 0이 될 수 없습니다.');
   }
 
-  return ((value / total) * 100).toFixed(fixedNumber);
+  return ((value / total) * PERCENT_MULTIPLY).toFixed(fixedNumber);
 };
