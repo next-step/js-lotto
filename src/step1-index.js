a7c387cd4cebdc15b34c836a4d75a346e3de9083
bxxmi <bmi6638@gmail.com>
2023-08-17 23:21:47 +0900

    [feat] 기능 실행 부분 로직 작성

---
+++ src/step1-index.js
@@ -0,0 +1,59 @@
+import {
+  createLottosForAmount,
+  processLottoPurchase,
+  validateInputBonusNumber,
+  validateInputPrice,
+  validateInputWinningNumbers,
+} from "./controller/lottoController.js";
+import { calcLottoCount } from "./utils/calculate.js";
+import { closeUserInput, getUserInput } from "./utils/consoleInput.js";
+import {
+  inputBonusNumberMessage,
+  inputLottoNumberMessage,
+  inputLottoPriceMessage,
+} from "./utils/consoleMessage.js";
+import {
+  displayAvailableLottoCount,
+  displayLottoNumbers,
+} from "./view/view.js";
+
+async function lottoGame() {
+  const inputPrice = await getUserInput(inputLottoPriceMessage);
+  const isAvaliablePrice = validateInputPrice(inputPrice);
+
+  if (!isAvaliablePrice) {
+    closeUserInput();
+    return false;
+  }
+
+  // 몇개 살 수 있는 지 출력
+  const avaliableCount = processLottoPurchase(inputPrice);
+  displayAvailableLottoCount(avaliableCount);
+
+  // 로또 번호 출력
+  const lottoNumbers = createLottosForAmount(calcLottoCount(inputPrice));
+  displayLottoNumbers(lottoNumbers);
+
+  // 사용자의 당첨 번호 입력
+  const inputWinningNumbers = await getUserInput(inputLottoNumberMessage);
+  const isAvaliableNumbers = validateInputWinningNumbers(inputWinningNumbers);
+
+  if (!isAvaliableNumbers) {
+    closeUserInput();
+    return false;
+  }
+
+  // 보너스 번호 입력
+  const inputBonusNumber = await getUserInput(inputBonusNumberMessage);
+  const isAvaliableBonusNumber = validateInputBonusNumber(
+    inputWinningNumbers,
+    inputBonusNumber
+  );
+
+  if (!isAvaliableBonusNumber) {
+    closeUserInput();
+    return false;
+  }
+}
+
+lottoGame();
