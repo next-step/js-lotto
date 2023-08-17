a5dc9313dfe6e949bda8d265c2f92964ddf11d33
손진영 <sm099@naver.com>
2023-08-17 14:44:56 +0900

    docs(jsDoc): jsDoc 모듈 내 전체적인 타입 변경
    1. jsDoc 모듈에 있는 Object 타입을 object로 변경
    
    2. WinTableEntry 내 프로퍼티 들을 모두 union 타입으로 변경
    
    3. WinTableKey, LottoResultKey에 대한 customType 추가
    
    4. WinTable을 Record 타입으로 변경

--- src/step1/utils/jsDoc.js
+++ src/step1/utils/jsDoc.js
@@ -1,52 +1,64 @@
 /**
  * "당첨 로또 정보(당첨 로또 번호, 보너스 번호)"에 대한 객체
- * @typedef {Object} WinningLottoInfo
+ * @typedef {object} WinningLottoInfo
  * @property {number[]} winningLottoNumbers - 당첨 로또 번호
  * @property {number} bonusNumber - 보너스 번호
  */
 
 /**
  * "당첨 횟수, 보너스 번호 일치 여부"에 대한 객체
- * @typedef {Object} LottoMatchingInfo
+ * @typedef {object} LottoMatchingInfo
  * @property {number} winningCount - 당첨 횟수
  * @property {number} hasBonusNumber - 보너스 번호가 일치하는지의 여부
  */
 
 /**
+ * "로또 당첨 조건 - 당첨 횟수"로 이루어진 Dictionary 형태의 객체
+ * @typedef {Record<LottoResultKey, number>} LottoResult
+ */
+
+/**
  * "당첨 정보"에 대한 객체
- * @typedef {Object} WinningInfo
+ * @typedef {object} WinningInfo
  * @property {LottoResult} lottoResult - 로또 당첨 결과
  * @property {string} rateOfReturn - 수익률
  */
 
 /**
  * "로또 당첨 테이블"에 대한 객체
- * @typedef {Object} WinTableEntry
- * @property {string} description - 당첨 조건 설명 (예: '3개 일치 (5,000원)')
- * @property {number} amount - 당첨 금액
+ * @typedef {object} WinTableEntry
+ * @property {'3개 일치 (5,000원)' | '4개 일치 (50,000원)' | '5개 일치 (1,500,000원)' | '5개 일치, 보너스 볼 일치 (30,000,000원)' | '6개 일치 (2,000,000,000원)'} description - 당첨 조건 설명 (예: '3개 일치 (5,000원)')
+ * @property {5000 | 50000 | 1500000 | 30000000 | 2000000000} amount - 당첨 금액
  */
 
 /**
  * "로또 당첨 조건 설명 및 당첨 금액"에 대한 Dictionary 형태의 객체
- * @typedef {Object.<string, WinTableEntry>} WinTable
+ * @typedef {Record<WinTableKey, WinTableEntry>} WinTable
  */
 
 /**
  * "당첨 정보"에 대한 객체
- * @typedef {Object} WinningInfo
+ * @typedef {object} WinningInfo
  * @property {LottoResult} lottoResult - 로또 당첨 결과
  * @property {number} winningAmount - 당첨 된 총 금액
  */
 
 /**
- * "로또 당첨 조건 - 당첨 횟수"로 이루어진 Dictionary 형태의 객체
- * @typedef {Object.<string, number>} LottoResult
- */
-
-/**
  * 생성할 로또의 최소 번호, 최대 번호, 생성할 로또 갯수에 대한 객체
- * @typedef {Object} LottoRangeInfo
+ * @typedef {object} LottoRangeInfo
  * @property {number} startNumber - 최소 번호
  * @property {number} endNumber - 최대 번호
  * @property {number} count - 생성할 로또 번호 갯수
  */
+
+/**
+ * LottoReward 내 존재하는 WinTable에 접근하기 위한 key
+ * @typedef {'3_NUMBER' | '4_NUMBER' | '5_NUMBER' | '5_NUMBER_WITH_BONUS' | '6_NUMBER'} WinTableKey
+ */
+
+/**
+ * 로또 당첨 결과에 대한 key
+ * @typedef {WinTableEntry['description']} LottoResultKey
+ */
+
+export {};
