b6bdc36015e903e9a47fe1744820c90973945627
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-08-18 03:33:25 +0900

    chore: 잘못된 prize 값 변경

--- src/constants/lotto.js
+++ src/constants/lotto.js
@@ -1,7 +1,7 @@
 export const LOTTO_PRIZE_BOARD = Object.freeze({
   3: 5_000,
   4: 5_0000,
-  5: { withBonus: 1_500_000, withoutBonus: 30_000_000 },
+  5: { withBonus: 30_000_000, withoutBonus: 1_500_000 },
   6: 2_000_000_000,
 });
 
