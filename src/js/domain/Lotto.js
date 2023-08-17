ca58c25fd2a87f6e9b7cc43d1c7e0307928c1cf9
hyejj19 <hyejj19@naver.com>
2023-08-17 19:18:10 +0900

    feat: Lotto 클래스 구현

---
+++ src/js/domain/Lotto.js
@@ -0,0 +1,50 @@
+import {
+  generateLottoNumbers,
+  generateNodDuplicatedExtraNumber,
+} from '../utils/generateLottoNumbers.js'
+
+class Lotto {
+  #numbers
+  #status
+
+  constructor() {
+    this.#numbers = {
+      selectedNums: generateLottoNumbers(),
+      extraNum: generateNodDuplicatedExtraNumber(this.selectedNums),
+    }
+
+    this.#status = {}
+  }
+
+  setStatus(winningNumbers) {
+    const { selectedNums, extraNum } = this.#numbers
+
+    // winningNumbers.selectedNums 와 selectedNums 비교
+    const winningSelectdNumsSet = new Set(winningNumbers.selectedNums)
+    const matchSelectedNums = selectedNums.reduce((acc, cur) => {
+      if (winningSelectdNumsSet.has(cur)) return (acc += 1)
+      return acc
+    }, 0)
+
+    // winningNumbers.extraNum 과 extraNum 비교
+    const matchExtraNum = extraNum === winningNumbers.extraNum ? 1 : 0
+
+    // status 산출
+    const status = {
+      matchSelectedNums,
+      matchExtraNum,
+    }
+
+    this.#status = status
+  }
+
+  get numbers() {
+    return this.#numbers
+  }
+
+  get status() {
+    return this.#status
+  }
+}
+
+export default Lotto
