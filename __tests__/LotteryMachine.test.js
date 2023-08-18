3833e486e03f8f40f4ba834c0eda9666dc266762
ericagong <48196721+ericagong@users.noreply.github.com>
2023-08-18 16:12:12 +0900

    refactor: 파일명 수정

--- __tests__/LotteryMachine.test.js
+++ __tests__/LotteryMachine.test.js
@@ -6,7 +6,6 @@ const { issueLotto } = createLotteryMachine();
 
 describe("로또 발행 테스트", () => {
   const lotto = issueLotto(new FixedIssueStrategy([1, 2, 3, 4, 5, 6]));
-  const lottoNumbers = lotto.getLottoNumbers();
 
   it("Lotto 객체를 반환한다.", () => {
     expect(lotto).toBeInstanceOf(Lotto);
