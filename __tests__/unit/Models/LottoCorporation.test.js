b251808d8a3d46960369be64ce435da52863ba39
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-08-19 03:17:23 +0900

    test: lottoCorporation 테스트 코드 추가

---
+++ __tests__/unit/Models/LottoCorporation.test.js
@@ -0,0 +1,38 @@
+import { PRODUCTS } from '../../../src/constants';
+import { LottoCorporation, Store } from '../../../src/Model';
+
+describe('LottoCorporation', () => {
+  const lottoCorporation = new LottoCorporation(new Store(PRODUCTS));
+
+  test('로또회사가 인수한 상점에서 로또를 구매할 수 있다.', () => {
+    const purchaseAmount = 5_000;
+    const tickets = lottoCorporation.buyTickets(purchaseAmount);
+
+    tickets.forEach((ticket) =>
+      expect(ticket).toBeInstanceOf(PRODUCTS.LottoTicket.product)
+    );
+  });
+
+  test('로또 티켓의 결과를 확인한다.', () => {
+    const mockTicketNumbers = [
+      [1, 2, 3, 4, 5, 6],
+      [11, 12, 13, 14, 15, 16],
+    ];
+
+    const mockWinningNumbers = {
+      lottoNumbers: [1, 2, 3, 4, 5, 6],
+      bonusNumber: 7,
+    };
+
+    const mockExpect = [6, 0];
+
+    mockTicketNumbers.forEach((mockTicketNumber, index) => {
+      const { matchingCount } = lottoCorporation.checkTicketResult(
+        mockTicketNumber,
+        mockWinningNumbers
+      );
+
+      expect(matchingCount).toBe(mockExpect[index]);
+    });
+  });
+});
