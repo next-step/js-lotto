d26ee1a2257759f08e7079a8bb5e9df9da34927e
손진영 <sm099@naver.com>
2023-08-18 15:11:52 +0900

    test(Bank, Lotto, LottoMerchant): 테스트 케이스 내 context 추가

--- __tests__/model/Lotto/Exception.test.js
+++ __tests__/model/Lotto/Exception.test.js
@@ -4,83 +4,75 @@ import { LottoError } from '../../../src/step1/errors';
 import { Lotto } from '../../../src/step1/model';
 
 describe('Lotto 관련 예외 테스트', () => {
-  test.each([
-    { lottoNumbers: [null, 33, 1, 4, 22, 34] },
-    { lottoNumbers: [true, false, 5, 3, 11, 22] },
-    { lottoNumbers: [2, 22, {}, [], 23, 44] },
-    { lottoNumbers: [undefined, 12, 22, 13, 44, 33] },
-    { lottoNumbers: [Symbol('1'), Symbol('3'), '👍', 12, 22, 13] },
-    { lottoNumbers: [NaN, Infinity, 1, 5, 3, 11] },
-  ])('$lottoNumbers는 숫자 타입이 아니므로 에러가 발생한다.', ({ lottoNumbers }) => {
-    // given - when
-    const createLottos = () => new Lotto(lottoNumbers);
-    // then
-    expect(() => createLottos()).toThrow(LottoError);
-    expect(() => createLottos()).toThrow(ERROR_MESSAGE.TYPE_OF_NUMBER);
-  });
-  test.each([
-    { lottoNumbers: [47, 33, 1, 4, 22, 34] },
-    { lottoNumbers: [44, 47, 5, 3, 11, 22] },
-    { lottoNumbers: [45, 12, 46, 13, 44, 33] },
-    { lottoNumbers: [45, 2, 3, 47, 22, 13] },
-    { lottoNumbers: [45, 2, 3, 43, 66, 13] },
-    { lottoNumbers: [45, 2, 3, 43, 22, 99] },
-  ])(`$lottoNumbers의 범위는 ${LOTTO_TERMS.MAX_LOTTO_NUMBER}보다 큰 값이 있어 에러가 발생한다.`, ({ lottoNumbers }) => {
-    // given - when
-    const createLottos = () => new Lotto(lottoNumbers);
-    // then
-    expect(() => createLottos()).toThrow(LottoError);
-    expect(() => createLottos()).toThrow(ERROR_MESSAGE.INVALID_LOTTO_NUMBER_RANGE);
-  });
-
-  test.each([
-    { lottoNumbers: [0, 33, 34, 35, 36, 37] },
-    { lottoNumbers: [45, -1, 38, 39, 40, 41] },
-    { lottoNumbers: [42, 43, -2, 2, 3, 4] },
-    { lottoNumbers: [5, 6, 7, -3, 44, 9] },
-    { lottoNumbers: [10, 11, 12, 13, -4, 15] },
-    { lottoNumbers: [16, 17, 18, 19, 20, -5] },
-  ])(
-    `$lottoNumbers의 범위는 ${LOTTO_TERMS.MIN_LOTTO_NUMBER}보다 작은 값이 있어 에러가 발생한다.`,
-    ({ lottoNumbers }) => {
+  describe('로또 번호 내 다른 타입이 존재하는 case 테스트', () => {
+    test.each([
+      { lottoNumbers: [null, 33, 1, 4, 22, 34] },
+      { lottoNumbers: [true, false, 5, 3, 11, 22] },
+      { lottoNumbers: ['7', 4, 5, 3, 11, 22] },
+      { lottoNumbers: [2, 22, {}, [], 23, 44] },
+      { lottoNumbers: [undefined, 12, 22, 13, 44, 33] },
+      { lottoNumbers: [Symbol('1'), Symbol('3'), '👍', 12, 22, 13] },
+      { lottoNumbers: [NaN, Infinity, 1, 5, 3, 11] },
+    ])('$lottoNumbers는 숫자 타입이 아니므로 에러가 발생한다.', ({ lottoNumbers }) => {
       // given - when
       const createLottos = () => new Lotto(lottoNumbers);
       // then
       expect(() => createLottos()).toThrow(LottoError);
-      expect(() => createLottos()).toThrow(ERROR_MESSAGE.INVALID_LOTTO_NUMBER_RANGE);
-    },
-  );
-
-  test.each([
-    { lottoNumbers: [33, 33, 34, 35, 36, 37] },
-    { lottoNumbers: [45, 45, 38, 39, 40, 41] },
-    { lottoNumbers: [42, 43, 43, 2, 3, 4] },
-    { lottoNumbers: [5, 6, 7, 7, 44, 9] },
-    { lottoNumbers: [10, 11, 12, 13, 13, 15] },
-    { lottoNumbers: [16, 17, 18, 19, 20, 20] },
-  ])(`$lottoNumbers는 중복 되는 값이 존재하여 에러가 발생한다.`, ({ lottoNumbers }) => {
-    // given - when
-    const createLottos = () => new Lotto(lottoNumbers);
-    // then
-    expect(() => createLottos()).toThrow(LottoError);
-    expect(() => createLottos()).toThrow(ERROR_MESSAGE.DUPLICATE_LOTTO_NUMBERS);
+      expect(() => createLottos()).toThrow(ERROR_MESSAGE.TYPE_OF_NUMBER);
+    });
   });
 
-  test.each([
-    { lottoNumbers: [33, 34, 35, 36, 37] },
-    { lottoNumbers: [45, 38, 39, 40, 41, 42, 43] },
-    { lottoNumbers: [] },
-    { lottoNumbers: [5, 6, 7, 44, 9, 10, 11, 12] },
-    { lottoNumbers: [10, 11, 12] },
-    { lottoNumbers: [16, 17] },
-  ])(
-    `$lottoNumbers의 길이가 ${LOTTO_TERMS.DEFAULT_LIMIT_LOTTO_COUNT}개가 아니므로 에러가 발생한다.`,
-    ({ lottoNumbers }) => {
-      // given - when
-      const createLottos = () => new Lotto(lottoNumbers);
-      // then
-      expect(() => createLottos()).toThrow(LottoError);
-      expect(() => createLottos()).toThrow(ERROR_MESSAGE.NOT_DEFAULT_LIMIT_LOTTO_COUNT);
-    },
-  );
+  describe('생성된 로또 번호 중 유효하지 않은 로또 번호 범위의 값이 존재하는 case 테스트', () => {
+    test.each([{ lottoNumbers: [47, 33, 1, 4, 22, 34] }])(
+      `$lottoNumbers의 범위는 ${LOTTO_TERMS.MAX_LOTTO_NUMBER}보다 큰 값이 있어 에러가 발생한다.`,
+      ({ lottoNumbers }) => {
+        // given - when
+        const createLottos = () => new Lotto(lottoNumbers);
+        // then
+        expect(() => createLottos()).toThrow(LottoError);
+        expect(() => createLottos()).toThrow(ERROR_MESSAGE.INVALID_LOTTO_NUMBER_RANGE);
+      },
+    );
+
+    test.each([{ lottoNumbers: [0, 33, 34, 35, 36, 37] }, { lottoNumbers: [45, -1, 38, 39, 40, 41] }])(
+      `$lottoNumbers의 범위는 ${LOTTO_TERMS.MIN_LOTTO_NUMBER}보다 작은 값이 있어 에러가 발생한다.`,
+      ({ lottoNumbers }) => {
+        // given - when
+        const createLottos = () => new Lotto(lottoNumbers);
+        // then
+        expect(() => createLottos()).toThrow(LottoError);
+        expect(() => createLottos()).toThrow(ERROR_MESSAGE.INVALID_LOTTO_NUMBER_RANGE);
+      },
+    );
+  });
+
+  describe('생성된 로또 번호 중 중복 되는 값이 존재하는 case 테스트', () => {
+    test.each([{ lottoNumbers: [33, 33, 34, 35, 36, 37] }])(
+      `$lottoNumbers는 중복 되는 값이 존재하여 에러가 발생한다.`,
+      ({ lottoNumbers }) => {
+        // given - when
+        const createLottos = () => new Lotto(lottoNumbers);
+        // then
+        expect(() => createLottos()).toThrow(LottoError);
+        expect(() => createLottos()).toThrow(ERROR_MESSAGE.DUPLICATE_LOTTO_NUMBERS);
+      },
+    );
+  });
+
+  describe('로또 내 존재하는 로또 번호의 갯수가 올바른 갯수가 아닌 case 테스트', () => {
+    test.each([
+      { lottoNumbers: [33, 34, 35, 36, 37] },
+      { lottoNumbers: [45, 38, 39, 40, 41, 42, 43] },
+      { lottoNumbers: [] },
+    ])(
+      `$lottoNumbers의 길이가 ${LOTTO_TERMS.DEFAULT_LIMIT_LOTTO_COUNT}개가 아니므로 에러가 발생한다.`,
+      ({ lottoNumbers }) => {
+        // given - when
+        const createLottos = () => new Lotto(lottoNumbers);
+        // then
+        expect(() => createLottos()).toThrow(LottoError);
+        expect(() => createLottos()).toThrow(ERROR_MESSAGE.NOT_DEFAULT_LIMIT_LOTTO_COUNT);
+      },
+    );
+  });
 });
--- __tests__/model/Lotto/Feature.test.js
+++ __tests__/model/Lotto/Feature.test.js
@@ -4,75 +4,79 @@ import { Lotto } from '../../../src/step1/model';
 import { isDefaultLottoCount, isValidLottoNumbersRange } from '../../../src/step1/utils/validate/lotto/lottoValidate';
 
 describe('Lotto 관련 기능 테스트', () => {
-  test.each([{ count: 5 }, { count: 4 }, { count: 3 }, { count: 1 }])(
-    '랜덤 숫자들로 만들어진 로또는 총 $count개가 생성되어야 한다.',
-    ({ count }) => {
-      // given
-      const lottos = Lotto.fromByRandomNumberMaker(count);
-      // when
-      const lottosLength = lottos.map((lotto) => lotto.getLottoNumbers()).length;
-      // then
-      expect(lottosLength).toEqual(count);
-    },
-  );
+  describe('랜덤 번호를 통한 로또 생성 테스트', () => {
+    test.each([{ count: 5 }, { count: 1 }])(
+      '랜덤 숫자들로 만들어진 로또는 총 $count개가 생성되어야 한다.',
+      ({ count }) => {
+        // given
+        const lottos = Lotto.fromByRandomNumberMaker(count);
+        // when
+        const lottosLength = lottos.map((lotto) => lotto.getLottoNumbers()).length;
+        // then
+        expect(lottosLength).toEqual(count);
+      },
+    );
 
-  test.each([{ count: 5 }, { count: 4 }, { count: 3 }, { count: 1 }])(
-    `랜덤 숫자들로 만들어진 로또들은 모두 ${LOTTO_TERMS.DEFAULT_LIMIT_LOTTO_COUNT}개의 로또 번호를 가지고 있다.`,
-    ({ count }) => {
-      // given
-      const lottos = Lotto.fromByRandomNumberMaker(count);
-      // when
-      const hasSixLottoNumbers = lottos.every((lotto) => isDefaultLottoCount(lotto.getLottoNumbers()));
-      // then
-      expect(hasSixLottoNumbers).toBeTruthy();
-    },
-  );
+    test.each([{ count: 5 }])(
+      `랜덤 숫자들로 만들어진 로또들은 모두 ${LOTTO_TERMS.DEFAULT_LIMIT_LOTTO_COUNT}개의 로또 번호를 가지고 있다.`,
+      ({ count }) => {
+        // given
+        const lottos = Lotto.fromByRandomNumberMaker(count);
+        // when
+        const hasSixLottoNumbers = lottos.every((lotto) => isDefaultLottoCount(lotto.getLottoNumbers()));
+        // then
+        expect(hasSixLottoNumbers).toBeTruthy();
+      },
+    );
 
-  test.each([{ count: 5 }, { count: 4 }, { count: 3 }, { count: 1 }])(
-    `랜덤 숫자들로 만들어진 로또들은 모두 ${LOTTO_TERMS.MIN_LOTTO_NUMBER}에서 ${LOTTO_TERMS.MAX_LOTTO_NUMBER}의 숫자 범위를 가지고 있다.`,
-    ({ count }) => {
-      // given
-      const lottos = Lotto.fromByRandomNumberMaker(count);
-      // when
-      const isValidRange = lottos.every((lotto) => isValidLottoNumbersRange(lotto.getLottoNumbers()));
-      // then
-      expect(isValidRange).toBeTruthy();
-    },
-  );
-
-  test.each([
-    { lottoString: '1,22,33,4,43,12', seperator: SYMBOLS.COMMA },
-    { lottoString: '1.22.33.4.43.12', seperator: SYMBOLS.DOT },
-    { lottoString: '1:22:33:4:43:12', seperator: SYMBOLS.COLON },
-  ])('문자열로 만들어진 로또가 생성될 수 있다.', ({ lottoString, seperator }) => {
-    const lotto = Lotto.fromByString(lottoString, seperator);
-    const isLotto = lotto instanceof Lotto;
-    expect(isLotto).toBeTruthy();
+    test.each([{ count: 5 }])(
+      `랜덤 숫자들로 만들어진 로또들은 모두 ${LOTTO_TERMS.MIN_LOTTO_NUMBER}에서 ${LOTTO_TERMS.MAX_LOTTO_NUMBER}의 숫자 범위를 가지고 있다.`,
+      ({ count }) => {
+        // given
+        const lottos = Lotto.fromByRandomNumberMaker(count);
+        // when
+        const isValidRange = lottos.every((lotto) => isValidLottoNumbersRange(lotto.getLottoNumbers()));
+        // then
+        expect(isValidRange).toBeTruthy();
+      },
+    );
   });
 
-  test.each([
-    { lottoString: '1,22,33,4,43,12', seperator: SYMBOLS.COMMA },
-    { lottoString: '1.22.33.4.43.12', seperator: SYMBOLS.DOT },
-    { lottoString: '1:22:33:4:43:12', seperator: SYMBOLS.COLON },
-  ])(
-    `문자열로 만들어진 로또는 총 ${LOTTO_TERMS.DEFAULT_LIMIT_LOTTO_COUNT}개의 로또 번호를 갖는다.`,
-    ({ lottoString, seperator }) => {
-      const lotto = Lotto.fromByString(lottoString, seperator);
-      const isValid = isDefaultLottoCount(lotto.getLottoNumbers());
-      expect(isValid).toBeTruthy();
-    },
-  );
+  describe('문자열을 통한 로또 생성 테스트', () => {
+    test.each([
+      { lottoString: '1,22,33,4,43,12', separator: SYMBOLS.COMMA },
+      { lottoString: '1.22.33.4.43.12', separator: SYMBOLS.DOT },
+      { lottoString: '1:22:33:4:43:12', separator: SYMBOLS.COLON },
+    ])('$separator인 구분자가 있는 문자열을 통해 로또가 생성될 수 있다.', ({ lottoString, separator }) => {
+      const lotto = Lotto.fromByString(lottoString, separator);
+      const isLotto = lotto instanceof Lotto;
+      expect(isLotto).toBeTruthy();
+    });
 
-  test.each([
-    { lottoString: '1,22,33,4,43,12', seperator: SYMBOLS.COMMA },
-    { lottoString: '1.22.33.4.43.12', seperator: SYMBOLS.DOT },
-    { lottoString: '1:22:33:4:43:12', seperator: SYMBOLS.COLON },
-  ])(
-    `문자열로 만들어진 로또는 모두 ${LOTTO_TERMS.MIN_LOTTO_NUMBER}에서 ${LOTTO_TERMS.MAX_LOTTO_NUMBER}의 숫자 범위를 갖는다.`,
-    ({ lottoString, seperator }) => {
-      const lotto = Lotto.fromByString(lottoString, seperator);
-      const isValid = isValidLottoNumbersRange(lotto.getLottoNumbers());
-      expect(isValid).toBeTruthy();
-    },
-  );
+    test.each([
+      { lottoString: '1,22,33,4,43,12', separator: SYMBOLS.COMMA },
+      { lottoString: '1.22.33.4.43.12', separator: SYMBOLS.DOT },
+      { lottoString: '1:22:33:4:43:12', separator: SYMBOLS.COLON },
+    ])(
+      `$separator인 구분자가 있는 문자열로 만들어진 로또는 총 ${LOTTO_TERMS.DEFAULT_LIMIT_LOTTO_COUNT}개의 로또 번호를 갖는다.`,
+      ({ lottoString, separator }) => {
+        const lotto = Lotto.fromByString(lottoString, separator);
+        const isValid = isDefaultLottoCount(lotto.getLottoNumbers());
+        expect(isValid).toBeTruthy();
+      },
+    );
+
+    test.each([
+      { lottoString: '1,22,33,4,43,12', separator: SYMBOLS.COMMA },
+      { lottoString: '1.22.33.4.43.12', separator: SYMBOLS.DOT },
+      { lottoString: '1:22:33:4:43:12', separator: SYMBOLS.COLON },
+    ])(
+      `$separator인 구분자가 있는 문자열로 만들어진 로또는 모두 ${LOTTO_TERMS.MIN_LOTTO_NUMBER}에서 ${LOTTO_TERMS.MAX_LOTTO_NUMBER}의 숫자 범위를 갖는다.`,
+      ({ lottoString, separator }) => {
+        const lotto = Lotto.fromByString(lottoString, separator);
+        const isValid = isValidLottoNumbersRange(lotto.getLottoNumbers());
+        expect(isValid).toBeTruthy();
+      },
+    );
+  });
 });
