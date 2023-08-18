7439dbc63857de318924c557c76d8c01b935a746
손진영 <sm099@naver.com>
2023-08-18 15:12:56 +0900

    test(LottoMerchant): 기능 테스트 내 불필요한 테스트 제거
    Lotto에서 이미 검증하고 있으므로 불필요한 테스트 코드 제거

--- __tests__/model/LottoMerchant/Feature.test.js
+++ __tests__/model/LottoMerchant/Feature.test.js
@@ -1,6 +1,4 @@
-import { LOTTO_TERMS } from '../../../src/step1/constants/lotto';
 import { LottoMerchant } from '../../../src/step1/model';
-import { isValidLottoNumbersRange } from '../../../src/step1/utils/validate/lotto/lottoValidate';
 
 describe('LottoMerchant 관련 기능 테스트', () => {
   test.each([
@@ -17,45 +15,4 @@ describe('LottoMerchant 관련 기능 테스트', () => {
     // then
     expect(expectLottoLength).toEqual(lottosLength);
   });
-
-  test.each([
-    { investmentAmount: 3000, lottosLength: 3 },
-    { investmentAmount: 4000, lottosLength: 4 },
-    { investmentAmount: 6000, lottosLength: 6 },
-    { investmentAmount: 2000, lottosLength: 2 },
-    { investmentAmount: 8000, lottosLength: 8 },
-  ])(
-    `$lottosLength개의 로또 번호는 모두 ${LOTTO_TERMS.DEFAULT_LIMIT_LOTTO_COUNT}개여야 한다.`,
-    ({ investmentAmount }) => {
-      // given
-      const lottoMerchant = LottoMerchant.from(investmentAmount);
-      // when
-      const lottos = lottoMerchant.sellLotto();
-      const hasSixNumbersInLotto = lottos.every(
-        (lotto) => lotto.getLottoNumbers().length === LOTTO_TERMS.DEFAULT_LIMIT_LOTTO_COUNT,
-      );
-      // then
-      expect(hasSixNumbersInLotto).toBeTruthy();
-    },
-  );
-
-  test.each([
-    { investmentAmount: 3000, lottosLength: 3 },
-    { investmentAmount: 4000, lottosLength: 4 },
-    { investmentAmount: 6000, lottosLength: 6 },
-    { investmentAmount: 2000, lottosLength: 2 },
-    { investmentAmount: 8000, lottosLength: 8 },
-  ])(
-    `$lottosLength개의 로또 번호는 모두 ${LOTTO_TERMS.MIN_LOTTO_NUMBER} ~ ${LOTTO_TERMS.MAX_LOTTO_NUMBER}의 범위를 가진다.`,
-    ({ investmentAmount }) => {
-      // given
-      const lottoMerchant = LottoMerchant.from(investmentAmount);
-      // when
-      const isValidLottoNumbers = lottoMerchant
-        .sellLotto()
-        .every((lotto) => isValidLottoNumbersRange(lotto.getLottoNumbers()));
-      // then
-      expect(isValidLottoNumbers).toBeTruthy();
-    },
-  );
 });
