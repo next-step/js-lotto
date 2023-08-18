6eb9f7b3a6714ed13e352590c430f799702ebe31
ericagong <48196721+ericagong@users.noreply.github.com>
2023-08-18 22:10:54 +0900

    feat: Lotto 객체에 유효성 검사 로직 재설정

--- src/js/domain/Lotto/index.js
+++ src/js/domain/Lotto/index.js
@@ -1,18 +1,56 @@
-import { LottoMatchResultNotSetError } from "./errors";
+import {
+  LottoNumbersNotArrayError,
+  LottoNumbersLengthNotSixError,
+  LottoNumbersElementNotNumberError,
+  LottoNumbersElementOutOfRangeError,
+  LottoNumbersElementDuplicatedError,
+  LottoMatchResultNotSetError,
+} from "./errors";
 
 export default class Lotto {
   #lottoNumbers;
   #matchCount;
   #isBonusMatch;
 
+  static LOWER_BOUND = 1;
+  static UPPER_BOUND = 45;
+
   static of(numbers) {
     return new Lotto(numbers);
   }
 
   constructor(numbers) {
+    this.#validateLottoNumbers(numbers);
     this.#lottoNumbers = numbers;
     this.#matchCount = null;
     this.#isBonusMatch = null;
+  }
+
+  #hasNonNumericElement(numbers) {
+    return numbers.some((number) => typeof number !== "number");
+  }
+
+  #isOutOfRange(number) {
+    return number < Lotto.LOWER_BOUND || number > Lotto.UPPER_BOUND;
+  }
+
+  #hasOutOfRangeElement(numbers) {
+    return numbers.some(this.#isOutOfRange);
+  }
+
+  #hasDuplicatedElement(numbers) {
+    return new Set(numbers).size !== numbers.length;
+  }
+
+  #validateLottoNumbers(numbers) {
+    if (!Array.isArray(numbers)) throw new LottoNumbersNotArrayError();
+    if (numbers.length !== 6) throw new LottoNumbersLengthNotSixError();
+    if (this.#hasNonNumericElement(numbers))
+      throw new LottoNumbersElementNotNumberError();
+    if (this.#hasOutOfRangeElement(numbers))
+      throw new LottoNumbersElementOutOfRangeError();
+    if (this.#hasDuplicatedElement(numbers))
+      throw new LottoNumbersElementDuplicatedError();
   }
 
   getLottoNumbers() {
