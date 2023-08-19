4c9f240d4c518292cff24d7b0406c3a1a6599530
gusrb3164 <gusrb3164@gmail.com>
2023-08-19 18:03:38 +0900

    refactor: shuffle 이용한 로또번호 생성 로직 최적화

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,27 +1,20 @@
-const LOTTO_PRICE = 1000;
+import {shuffle} from '../utils';
+const LOTTO_NUMBERS = Array.from({length: 45}).map((_, index) => index + 1);
 
 export class Lotto {
   #lotteries = [];
 
   constructor(lotteryCount) {
-    this.#lotteries = Array.from({length: lotteryCount}, this.#generateLottery);
+    this.#lotteries = Array.from({length: lotteryCount}, () => this.#generateLottoNumbers());
   }
 
   get lotteries() {
     return this.#lotteries;
   }
 
-  #generateLottery() {
-    const lottery = [];
-
-    while (lottery.length < 6) {
-      const number = Math.floor(Math.random() * 45) + 1;
-
-      if (!lottery.includes(number)) {
-        lottery.push(number);
-      }
-    }
-
-    return lottery.sort((a, b) => a - b);
+  #generateLottoNumbers(size = 6) {
+    return shuffle(LOTTO_NUMBERS)
+      .slice(0, size)
+      .sort((a, b) => a - b);
   }
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,4 @@ export {getDividedInteger} from './getDividedInteger';
 export {getPercentage} from './getPercentage';
 export {splitNumbers} from './splitNumbers';
 export {validateOnlyNumber, VALIDATE_ONLY_NUMBER_ERROR} from './validateOnlyNumber';
+export {shuffle} from './shuffle';
