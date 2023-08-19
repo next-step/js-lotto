8c3148a37981c006ca4787d5cf8af010db3a4b73
gusrb3164 <gusrb3164@gmail.com>
2023-08-19 19:16:54 +0900

    refactor: 당첨 번호 최소 최댓값 상수 참조

--- src/domain/__tests__/validateWinningNumbers.test.js
+++ src/domain/__tests__/validateWinningNumbers.test.js
@@ -1,9 +1,10 @@
 import {VALIDATE_ONLY_NUMBER_ERROR} from '../../utils/validateOnlyNumber';
+import {MAX_LOTTO_NUMBER, MIN_LOTTO_NUMBER} from '../constants';
 import {validateWinningNumbers, VALIDATE_WINNING_NUMBERS_ERROR} from '../validateWinningNumbers';
 
 describe('validateWinningNumber function', () => {
   test.each(['1,2,3,4,5,6', '15,10,9,44,43,45', '1,20,25,30,40,45'])(
-    '문자열은 1~45사이의 6개의 숫자가 ,로 구분되어야 한다.',
+    `문자열은 ${MIN_LOTTO_NUMBER}~${MAX_LOTTO_NUMBER}사이의 6개의 숫자가 ,로 구분되어야 한다.`,
     text => {
       expect(() => validateWinningNumbers(text)).not.toThrow();
     },
@@ -23,9 +24,12 @@ describe('validateWinningNumber function', () => {
     },
   );
 
-  test.each(['1,2,3,4,5,0', '1,2,3,4,5,46', '0,2,3,4,5,6'])('1~45사이의 숫자가 아니면 에러가 발생한다.', text => {
-    expect(() => validateWinningNumbers(text)).toThrow(VALIDATE_WINNING_NUMBERS_ERROR.NOT_IN_RANGE);
-  });
+  test.each(['1,2,3,4,5,0', '1,2,3,4,5,46', '0,2,3,4,5,6'])(
+    `${MIN_LOTTO_NUMBER}~${MAX_LOTTO_NUMBER}사이의 숫자가 아니면 에러가 발생한다.`,
+    text => {
+      expect(() => validateWinningNumbers(text)).toThrow(VALIDATE_WINNING_NUMBERS_ERROR.NOT_IN_RANGE);
+    },
+  );
 
   test.each(['1,1,3,4,5,6', '1,2,2,4,5,6', '1,1,1,1,1,1', '45,45,45,45,45,45'])(
     '중복되는 숫자가 존재하면 에러가 발생한다.',
--- src/domain/validateWinningNumbers.js
+++ src/domain/validateWinningNumbers.js
@@ -1,8 +1,9 @@
 import {validateOnlyNumber} from '../utils';
+import {MAX_LOTTO_NUMBER, MIN_LOTTO_NUMBER} from './constants';
 
 export const VALIDATE_WINNING_NUMBERS_ERROR = {
   INVALID_LENGTH: '6개의 숫자를 ,로 구분해서 입력해주세요.',
-  NOT_IN_RANGE: '1~45사이의 숫자를 입력해주세요.',
+  NOT_IN_RANGE: `${MIN_LOTTO_NUMBER}~${MAX_LOTTO_NUMBER}사이의 숫자를 입력해주세요.`,
   DUPLICATED: '중복되는 숫자없이 입력해주세요.',
 };
 
@@ -18,7 +19,7 @@ export const validateWinningNumbers = text => {
 
     const number = Number(numberString);
 
-    if (number < 1 || number > 45) {
+    if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {
       throw new Error(VALIDATE_WINNING_NUMBERS_ERROR.NOT_IN_RANGE);
     }
   });
