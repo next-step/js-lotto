535a58a20e5056ab24e564d3cb215ab9168b712a
gusrb3164 <gusrb3164@gmail.com>
2023-08-19 19:05:31 +0900

    refactor: 보너스 번호 로직도 로또 번호 상수 참조하도록 수정

--- src/domain/__tests__/validateBonusNumber.test.js
+++ src/domain/__tests__/validateBonusNumber.test.js
@@ -1,20 +1,27 @@
 import {VALIDATE_BONUS_NUMBER_ERROR, validateBonusNumber} from '../validateBonusNumber';
 import {VALIDATE_ONLY_NUMBER_ERROR} from '../../utils/validateOnlyNumber';
+import {MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER} from '../constants';
 
 describe('validateBonusNumber function', () => {
   const winningNumbers = [1, 2, 3, 4, 5, 6];
 
-  test.each(['7', '8', '45'])('문자열은 1~45 사이의 당첨번호와 중복되지 않는 숫자이다.', text => {
-    expect(() => validateBonusNumber(text, winningNumbers)).not.toThrow();
-  });
+  test.each(['7', '8', '45'])(
+    `문자열은 ${MIN_LOTTO_NUMBER}~${MAX_LOTTO_NUMBER} 사이의 당첨번호와 중복되지 않는 숫자이다.`,
+    text => {
+      expect(() => validateBonusNumber(text, winningNumbers)).not.toThrow();
+    },
+  );
 
   test.each(['a', '', ' ', '-', '1a'])('문자열이 숫자가 아니면 에러가 발생한다.', text => {
     expect(() => validateBonusNumber(text, winningNumbers)).toThrow(VALIDATE_ONLY_NUMBER_ERROR.NOT_NUMBER);
   });
 
-  test.each(['0', '46', '100'])('1~45사이의 숫자가 아니면 에러가 발생한다.', text => {
-    expect(() => validateBonusNumber(text, winningNumbers)).toThrow(VALIDATE_BONUS_NUMBER_ERROR.NOT_IN_RANGE);
-  });
+  test.each(['0', '46', '100'])(
+    `${MIN_LOTTO_NUMBER}~${MAX_LOTTO_NUMBER}사이의 숫자가 아니면 에러가 발생한다.`,
+    text => {
+      expect(() => validateBonusNumber(text, winningNumbers)).toThrow(VALIDATE_BONUS_NUMBER_ERROR.NOT_IN_RANGE);
+    },
+  );
 
   test.each(['1', '2', '3', '4', '5', '6'])('당첨 번호와 중복되면 에러가 발생한다.', text => {
     expect(() => validateBonusNumber(text, winningNumbers)).toThrow(
--- src/domain/validateBonusNumber.js
+++ src/domain/validateBonusNumber.js
@@ -1,7 +1,8 @@
 import {validateOnlyNumber} from '../utils';
+import {MAX_LOTTO_NUMBER, MIN_LOTTO_NUMBER} from './constants';
 
 export const VALIDATE_BONUS_NUMBER_ERROR = {
-  NOT_IN_RANGE: '1~45사이의 숫자를 입력해주세요.',
+  NOT_IN_RANGE: `${MIN_LOTTO_NUMBER}~${MAX_LOTTO_NUMBER}사이의 숫자를 입력해주세요.`,
   DUPLICATED_WINNING_NUMBER: '당첨 번호와 중복되지 않는 숫자를 입력해주세요.',
 };
 export const validateBonusNumber = (text, winningNumbers) => {
@@ -9,7 +10,7 @@ export const validateBonusNumber = (text, winningNumbers) => {
 
   const bonusNumber = Number(text);
 
-  if (bonusNumber < 1 || bonusNumber > 45) {
+  if (bonusNumber < MIN_LOTTO_NUMBER || bonusNumber > MAX_LOTTO_NUMBER) {
     throw new Error(VALIDATE_BONUS_NUMBER_ERROR.NOT_IN_RANGE);
   }
 
