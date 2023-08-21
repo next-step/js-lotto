a01e7d75fbef9c9e4db8c64562200c69c372e45d
sonsurim <surim014@naver.com>
2023-08-21 19:55:51 +0900

    feat: step1 entry point 구현

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: './src/js/step1-index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
