4b4755dff01d28559c837486f9c16f847e2fe7ee
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2023-08-22 02:53:43 +0900

    test: lottoCorporation 테스트코드 분리

--- __tests__/unit/Models/LottoCorporation.test.js
+++ __tests__/unit/Models/LottoCorporation.test.js
@@ -1,35 +1,76 @@
 import { LottoCorporation } from '../../../src/Model';
+import { NUMBER } from '../../../src/constants';
+import { isLottoNumber } from '../../../src/utils/Validator';
 
 describe('LottoCorporation', () => {
   const lottoCorporation = new LottoCorporation();
 
-  test('로또회사가 인수한 상점에서 로또를 구매할 수 있다.', () => {
-    const purchaseAmount = 5_000;
-    const tickets = lottoCorporation.buyTickets(purchaseAmount);
+  const purchaseAmount = 5_000;
+  const tickets = lottoCorporation.buyTickets(purchaseAmount);
 
-    expect(ticket).toBe('구현하기');
+  test('구매금액에 맞는 로또의 개수를 반환하는지 확인한다.', () => {
+    const TICKET_PRICE = NUMBER.DEFAULT_TICKET_PRICE;
+    const expectedTicketCount = parseInt(purchaseAmount / TICKET_PRICE);
+
+    expect(tickets.length).toBe(expectedTicketCount);
   });
 
-  test('로또 티켓의 결과를 확인한다.', () => {
-    const mockTicketNumbers = [
-      [1, 2, 3, 4, 5, 6],
-      [11, 12, 13, 14, 15, 16],
-    ];
+  test.each(tickets)(
+    '발급된 로또의 숫자 범위가 유효한지 확인한다.',
+    (ticket) => {
+      const numbers = ticket.getTicketNumbers();
+      const numberValidation = numbers.every(isLottoNumber);
 
-    const mockWinningNumbers = {
-      lottoNumbers: [1, 2, 3, 4, 5, 6],
-      bonusNumber: 7,
-    };
+      expect(numberValidation).toBeTruthy();
+    }
+  );
 
-    const mockExpect = [6, 0];
+  test.each(tickets)(
+    '발급된 로또엔 중복된 숫자가 존재하지 않는다.',
+    (ticket) => {
+      const ticketNumbers = ticket.getTicketNumbers();
+      const uniqueNumbers = new Set(ticketNumbers);
 
-    mockTicketNumbers.forEach((mockTicketNumber, index) => {
-      const { matchingCount } = lottoCorporation.checkTicketResult(
-        mockTicketNumber,
-        mockWinningNumbers
+      expect(uniqueNumbers.size === ticketNumbers.length).toBeTruthy();
+    }
+  );
+
+  test.each(tickets)(
+    '발급된 로또의 숫자들은 오름차순으로 정렬되어있다.',
+    (ticket) => {
+      const ticketNumbers = ticket.getTicketNumbers();
+      const sortedTicketNumbers = [...ticketNumbers].sort((a, b) => a - b);
+
+      const sorted = ticketNumbers.every(
+        (number, index) => number === sortedTicketNumbers[index]
       );
 
-      expect(matchingCount).toBe(mockExpect[index]);
-    });
-  });
+      expect(sorted).toBeTruthy();
+    }
+  );
+
+  const mockCases = [
+    {
+      ticketNumbers: [1, 2, 3, 4, 5, 6],
+      winningNumbers: { lottoNumbers: [1, 2, 3, 4, 5, 6], bonusNumber: 7 },
+      expectedValue: 6,
+    },
+    {
+      ticketNumbers: [11, 12, 13, 14, 15, 16],
+      winningNumbers: { lottoNumbers: [1, 2, 3, 4, 5, 6], bonusNumber: 7 },
+      expectedValue: 0,
+    },
+  ];
+
+  test.each(mockCases)(
+    '로또 티켓의 결과를 확인한다.',
+    ({ ticketNumbers, winningNumbers, expectedValue }) => {
+      const { matchingCount } = lottoCorporation.checkTicketResult(
+        ticketNumbers,
+        winningNumbers
+      );
+
+      expect(matchingCount).toBe(expectedValue);
+    }
+  );
 });
