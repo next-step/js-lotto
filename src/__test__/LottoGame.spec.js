6bb8f2c87c86af4883c4f698e43e726affb884e5
Gn0lee <jinho605@gmail.com>
2023-08-22 22:55:28 +0900

    LottoGame 클래스 테스트 코드 작성

---
+++ src/__test__/LottoGame.spec.js
@@ -0,0 +1,14 @@
+describe("LottoGame 클래스 테스트", () => {
+  test.each`
+    payment | prize   | profitRate
+    ${0}    | ${0}    | ${0}
+    ${8000} | ${5000} | ${0.625}
+  `(
+    "지불 금액이 $payment , 상금이 $prize 일때 수익률은 $profitRate 이다.",
+    ({ payment, prize, profitRate }) => {
+      const game = new LottoGame();
+
+      expect(game.calculateProfitRate(payment, prize)).toBe(profitRate);
+    },
+  );
+});
