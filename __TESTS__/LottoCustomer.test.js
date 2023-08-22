fc01efdb151fd49958eb624e7218046e15d5d20e
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-08-22 21:35:58 +0900

    🧪 test: 로또 구매 테스트 코드 추가

--- __TESTS__/LottoCustomer.test.js
+++ __TESTS__/LottoCustomer.test.js
@@ -1,10 +1,15 @@
-import { DUMMY_AMOUNT, DUMMY_INCORRECT_AMOUNT } from './constants';
-import { ERROR_MESSAGE } from '../src/domain/constants/index';
-import { LottoCustomer } from '../src/domain/classes/index';
+import {
+  DUMMY_AMOUNT,
+  DUMMY_INCORRECT_AMOUNT,
+  DUMMY_PURCHASABLE_AMOUNT,
+  DUMMY_NOT_PURCHASABLE_AMOUNT
+} from './constants';
+import { ERROR_MESSAGE, LOTTO_MODE, LOTTO_PRICE } from '../src/domain/constants/index';
+import { LottoCustomer, LottoSeller, LottoOrganizer } from '../src/domain/classes/index';
 
 describe('로또 구매 테스트', () => {
   test.each(DUMMY_AMOUNT)('로또 구매자가 갖는 금액은 반드시 양수이어야 한다.($amount)', ({ amount }) => {
-    const lottoCustomer = new LottoCustomer(amount);
+    const lottoCustomer = new LottoCustomer(amount, LOTTO_MODE.AUTO);
     expect(lottoCustomer.amount).toBeGreaterThan(0);
   });
 
@@ -12,8 +17,30 @@ describe('로또 구매 테스트', () => {
     '로또 구매자가 갖는 금액이 양수가 아니면 오류가 발생한다.($amount)',
     ({ amount }) => {
       expect(() => {
-        new LottoCustomer(amount);
+        new LottoCustomer(amount, LOTTO_MODE.AUTO);
       }).toThrowError(ERROR_MESSAGE.INVALID_AMOUNT_BY_NOT_POSITIVE_AMOUNT);
+    }
+  );
+  test.each(DUMMY_PURCHASABLE_AMOUNT)(
+    `로또 구매자가 갖는 금액($amount)이 로또 가격(${LOTTO_PRICE})보다 같거나 크다면 구입금액만큼 로또를 구매한다.`,
+    ({ amount }) => {
+      const lottoOrganizer = new LottoOrganizer();
+      const lottoSeller = new LottoSeller(lottoOrganizer);
+      const lottoCustomer = new LottoCustomer(amount, LOTTO_MODE.AUTO);
+      lottoCustomer.buyLottoTicket(lottoSeller);
+      expect(lottoCustomer.tickets).toHaveLength(lottoSeller.sellCount);
+    }
+  );
+
+  test.each(DUMMY_NOT_PURCHASABLE_AMOUNT)(
+    `로또 구매자가 갖는 금액($amount)이 로또 가격(${LOTTO_PRICE})보다 작다면 오류가 발생한다.`,
+    ({ amount }) => {
+      const lottoOrganizer = new LottoOrganizer();
+      const lottoSeller = new LottoSeller(lottoOrganizer);
+      const lottoCustomer = new LottoCustomer(amount, LOTTO_MODE.AUTO);
+      expect(() => {
+        lottoCustomer.buyLottoTicket(lottoSeller);
+      }).toThrowError(ERROR_MESSAGE.NOT_ENOUGH_AMOUNT);
     }
   );
 });
--- __TESTS__/constants.js
+++ __TESTS__/constants.js
@@ -1,3 +1,4 @@
+/** LottoCustomer */
 export const DUMMY_AMOUNT = [{ amount: 1 }, { amount: 100 }, { amount: 999 }, { amount: 1000 }, { amount: 5000 }];
 export const DUMMY_INCORRECT_AMOUNT = [
   { amount: {} },
@@ -8,4 +9,18 @@ export const DUMMY_INCORRECT_AMOUNT = [
   { amount: undefined }
 ];
 
-// export const DUMMY_INCORRECT_AMOUNT = [{ amount: {}, amount: NaN } () => {}, -1000];
+export const DUMMY_PURCHASABLE_AMOUNT = [
+  { amount: 1000 },
+  { amount: 2000 },
+  { amount: 3000 },
+  { amount: 4000 },
+  { amount: 5000 }
+];
+
+export const DUMMY_NOT_PURCHASABLE_AMOUNT = [
+  { amount: 100 },
+  { amount: 999 },
+  { amount: 800 },
+  { amount: 500 },
+  { amount: 900 }
+];
