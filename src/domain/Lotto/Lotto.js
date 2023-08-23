e028bcbab636981b1f6ef24f046ee08a7cd140ea
함인규 <hig1207@midasin.com>
2023-08-23 19:55:34 +0900

    INIT :: 다 지우고 다시 로또 모델부터 생성

--- src/domain/Lotto/Lotto.js
+++ src/domain/Lotto/Lotto.js
@@ -1,22 +1,9 @@
 export class Lotto {
-    #lottoCompany
-    #lottoCustomer
     #lottoNumbers
     #winningRank
 
-    constructor(lottoCompany, lottoCustomer, lottoNumbers) {
-        this.#lottoCompany = lottoCompany;
-        this.#lottoCustomer = lottoCustomer;
+    constructor(lottoNumbers) {
         this.#lottoNumbers = lottoNumbers;
-        this.#winningRank = 0;
-    }
-
-    get lottoCompany() {
-        return this.#lottoCompany;
-    }
-
-    get lottoCustomer() {
-        return this.#lottoCustomer;
     }
 
     get lottoNumbers() {
@@ -27,7 +14,37 @@ export class Lotto {
         return this.#winningRank;
     }
 
-    setWinningRank(winningRank) {
-        this.#winningRank = winningRank;
+    #matchedWinningNumberSize(winningNumbers) {
+        return winningNumbers.filter(number => this.lottoNumbers.includes(number)).length;
+    }
+
+    #matchBonusNumber(bonusNumber) {
+        return this.lottoNumbers.includes(bonusNumber);
+    }
+
+    setWinningRank(winningNumbers, bonusNumber) {
+        const winningNumberMatchSize = this.#matchedWinningNumberSize(winningNumbers);
+        const bonusNumberMatch = this.#matchBonusNumber(bonusNumber);
+        if(winningNumberMatchSize === 6) {
+            this.#winningRank = 1;
+            return;
+        }
+        if(winningNumberMatchSize === 5 && bonusNumberMatch) {
+            this.#winningRank = 2;
+            return;
+        }
+        if(winningNumberMatchSize === 5) {
+            this.#winningRank = 3;
+            return;
+        }
+        if(winningNumberMatchSize === 4) {
+            this.#winningRank = 4;
+            return;
+        }
+        if(winningNumberMatchSize === 3) {
+            this.#winningRank = 5;
+            return;
+        }
+        this.#winningRank = 0;
     }
 }
--- src/domain/Lotto/Lotto.test.js
+++ src/domain/Lotto/Lotto.test.js
@@ -1,20 +1,60 @@
-import {Lotto} from "./Lotto";
-import {LottoCompany} from "../LottoCompany/LottoCompany";
-import {LottoCustomer} from "../LottoCustomer/LottoCustomer";
-import {DEFAULT_LOTTO_INFO} from "../../consts/lotto/lotto.const.js";
-import {LOTTO_NUMBERS_CREATED} from "../../consts/lotto/lotto.test.const.js";
+import {Lotto} from "./Lotto.js";
+
+const WINNING_NUMBERS = [1, 2, 3, 4, 5, 6];
+const BONUS_NUMBER = 7;
+
+const LOTTO_LIST = [
+    {
+        lottoNumbers: [1, 2, 3, 4, 5, 6],
+        matchingCount: 6,
+        bonusNumberMatch: false,
+        winningRank: 1
+    },
+    {
+        lottoNumbers: [1, 2, 3, 4, 5, 7],
+        matchingCount: 5,
+        bonusNumberMatch: true,
+        winningRank: 2
+    },
+    {
+        lottoNumbers: [1, 2, 3, 4, 5, 8],
+        matchingCount: 5,
+        bonusNumberMatch: false,
+        winningRank: 3
+    },
+    {
+        lottoNumbers: [1, 2, 3, 4, 7, 8],
+        matchingCount: 4,
+        bonusNumberMatch: false,
+        winningRank: 4
+    },
+    {
+        lottoNumbers: [1, 2, 3, 8, 9, 10],
+        matchingCount: 3,
+        bonusNumberMatch: false,
+        winningRank: 5
+    },
+    {
+        lottoNumbers: [1, 2, 8, 9, 10, 11],
+        matchingCount: 2,
+        bonusNumberMatch: false,
+        winningRank: 0
+    }
+];
+
+
 
 describe('로또', () => {
-    const lottoCompany = new LottoCompany(DEFAULT_LOTTO_INFO.PRICE, DEFAULT_LOTTO_INFO.PRIZE);
-    const lottoCustomer = new LottoCustomer();
+    context('로또 생성 시', () => {
 
-
-    context('로또 생성을 하면', () => {
-        const lotto = new Lotto(lottoCompany, lottoCustomer, LOTTO_NUMBERS_CREATED[0]);
-        it('로또에 회사정보, 구매자 정보, 로또 번호가 저장된다.', () => {
-            expect(lotto.lottoCompany).toEqual(lottoCompany);
-            expect(lotto.lottoCustomer).toEqual(lottoCustomer);
-            expect(lotto.lottoNumbers).toEqual(LOTTO_NUMBERS_CREATED[0]);
-        });
     });
+    context('당첨 번호와 보너스 번호가 주어졌을 때,', () => {
+        LOTTO_LIST.forEach(LOTTO => {
+            it(`로또 번호가 ${LOTTO.matchingCount}개 일치, 보너스 번호 일치 여부는 ${LOTTO.bonusNumberMatch}이면, ${LOTTO.winningRank}등이다.`, () => {
+                const lotto = new Lotto(LOTTO.lottoNumbers);
+                lotto.setWinningRank(WINNING_NUMBERS, BONUS_NUMBER);
+                expect(lotto.winningRank).toEqual(LOTTO.winningRank);
+            });
+        })
+    })
 });
