6c3f2754c8cb683342a7fc70694ac44aee2fc7b5
froggy1014 <clcl6084@gmail.com>
2023-08-24 01:22:48 +0900

    feat: refactor output

--- src/domain/view/LottoOutput.js
+++ src/domain/view/LottoOutput.js
@@ -1,23 +1,54 @@
-import { MESSAGES, LOTTO_WINNIG_PRIZE } from '../constants/index.js';
+import { LOTTO_WINNIG_PRIZE } from '../constants/index.js';
 import { sortNumberArray } from '../../util/index.js';
 
 export const LottoOutput = {
-  LOTTO_COUNT(count) {
-    console.log(`${count}개를 구매했습니다.`);
-  },
-
-  LOTTO_NUMBERS(numbers) {
-    console.log(sortNumberArray(numbers));
+  LOTTO_TICKET(lotto) {
+    return `
+    <div class="lotto-ticket d-flex items-center">
+      <div class="mx-2 text-4xl">🎟️ </div>
+      <span  data-test-id="lotto-ticket" class="text-base hidden">${sortNumberArray(lotto).join(', ')}</span>
+    <div>`;
   },
 
   LOTTO_RESULT(RESULT, PROFIT) {
-    console.log(`${MESSAGES.LOTTO_RESULT}
---------------------
-3개 일치 (${LOTTO_WINNIG_PRIZE[0]}원)- ${RESULT[0]}개
-4개 일치 (${LOTTO_WINNIG_PRIZE[1]}원)- ${RESULT[1]}개
-5개 일치 (${LOTTO_WINNIG_PRIZE[2]}원)- ${RESULT[2]}개
-5개 일치, 보너스 볼 일치(${LOTTO_WINNIG_PRIZE[3]}원)- ${RESULT[3]}개
-6개 일치 (${LOTTO_WINNIG_PRIZE[4]}원)- ${RESULT[4]}개
-총 수익률은  ${PROFIT}%입니다.`);
+    return `
+    <table class="result-table border-collapse border border-black">
+      <thead>
+        <tr class="text-center">
+          <th class="p-3">일치 갯수</th>
+          <th class="p-3">당첨금</th>
+          <th class="p-3">당첨 갯수</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="text-center">
+          <td class="p-3">3개</td>
+          <td class="p-3">${LOTTO_WINNIG_PRIZE[0]}</td>
+          <td class="p-3">${RESULT[0]}개</td>
+        </tr>
+        <tr class="text-center">
+          <td class="p-3">4개</td>
+          <td class="p-3">${LOTTO_WINNIG_PRIZE[1]}</td>
+          <td class="p-3">${RESULT[1]}개</td>
+        </tr>
+        <tr class="text-center">
+          <td class="p-3">5개</td>
+          <td class="p-3">${LOTTO_WINNIG_PRIZE[2]}</td>
+          <td class="p-3">${RESULT[2]}개</td>
+        </tr>
+        <tr class="text-center">
+          <td class="p-3">5개 + 보너스볼</td>
+          <td class="p-3">${LOTTO_WINNIG_PRIZE[3]}</td>
+          <td class="p-3">${RESULT[3]}개</td>
+        </tr>
+        <tr class="text-center">
+          <td class="p-3">6개</td>
+          <td class="p-3">${LOTTO_WINNIG_PRIZE[4]}</td>
+          <td class="p-3">${RESULT[4]}개</td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+    <p class="text-center font-bold">당신의 총 수익률은 ${PROFIT}%입니다.</p>`;
   },
 };
