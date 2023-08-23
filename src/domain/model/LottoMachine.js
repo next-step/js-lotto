1be769c20d55295b7fed3f72b24b958e086f94d2
froggy1014 <clcl6084@gmail.com>
2023-08-24 01:44:46 +0900

    feat: add selector and method on machine model

--- src/domain/model/LottoMachine.js
+++ src/domain/model/LottoMachine.js
@@ -1,9 +1,26 @@
 import { LottoOutput } from '../view/LottoOutput.js';
+import { SELECTOR } from '../constants/index.js';
 
 export class LottoMachine {
-  constructor(count) {
+  constructor(app, count) {
+    this.lottoCount = app.querySelector(SELECTOR.LOTTO_COUNT);
+    this.lottoList = app.querySelector(SELECTOR.LOTTO_LIST);
+    this.lottoToggle = app.querySelector(SELECTOR.LOTTO_TOGGLE);
+    this.lottoToggleWrapper = app.querySelector(SELECTOR.LOTTO_TOGGLE_WRAPPER);
+    this.lottoSection = app.querySelector(SELECTOR.LOTTO_SECTION);
+
+    this.count = count;
     this.output = LottoOutput;
     this.lottos = Array.from({ length: count }, () => this.issueLotto());
+
+    this.render();
+  }
+
+  render() {
+    this.lottoCount.innerHTML = `총 ${this.count}개를 구매했습니다.`;
+    this.lottoToggleWrapper.classList.remove('hidden');
+    this.lottos.forEach((lotto) => (this.lottoList.innerHTML += this.output.LOTTO_TICKET(lotto)));
+    this.lottoSection.classList.remove('hidden');
   }
 
   issueLotto() {
@@ -14,7 +31,6 @@ export class LottoMachine {
         lottoNumbers.push(number);
       }
     }
-    this.output.LOTTO_NUMBERS(lottoNumbers);
     return lottoNumbers;
   }
 }
