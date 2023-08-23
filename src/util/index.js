b410eb475a146c788b982e0637876954fa9a7a71
froggy1014 <clcl6084@gmail.com>
2023-08-24 01:08:47 +0900

    fix: disable manipulate readline

--- src/util/index.js
+++ src/util/index.js
@@ -1,4 +1,4 @@
 export * from './getProfitRate.js';
 export * from './parseSeparatedNumbers.js';
-export * from './manipulateReadline.js';
+// export * from './manipulateReadline.js';
 export * from './sortNumberArray.js';
