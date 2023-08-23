f1896300d63e83b92ff3d8eb027aa4d45d5767be
froggy1014 <clcl6084@gmail.com>
2023-08-24 01:43:53 +0900

    feat: add initial selector and method in controller

--- src/domain/controller/LottoClerk.js
+++ src/domain/controller/LottoClerk.js
@@ -1,61 +1,158 @@
+import { LOTTO_PRICE, SELECTOR } from '../constants/index.js';
 import { getProfitRate } from '../../util/index.js';
-import { LOTTO_PRICE } from '../constants/index.js';
 import { Customer } from '../model/Customer.js';
 import { LottoMachine } from '../model/LottoMachine.js';
-import { LottoInput } from '../view/LottoInput.js';
+import { validateInputPrice, validateBonusNumer, validateWinningNumber } from '../validator.js';
 import { LottoOutput } from '../view/LottoOutput.js';
 
 export class LottoClerk {
-  lottoPrice = LOTTO_PRICE;
-  output = LottoOutput;
-  input = LottoInput;
-
-  constructor() {
-    this.enterLottoStore();
+  constructor({ app }) {
+    this.budgetForm = app.querySelector(SELECTOR.BUDGET_FORM);
+    this.budgetInput = app.querySelector(SELECTOR.BUDGET_INPUT);
+    this.budgetButton = app.querySelector(SELECTOR.BUDGET_BUTTON);
+    this.budgetError = app.querySelector(SELECTOR.BUDGET_ERROR);
+    this.lottoCount = app.querySelector(SELECTOR.LOTTO_COUNT);
+    this.lottoList = app.querySelector(SELECTOR.LOTTO_LIST);
+    this.lottoToggle = app.querySelector(SELECTOR.LOTTO_TOGGLE);
+    this.lottoToggleWrapper = app.querySelector(SELECTOR.LOTTO_TOGGLE_WRAPPER);
+    this.lottoSection = app.querySelector(SELECTOR.LOTTO_SECTION);
+    this.winningForm = app.querySelector(SELECTOR.WINNING_FORM);
+    this.winningButton = app.querySelector(SELECTOR.WINNING_BUTTON);
+    this.winningError = app.querySelector(SELECTOR.WINNING_ERROR);
+    this.modal = app.querySelector(SELECTOR.MODAL);
+    this.modalBody = app.querySelector(SELECTOR.MODAL_BODY);
+    this.modalCloseButton = app.querySelector(SELECTOR.MODAL_CLOSE);
+    this.retryButton = app.querySelector(SELECTOR.RETRY_BUTTON);
+    this.output = LottoOutput;
+    this.initState();
+    this.initRender();
+    this.bindEvent();
   }
 
-  async enterLottoStore() {
-    const money = await this.input.ASK_MONEY();
-    this.purchaseLotto(money);
+  initState = () => {
+    this.budget = 0;
+    this.lottos = [];
+    this.lottoPrice = LOTTO_PRICE;
+    this.winningNumber = Array.from({ length: 6 });
+    this.lottoCount = 0;
+    this.lottoCycle = false;
+    delete this.machine;
+    delete this.customer;
+  };
+  
+  initRender = () => {
+    this.winningForm.querySelectorAll('input')
+    .forEach((input) => (input.value = ''));
+    this.winningForm.classList.add('hidden');
+    this.lottoToggleWrapper.classList.add('hidden');
+    this.lottoSection.classList.add('hidden');
+    this.lottoList.classList.remove('flex-col');
+    this.lottoToggle.checked = false;
+    this.budgetInput.disabled = false;
+    this.budgetButton.disabled = false;
+    this.budgetButton.disabled = true;
+    this.lottoList.innerHTML = '';
+    this.budgetInput.value = '';
+  };
 
-    const winningNumbers = await this.input.ASK_WINNING_NUMBERS();
-    const bonus = await this.input.ASK_BONUS_NUMBER(winningNumbers);
-    this.checkoutLotto(winningNumbers, bonus);
+  bindEvent() {
+    this.budgetForm.addEventListener('submit', this.enterLottoStore);
+
+    this.winningForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      try {
+        validateWinningNumber(this.winningNumber);
+        validateBonusNumer(this.winningNumber, this.bonusNumber);
+        this.winningError.innerHTML = '';
+        this.checkoutLotto();
+      } catch (error) {
+        this.winningError.innerHTML = error.message;
+      }
+    });
+
+    this.winningForm.querySelectorAll('input').forEach((input, index) => {
+      input.addEventListener('input', (event) => {
+        index === 6
+          ? (this.bonusNumber = Number(event.target.value))
+          : (this.winningNumber[index] = Number(event.target.value));
+      });
+    });
+
+    this.budgetInput.addEventListener('input', (event) => {
+      try {
+        validateInputPrice(event.target.value);
+        this.budgetError.innerHTML = '';
+        this.budgetButton.disabled = false;
+      } catch (error) {
+        this.budgetError.innerHTML = error.message;
+        this.budgetButton.disabled = true;
+      } finally {
+        this.budget = Math.round(event.target.value / 1000) * 1000;
+        this.lottoCount = this.budget / this.lottoPrice;
+      }
+    });
+
+    this.budgetButton.addEventListener('click', this.enterLottoStore);
+
+    this.lottoToggle.addEventListener('change', this.toggleLotto);
+
+    this.retryButton.addEventListener('click', this.retryLotto);
+
+    this.modalCloseButton.addEventListener('click', this.closeModal);
   }
 
-  purchaseLotto(money) {
-    const count = Math.floor(money / this.lottoPrice);
+  enterLottoStore = (event) => {
+    event.preventDefault();
 
-    this.customer = new Customer(count * this.lottoPrice);
-    this.output.LOTTO_COUNT(count);
-    this.machine = new LottoMachine(count);
-  }
+    this.machine = new LottoMachine(app, Math.floor(this.budget / this.lottoPrice));
+    this.customer = new Customer(this.budget);
 
-  checkoutLotto(winning, bonus) {
-    const totalCounts = this.machine.lottos.map((lotto) =>
-      this.#countWinningNumber(lotto, winning, bonus),
-    );
+    this.winningForm.classList.remove('hidden');
+    this.budgetInput.disabled = true;
+    this.budgetButton.disabled = true;
+  };
+
+  toggleLotto = (event) => {
+    const { target } = event;
+    this.icons = document.querySelectorAll(SELECTOR.LOTTO_TICKET);
+
+    const addClass = target.checked;
+    this.lottoList.classList.toggle('flex-col', addClass);
+    this.icons.forEach((child) => child.classList.toggle('hidden', !addClass));
+  };
+
+  checkoutLotto = () => {
+    const totalCounts = this.machine.lottos.map((lotto) => this.#countWinningNumber(lotto));
     totalCounts.forEach((count) => this.customer.countResult(count));
     this.announceResult();
-  }
+  };
+
   announceResult() {
-    const profitRate = getProfitRate(this.customer.money, this.customer.amount);
-    this.output.LOTTO_RESULT(this.customer.results, profitRate);
-    this.askRetry();
-  }
-
-  async askRetry() {
-    const retry = await this.input.ASK_RETRY();
-    if (retry === 'y') {
-      this.enterLottoStore();
-    } else {
-      this.input.EXIT_LOTTO_STORE();
+    if (!this.lottoCycle) {
+      const profitRate = getProfitRate(this.customer.money, this.customer.amount);
+      this.modalBody.innerHTML = this.output.LOTTO_RESULT(this.customer.results, profitRate);
     }
+    this.modal.classList.add('open');
   }
 
-  #countWinningNumber(lotto, winning, bonus) {
-    const winningCount = winning.filter((number) => lotto.includes(number)).length;
-    const bonusCount = lotto.includes(bonus) ? 2 : 0;
-    return winningCount + bonusCount;
+  retryLotto = () => {
+    this.modal.classList.remove('open');
+    this.initState();
+    this.initRender();
+  };
+
+  closeModal = () => {
+    this.modal.classList.remove('open');
+    this.lottoCycle = true;
+  };
+
+  #countWinningNumber(lotto) {
+    const winningCount = this.winningNumber.filter((number) => lotto.includes(number));
+    const bonusCount = lotto.includes(this.bonusNumber);
+
+    if (bonusCount) {
+      return winningCount.length === 4 ? 6 : winningCount.length === 5 ? 7 : winningCount.length;
+    }
+    return winningCount.length === 6 ? 7 : winningCount.length;
   }
 }
