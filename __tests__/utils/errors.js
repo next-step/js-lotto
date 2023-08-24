e49171254b4dbae08a9d5673baef34b62927799f
ericagong <48196721+ericagong@users.noreply.github.com>
2023-08-24 17:40:04 +0900

    refactor: test layer과 product layer 분리 및 FixedIssueStrategy 테스트코드 삭제

---
+++ __tests__/utils/errors.js
@@ -0,0 +1,39 @@
+export class FixedNumberNotArrayError extends RuntimeError {
+  static MESSAGE = "고정 숫자는 배열이어야합니다.";
+
+  constructor() {
+    super(FixedNumberNotArrayError.MESSAGE);
+  }
+}
+
+export class FixedNumberLengthNotSixError extends RuntimeError {
+  static MESSAGE = "고정 숫자는 6개여야합니다.";
+
+  constructor() {
+    super(FixedNumberLengthNotSixError.MESSAGE);
+  }
+}
+
+export class FixedNumberElementNotNumberError extends RuntimeError {
+  static MESSAGE = "고정 숫자의 요소는 숫자여야합니다.";
+
+  constructor() {
+    super(FixedNumberElementNotNumberError.MESSAGE);
+  }
+}
+
+export class FixedNumberElementOutOfRangeError extends RuntimeError {
+  static MESSAGE = "고정 숫자의 요소는 1부터 45 사이여야합니다.";
+
+  constructor() {
+    super(FixedNumberElementOutOfRangeError.MESSAGE);
+  }
+}
+
+export class FixedNumberElementDuplicatedError extends RuntimeError {
+  static MESSAGE = "고정 숫자의 요소는 중복되지 않아야합니다.";
+
+  constructor() {
+    super(FixedNumberElementDuplicatedError.MESSAGE);
+  }
+}
