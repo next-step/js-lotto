0bc2c72cc756c1de3f76ebc328f65b6d6a7d7d79
Youngchai Kim <yckim1224@kigam.re.kr>
2023-08-24 23:07:09 +0900

    test: generateRandomLottoNumbers 함수 테스트 보강

--- __tests__/lotto.test.js
+++ __tests__/lotto.test.js
@@ -5,11 +5,15 @@ import {
 } from "../src/js/constants";
 import { generateRandomLottoNumbers } from "../src/js/lotto";
 
-test("generateRandomLottoNumbers function should generate correct lotto numbers", () => {
-  const lottoNumbers = generateRandomLottoNumbers();
-  expect(lottoNumbers.length).toEqual(NUMBER_OF_LOTTO_NUMBERS);
-  lottoNumbers.forEach((number) => {
-    expect(number).toBeGreaterThanOrEqual(MIN_LOTTO_NUMBER);
-    expect(number).toBeLessThanOrEqual(MAX_LOTTO_NUMBER);
-  });
-});
+test.each(Array.from({ length: 20 }))(
+  "generateRandomLottoNumbers function should generate correct lotto numbers",
+  () => {
+    const lottoNumbers = generateRandomLottoNumbers();
+    console.log(lottoNumbers);
+    expect(lottoNumbers.length).toEqual(NUMBER_OF_LOTTO_NUMBERS);
+    lottoNumbers.forEach((number) => {
+      expect(number).toBeGreaterThanOrEqual(MIN_LOTTO_NUMBER);
+      expect(number).toBeLessThanOrEqual(MAX_LOTTO_NUMBER);
+    });
+  }
+);
