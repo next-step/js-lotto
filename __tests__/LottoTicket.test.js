f59b317ce3e8c379850210a3bd58713f932c7fd6
Youngchai Kim <yckim1224@kigam.re.kr>
2023-08-25 00:05:39 +0900

    test: LottoTicket 테스트 코드 작성

---
+++ __tests__/LottoTicket.test.js
@@ -0,0 +1,17 @@
+class LottoTicket {}
+
+describe("A lotto ticket", () => {
+  it.each([
+    [], // it's empty
+    [1, 2, 3, 4, 5], // only 5 numbers
+    [1, 2, 3, 4, 5, 6, 7], // too many numbers
+    [1, 2, 3, 4, 5, 5], // duplicate
+    [0, 1, 2, 3, 4, 5], // first number is too small
+    [1, 2, 3, 4, 5, 46], // last number is too big
+  ])(
+    "should throw an error when invalid lotto numbers are given",
+    (invalidLottoNumbers) => {
+      expect(() => new LottoTicket(invalidLottoNumbers)).toThrow();
+    }
+  );
+});
