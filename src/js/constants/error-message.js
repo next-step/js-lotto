61d7af26fbef0d0ed8531ffd33df1376e89bd133
Jason <jasondal91@gmail.com>
2023-08-24 21:21:07 +0900

    chore: error message 추가

--- src/js/constants/error-message.js
+++ src/js/constants/error-message.js
@@ -7,4 +7,5 @@ export const ERROR_MESSAGE = Object.freeze({
   NOT_VALID_LOTTO_AMOUNT_UNIT: (unit) =>
     `로또 구입 금액을 ${unit}원 단위로 입력해 주세요.`,
   NOT_VALID_RANDOM_RANGE: '범위의 시작 숫자기 끝 숫자보다 클 수 없습니다.',
+  NOT_VALID_LOTTO_NUMBER: '로또 번호는 정수이어야 합니다.',
 });
