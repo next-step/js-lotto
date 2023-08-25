9f52f3c08b7e7458b94d8a95f398358cbbfbe9df
함인규 <hig1207@midasin.com>
2023-08-25 10:35:52 +0900

    FEAT :: LottoWinningCondition 테스트 작성 완료

--- src/domain/LottoWinningCondition/LottoWinningCondition.js
+++ src/domain/LottoWinningCondition/LottoWinningCondition.js
@@ -5,19 +5,22 @@ class LottoWinningCondition {
     #bonusNumber;
 
     constructor(winningNumbersString, BonusNumberString) {
-        this.#setWinningNumbers(winningNumbersString);
-        this.#setBonusNumber(BonusNumberString);
+        const winningNumbers = winningNumbersString.split(",").map((number) => Number(number));
+        const bonusNumber = Number(BonusNumberString);
+
+        if(!this.#isAllUniqueNumber([...winningNumbers, bonusNumber]))
+            throw new Error("당첨 번호는 중복될 수 없습니다.");
+        this.#setWinningNumbers(winningNumbers);
+        this.#setBonusNumber(bonusNumber);
     }
 
-    #setWinningNumbers(winningNumbersString) {
-       const winningNumbers = winningNumbersString.split(",").map((number) => Number(number));
+    #setWinningNumbers(winningNumbers) {
        if(!this.#isAllValidLottoNumber(winningNumbers))
              throw new Error(`입력은 ${LOTTO_INFO.NUMBER_MIN} ~ ${LOTTO_INFO.NUMBER_MAX} 사이의 숫자여야 합니다.`);
        this.#winningNumbers = winningNumbers;
     }
 
-    #setBonusNumber(bonusNumberString) {
-        const bonusNumber = Number(bonusNumberString);
+    #setBonusNumber(bonusNumber) {
         if(!this.#isValidateLottoNumber(bonusNumber))
             throw new Error(`입력은 ${LOTTO_INFO.NUMBER_MIN} ~ ${LOTTO_INFO.NUMBER_MAX} 사이의 숫자여야 합니다.`);
         this.#bonusNumber = bonusNumber;
@@ -29,6 +32,10 @@ class LottoWinningCondition {
 
     #isValidateLottoNumber (number) {
         return Number(number) >= LOTTO_INFO.NUMBER_MIN && Number(number) <= LOTTO_INFO.NUMBER_MAX;
+    }
+
+    #isAllUniqueNumber (numbers) {
+        return numbers.length === new Set(numbers).size;
     }
 
     get winningNumbers() {
