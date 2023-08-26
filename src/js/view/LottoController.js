41dad402674f39a7b6b295d5385379a52d05bf80
Jason <jasondal91@gmail.com>
2023-08-26 14:48:39 +0900

    feat: controller restart 추가

--- src/js/view/LottoController.js
+++ src/js/view/LottoController.js
@@ -1,3 +1,4 @@
+import { ERROR_MESSAGE } from '../constants/error-message.js';
 import { LOTTO_AMOUNT_UNIT } from '../constants/lotto.js';
 import { Lotto } from '../domain/Lotto.js';
 import { LottoMachine } from '../domain/LottoMachine.js';
@@ -25,21 +26,55 @@ export class LottoController {
     );
 
     const purchaseAmount = this.#lottos.length * LOTTO_AMOUNT_UNIT;
+
     this.#lottoView.printLottoResult(lottoResult, purchaseAmount);
+
+    await this.#restart();
+
+    this.#lottoView.closeInput();
+  }
+
+  async #restart() {
+    try {
+      const inputValue = await this.#lottoView.inputRestart();
+      if (this.#isRestart(inputValue)) {
+        await this.start();
+      }
+    } catch (error) {
+      this.#lottoView.printError(error);
+      await this.#restart();
+    }
   }
 
   async #setLotto() {
-    const purchaseAmount = await this.#lottoView.inputPurchaseAmount();
-    this.#lottos = this.#lottoMachine.issueLotto(purchaseAmount);
-    this.#lottoView.printPurchaseAmount(this.#lottos);
+    try {
+      const purchaseAmount = await this.#lottoView.inputPurchaseAmount();
+      this.#lottos = this.#lottoMachine.issueLotto(purchaseAmount);
+      this.#lottoView.printPurchaseAmount(this.#lottos);
+    } catch (error) {
+      this.#lottoView.printError(error);
+      await this.#setLotto();
+    }
   }
 
   async #setWinningLotto() {
-    const winningLottoNunbers = await this.#lottoView.inputWinningNumber();
-    const bonusNumber = await this.#lottoView.inputBonusNumber();
-    this.#winningLotto = new WinningLotto(
-      new Lotto(winningLottoNunbers.split(',')),
-      bonusNumber
-    );
+    try {
+      const winningLottoNunbers = await this.#lottoView.inputWinningNumber();
+      const bonusNumber = await this.#lottoView.inputBonusNumber();
+      this.#winningLotto = new WinningLotto(
+        new Lotto(winningLottoNunbers.split(',')),
+        bonusNumber
+      );
+    } catch (error) {
+      this.#lottoView.printError(error);
+      await this.#setWinningLotto();
+    }
+  }
+
+  #isRestart(value) {
+    if (value === 'y' || value === 'n') {
+      return value === 'y' ? true : false;
+    }
+    throw new Error(ERROR_MESSAGE.NOT_VALID_RESTART_ANSWER);
   }
 }
