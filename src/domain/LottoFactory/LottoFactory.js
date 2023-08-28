1405421ccc365372df34d4a92da6c3fc04014072
함인규 <hig1207@midasin.com>
2023-08-28 20:14:02 +0900

    FIX :: 랜덤한 숫자 반환하는 Util 생성

--- src/domain/LottoFactory/LottoFactory.js
+++ src/domain/LottoFactory/LottoFactory.js
@@ -1,6 +1,7 @@
 import {LOTTO_INFO} from "../../consts/Lotto.js";
 import {Lotto} from "../Lotto/Lotto.js";
 import {LottoList} from "../LottoList/LottoList.js";
+import {getRandomNumber} from "../../utils/random.js";
 
 //랜덤한 로또 생성을 담당하는 객체 (모든 요소가 랜덤하기 때문에 테스트할 필요는 없어보임)
 class LottoFactory {
@@ -16,7 +17,7 @@ class LottoFactory {
     }
 
     static #getRandomLottoNumber() {
-        return Math.floor(Math.random() * LOTTO_INFO.NUMBER_MAX) + LOTTO_INFO.NUMBER_MIN;
+        return getRandomNumber(LOTTO_INFO.NUMBER_MIN, LOTTO_INFO.NUMBER_MAX);
     }
 
     static createLottoList(lottoCount) {
