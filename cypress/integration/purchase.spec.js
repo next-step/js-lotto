c3cfa19da5f991b9e2abff14c4e21987072dfd96
Gn0lee <jinho605@gmail.com>
2023-08-30 12:58:46 +0900

    중간 저장

--- cypress/integration/purchase.spec.js
+++ cypress/integration/purchase.spec.js
@@ -12,11 +12,36 @@ describe("로또 구매부분 테스트", () => {
     cy.get("button").get(".purchase").should("be.visible");
   });
 
-  it("구입 금액이 0인 상태로 확인 버튼을 누르면 alert이 발생한다.", () => {
+  xit("구입 금액이 0인 상태로 확인 버튼을 누르면 alert이 발생한다.", () => {
     cy.get("input").get(".payment").type("-1000");
     cy.get("button").get(".purchase").click();
     cy.get("@alertStub")
       .should("have.been.calledOnce")
       .and("have.been.calledWith", "지불 금액은 0이상 이어야 합니다.");
+  });
+
+  it("구입금액 3000을 입력후 확인 버튼을 클릭하면 로또 3개가 구입되며 구입 정보가 화면에 표시된다.", () => {
+    cy.get(".purchase-info").should("not.exist");
+
+    cy.get("input").get(".payment").type("3000");
+    cy.get("button").get(".purchase").click();
+
+    cy.get(".purchase-info")
+      .should("be.visible")
+      .and("contain.text", "총 3개를 구매하였습니다.");
+
+    cy.get(".switch").should("be.visible");
+  });
+
+  it("3000원 구매후 번호 보기를 클릭하면 로또 3장이 화면에 표시된다.", () => {
+    cy.get("input").get(".payment").type("3000");
+    cy.get("button").get(".purchase").click();
+
+    cy.get(".lotto-box").should("not.be.visible");
+
+    cy.get("label").get(".switch").click();
+
+    cy.get(".lotto-box").should("be.visible");
+    cy.get(".lotto-ticket").should("be.visible").and("have.length", 3);
   });
 });
--- src/class/step3/Viewer.js
+++ src/class/step3/Viewer.js
@@ -1,10 +1,18 @@
 export default class Viewer {
   #purchaseButton;
   #paymentInput;
+  #purchaseInfoBox;
+  #showTicketsSwitch;
+  #lottoBox;
+  #lottoTickets;
 
   constructor() {
     this.#purchaseButton = document.querySelector("button.purchase");
     this.#paymentInput = document.querySelector("input.payment");
+    this.#purchaseInfoBox = document.querySelector(".purchase-info");
+    this.#showTicketsSwitch = document.querySelector("label.switch");
+    this.#lottoBox = document.querySelector(".lotto-box");
+    this.#lottoTickets = document.querySelectorAll(".lotto-ticket");
   }
 
   addPurchaseButtonClickListener(callback) {
