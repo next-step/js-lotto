d111be7614b5a63e72a856da748207ec19f6a81a
Youngchai Kim <yckim1224@kigam.re.kr>
2023-08-31 20:32:26 +0900

    fix: 의도치 않은 bitwise or 연산자 사용 변경

--- src/step1-index.js
+++ src/step1-index.js
@@ -63,13 +63,13 @@ import { Console } from "./js/view/Console";
   view.print("");
   view.print("당첨 통계");
   view.print("--------------------");
-  view.print(`3개 일치 (5,000원) - ${statistics.placeMap[5] | 0}개`);
-  view.print(`4개 일치 (50,000원) - ${statistics.placeMap[4] | 0}개`);
-  view.print(`5개 일치 (1,500,000원) - ${statistics.placeMap[3] | 0}개`);
+  view.print(`3개 일치 (5,000원) - ${statistics.placeMap[5] ?? 0}개`);
+  view.print(`4개 일치 (50,000원) - ${statistics.placeMap[4] ?? 0}개`);
+  view.print(`5개 일치 (1,500,000원) - ${statistics.placeMap[3] ?? 0}개`);
   view.print(
-    `5개 일치, 보너스 볼 일치 (30,000,000원) - ${statistics.placeMap[2] | 0}개`
+    `5개 일치, 보너스 볼 일치 (30,000,000원) - ${statistics.placeMap[2] ?? 0}개`
   );
-  view.print(`6개 일치 (2,000,000,000원) - ${statistics.placeMap[1] | 0}개`);
+  view.print(`6개 일치 (2,000,000,000원) - ${statistics.placeMap[1] ?? 0}개`);
   view.print(`총 수익률은 ${statistics.rateOfReturn.toFixed(2)}%입니다.`);
   view.close();
 })();
