eb5fc757f2177bcc8618e57aff34c1011a6cbdd4
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-09-01 14:29:39 +0900

    chore: step3 환경설정

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,1 +1,19 @@
-console.log('step3!');
+import { View } from './View';
+import { GameController } from './Controller/GameController';
+
+class App {
+  #view;
+  #controller;
+
+  constructor() {
+    this.#view = new View();
+    this.#controller = new GameController(this.#view);
+  }
+
+  play() {
+    this.#controller.LottoGameProcess();
+  }
+}
+
+const app = new App();
+app.play();
---
+++ src/step3-index.js
@@ -0,0 +1,5 @@
+class App {}
+
+const app = new App();
+
+console.log('step3');
