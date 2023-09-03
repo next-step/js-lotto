262b3682e21a2be2bf135c9e39c5b944a9ace65e
손진영 <sm099@naver.com>
2023-09-03 20:37:49 +0900

    style(cypress): 불필요한 파일 제거 및 주석 제거

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
-      // implement node event listeners here
-      console.log(on, config);
-    },
     baseUrl: 'http://localhost:9000',
   },
 });
