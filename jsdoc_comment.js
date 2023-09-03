a7240842e942d5a99911a6100c39771b94763cb7
Frank Kim <frankuserconnect@gmail.com>
2023-09-03 11:51:35 +0900

    refactor(UI): adding js doc documentation + refactoring switch case to object function

---
+++ jsdoc_comment.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {Object} RankPriceStatsFunctions
+ * @property {(count: number) => string} firstWinner
+ * @property {(count: number) => string} secondWinner
+ * @property {(count: number) => string} thirdWinner
+ * @property {(count: number) => string} fourthWinner
+ * @property {(count: number) => string} fifthWinner
+ */
+
+/**
+ * @typedef {Object} LottoStats
+ * @property {number} firstWinner -  6
+ * @property {number} secondWinner - 5 + bonus
+ * @property {number} thirdWinner - 5
+ * @property {number} fourthWinner - 4
+ * @property {number} fifthWinner - 3
+ */
--- src/UI/View/OutputView.js
+++ src/UI/View/OutputView.js
@@ -1,59 +1,37 @@
 import { LOTTO_PRICE } from "../../domain/constant.js";
 
-/**
- * @param {number} profitRate
- * @returns
- */
+require("../../../jsdoc_comment.js");
+
+/** @typedef {RankPriceStatsFunctions} */
+const RANK_PRICE_STATS = {
+  firstWinner: count =>
+    `6개 일치 (${LOTTO_PRICE.FIRST_WINNER.toLocaleString()}원) - ${count}개`,
+  secondWinner: count =>
+    `5개 일치, 보너스 볼 일치 (${LOTTO_PRICE.SECOND_WINNER.toLocaleString()}원) - ${count}개`,
+  thirdWinner: count =>
+    `5개 일치 (${LOTTO_PRICE.THIRD_WINNER.toLocaleString()}원) - ${count}개`,
+  fourthWinner: count =>
+    `4개 일치 (${LOTTO_PRICE.FOURTH_WINNER.toLocaleString()}원) - ${count}개`,
+  fifthWinner: count =>
+    `3개 일치 (${LOTTO_PRICE.FIFTH_WINNER.toLocaleString()}원) - ${count}개`,
+};
+
+/** @param {number} profitRate */
 const ViewProfitRate = profitRate =>
   console.log(`총 수익률: ${profitRate.toFixed(1)}%`);
 
-/**
- * @param {number[][]} buyerLottoList
- */
+/** @param {number[][]} buyerLottoList */
 const ViewBuyerLottoList = buyerLottoList => {
   buyerLottoList.forEach(list => console.log(list));
 };
 
-/**
- * @typedef {Object} LottoStats
- * @property {number} firstWinner -  6
- * @property {number} secondWinner - 5 + bonus
- * @property {number} thirdWinner - 5
- * @property {number} fourthWinner - 4
- * @property {number} fifthWinner - 3
- */
+/** @typedef {LottoStats} */
 const ViewWinnerPriceStats = stats => {
   console.log("당첨 통계");
   console.log("--------------------");
 
   for (const [rank, count] of Object.entries(stats)) {
-    switch (rank) {
-      case "firstWinner":
-        console.log(
-          `6개 일치 (${LOTTO_PRICE.FIRST_WINNER.toLocaleString()}원) - ${count}개`
-        );
-        break;
-      case "secondWinner":
-        console.log(
-          `5개 일치, 보너스 볼 일치 (${LOTTO_PRICE.SECOND_WINNER.toLocaleString()}원) - ${count}개`
-        );
-        break;
-      case "thirdWinner":
-        console.log(
-          `5개 일치 (${LOTTO_PRICE.THIRD_WINNER.toLocaleString()}원) - ${count}개`
-        );
-        break;
-      case "fourthWinner":
-        console.log(
-          `4개 일치 (${LOTTO_PRICE.FOURTH_WINNER.toLocaleString()}원) - ${count}개`
-        );
-        break;
-      case "fifthWinner":
-        console.log(
-          `3개 일치 (${LOTTO_PRICE.FIFTH_WINNER.toLocaleString()}원) - ${count}개`
-        );
-        break;
-    }
+    console.log(RANK_PRICE_STATS[rank](count));
   }
 };
 
