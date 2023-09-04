a5cf97ceea83b6e9a42ddda8cffa6d551b26e85e
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-09-04 21:32:41 +0900

    ♻️ refactor: 로또 실행 계층 분리 (로또구매&실행 - 로또당첨머신 설정 - 당첨계산)

--- src/domain/lottoApp.js
+++ src/domain/lottoApp.js
@@ -3,25 +3,37 @@ import { createPurchaseMessage } from './lottoMessageCreator';
 import { LottoCustomer, LottoSeller, LottoOrganizer, LottoMachine, LottoCalculator } from './classes/index';
 import { readPurchaseAmount, readLottoNumberAndBonusNumber, endPrompter } from './lottoPrompter';
 
+const buyAndPrintLottoTickets = async (lottoCustomer, lottoSeller) => {
+  lottoCustomer.buyAutoLottoTicket(lottoSeller);
+  const lottoNumbers = lottoCustomer.lottoTickets.map(({ lottoNumber }) => lottoNumber);
+
+  console.log(createPurchaseMessage(lottoCustomer.lottoTickets.length));
+  lottoNumbers.forEach((lottoNumber) => {
+    console.log(lottoNumber.sort((a, b) => a - b));
+  });
+};
+
+const asyncSetupLottoMachine = async () => {
+  const { lottoNumber: winningLottoNumber, bonusNumber } = await readLottoNumberAndBonusNumber();
+  return new LottoMachine(winningLottoNumber, bonusNumber);
+};
+
+const calculateLottoWiningRate = (lottoMachine, lottoCustomer) => {
+  const lottoCalculator = new LottoCalculator(lottoMachine.winningLottoNumber, lottoMachine.bonusNumber);
+  const lottoOrganizer = new LottoOrganizer(lottoCalculator);
+  lottoOrganizer.matchToLottoTickets(lottoCustomer.lottoTickets);
+  return lottoOrganizer.getWinningReturnRate(lottoCustomer.lottoTickets);
+};
+
 const lottoApp = async () => {
   try {
     const { purchaseAmount } = await readPurchaseAmount();
     const lottoCustomer = new LottoCustomer(purchaseAmount);
     const lottoSeller = new LottoSeller();
-    lottoCustomer.buyAutoLottoTicket(lottoSeller);
-    const lottoNumbers = lottoCustomer.lottoTickets.map(({ lottoNumber }) => lottoNumber);
-    console.log(createPurchaseMessage(lottoCustomer.lottoTickets.length));
-    lottoNumbers.forEach((lottoNumber) => {
-      console.log(lottoNumber.sort((a, b) => a - b));
-    });
 
-    const { lottoNumber: winningLottoNumber, bonusNumber } = await readLottoNumberAndBonusNumber();
-    const lottoMachine = new LottoMachine(winningLottoNumber, bonusNumber);
-    const lottoCalculator = new LottoCalculator(lottoMachine.winningLottoNumber, lottoMachine.bonusNumber);
-    const lottoOrganizer = new LottoOrganizer(lottoCalculator);
-    lottoOrganizer.matchToLottoTickets(lottoCustomer.lottoTickets);
-
-    const winningRate = lottoOrganizer.getWinningReturnRate(lottoCustomer.lottoTickets);
+    buyAndPrintLottoTickets(lottoCustomer, lottoSeller);
+    const lottoMachine = await asyncSetupLottoMachine();
+    const winningRate = calculateLottoWiningRate(lottoMachine, lottoCustomer);
     const lottoTickets = lottoCustomer.lottoTickets.map(({ lottoNumber, result }) => ({ lottoNumber, result }));
 
     return {
