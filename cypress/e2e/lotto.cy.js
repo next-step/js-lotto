fcf3df7183dbfe4e60620b919ee5abf03b6aa019
ding-co <pangoons@naver.com>
2023-09-05 18:43:59 +0900

    feat: Add Lotto Answer/bonus number input, statistics modal with cypress e2e test

--- cypress/e2e/lotto.cy.js
+++ cypress/e2e/lotto.cy.js
@@ -4,6 +4,13 @@ const SELECTOR = Object.freeze({
   LOTTO_ICON: '[data-ticket="purchase-lottos__ticket"]',
   LOTTO_SWITCH: '#lotto-numbers-toggle-button',
   LOTTO_CANDIDATE: '[data-candidate="lotto-candidate"]',
+  ANSWERS_FORM: '#lotto-answers-form',
+  WINNING_NUMBER: '[data-winning="winning-number"]',
+  BONUS_NUMBER: '#bonus-number',
+  RESULT_BUTTON: '.open-result-modal-button',
+  MODAL: '.modal',
+  RESTART_BUTTON: '#restart-button',
+  PURCHASE_AMOUNT_INPUT: '#purchase-amount__input',
 });
 
 const isVisible = (selector) => {
@@ -110,6 +117,80 @@ describe('로또 구입, 발행', () => {
       const lottoNumbers = $el.text().split(',').map(Number);
       const sortedNumbers = lottoNumbers.sort((a, b) => a - b);
       expect(lottoNumbers).to.deep.equal(sortedNumbers);
+    });
+  });
+});
+
+describe('로또 당첨 번호 및 보너스 번호 지정, 당첨 통계 확인', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/index.html');
+    typing(SELECTOR.PRICE_FORM, 5_000);
+    click(SELECTOR.PRICE_BUTTON);
+    check(SELECTOR.LOTTO_SWITCH);
+  });
+
+  it('로또 당첨 번호 입력 폼이 있는지 확인한다.', () => {
+    isVisible(SELECTOR.ANSWERS_FORM);
+  });
+
+  it('로또 당첨 번호 입력 6개와 보너스 번호 입력 1개 폼이 있는지 확인한다.', () => {
+    cy.get(SELECTOR.WINNING_NUMBER).should(($el) => {
+      expect($el).to.have.length(6);
+    });
+
+    isVisible(SELECTOR.BONUS_NUMBER);
+  });
+
+  it('입력한 로또 당첨 번호 6개, 보너스 번호 1개가 1~45인지 확인한다.', () => {
+    cy.get(SELECTOR.WINNING_NUMBER).each(($el, idx) => {
+      typing($el, idx + 1);
+      expect(idx + 1).to.be.within(1, 45);
+    });
+
+    typing(SELECTOR.BONUS_NUMBER, 7);
+    expect(7).to.be.within(1, 45);
+  });
+
+  it('입력한 로또 당첨 번호 6개, 보너스 번호 1개는 중복되지 않는다.', () => {
+    const winningNumbersWithBonusNumber = [];
+
+    cy.get(SELECTOR.WINNING_NUMBER).each(($el, idx) => {
+      typing($el, idx + 1);
+    });
+    cy.get(SELECTOR.WINNING_NUMBER).each(($el) => {
+      const winningNumber = Number($el.text());
+      winningNumbersWithBonusNumber.push(winningNumber);
+    });
+
+    typing(SELECTOR.BONUS_NUMBER, 7);
+    winningNumbersWithBonusNumber.push(7);
+
+    const uniqueNumbers = [...new Set(winningNumbersWithBonusNumber)];
+    expect(winningNumbersWithBonusNumber).to.have.length(uniqueNumbers.length);
+  });
+
+  it('로또 당첨 번호 6개, 보너스 번호 1개를 입력하고 결과 확인하기 버튼을 누르면 통계 모달이 나온다.', () => {
+    cy.get(SELECTOR.WINNING_NUMBER).each(($el, idx) => {
+      typing($el, idx + 1);
+    });
+    typing(SELECTOR.BONUS_NUMBER, 7);
+    click(SELECTOR.RESULT_BUTTON);
+
+    isVisible(SELECTOR.MODAL);
+  });
+
+  it('통계 모달이 나왔을 때 다시 시작하기 버튼을 누르면 모달이 꺼지고 초기 상태로 돌아간다.', () => {
+    cy.get(SELECTOR.WINNING_NUMBER).each(($el, idx) => {
+      typing($el, idx + 1);
+    });
+    typing(SELECTOR.BONUS_NUMBER, 7);
+    click(SELECTOR.RESULT_BUTTON);
+
+    click(SELECTOR.RESTART_BUTTON);
+    cy.get(SELECTOR.MODAL).should('not.be.visible');
+    cy.get(SELECTOR.PURCHASE_AMOUNT_INPUT).then(($el) => {
+      const value = $el.text();
+      expect(value).to.have.length(0);
     });
   });
 });
