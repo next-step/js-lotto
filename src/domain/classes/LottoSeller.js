28eab28ab5a8ef7eff7258e5834594e5b819b63d
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-09-06 11:41:49 +0900

    ♻️ refactor: 티켓 판매 메서드 관심사 분리

--- src/domain/classes/LottoSeller.js
+++ src/domain/classes/LottoSeller.js
@@ -38,20 +38,20 @@ class LottoSeller {
     return lottoTickets;
   }
 
-  sellToManualLottoTicket(amount, lottoNumber) {
-    this.#validateLottoPurchaseAvailability.validateLottoPurchaseAvailability(amount, this.#lottoPrice);
+  #sellTickets(amount, createTicket) {
+    this.#validateLottoPurchaseAvailability(amount, this.#lottoPrice);
     const sellCount = this.#calculateSellCount(amount);
-    const lottoTickets = this.#createManualLottoTickets(sellCount, lottoNumber);
+    const lottoTickets = createTicket(sellCount);
     this.#increaseSellCount(sellCount);
     return { changeAmount: amount % this.#lottoPrice, lottoTickets };
   }
 
+  sellToManualLottoTicket(amount, lottoNumber) {
+    return this.#sellTickets(amount, (sellCount) => this.#createManualLottoTickets(sellCount, lottoNumber));
+  }
+
   sellToAutoLottoTicket(amount) {
-    this.#validateLottoPurchaseAvailability(amount, this.#lottoPrice);
-    const sellCount = this.#calculateSellCount(amount);
-    const lottoTickets = this.#createAutoLottoTickets(sellCount);
-    this.#increaseSellCount(sellCount);
-    return { changeAmount: amount % this.#lottoPrice, lottoTickets };
+    return this.#sellTickets(amount, this.#createAutoLottoTickets.bind(this));
   }
 }
 
