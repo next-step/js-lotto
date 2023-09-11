9223d91c37a7e2806f065dc1c4434bfca25dda7d
callmejeje <choije401@naver.com>
2023-09-11 22:18:30 +0900

    refactor: 상수 사용 및 로또 구입 테스트 수정

--- __test__/step1.test.js
+++ __test__/step1.test.js
@@ -1,34 +1,36 @@
+import { createInterface } from "node:readline/promises";
+import { LOTTO_MAX_NUM, LOTTO_MIN_NUM } from "../src/js/constants.js";
 import LottoController from "../src/js/controllers/step1/LottoController.js";
-import LottoTicket from "../src/js/models/LottoTicket.js";
 
 describe(`로또 당첨 번호 테스트: `, () => {
-  it(`로또 번호는 1에서 45 사이여야 한다.`, () => {
-    const lottoController = new LottoController();
+  const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  it(`로또 번호는 ${LOTTO_MIN_NUM}에서 ${LOTTO_MAX_NUM} 사이여야 한다.`, () => {
+    const lottoController = new LottoController(readline);
     const winningNumbers = lottoController.drawLottoNumbers();
 
     winningNumbers.forEach((number) => {
-      expect(number).toBeGreaterThanOrEqual(1);
-      expect(number).toBeLessThanOrEqual(45);
+      expect(number).toBeGreaterThanOrEqual(LOTTO_MIN_NUM);
+      expect(number).toBeLessThanOrEqual(LOTTO_MAX_NUM);
     });
   });
 
-  it(`당첨 번호는 1에서 45 사이여야 한다.`, () => {
+  it(`당첨 번호는 ${LOTTO_MIN_NUM}에서 ${LOTTO_MAX_NUM} 사이여야 한다.`, () => {
     const lottoController = new LottoController();
     const winningNumbers = lottoController.drawLottoNumbers();
 
     winningNumbers.forEach((number) => {
-      expect(number).toBeGreaterThanOrEqual(1);
-      expect(number).toBeLessThanOrEqual(45);
+      expect(number).toBeGreaterThanOrEqual(LOTTO_MIN_NUM);
+      expect(number).toBeLessThanOrEqual(LOTTO_MAX_NUM);
     });
   });
 });
 
 describe(`로또 구입 테스트: `, () => {
-  it(`로또 한 장의 가격은 1000원이다.`, () => {
-    expect(LottoTicket.price).toBe(1000);
-  });
-
-  it(`로또 구입 금액을 입력한만큼 로또를 발행해야 한다.`, () => {
+  it("로또 구입 금액으로 8,000원을 입력하면 로또 8장이 발행되어야 한다.", () => {
     const lottoController = new LottoController();
     const lottoTickets = lottoController.issueLottoTickets(8000);
 
