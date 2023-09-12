68a78ae773ea21f8d022a94a91e5acd9dd6862bf
callmejeje <choije401@naver.com>
2023-09-12 16:52:47 +0900

    refactor: 에러메세지 상수화

--- src/js/constants.js
+++ src/js/constants.js
@@ -12,3 +12,12 @@ export const MATCH_MESSAGE = (criteria, countOfWin) =>
   `${criteria.winningCount}개 일치${criteria.hasToWinBonus ? ", 보너스 볼 일치" : ""} (${
     criteria.winningAmount
   }원) - ${countOfWin}개`;
+
+export const MONEY_TYPE_ERROR_MESSAGE = "금액은 숫자 타입이어야 합니다.";
+export const MONEY_RANGE_ERROR_MESSAGE = `금액은 ${LOTTO_TICKET_PRICE}원 이상이어야 합니다.`;
+export const MONEY_UNIT_ERROR_MESSAGE = `금액은 ${LOTTO_TICKET_PRICE}원 단위여야 합니다.`;
+export const NUMBER_TYPE_ERROR_MESSAGE = "번호는 숫자 타입이어야 합니다.";
+export const NUMBER_RANGE_ERROR_MESSAGE = `번호는 ${LOTTO_MIN_NUM} 이상 ${LOTTO_MAX_NUM} 이하 숫자여야 합니다.`;
+export const WINNING_NUMBERS_LENGTH_ERROR_MESSAGE = "당첨 번호는 6개의 숫자여야 합니다.";
+export const DUPLICATE_WINNING_NUMBERS_ERROR_MESSAGE = "당첨 번호는 중복되면 안됩니다.";
+export const DUPLICATE_BONUS_NUMBER_ERROR_MESSAGE = "보너스 번호는 당첨 번호와 중복되면 안됩니다.";
