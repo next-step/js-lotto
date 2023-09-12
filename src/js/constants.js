a7aa4d06f44b2ce62992668b37bb255172f74fbd
callmejeje <choije401@naver.com>
2023-09-12 15:26:40 +0900

    feat: 메세지 상수 추가

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,3 +1,8 @@
 export const LOTTO_TICKET_PRICE = 1000;
 export const LOTTO_MIN_NUM = 1;
 export const LOTTO_MAX_NUM = 45;
+
+export const MATCH_MESSAGE = (criteria, countOfWin) =>
+  `${criteria.winningCount}개 일치${criteria.hasToWinBonus ? ", 보너스 볼 일치" : ""} (${
+    criteria.winningAmount
+  }원) - ${countOfWin}개`;
