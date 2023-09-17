67dea1948b1a8cc58db3b1a935ea560f059c02c5
Gn0lee <jinho605@gmail.com>
2023-09-17 23:41:44 +0900

    purchase test 중복 동작 함수화 및 로또 재구매 테스트 케이스 추가

--- cypress/integration/purchase.spec.js
+++ cypress/integration/purchase.spec.js
@@ -5,6 +5,11 @@ const ELEMENT = Object.freeze({
   LOTTO_BOX: () => cy.get(".lotto-box"),
 });
 
+const purchaseLotto = (payment) => {
+  ELEMENT.PAYMENT_INPUT().type(payment);
+  ELEMENT.PURCHASE_BUTTON().click();
+};
+
 describe("로또 구매부분 테스트", () => {
   beforeEach(() => {
     cy.visit("http://localhost:9000/");
@@ -20,18 +25,15 @@ describe("로또 구매부분 테스트", () => {
   });
 
   it("구입 금액이 0보다 작으면 alert이 발생한다.", () => {
-    ELEMENT.PAYMENT_INPUT().type("-1000");
-    ELEMENT.PURCHASE_BUTTON().click();
+    purchaseLotto("-1000");
+
     cy.get("@alertStub")
       .should("have.been.calledOnce")
       .and("have.been.calledWith", "지불 금액은 0이상 이어야 합니다.");
   });
 
   it("구입금액 3000을 입력후 확인 버튼을 클릭하면 로또 3개가 구입되며 구입 정보가 화면에 표시된다.", () => {
-    ELEMENT.PURCHASE_INFO().should("not.exist");
-
-    ELEMENT.PAYMENT_INPUT().type("3000");
-    ELEMENT.PURCHASE_BUTTON().click();
+    purchaseLotto("3000");
 
     ELEMENT.PURCHASE_INFO()
       .should("be.visible")
@@ -41,8 +43,7 @@ describe("로또 구매부분 테스트", () => {
   });
 
   it("3000원 구매후 번호 보기를 클릭하면 로또 3장이 화면에 표시된다.", () => {
-    ELEMENT.PAYMENT_INPUT().type("3000");
-    ELEMENT.PURCHASE_BUTTON().click();
+    purchaseLotto("3000");
 
     ELEMENT.LOTTO_BOX().should("not.be.visible");
 
@@ -50,5 +51,17 @@ describe("로또 구매부분 테스트", () => {
 
     ELEMENT.LOTTO_BOX().should("be.visible");
     cy.get(".lotto-ticket").should("be.visible").and("have.length", 3);
+  });
+
+  it("로또 구매후 초기화 하지 않고 다시 구매하면 alert가 발생한다.", () => {
+    purchaseLotto("3000");
+
+    ELEMENT.PAYMENT_INPUT().clear();
+
+    purchaseLotto("2000");
+
+    cy.get("@alertStub")
+      .should("have.been.calledOnce")
+      .and("have.been.calledWith", "이미 로또를 구매하였습니다.");
   });
 });
