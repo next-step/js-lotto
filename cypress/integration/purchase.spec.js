9931ae5b7749373319ad459476a509d35833a3de
Gn0lee <jinho605@gmail.com>
2023-09-17 23:25:25 +0900

    purchase test 리팩토링

--- cypress/integration/purchase.spec.js
+++ cypress/integration/purchase.spec.js
@@ -1,3 +1,10 @@
+const ELEMENT = Object.freeze({
+  PAYMENT_INPUT: () => cy.get("input.payment"),
+  PURCHASE_BUTTON: () => cy.get("button.purchase"),
+  PURCHASE_INFO: () => cy.get(".purchase-info"),
+  LOTTO_BOX: () => cy.get(".lotto-box"),
+});
+
 describe("로또 구매부분 테스트", () => {
   beforeEach(() => {
     cy.visit("http://localhost:9000/");
@@ -8,25 +15,25 @@ describe("로또 구매부분 테스트", () => {
   });
 
   it("구입 금액을 입력하는 input과 구매를 진행하는 button이 존재한다.", () => {
-    cy.get("input.payment").should("be.visible");
-    cy.get("button.purchase").should("be.visible");
+    ELEMENT.PAYMENT_INPUT().should("be.visible");
+    ELEMENT.PURCHASE_BUTTON().should("be.visible");
   });
 
   it("구입 금액이 0보다 작으면 alert이 발생한다.", () => {
-    cy.get("input.payment").type("-1000");
-    cy.get("button.purchase").click();
+    ELEMENT.PAYMENT_INPUT().type("-1000");
+    ELEMENT.PURCHASE_BUTTON().click();
     cy.get("@alertStub")
       .should("have.been.calledOnce")
       .and("have.been.calledWith", "지불 금액은 0이상 이어야 합니다.");
   });
 
   it("구입금액 3000을 입력후 확인 버튼을 클릭하면 로또 3개가 구입되며 구입 정보가 화면에 표시된다.", () => {
-    cy.get(".purchase-info").should("not.exist");
+    ELEMENT.PURCHASE_INFO().should("not.exist");
 
-    cy.get("input.payment").type("3000");
-    cy.get("button.purchase").click();
+    ELEMENT.PAYMENT_INPUT().type("3000");
+    ELEMENT.PURCHASE_BUTTON().click();
 
-    cy.get(".purchase-info")
+    ELEMENT.PURCHASE_INFO()
       .should("be.visible")
       .and("contain.text", "총 3개를 구매하였습니다.");
 
@@ -34,14 +41,14 @@ describe("로또 구매부분 테스트", () => {
   });
 
   it("3000원 구매후 번호 보기를 클릭하면 로또 3장이 화면에 표시된다.", () => {
-    cy.get("input.payment").type("3000");
-    cy.get("button.purchase").click();
+    ELEMENT.PAYMENT_INPUT().type("3000");
+    ELEMENT.PURCHASE_BUTTON().click();
 
-    cy.get(".lotto-box").should("not.be.visible");
+    ELEMENT.LOTTO_BOX().should("not.be.visible");
 
     cy.get("label.switch").click();
 
-    cy.get(".lotto-box").should("be.visible");
+    ELEMENT.LOTTO_BOX().should("be.visible");
     cy.get(".lotto-ticket").should("be.visible").and("have.length", 3);
   });
 });
