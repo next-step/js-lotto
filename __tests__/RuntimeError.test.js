6946571403956e814afb931fd9c355a91ae13a84
Erica Gong <48196721+ericagong@users.noreply.github.com>
2023-09-18 10:34:07 +0900

    Revert "[클린코드 5기 공은채] 로또 미션 STEP 1"

--- __tests__/RuntimeError.test.js
+++
@@ -1,6 +0,0 @@
-import RuntimeError from "../src/js/RuntimeError";
-it("RuntimeError 에러 메시지 반환 테스트", () => {
-  const message = "RuntimeError 메시지";
-  const error = new RuntimeError(message);
-  expect(error.getMessage()).toBe(message);
-});
--- __tests__/utils/EmptyIssueStrategy.js
+++
@@ -1,7 +0,0 @@
-import IssueStrategy from "../../src/js/domain/LotteryMachine/IssueStrategy";
-
-export default class EmptyIssueStrategy extends IssueStrategy {
-  constructor() {
-    super();
-  }
-}
