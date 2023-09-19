ef324205cd71158c35d957703bfb4d0a5ae9217a
Jason <jasondal91@gmail.com>
2023-09-20 08:37:08 +0900

    chore: entry 수정

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,4 @@
-import { LOTTO_AMOUNT_UNIT } from './constants/lotto.js';
-import { Lotto } from './domain/Lotto.js';
-import { LottoMachine } from './domain/LottoMachine.js';
-import { WinningLotto } from './domain/WinningLotto.js';
-import { LottoController } from './view/LottoController.js';
-import LottoView from './view/LottoVIew.js';
+import { LottoController } from './view-step3/LottoController';
 
 export class App {
   #controller;
