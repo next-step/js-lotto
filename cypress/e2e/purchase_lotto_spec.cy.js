17ee5c92d8387d5d0ecf42a45c96d29dfb866dcb
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-09-19 18:08:14 +0900

    chore: 이전 step 파일 및 script 제거

--- cypress/e2e/purchase_lotto_spec.cy.js
+++ cypress/e2e/purchase_lotto_spec.cy.js
@@ -1,21 +1,20 @@
 describe('로또 구매', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:9000'); // 로컬 서버 주소
+    cy.visit('http://localhost:9000');
   });
 
   it('로또 구매 금액을 입력하면 로또 티켓이 표시된다', () => {
     const purchaseAmount = 5000;
-
     cy.get('.test-purchase-input').type(purchaseAmount);
     cy.get('.test-ticket-form').submit();
 
-    cy.get('.test-ticket-amount').should('contain', '총 5개를 구매하였습니다.'); // 로또 티켓 가격을 1000원으로 가정
+    cy.get('.test-ticket-amount').should('contain', '총 5개를 구매하였습니다.');
     cy.get('.test-tickets').children().should('have.length', 5);
   });
 
   it('번호보기 토글을 클릭하면 로또 번호가 표시된다', () => {
-    // 이 부분에 로또 구매 과정 추가
-
+    cy.get('.test-purchase-input').type(purchaseAmount);
+    cy.get('.test-ticket-form').submit();
     cy.get('.lotto-numbers-toggle-button').check();
     cy.get('.lotto-numbers').should('be.visible');
   });
@@ -30,6 +29,4 @@ describe('로또 구매', () => {
 
     cy.get('.test-bonus-number').type(bonusNumber);
   });
-
-  // 결과 확인하기 버튼을 클릭하는 테스트나 다른 기능들에 대한 테스트도 추가 가능
 });
