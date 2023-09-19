2fe8c65848b75fb08e565ee002eff3d781a8af78
Pengoose <73521518+pengooseDev@users.noreply.github.com>
2023-09-19 11:56:44 +0900

    refactor: lottoPrize 객체 제거 및 util함수 변경

--- src/Model/index.js
+++ src/Model/index.js
@@ -1,6 +1,5 @@
 export * from './LottoStore';
 export * from './LottoTicket';
-export * from './LottoPrize';
 export * from './LottoCorporation';
 export * from './EventBinder';
 export * from './LottoEvents';
