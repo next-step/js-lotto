77dcf9516398ca7fcc8734249dc0ad0e9431a9fc
kimhaneui <103249275+kimhaneui2@users.noreply.github.com>
2023-09-19 09:17:39 +0900

    Fixed: clean code

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,4 +1,4 @@
-const { LOTTO_PRICE, ERROR_MESSAGE, WIN_NUMBER_COUNT, BONUS_NUMBER_COUNT } = require('../js/utils/constants.js')
+const { LOTTO_PRICE, ERROR_MESSAGE, WIN_NUMBER_COUNT,LOTTO_RANDOM_MAX } = require('../js/utils/constants.js')
 const LottoValidator = {
     amountValidate(amount) {
         if (isNaN(amount) || amount <= 0) {
@@ -11,18 +11,23 @@ const LottoValidator = {
     },
 
     checkWinningNumbers(winningNumbers) {
-        console.log(winningNumbers,'winningNumbers');
         if (winningNumbers.length !== WIN_NUMBER_COUNT) {
             throw new Error(ERROR_MESSAGE.PUT_WIN_NUMBER_COUNT)
         }
-        
+        const uniqueNumbers = new Set(winningNumbers);
+
+        if (uniqueNumbers.size !== winningNumbers.length) {
+            throw new Error(ERROR_MESSAGE.DUPLICATE_NUMBERS);
+        }
+        if (winningNumbers.some(number => number > LOTTO_RANDOM_MAX)) {
+            throw new Error(ERROR_MESSAGE.NUMBER_OVER_LIMIT);
+        }
         return winningNumbers
     },
     checkBonusNumber(bonusNumber) {
-        if (bonusNumber.length !== BONUS_NUMBER_COUNT) {
-            throw new Error(ERROR_MESSAGE.PUT_BONUS_NUMBER_COUNT)
+        if (bonusNumber > LOTTO_RANDOM_MAX) {
+            throw new Error(ERROR_MESSAGE.NUMBER_OVER_LIMIT);
         }
-
         return bonusNumber
     }
 }
