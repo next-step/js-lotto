28bc0dd34da96741773814f0df1baa6692c35792
Pengoose <pengoose@Pengooseui-MacBookPro.local>
2023-09-23 02:10:51 +0900

    refactor: Map 초기화 내장메서드 적용

--- src/Controller/WebController.js
+++ src/Controller/WebController.js
@@ -64,7 +64,7 @@ export class WebController {
 
   async #restartGame() {
     this.#view.closeModal();
-    this.#storage = new Map();
+    this.#storage.clear();
     this.#view.resetView();
   }
 }
