15fdf538d0678de030f3a660da9b968d240c4c1e
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-09-25 04:02:05 +0900

    🚚 rename: 웹 UI 텍스트 상수 파일 이름 변경

--- src/domain/constants/webConfigure.js
+++
@@ -1,6 +0,0 @@
-export const TITLE = '🎱 행운의 로또';
-export const HEADING_TITLE = '🎱 행운의 로또';
-export const TEXT_CONTENTS = Object.freeze({
-  AMOUNT_FORM_LABEL: '구입 금액을 입력해주세요.',
-  LOTTO_NUMBER_FORM_LABEL: '지난 주 당첨 번호를 입력해주세요.'
-});
---
+++ src/domain/constants/webUiMessages.js
@@ -0,0 +1,7 @@
+export const TITLE = '🎱 행운의 로또';
+export const HEADING_TITLE = '🎱 행운의 로또';
+export const AMOUNT_PLACEHOLDER = '구입할 금액을 입력해주세요.';
+export const LABEL_TEXT_CONTENTS = Object.freeze({
+  INPUT_AMOUNT: '구입 금액을 입력해주세요.',
+  INPUT_LOTTO_NUMBER: '지난 주 당첨 번호를 입력해주세요.'
+});
