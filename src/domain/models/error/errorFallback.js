41ca63ec6ebd74599fc354e69838439bdb609f54
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-09-26 03:27:33 +0900

    🏗️ structure: error fallback 위치 변경

--- src/domain/models/error/errorFallback.js
+++ src/domain/models/error/errorFallback.js
@@ -1,4 +1,4 @@
-import { ERROR_MARKER } from '../constants/index.js';
+import { ERROR_MARKER } from '../../constants/index.js';
 
 const errorFallback = (errorMessage) => {
   console.error(`${ERROR_MARKER} ${errorMessage}`);
