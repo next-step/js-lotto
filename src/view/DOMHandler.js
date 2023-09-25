1bd1c46bc2b0081840749ad5f9f76800bbf0fa16
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-09-26 02:57:18 +0900

    ✨ feat: display 스타일 속성 제어 함수 추가

--- src/view/DOMHandler.js
+++ src/view/DOMHandler.js
@@ -1,7 +1,7 @@
 export const getQuerySelector = (query, $target) =>
   $target ? $target.querySelector(query) : document.querySelector(query);
 export const getQuerySelectorAll = (query, $target) =>
-  $target ? $target.querySelector(query) : document.querySelector(query);
+  $target ? $target.querySelectorAll(query) : document.querySelectorAll(query);
 export const createElement = (element) => document.createElement(element);
 export const createFragment = () => document.createDocumentFragment();
 export const createHeading = (headingType, textContent) => {
@@ -14,3 +14,7 @@ export const findElementByTag = (parent, tagName) => parent.getElementsByTagName
 export const clearClassNames = (element) => (element.className = '');
 export const addClassNames = (element, classNames) => element.classList.add(...classNames);
 export const removeClassNames = (element, classNames) => element.classList.remove(...classNames);
+export const showElement = (element, displayType) => {
+  element.style.display = displayType || 'block';
+};
+export const hideElement = (element) => (element.style.display = 'none');
