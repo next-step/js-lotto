2244f3bee31548bb9f84575629831b6b1b17878b
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-09-27 16:50:10 +0900

    ✨ feat: Web UI에 사용되는 메시지 상수 파일 추가

--- src/domain/constants/webUiMessages.js
+++ src/domain/constants/webUiMessages.js
@@ -1,7 +1,14 @@
 export const TITLE = '🎱 행운의 로또';
+export const LOTTO_TICKET_MARKER = '🎟️';
 export const HEADING_TITLE = '🎱 행운의 로또';
 export const AMOUNT_PLACEHOLDER = '구입할 금액을 입력해주세요.';
+export const LOTTO_TOGGLE_MESSAGE = '번호보기';
+export const HEADING_TEXT_CONTENTS = Object.freeze({
+  WINNING_LOTTO_NUMBER_INPUT: '당첨 번호',
+  BONUS_NUMBER_INPUT: '보너스 번호'
+});
 export const LABEL_TEXT_CONTENTS = Object.freeze({
   INPUT_AMOUNT: '구입 금액을 입력해주세요.',
-  INPUT_LOTTO_NUMBER: '지난 주 당첨 번호를 입력해주세요.'
+  INPUT_LOTTO_NUMBER: '지난 주 당첨 번호를 입력해주세요.',
+  WINNING_NUMBER_FORM: '지난 주 당첨 번호 6개와 보너스 넘버 1개를 입력해주세요.'
 });
