4f30eb4ff08c93142bae846f4a24a8d36a9704f0
ChanwooPark <chanw9503@gmail.com>
2024-05-08 23:44:39 +0900

    Feat. 로또 객체에 로또 결과를 알 수 있는 변수 추가

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -2,9 +2,12 @@ export const ERROR_MESSAGE_LOTTO_LENGTH = '로또번호는 6개 입니다.';
 
 class Lotto {
   #numbers;
+  #result;
+
   constructor(numbers) {
     this.validLottoLength(numbers);
     this.#numbers = numbers;
+    this.#result = 0;
   }
 
   get numbers() {
