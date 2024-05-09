c4f6201b7c243d3abb9617668f600d75a151195c
Vespucci95 <donglonk@naver.com>
2024-05-09 18:39:05 +0900

    refactor: LottoTicket 리팩터링

--- __tests__/LottoTicket.test.js
+++ __tests__/LottoTicket.test.js
@@ -1,13 +1,26 @@
 import LottoTicket from '../src/js/domain/LottoTicket';
+import Product from '../src/js/domain/Product';
 import { generateLottoNumberArray } from '../src/js/utils/LottoUtil';
 
 describe('로또 티켓 기능 테스트', () => {
+  it('로또 티켓은 상품이다.', () => {
+    expect(new LottoTicket()).toBeInstanceOf(Product);
+  });
   it('6개의 번호를 가진다', () => {
-    const lottoTicket = new LottoTicket(generateLottoNumberArray());
+    // given
+    const lottoTicket = new LottoTicket();
 
+    // when
+    lottoTicket.lottoNumbers = generateLottoNumberArray();
+
+    // then
     expect(lottoTicket.lottoNumbers).toBeTruthy();
   });
   it('올바른 로또번호가 아닐 경우 에러를 발생한다.', () => {
-    expect(() => new LottoTicket([1, 1, 2, 10, 30, 40])).toThrow();
+    const lottoTicket = new LottoTicket();
+
+    expect(() => {
+      lottoTicket.lottoNumbers = [1, 1, 2, 10, 30, 40];
+    }).toThrow();
   });
 });
--- src/js/domain/LottoTicket.js
+++ src/js/domain/LottoTicket.js
@@ -1,16 +1,22 @@
 import { isValidLottoNumberArray } from '../utils/LottoUtil';
+import Product from './Product';
+import { LOTTO } from '../constants';
 
-class LottoTicket {
+class LottoTicket extends Product {
   #lottoNumbers;
-  constructor(lottoNumbers) {
-    if (!isValidLottoNumberArray(lottoNumbers)) {
-      throw new TypeError('올바른 로또 형식이 아닙니다.');
-    }
-    this.#lottoNumbers = lottoNumbers;
+  constructor(name = LOTTO.NAME_KR, price = LOTTO.PRICE) {
+    super(name, price);
   }
 
   get lottoNumbers() {
     return this.#lottoNumbers;
+  }
+
+  set lottoNumbers(lottoNumbers) {
+    if (!isValidLottoNumberArray(lottoNumbers)) {
+      throw new TypeError('올바른 로또 형식이 아닙니다.');
+    }
+    this.#lottoNumbers = lottoNumbers;
   }
 }
 
