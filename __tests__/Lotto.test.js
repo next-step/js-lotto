b91b7958d63823d420444ce092c61a42d0451794
oxlzlo <yeojin58@naver.com>
2024-05-10 16:27:00 +0900

    feat: 로또 구입 금액을 입력하면 구입 금액에 해당하는 만큼 로또를 발행해야 한다

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -11,4 +11,15 @@ describe('로또 기능 테스트', () => {
 		// then
 		expect(price).toBe(1000);
 	});
+
+	test('로또 구입 금액을 입력하면 구입 금액에 해당하는 만큼 로또를 발행해야 한다.', () => {
+		// given
+		const lotto = new Lotto();
+
+		// when
+		const qty = lotto.purchase(5000);
+
+		// then
+		expect(qty).toBe(5);
+	});
 });
--- src/tests/Lotto.js
+++ src/tests/Lotto.js
@@ -3,13 +3,16 @@ import { LOTTO_PRICE } from './constant';
 class Lotto {
 	price = LOTTO_PRICE;
 	qty;
+	purchaseAmount;
 
 	constructor(qty) {
 		this.qty = qty;
 	}
 
-	get totalPrice() {
-		return this.qty * this.price;
+	purchase(purchaseAmount) {
+		// 구매 금액을 로또 가격으로 나눈 몫이 qty
+		this.qty = purchaseAmount / this.price;
+		return this.qty;
 	}
 }
 
