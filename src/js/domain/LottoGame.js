47860f4fcd54cee3ae9f8927affaf62cdc58ca2e
DongHyeonWon <dhe7700@naver.com>
2024-05-11 15:04:40 +0900

    feat: 사용자가 구매한 로또 번호와 당첨 번호를 비교한다.

--- src/js/domain/LottoGame.js
+++ src/js/domain/LottoGame.js
@@ -1,13 +1,51 @@
-import { LOTTO_GAME } from "../../constants/lottoGame";
-import { Lotto } from "./lotto";
-
+import { LOTTO } from "../../constants/lotto";
 export class LottoGame {
-  #purchasePrice;
   #lottos;
-  #lotto;
   #winningNumbers;
-  constructor(price) {
-    this.#purchasePrice = price;
-    this.#winningNumbers = [];
+  #bonusNumber;
+  #result;
+  constructor(lottos, winnerNumbers, bonusNumber) {
+    this.#lottos = lottos;
+    this.#winningNumbers = winnerNumbers;
+    this.#bonusNumber = bonusNumber;
+    this.#result = new Map(
+      Array.from({ length: LOTTO.NUMBERS_COUNT }, (_, index) => [index + 1, 0])
+    );
+  }
+
+  #checkUnitLotto(lotto) {
+    const checkResult = lotto.filter((number) =>
+      this.#winningNumbers.includes(number)
+    ).length;
+
+    return this.#convertCheckResultToRank(lotto, checkResult);
+  }
+
+  #convertCheckResultToRank = (lotto, checkResult) => {
+    switch (checkResult) {
+      case 3:
+        return 5;
+      case 4:
+        return 4;
+      case 5:
+        return lotto.includes(this.#bonusNumber) ? 2 : 3;
+      case 6:
+        return 1;
+      default:
+        return 0;
+    }
+  };
+
+  checkLottos() {
+    this.#lottos.map((lotto) =>
+      this.#result.set(
+        this.#checkUnitLotto(lotto),
+        this.#result.get(this.#checkUnitLotto(lotto)) + 1
+      )
+    );
+  }
+
+  get result() {
+    return this.#result;
   }
 }
