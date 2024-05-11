24516a33036c1558d0aaef39e639a79ce9ee6b46
jiyeon <ejy1105@gmail.com>
2024-05-11 22:36:52 +0900

    feat: init 폴더링

---
+++ src/domain/Lotto.js
@@ -0,0 +1,5 @@
+class Lotto {
+  constructor() {}
+}
+
+export default Lotto;
---
+++ src/util/readLine.js
@@ -0,0 +1,23 @@
+import readline from "readline";
+
+export function readLineAsync(query) {
+  return new Promise((resolve, reject) => {
+    if (arguments.length !== 1) {
+      reject(new Error("arguments must be 1"));
+    }
+
+    if (typeof query !== "string") {
+      reject(new Error("query must be string"));
+    }
+
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question(query, (input) => {
+      rl.close();
+      resolve(input);
+    });
+  });
+}
