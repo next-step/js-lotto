2a9a394d1dbe6a3c952a6b85552a06ade2ee1cde
sochong <hsju0202@gmail.com>
2024-05-12 01:12:52 +0900

    feat: 로또 클래스 개발
    - 6개의 번호를 가진다.
      - 번호는 중복되지않는다.
      - 번호는 1 ~ 45의 숫자 중 하나이다.
    - 번호와 당첨번호, 보너스 번호를 비교하여 등수를 반환한다.
      - 등수 기준
        - 1등: 6개 번호 일치 / 2,000,000,000원
        - 2등: 5개 번호 + 보너스 번호 일치 / 30,000,000원
        - 3등: 5개 번호 일치 / 1,500,000원
        - 4등: 4개 번호 일치 / 50,000원
        - 5등: 3개 번호 일치 / 5,000원
      - 당첨번호는 로또 번호와 같은 유효성 체크를 한다.
      - 보너스 번호는 1~45 중의 숫자중 하나이다.

---
+++ src/js/domain/enum/LottoRank.js
@@ -0,0 +1,8 @@
+export const LottoRank = Object.freeze({
+  FIRST: { rank: 1, reward: 2_000_000_000 },
+  SECOND: { rank: 2, reward: 30_000_000 },
+  THIRD: { rank: 3, reward: 1_500_000 },
+  FOURTH: { rank: 4, reward: 50_000 },
+  FIFTH: { rank: 5, reward: 5000 },
+  NOTHING: { rank: "nothing", reward: 0 },
+});
