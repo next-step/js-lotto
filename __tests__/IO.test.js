21a7c95119af41a65b9a521671e1349366604ad9
Paik <gwbaik9717@icloud.com>
2024-05-12 12:41:10 +0900

    test: 불필요한 IO 테스트 제거

--- __tests__/IO.test.js
+++ __tests__/IO.test.js
@@ -1,11 +1,4 @@
-import {
-  ErrorLottoBonusNumber,
-  ErrorLottoNumber,
-  ErrorLottoNumbers,
-  ErrorLottoPurchasedAmount,
-} from "../src/constants/error";
 import Lotto from "../src/js/domain/Lotto";
-import LottoResult from "../src/js/domain/LottoResult";
 import Input from "../src/js/view/Input";
 import Output from "../src/js/view/Output";
 import * as io from "../src/utils/readlineAsync";
@@ -27,34 +20,12 @@ describe("입출력 기능 테스트", () => {
     const lottoPurchasedAmount = await Input.getLottoPurchasedAmount();
 
     // then
-    expect(lottoPurchasedAmount).toBe(10000);
-  });
-
-  test("로또를 구매할 금액을 입력받을 때 0 미만이거나 숫자가 아니면 에러메시지 출력 후 다시 입력을 받는다.", async () => {
-    // given
-    readLineAsyncSpy
-      .mockImplementationOnce(() => Promise.resolve("-1"))
-      .mockImplementationOnce(() => Promise.resolve("가나다"))
-      .mockImplementationOnce(() => Promise.resolve("1000"));
-
-    // when
-    const lottoPurchasedAmount = await Input.getLottoPurchasedAmount();
-
-    // then
-    expect(readLineAsyncSpy).toHaveBeenCalledTimes(3);
-    expect(logSpy).toHaveBeenCalledWith(
-      ErrorLottoPurchasedAmount.ERROR_LOTTO_PURCHASED_AMOUNT_NOT_POSITIVE
-    );
-    expect(logSpy).toHaveBeenCalledWith(
-      ErrorLottoPurchasedAmount.ERROR_LOTTO_PURCHASED_AMOUNT_NOT_NUMBER
-    );
-    expect(lottoPurchasedAmount).toBe(1000);
+    expect(lottoPurchasedAmount).toBe("10000");
   });
 
   test("로또 구매 금액으로 발급한 최대 개수의 로또의 수를 출력한다.", () => {
     // given
-    const purchasedAmount = 10001;
-    const availableLottoCount = Lotto.getAvailableLottoCount(purchasedAmount);
+    const availableLottoCount = 10;
 
     // when
     Output.printGeneratedLottosCount(availableLottoCount);
@@ -69,7 +40,7 @@ describe("입출력 기능 테스트", () => {
     const lottos = [];
 
     for (let i = 0; i < lottosCount; i++) {
-      const lottoNumbers = Lotto.generateLottoNumbers();
+      const lottoNumbers = Lotto.generateRandomLottoNumbers();
       lottos.push(new Lotto(lottoNumbers));
     }
 
@@ -95,45 +66,6 @@ describe("입출력 기능 테스트", () => {
     expect(winningNumbers).toEqual("1,2,3,4,5,6");
   });
 
-  test("로또 당첨 번호를 입력 받을 때 정수가 아니거나 1미만 또는 45 초과의 정수를 입력하면 에러메시지 출력 후 다시 입력을 받는다.", async () => {
-    // given
-    readLineAsyncSpy
-      .mockImplementationOnce(() => Promise.resolve("가나다,2,3,4,5,6"))
-      .mockImplementationOnce(() => Promise.resolve("-1,2,3,4,5,6"))
-      .mockImplementationOnce(() => Promise.resolve("1,2,3,4,5,47"))
-      .mockImplementationOnce(() => Promise.resolve("1,2,3,4,5,6"));
-
-    // when
-    const winningNumbers = await Input.getWinningNumbers();
-
-    // then
-    expect(readLineAsyncSpy).toHaveBeenCalledTimes(4);
-    expect(logSpy).toHaveBeenCalledWith(
-      ErrorLottoNumber.ERROR_LOTTO_NUMBER_NOT_NUMBER
-    );
-    expect(logSpy).toHaveBeenCalledWith(
-      ErrorLottoNumber.ERROR_LOTTO_NUMBER_NOT_VALID_INTEGER
-    );
-    expect(winningNumbers).toBe("1,2,3,4,5,6");
-  });
-
-  test("로또 당첨 번호를 입력 받을 때 1이상 45이하의 정수 6개 중 겹치는 숫자가 있다면 에러메시지 출력 후 다시 입력을 받는다.", async () => {
-    // given
-    readLineAsyncSpy
-      .mockImplementationOnce(() => Promise.resolve("1,1,2,2,3,3"))
-      .mockImplementationOnce(() => Promise.resolve("1,2,3,4,5,6"));
-
-    // when
-    const winningNumbers = await Input.getWinningNumbers();
-
-    // then
-    expect(readLineAsyncSpy).toHaveBeenCalledTimes(2);
-    expect(logSpy).toHaveBeenCalledWith(
-      ErrorLottoNumbers.ERROR_LOTTO_NUMBERS_DUPLICATED
-    );
-    expect(winningNumbers).toBe("1,2,3,4,5,6");
-  });
-
   test("로또 보너스 번호를 입력 받을 때 1이상 45이하의 정수이면서 당첨번호로 선택한 수들과 다른 수를 입력 하면 정상적으로 종료된다.", async () => {
     // given
     readLineAsyncSpy.mockImplementationOnce(() => Promise.resolve("7"));
@@ -143,79 +75,30 @@ describe("입출력 기능 테스트", () => {
     const bonusNumber = await Input.getBonusNumber(winningNumbers);
 
     // then
-    expect(bonusNumber).toBe(7);
-  });
-
-  test("로또 보너스 번호를 입력 받을 때 1이상 45이하의 정수이면서 당첨번호로 선택한 수들과 겹치면 에러 메시지 출력 후 정상적으로 종료된다.", async () => {
-    // given
-    readLineAsyncSpy
-      .mockImplementationOnce(() => Promise.resolve("6"))
-      .mockImplementationOnce(() => Promise.resolve("7"));
-    const winningNumbers = [1, 2, 3, 4, 5, 6];
-
-    // when
-    const bonusNumber = await Input.getBonusNumber(winningNumbers);
-
-    // then
-    expect(readLineAsyncSpy).toHaveBeenCalledTimes(2);
-    expect(logSpy).toHaveBeenCalledWith(
-      ErrorLottoBonusNumber.ERROR_LOTTO_BONUS_NUMBER_DUPLICATED
-    );
-    expect(bonusNumber).toBe(7);
+    expect(bonusNumber).toBe("7");
   });
 
   test("로또 당첨 결과를 출력한다.", () => {
     // given
-    const lottosCount = 10;
-    const lottos = [];
-
-    for (let i = 0; i < lottosCount; i++) {
-      const lottoNumbers = Lotto.generateLottoNumbers();
-      lottos.push(new Lotto(lottoNumbers));
-    }
-    const winningNumbers = [1, 2, 3, 4, 5, 6];
-    const bonusNumber = 7;
-    const lottoResult = new LottoResult(winningNumbers, bonusNumber);
-    const lottoRankingStatistics =
-      lottoResult.getLottoRankingStatistics(lottos);
+    const lottoRankingStatistics = [2, 1, 0, 0, 0, 1];
 
     // when
     Output.printLottoRankingStatistics(lottoRankingStatistics);
 
     // then
     expect(logSpy).toHaveBeenCalledWith("\n당첨 통계\n--------------------");
-    expect(logSpy).toHaveBeenCalledTimes(6);
+    expect(logSpy).toHaveBeenCalledWith("3개 일치 (5,000원) - 1개");
+    expect(logSpy).toHaveBeenCalledWith("4개 일치 (50,000원) - 0개");
+    expect(logSpy).toHaveBeenCalledWith("5개 일치 (1,500,000원) - 0개");
+    expect(logSpy).toHaveBeenCalledWith(
+      "5개 일치, 보너스 볼 일치 (30,000,000원) - 0개"
+    );
+    expect(logSpy).toHaveBeenCalledWith("6개 일치 (2,000,000,000원) - 1개");
   });
 
   test("로또 당첨 수익률을 출력한다.", () => {
     // given
-    const purchasedAmount = 8000;
-    const availableLottoCount = Lotto.getAvailableLottoCount(purchasedAmount);
-    const lottoNumbers = [
-      [8, 21, 23, 41, 42, 43],
-      [3, 5, 11, 16, 32, 38],
-      [7, 11, 16, 35, 36, 44],
-      [1, 8, 11, 31, 41, 42],
-      [13, 14, 16, 38, 42, 45],
-      [7, 11, 30, 40, 42, 43],
-      [2, 13, 22, 32, 38, 45],
-      [1, 3, 5, 14, 22, 45],
-    ];
-    const lottos = [];
-
-    for (let i = 0; i < availableLottoCount; i++) {
-      lottos.push(new Lotto(lottoNumbers[i]));
-    }
-
-    const winningNumbers = [1, 2, 3, 4, 5, 6];
-    const bonusNumber = 7;
-    const lottoResult = new LottoResult(winningNumbers, bonusNumber);
-    const totalLottoWinningPrice =
-      lottoResult.getTotalLottoWinningPrice(lottos);
-    const totalLottoProfitRate = LottoResult.getTotalLottoProfitRate(
-      totalLottoWinningPrice,
-      purchasedAmount
-    );
+    const totalLottoProfitRate = 62.5;
 
     // when
     Output.printLottoProfitRate(totalLottoProfitRate);
--- src/js/view/Input.js
+++ src/js/view/Input.js
@@ -1,46 +1,15 @@
 import { readLineAsync } from "../../utils/readlineAsync";
-import Lotto from "../domain/Lotto";
-import LottoResult from "../domain/LottoResult";
-
 const Input = {
   async getLottoPurchasedAmount() {
-    while (true) {
-      const input = await readLineAsync("구입금액을 입력해 주세요.");
-      try {
-        Lotto.validateLottoPurchasedAmount(input);
-        return Number(input);
-      } catch (e) {
-        console.log(e.message);
-        console.log();
-      }
-    }
+    return await readLineAsync("구입금액을 입력해 주세요.");
   },
 
   async getWinningNumbers() {
-    while (true) {
-      const input = await readLineAsync("\n당첨 번호를 입력해 주세요.");
-      try {
-        Lotto.validateLottoNumbers(input);
-        return input;
-      } catch (e) {
-        console.log(e.message);
-        console.log();
-      }
-    }
+    return await readLineAsync("\n당첨 번호를 입력해 주세요.");
   },
 
-  async getBonusNumber(winningNumbers) {
-    while (true) {
-      const input = await readLineAsync("\n보너스 번호를 입력해 주세요.");
-      try {
-        LottoResult.validateBonusNumber(input, winningNumbers);
-        const bonusNumber = Number(input);
-        return bonusNumber;
-      } catch (e) {
-        console.log(e.message);
-        console.log();
-      }
-    }
+  async getBonusNumber() {
+    return await readLineAsync("\n보너스 번호를 입력해 주세요.");
   },
 };
 
