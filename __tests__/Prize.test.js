61364b62be9081e29a66c6b56c3ed605267c1ec3
oxlzlo <yeojin58@naver.com>
2024-05-13 02:15:45 +0900

    refactor: Winning 클래스 변경으로 인한 테스트 코드 인스턴스 변경 및 모킹 제거

--- __tests__/Prize.test.js
+++ __tests__/Prize.test.js
@@ -4,19 +4,16 @@ import Winning from '../src/tests/Winning';
 describe('상금 테스트', () => {
 	test('수익률은 (당첨금액 / 구매금액 * 100) 이다', () => {
 		// given
-		const purchaseAmount = 5000;
+		const purchaseAmount = 8000;
 		const userInputWinningNumber = [1, 2, 3, 4, 5, 6];
 		const mockLottoNumbers = [1, 2, 3, 7, 8, 9];
 		const mockBonusNumber = 10;
 
 		const lotto = new Lotto();
 		lotto.purchase(purchaseAmount);
-		lotto.lottoNumbers = [...Array(45).keys()].map(x => x + 1);
+		lotto.purchaseAmount = purchaseAmount;
 
-		lotto.createLottoNumbers = jest.fn().mockReturnValue(mockLottoNumbers);
-		lotto.createBonusNumber = jest.fn().mockReturnValue(mockBonusNumber);
-
-		const winning = new Winning(userInputWinningNumber);
+		const winning = new Winning(userInputWinningNumber, mockLottoNumbers, mockBonusNumber);
 		winning.lotto = lotto;
 
 		// when
