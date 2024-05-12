9a9d0b46d7706bb9be4a5a523887ce76563ccc26
Paik <gwbaik9717@icloud.com>
2024-05-12 12:48:08 +0900

    fix: console 실행 로직 변경

--- src/step-console-index.js
+++ src/step-console-index.js
@@ -3,41 +3,46 @@ import LottoResult from "./js/domain/LottoResult";
 import Input from "./js/view/Input";
 import Output from "./js/view/Output";
 
-// 로또를 구입할 금액 입력
-const purchasedAmount = await Input.getLottoPurchasedAmount();
+try {
+  // 로또를 구입할 금액 입력
+  const purchasedAmount = await Input.getLottoPurchasedAmount();
 
-// 로또를 구입한 금액만큼 최대 개수의 로또 발급
-const availableLottoCount = Lotto.getAvailableLottoCount(purchasedAmount);
-const lottos = [];
+  // 로또를 구입한 금액만큼 최대 개수의 로또 발급
+  const availableLottoCount = Lotto.getAvailableLottoCount(purchasedAmount);
 
-for (let i = 0; i < availableLottoCount; i++) {
-  const lottoNumbers = Lotto.generateLottoNumbers();
-  lottos.push(new Lotto(lottoNumbers));
+  const lottos = [];
+  for (let i = 0; i < availableLottoCount; i++) {
+    const lottoNumbers = Lotto.generateRandomLottoNumbers();
+    lottos.push(new Lotto(lottoNumbers));
+  }
+
+  // 로또를 발급한 개수 출력
+  Output.printGeneratedLottosCount(lottos.length);
+
+  // 발급한 로또들의 각 로또 번호들을 출력
+  Output.printGeneratedLottosNumbers(lottos);
+
+  // 당첨 번호 입력
+  const winningNumbers = await Input.getWinningNumbers();
+
+  // 보너스 번호 입력
+  const bonusNumber = await Input.getBonusNumber(winningNumbers);
+
+  // 로또 당첨 결과 생성
+  const lottoResult = new LottoResult(winningNumbers, bonusNumber);
+
+  // 로또 당첨 결과 통계 출력
+  const lottoRankingStatistics = lottoResult.getLottoRankingStatistics(lottos);
+  Output.printLottoRankingStatistics(lottoRankingStatistics);
+
+  // 로또 수익률 출력
+  const totalLottoWinningPrice = lottoResult.getTotalLottoWinningPrice(lottos);
+  const totalLottoProfitRate = LottoResult.getTotalLottoProfitRate(
+    totalLottoWinningPrice,
+    purchasedAmount
+  );
+  Output.printLottoProfitRate(totalLottoProfitRate);
+} catch (e) {
+  console.log("\n------------------------\nERROR");
+  console.log(e.message);
 }
-
-// 로또를 발급한 개수 출력
-Output.printGeneratedLottosCount(lottos.length);
-
-// 발급한 로또들의 각 로또 번호들을 출력
-Output.printGeneratedLottosNumbers(lottos);
-
-// 당첨 번호 입력
-const winningNumbers = await Input.getWinningNumbers();
-
-// 보너스 번호 입력
-const bonusNumber = await Input.getBonusNumber(winningNumbers);
-
-// 로또 당첨 결과 생성
-const lottoResult = new LottoResult(winningNumbers, bonusNumber);
-
-// 로또 당첨 결과 통계 출력
-const lottoRankingStatistics = lottoResult.getLottoRankingStatistics(lottos);
-Output.printLottoRankingStatistics(lottoRankingStatistics);
-
-// 로또 수익률 출력
-const totalLottoWinningPrice = lottoResult.getTotalLottoWinningPrice(lottos);
-const totalLottoProfitRate = LottoResult.getTotalLottoProfitRate(
-  totalLottoWinningPrice,
-  purchasedAmount
-);
-Output.printLottoProfitRate(totalLottoProfitRate);
