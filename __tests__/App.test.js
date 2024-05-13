08bc187afa4b4319a0c2494774c10ad02df41faf
seungdeok <akk9132@naver.com>
2024-05-14 00:50:00 +0900

    feat: 구입 금액에 해당하는 만큼 로또를 발행한다.

---
+++ __tests__/App.test.js
@@ -0,0 +1,27 @@
+import { App } from "../src/App";
+import { ERROR_CODES } from "../src/constants/error";
+
+describe("로또 게임 테스트", () => {
+  test("구입 금액에 해당하는 만큼 로또를 발행한다.", () => {
+    // give
+    const app = new App();
+    const price = 8000;
+
+    // when
+    const count = app.getLottoAmount(price);
+
+    // then
+    expect(count).toBe(8);
+  });
+
+  test("구입 금액이 로또 금액보다 작으면 발행을 실패한다.", () => {
+    // give
+    const app = new App();
+    const price = 800;
+
+    const count = () => app.buyLotto(price);
+
+    // when + then
+    expect(count).toThrow(ERROR_CODES.ERROR_AMOUNT_TOO_SMALL);
+  });
+});
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { ERROR_CODES } from "./constants/error";
+import { Lotto } from "./domain/Lotto";
+
+export class App {
+  getLottoAmount(amount) {
+    return Math.floor(amount / Lotto.PRICE);
+  }
+
+  buyLotto(amount) {
+    this.validateAmount(amount);
+  }
+
+  validateAmount(amount) {
+    if (amount < Lotto.PRICE) {
+      throw new Error(ERROR_CODES.ERROR_AMOUNT_TOO_SMALL);
+    }
+  }
+}
+
+new App().play();
