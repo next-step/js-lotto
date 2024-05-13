2d11faa63559c1feafc03bfa85622b401b5a24e8
ChanwooPark <chanw9503@gmail.com>
2024-05-14 01:03:38 +0900

    Feat. Web 상에서 Lotto 값 관련 함수 생성

--- src/step-web-index.js
+++ src/step-web-index.js
@@ -3,4 +3,52 @@
  * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
 
-console.log("Web Browser!");
+import LottoMachine from './domain/LottoMachine';
+
+console.log('Web Browser!');
+
+const confirmButton = document.getElementById('confirmButton');
+const inputPrice = document.getElementById('price');
+const toggleShowNumber = document.querySelector('.lotto-numbers-toggle-button');
+const lottoNumbersDiv = document.querySelector('.lotto-numbers');
+
+const purchaseMessage = document.getElementById('purchase_message');
+
+let lottos = [];
+
+function handleGetPrice() {
+  const { value } = inputPrice;
+}
+
+function handleClickConfirm() {
+  const machine = new LottoMachine();
+  const { value } = inputPrice;
+  lottos = machine.createLottos(value);
+
+  purchaseMessage.innerText = `${lottos.length}개 구매하였습니다.`;
+}
+
+function toggleLottoNumbers() {
+  if (lottoNumbersDiv.style.display === 'none') {
+    lottoNumbersDiv.style.display = 'block'; // 요소 보이기
+    displayLottoTickets();
+  } else {
+    lottoNumbersDiv.style.display = 'none'; // 요소 숨기기
+    lottoNumbersDiv.innerHTML = '';
+  }
+}
+
+function displayLottoTickets() {
+  lottoNumbersDiv.innerHTML = ''; // 기존 티켓을 지우고 새로 시작
+  lottos.forEach((lotto) => {
+    const span = document.createElement('span');
+    span.className = 'mx-1 text-4xl';
+    span.textContent = `🎟️ ${lotto.numbers}`;
+    lottoNumbersDiv.appendChild(span);
+  });
+}
+
+inputPrice.addEventListener('input', handleGetPrice);
+confirmButton.addEventListener('click', handleClickConfirm);
+
+toggleShowNumber.addEventListener('click', toggleLottoNumbers);
