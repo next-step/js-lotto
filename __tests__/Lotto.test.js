fae4e17aece2235ecb8dc5481ad27c3bd708e82e
seungdeok <akk9132@naver.com>
2024-05-14 00:31:00 +0900

    feat: 6개의 중복되지 않는 숫자로 생성한다, 로또의 번호는 1~45까지의 자연수이다

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,3 +1,4 @@
+import { ERROR_CODES } from "../src/constants/error";
 import { Lotto } from "../src/domain/Lotto";
 
 describe("로또 테스트", () => {
@@ -7,5 +8,27 @@ describe("로또 테스트", () => {
 
     // when + then
     expect(price).toBe(1000);
+  });
+
+  test("6개의 중복되지 않는 숫자로 생성한다.", () => {
+    // give
+    const lotto1 = () => new Lotto([1, 2, 3, 4, 5, 5]);
+    const lotto2 = () => new Lotto([1, 2, 3, 4, 5, 6]);
+    const lotto3 = () => new Lotto([1, 2, 3, 4, 5]);
+
+    // when + then
+    expect(lotto1).toThrow(ERROR_CODES.ERROR_DUPLICATE_NUMBER);
+    expect(lotto2).not.toThrow();
+    expect(lotto3).toThrow(ERROR_CODES.ERROR_INVALID_LENGTH);
+  });
+
+  test("로또의 번호는 1~45까지의 자연수이다.", () => {
+    [0, -1, "a"].forEach((input) => {
+      // give
+      const lotto = () => new Lotto([1, 2, 3, 4, 5, input]);
+
+      // when + then
+      expect(lotto).toThrow(ERROR_CODES.ERROR_INVALID_NUMBER);
+    });
   });
 });
--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,3 +1,23 @@
+import { ERROR_CODES } from "../constants/error";
+
 export class Lotto {
-  static PRICE = 1000;  
+  static PRICE = 1000;
+
+  constructor(numbers) {
+    this.validateNumbers(numbers);
+  }
+
+  validateNumbers(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error(ERROR_CODES.ERROR_INVALID_LENGTH);
+    }
+
+    if (numbers.some((num) => isNaN(num) || num < 1 || num > 45)) {
+      throw new Error(ERROR_CODES.ERROR_INVALID_NUMBER);
+    }
+
+    if (new Set(numbers).size !== 6) {
+      throw new Error(ERROR_CODES.ERROR_DUPLICATE_NUMBER);
+    }
+  }
 }
