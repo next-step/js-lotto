38507c8cb50a99534ef1b55122fbb2bcc2f580ed
Vespucci95 <donglonk@naver.com>
2024-05-16 00:55:59 +0900

    refactor: Product 테스트 코드 명세 변경

--- __tests__/Product.test.js
+++ __tests__/Product.test.js
@@ -1,6 +1,7 @@
 import Product from '../src/js/domain/Product';
 import { LOTTO } from '../src/js/constants';
 
+const context = describe;
 const INVALID_PRODUCT_NAME = [
   ['ABCDEABCDEABCDEA'],
   [20],
@@ -21,31 +22,40 @@ const INVALID_PRODUCT_PRICE = [
 ];
 
 describe('상품 기능 테스트', () => {
-  it('상품명, 상품 금액을 가진다.', () => {
-    // given
+  describe('Product 생성자 테스트', () => {
+    context(
+      `상품명: ${LOTTO.NAME_KR}, 상품 금액: ${LOTTO.PRICE}을 인자로 가질 때`,
+      () => {
+        it('name, price 프로퍼티를 가진다.', () => {
+          const lotto = new Product(LOTTO.NAME_KR, LOTTO.PRICE);
 
-    // when
-    const lotto = new Product(LOTTO.NAME_KR, LOTTO.PRICE);
-
-    // then
-    expect(lotto).toHaveProperty('name');
-    expect(lotto).toHaveProperty('price');
+          expect(lotto).toHaveProperty('name');
+          expect(lotto).toHaveProperty('price');
+        });
+      }
+    );
   });
 
   describe('상품명 기능 테스트', () => {
-    it.each(INVALID_PRODUCT_NAME)(
-      '%s 는 상품명이 될 수 없다.',
+    context.each(INVALID_PRODUCT_NAME)(
+      '상품명으로 %s 가 주어질 때',
       (invalidProductName) => {
-        expect(() => new Product(invalidProductName, LOTTO.PRICE)).toThrow();
+        it('에러를 TypeError를 Throw 한다.', () => {
+          expect(() => new Product(invalidProductName, LOTTO.PRICE)).toThrow();
+        });
       }
     );
   });
 
   describe('상품 금액 기능 테스트', () => {
-    it.each(INVALID_PRODUCT_PRICE)(
-      '%s 는 상품 금액이 될 수 없다.',
+    context.each(INVALID_PRODUCT_PRICE)(
+      '상품금액으로 %s 는가 주어질 때',
       (invalidProductPrice) => {
-        expect(() => new Product(LOTTO.NAME_KR, invalidProductPrice)).toThrow();
+        it('에러를 TypeError를 Throw 한다.', () => {
+          expect(
+            () => new Product(LOTTO.NAME_KR, invalidProductPrice)
+          ).toThrow();
+        });
       }
     );
   });
