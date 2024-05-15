8f4e0ae313748aeff95009fd106ef549c7f125b0
Vespucci95 <donglonk@naver.com>
2024-05-16 01:20:42 +0900

    refactor: LottoMachine 테스트 코드 명세 변경

--- __tests__/LottoMachine.test.js
+++ __tests__/LottoMachine.test.js
@@ -5,124 +5,202 @@ import {
   isValidLottoNumberArray,
 } from '../src/js/utils/LottoUtil';
 
+const context = describe;
+const DUPLICATE_BONUS_NUMBERS = [[1], [2], [3], [4], [5], [6]];
+const INVALID_WINNING_AMOUNTS = [
+  [[]],
+  [[1, 2, 3, 4, 5]],
+  [[1, 2, 3, 4, 5, 6, 7]],
+];
+
 describe('로또 머신 기능 테스트', () => {
-  it('로또 머신은 당첨번호, 보너스번호를 가진다.', () => {
-    const lottoMachine = new LottoMachine();
-
-    expect(lottoMachine).toHaveProperty('winningNumbers');
-    expect(lottoMachine).toHaveProperty('bonusWinningNumber');
-  });
-
-  describe('당첨 번호 설정', () => {
-    it('당첨 번호를 설정할 수 있다.', () => {
-      // given
-      const lottoMachine = new LottoMachine();
-
-      // when
-      lottoMachine.winningNumbers = generateLottoNumberArray();
-
-      // then
-      expect(isValidLottoNumberArray(lottoMachine.winningNumbers)).toBeTruthy();
-    });
-
-    it('보너스 번호를 설정할 수 있다.', () => {
-      // given
-      const lottoMachine = new LottoMachine();
-      lottoMachine.winningNumbers = [1, 2, 3, 4, 5, 6];
-
-      // when
-      lottoMachine.bonusWinningNumber = 7;
-
-      // then
-      expect(lottoMachine.bonusWinningNumber).toBe(7);
-      expect(() => (lottoMachine.bonusWinningNumber = 0)).toThrow();
-      expect(() => (lottoMachine.bonusWinningNumber = 6)).toThrow();
-    });
-  });
-
-  it('당첨 번호 설정', () => {
-    // given
-    const lottoMachine = new LottoMachine();
-
-    // when
-    lottoMachine.winningNumbers = generateLottoNumberArray();
-
-    // then
-    expect(isValidLottoNumberArray(lottoMachine.winningNumbers)).toBeTruthy();
-    expect(() => {
-      lottoMachine.winningNumbers = [0, 0, 0, 45, 45, 46];
-    }).toThrow();
-  });
-
-  describe('당첨 금액 설정', () => {
-    it.each([[[]], [[1, 2, 3, 4, 5]], [[1, 2, 3, 4, 5, 6, 7]]])(
-      '%s는 당첨 금액으로 설정할 수 없다.',
-      (winningAmount) => {
-        expect(() => new LottoMachine(winningAmount)).toThrow();
-      }
-    );
-  });
-
-  describe('로또 발권', () => {
-    it('지불할 금액이 로또 금액보다 적다면 에러를 발생한다.', () => {
-      const lottoMachine = new LottoMachine();
-
-      expect(() => lottoMachine.sellAutoLottoTicket(800)).toThrow();
-    });
-
-    it('지불한 금액에 따라 n개의 로또를 발권한다. (자동)', () => {
-      // given
-      const lottoMachine = new LottoMachine();
-
-      // when
-      const lottoTickets = lottoMachine.sellAutoLottoTicket(2_000);
-
-      // then
-      expect(lottoTickets).toHaveLength(2);
-      expect(
-        lottoTickets.every((lotto) => lotto instanceof LottoTicket)
-      ).toBeTruthy();
-    });
-
-    it.each([['1000'], [Infinity], [Number.MAX_SAFE_INTEGER + 1]])(
-      '"%s" 는 sellAutoLottoTicket() 매개변수로 전달될 수 없습니다.',
-      (invalidValue) => {
+  describe('LottoMachine 생성자 테스트', () => {
+    context('LottoMachine 가 생성될 때', () => {
+      it('winningNumbers(당첨번호), bonusWinningNumber(보너스번호)를 프로퍼티로 가진다.', () => {
         const lottoMachine = new LottoMachine();
 
-        expect(() => lottoMachine.sellAutoLottoTicket(invalidValue)).toThrow();
+        expect(lottoMachine).toHaveProperty('winningNumbers');
+        expect(lottoMachine).toHaveProperty('bonusWinningNumber');
+      });
+    });
+  });
+
+  describe('LottoMachine setter 테스트', () => {
+    context(
+      'winningNumbers(당첨 번호)로 [1, 2, 3, 4, 5, 6] 가 주어질 때',
+      () => {
+        it('유효한 로또 번호 검사 결과가 ture 이다.', () => {
+          // given
+          const lottoMachine = new LottoMachine();
+
+          // when
+          lottoMachine.winningNumbers = [1, 2, 3, 4, 5, 6];
+
+          // then
+          expect(
+            isValidLottoNumberArray(lottoMachine.winningNumbers)
+          ).toBeTruthy();
+        });
+      }
+    );
+
+    context(
+      'winningNumbers가 generateLottoNumberArray()를 통해 값이 주어질 때',
+      () => {
+        it('유효성 검사 결과가 true 이다.', () => {
+          // given
+          const lottoMachine = new LottoMachine();
+
+          // when
+          lottoMachine.winningNumbers = generateLottoNumberArray();
+
+          // then
+          expect(
+            isValidLottoNumberArray(lottoMachine.winningNumbers)
+          ).toBeTruthy();
+        });
+      }
+    );
+
+    context(
+      'winningNumbers가 [0, 0, 0, 45, 45, 46]와 같이 중복된 값이 주어질 때',
+      () => {
+        it('TypeError를 Throw 한다.', () => {
+          // given
+          const lottoMachine = new LottoMachine();
+
+          // when
+          lottoMachine.winningNumbers = generateLottoNumberArray();
+
+          // then
+          expect(() => {
+            lottoMachine.winningNumbers = [0, 0, 0, 45, 45, 46];
+          }).toThrow();
+        });
+      }
+    );
+
+    context('bonusWinningNumber(보너스 번호)로 7이 주어질 때', () => {
+      it('bonusWinningNumber의 값은 7이다.', () => {
+        // given
+        const lottoMachine = new LottoMachine();
+        lottoMachine.winningNumbers = [1, 2, 3, 4, 5, 6];
+
+        // when
+        lottoMachine.bonusWinningNumber = 7;
+
+        // then
+        expect(lottoMachine.bonusWinningNumber).toBe(7);
+      });
+    });
+
+    context.each(DUPLICATE_BONUS_NUMBERS)(
+      'winningNumbers가 [1,2,3,4,5,6]이고 bonusWinningNumber가 %s 일때',
+      (bonusNumber) => {
+        it('번호가 중복되므로 TypeError를 Throw한다.', () => {
+          const lottoMachine = new LottoMachine();
+          lottoMachine.winningNumbers = [1, 2, 3, 4, 5, 6];
+
+          expect(
+            () => (lottoMachine.bonusWinningNumber = bonusNumber)
+          ).toThrow();
+        });
       }
     );
   });
 
-  it('n개의 로또의 결과를 반환한다.', () => {
-    // given
-    const lottoMachine = new LottoMachine();
-    lottoMachine.winningNumbers = [1, 2, 3, 4, 5, 6];
-    lottoMachine.bonusWinningNumber = 7;
-
-    const [lotto1st, lotto2st, lotto3st] = Array.from(
-      { length: 3 },
-      () => new LottoTicket()
+  describe('당첨 금액 설정 테스트', () => {
+    context.each(INVALID_WINNING_AMOUNTS)(
+      '%s와 같이 유효하지 않은 당첨 금액이 주어질 때',
+      (winningAmount) => {
+        it('TypeError를 Throw 한다.', () => {
+          expect(() => new LottoMachine(winningAmount)).toThrow();
+        });
+      }
     );
-    lotto1st.lottoNumbers = [1, 2, 3, 4, 5, 6];
-    lotto2st.lottoNumbers = [1, 2, 3, 4, 5, 7];
-    lotto3st.lottoNumbers = [1, 2, 3, 4, 5, 11];
+  });
 
-    // when
-    const { netReturn, chart } = lottoMachine.getStatistics([
-      lotto1st,
-      lotto2st,
-      lotto3st,
-    ]);
+  describe('로또 발권 테스트', () => {
+    context('로또금액 1_000원 보다 적은 800을 지불할 때', () => {
+      it(`"${LottoMachine.NOT_ENOUGH_MONEY}" 에러를 Throw한다.`, () => {
+        const lottoMachine = new LottoMachine();
 
-    // then
-    expect(netReturn).toBe(1500000 + 30000000 + 2000000000);
-    expect(chart).toEqual([
-      [5, { lottoTickets: [], winningAmount: 5000, matchCount: 3 }],
-      [4, { lottoTickets: [], winningAmount: 50000, matchCount: 4 }],
-      [3, { lottoTickets: [{}], winningAmount: 1500000, matchCount: 5 }],
-      [2, { lottoTickets: [{}], winningAmount: 30000000, matchCount: 5 }],
-      [1, { lottoTickets: [{}], winningAmount: 2000000000, matchCount: 6 }],
-    ]);
+        expect(() => lottoMachine.sellAutoLottoTicket(800)).toThrow();
+      });
+    });
+
+    context('로또 금액 1_000원 보다 많은 2_000원 을 지불할 때', () => {
+      it('lottoTickets의 길이는 2이다.', () => {
+        // given
+        const lottoMachine = new LottoMachine();
+
+        // when
+        const lottoTickets = lottoMachine.sellAutoLottoTicket(2_000);
+
+        // then
+        expect(lottoTickets).toHaveLength(2);
+      });
+      it('lottoTickets은 모두 LottoTicket 인스턴스를 가져야 한다. 기대값: true', () => {
+        // given
+        const lottoMachine = new LottoMachine();
+
+        // when
+        const lottoTickets = lottoMachine.sellAutoLottoTicket(2_000);
+
+        // then
+        expect(
+          lottoTickets.every((lotto) => lotto instanceof LottoTicket)
+        ).toBeTruthy();
+      });
+    });
+
+    context.each([['1000'], [Infinity], [Number.MAX_SAFE_INTEGER + 1]])(
+      'sellAutoLottoTicket() 매개변수로 %s가 주어질 때',
+      (invalidValue) => {
+        it('TypeError를 Throw한다.', () => {
+          const lottoMachine = new LottoMachine();
+
+          expect(() =>
+            lottoMachine.sellAutoLottoTicket(invalidValue)
+          ).toThrow();
+        });
+      }
+    );
+  });
+
+  describe('로또 통계 테스트', () => {
+    context('1,2,3등인 3개의 로또를 통계를 내렸을 때', () => {
+      it('수익률과 차트 배열을 반환한다.', () => {
+        // given
+        const lottoMachine = new LottoMachine();
+        lottoMachine.winningNumbers = [1, 2, 3, 4, 5, 6];
+        lottoMachine.bonusWinningNumber = 7;
+
+        const [lotto1st, lotto2st, lotto3st] = Array.from(
+          { length: 3 },
+          () => new LottoTicket()
+        );
+        lotto1st.lottoNumbers = [1, 2, 3, 4, 5, 6];
+        lotto2st.lottoNumbers = [1, 2, 3, 4, 5, 7];
+        lotto3st.lottoNumbers = [1, 2, 3, 4, 5, 11];
+
+        // when
+        const { netReturn, chart } = lottoMachine.getStatistics([
+          lotto1st,
+          lotto2st,
+          lotto3st,
+        ]);
+
+        // then
+        expect(netReturn).toBe(1500000 + 30000000 + 2000000000);
+        expect(chart).toEqual([
+          [5, { lottoTickets: [], winningAmount: 5000, matchCount: 3 }],
+          [4, { lottoTickets: [], winningAmount: 50000, matchCount: 4 }],
+          [3, { lottoTickets: [{}], winningAmount: 1500000, matchCount: 5 }],
+          [2, { lottoTickets: [{}], winningAmount: 30000000, matchCount: 5 }],
+          [1, { lottoTickets: [{}], winningAmount: 2000000000, matchCount: 6 }],
+        ]);
+      });
+    });
   });
 });
