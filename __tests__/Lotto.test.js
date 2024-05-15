109c5a0217c4db79128afdeba9eda646a219f3c0
jiyeon <ejy1105@gmail.com>
2024-05-16 02:05:36 +0900

    feat: lotto 테스트 추가

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,9 +1,19 @@
+import WinningLotto from "../src/domain/WinningLotto";
+
 describe("로또 입력 기능", () => {
   test("로또 한장 가격은 1,000원이다", () => {});
 
-  test("당첨 번호는 1-45사이의 정수이다", () => {});
+  test("당첨 번호는 1-45사이의 정수이다", () => {
+    expect(() => new WinningLotto([2, 9, 33, 34, 40, 55], 7)).toThrow(
+      Error.OVER_MIN_MAX_NUMBER
+    );
+  });
 
-  test("로또 당첨 번호는 보너스 번호와 중복되지 않는다", () => {});
+  test("로또 당첨 번호는 보너스 번호와 중복되지 않는다", () => {
+    expect(() => new WinningLotto([2, 9, 33, 34, 40, 41], 2)).toThrow(
+      Error.BONUS_NUMBER_DUPLICATED
+    );
+  });
 
   test("입력받은 금액 만큼 로또를 발행한다", () => {});
 });
