44687e4f9080a274a4be187c121e6fddd3e869c4
Vespucci95 <donglonk@naver.com>
2024-05-16 00:56:18 +0900

    refactor: LottoUtil 테스트 코드 명세 변경

--- __tests__/LottoUtil.test.js
+++ __tests__/LottoUtil.test.js
@@ -3,24 +3,29 @@ import {
   isValidLottoNumberArray,
 } from '../src/js/utils/LottoUtil';
 
+const context = describe;
+const INVALID_LOTTO_NUMBER = [['1'], [0], [46], [Infinity], [-1], [''], ['  ']];
+const INVALID_LOTTO_NUMBERS = [
+  [[0, 0, 0, 0, 0, 0], '모든 번호가 0인 경우'],
+  [[1, 1, 1, 1, 1, 1], '1부터 6까지 중복된 경우'],
+  [[1, 2, 3, 4, 5], '번호가 6개가 아닌 경우'],
+  [[7, 15, 21, 32, 44, 49], '유효한 번호 범위를 벗어난 경우'],
+];
+
 describe('로또 유틸 기능 테스트', () => {
-  describe('번호는 1~45 사이의 숫자값이다.', () => {
-    it.each([['1'], [0], [46], [Infinity], [-1], [''], ['  ']])(
-      '%s 는 유효하지 않는 로또 번호입니다.',
-      (lottoNumber) => {
+  describe('로또 번호 유효성 테스트 : 번호는 1~45 사이의 숫자값이다.', () => {
+    context.each(INVALID_LOTTO_NUMBER)('%s 가 주어진 경우', (lottoNumber) => {
+      it('false를 반환한다.', () => {
         expect(isValidLottoNumber(lottoNumber)).toBeFalsy();
-      }
-    );
+      });
+    });
   });
 
-  describe('유효한 로또 번호 테스트', () => {
-    it.each([
-      [[0, 0, 0, 0, 0, 0], '모든 번호가 0인 경우'],
-      [[1, 1, 1, 1, 1, 1], '1부터 6까지 중복된 경우'],
-      [[1, 2, 3, 4, 5], '번호가 6개가 아닌 경우'],
-      [[7, 15, 21, 32, 44, 49], '유효한 번호 범위를 벗어난 경우'],
-    ])('"%s" %s', (lottoNumber) => {
-      expect(isValidLottoNumberArray(lottoNumber)).toBeFalsy();
+  describe('로또 번호 (배열) 유효성 테스트', () => {
+    context.each(INVALID_LOTTO_NUMBERS)('%s 와 같이 %s일 때', (lottoNumber) => {
+      it('false를 반환한다.', () => {
+        expect(isValidLottoNumberArray(lottoNumber)).toBeFalsy();
+      });
     });
   });
 });
