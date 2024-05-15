e935e346ac9c8552638134324af0e52d8fd55e18
Paik <gwbaik9717@icloud.com>
2024-05-15 09:41:31 +0900

    refactor: LottoNumber 도메인 추가

---
+++ __tests__/LottoNumber.test.js
@@ -0,0 +1,112 @@
+import {
+  ErrorLottoBonusNumber,
+  ErrorLottoNumber,
+  ErrorLottoNumbers,
+} from "../src/js/constants/error";
+import LottoNumber from "../src/js/domain/LottoNumber";
+
+describe("로또 번호 기능 테스트", () => {
+  test("로또 번호 중 중복되는 수가 있다면 에러가 발생한다", () => {
+    // given
+    const lottoNumbers = "1,1,2,3,4,5";
+
+    // when
+    const validateLottoNumbers = () =>
+      LottoNumber.validateLottoNumbers(lottoNumbers);
+
+    // then
+    expect(validateLottoNumbers).toThrow(
+      ErrorLottoNumbers.ERROR_LOTTO_NUMBERS_DUPLICATED
+    );
+  });
+
+  test("한 개 이상의 로또 번호가 정수가 아니면 에러가 발생한다", () => {
+    // given
+    const lottoNumbers = "가나다,1,2,3,4,5";
+
+    // when
+    const validateLottoNumbers = () =>
+      LottoNumber.validateLottoNumbers(lottoNumbers);
+
+    // then
+    expect(validateLottoNumbers).toThrow(
+      ErrorLottoNumber.ERROR_LOTTO_NUMBER_NOT_NUMBER
+    );
+  });
+
+  test("한 개 이상의 로또 번호가  1 미만 또는 45 초과이면 로또가 정상적으로 생성되지 않는다.", () => {
+    // given
+    const lottoNumbers = "0,1,2,3,4,46";
+
+    // when
+    const validateLottoNumbers = () =>
+      LottoNumber.validateLottoNumbers(lottoNumbers);
+
+    // then
+    expect(validateLottoNumbers).toThrow(
+      ErrorLottoNumber.ERROR_LOTTO_NUMBER_NOT_VALID_INTEGER
+    );
+  });
+
+  test("로또 번호를 자동으로 랜덤 생성할 때, 생성되는 로또 번호는 1이상 45이하의 서로 다른 정수이다.", () => {
+    // given
+    const randomLottoNumbers = LottoNumber.generateRandomLottoNumbers();
+
+    // when
+    const validate = () => {
+      LottoNumber.validateLottoNumbers(randomLottoNumbers);
+    };
+
+    // then
+    expect(validate()).toBeUndefined();
+  });
+
+  test("보너스 번호는 1 이상 45 미만의 정수이며 당첨 번호와 겹치지 않는다.", () => {
+    // given
+    const winningNumbers = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 7;
+
+    // when
+    const validate = () => {
+      LottoNumber.validateBonusNumber(bonusNumber, winningNumbers);
+    };
+
+    // then
+    expect(validate()).toBeUndefined();
+  });
+
+  test.each([
+    [
+      {
+        winningNumbers: [1, 2, 3, 4, 5, 6],
+        bonusNumber: 6,
+        erroMessage: ErrorLottoBonusNumber.ERROR_LOTTO_BONUS_NUMBER_DUPLICATED,
+      },
+      {
+        winningNumbers: [1, 2, 3, 4, 5, 6],
+        bonusNumber: 56,
+        erroMessage: ErrorLottoNumber.ERROR_LOTTO_NUMBER_NOT_VALID_INTEGER,
+      },
+      {
+        winningNumbers: [1, 2, 3, 4, 5, 6],
+        bonusNumber: "가나다",
+        erroMessage: ErrorLottoNumber.ERROR_LOTTO_NUMBER_NOT_NUMBER,
+      },
+    ],
+  ])(
+    "보너스 번호가 1 미만 45 초과의 정수이거나 당첨 번호와 겹치면 에러메시지를 출력한다.",
+    (testSet) => {
+      // given
+      // when
+      const validate = () => {
+        LottoNumber.validateBonusNumber(
+          testSet.bonusNumber,
+          testSet.winningNumbers
+        );
+      };
+
+      // then
+      expect(validate).toThrow(testSet.erroMessage);
+    }
+  );
+});
---
+++ src/js/domain/LottoNumber.js
@@ -0,0 +1,84 @@
+import {
+  ErrorLottoBonusNumber,
+  ErrorLottoNumber,
+  ErrorLottoNumbers,
+} from "../constants/error";
+
+const LottoNumber = {
+  LENGTH_LOTTO_NUMBERS: 6,
+  MAX_LOTTO_NUMBER: 45,
+  MIN_LOTTO_NUMBER: 1,
+
+  validateLottoNumbers(input) {
+    const lottoNumbers = this.convertLottoNumbersToArray(input);
+    const lottoNumbersSet = new Set(lottoNumbers);
+
+    if (lottoNumbers.length !== this.LENGTH_LOTTO_NUMBERS) {
+      throw new Error(ErrorLottoNumbers.ERROR_LOTTO_NUMBERS_NOT_VALID_LENGTH);
+    }
+
+    if (lottoNumbers.length !== lottoNumbersSet.size) {
+      throw new Error(ErrorLottoNumbers.ERROR_LOTTO_NUMBERS_DUPLICATED);
+    }
+
+    lottoNumbers.forEach((lottoNumber) => {
+      this.validateLottoNumber(lottoNumber);
+    });
+  },
+
+  validateBonusNumber(input, winningNumbers) {
+    this.validateLottoNumber(input);
+
+    if (winningNumbers.includes(Number(input))) {
+      throw new Error(
+        ErrorLottoBonusNumber.ERROR_LOTTO_BONUS_NUMBER_DUPLICATED
+      );
+    }
+  },
+
+  validateLottoNumber(input) {
+    if (isNaN(input)) {
+      throw new Error(ErrorLottoNumber.ERROR_LOTTO_NUMBER_NOT_NUMBER);
+    }
+
+    if (!Number.isInteger(Number(input))) {
+      throw new Error(ErrorLottoNumber.ERROR_LOTTO_NUMBER_NOT_VALID_INTEGER);
+    }
+
+    if (
+      Number(input) < this.MIN_LOTTO_NUMBER ||
+      Number(input) > this.MAX_LOTTO_NUMBER
+    ) {
+      throw new Error(ErrorLottoNumber.ERROR_LOTTO_NUMBER_NOT_VALID_INTEGER);
+    }
+  },
+
+  generateRandomLottoNumbers() {
+    const lottoNumbers = [];
+    const candidateLottoNumbers = Array.from(
+      { length: this.MAX_LOTTO_NUMBER },
+      (_, i) => i + 1
+    );
+
+    for (let i = 0; i < this.LENGTH_LOTTO_NUMBERS; i++) {
+      const randomIndex = Math.floor(
+        Math.random() * candidateLottoNumbers.length
+      );
+
+      const deletedNumbers = candidateLottoNumbers.splice(randomIndex, 1);
+      lottoNumbers.push(...deletedNumbers);
+    }
+
+    return lottoNumbers;
+  },
+
+  convertLottoNumbersToArray(lottoNumbers) {
+    if (typeof lottoNumbers !== "string") {
+      return lottoNumbers;
+    }
+
+    return lottoNumbers.split(",").map(Number);
+  },
+};
+
+export default LottoNumber;
