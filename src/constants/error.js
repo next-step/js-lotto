5118adbd2d29fa71ab6175192e2d974b7a82b729
jiyeon <ejy1105@gmail.com>
2024-05-16 02:06:02 +0900

    feat: number 상수, 에러메시지 추가

---
+++ src/constants/error.js
@@ -0,0 +1,6 @@
+export const Error = {
+  OVER_MIN_MAX_NUMBER: "로또 번호는 1~45 사이의 정수여야 합니다.",
+  NUMBER_LENGTH_SIX: "로또 번호는 6개여야 합니다.",
+  NUMBER_DUPLICATED: "로또 번호는 중복되지 않아야 합니다.",
+  BONUS_NUMBER_DUPLICATED: "보너스 번호는 당첨 번호와 중복될 수 없습니다.",
+};
---
+++ src/constants/number.js
@@ -0,0 +1,9 @@
+export const LOTTO_NUMBER_LENGTH = 6;
+export const MIN_NUMBER = 1;
+export const MAX_NUMBER = 45;
+
+export const FIRST_PRIZE = 2000000000;
+export const SECOND_PRIZE = 30000000;
+export const THIRD_PRIZE = 1500000;
+export const FOURTH_PRIZE = 50000;
+export const FIFTH_PRIZE = 5000;
