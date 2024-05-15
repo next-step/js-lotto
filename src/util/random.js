6d4838f2885a629be8124b2aa94e49c5510e6060
jiyeon <ejy1105@gmail.com>
2024-05-16 02:06:53 +0900

    feat: 랜덤 함수와 정렬 함수 추가

---
+++ src/util/random.js
@@ -0,0 +1,7 @@
+export const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+export const sortingNumber = (arrayNum) => {
+  return arrayNum.sort((a, b) => a - b);
+};
