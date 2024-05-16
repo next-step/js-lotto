d265495d04f36065e6706d28809dbbe6a55fc2e7
sochong <hsju0202@gmail.com>
2024-05-16 18:19:20 +0900

    test: drawUniqueItems 테스트 수정 및 추가
    - 기존 3개를 뽑는 테스트를 2개를 뽑는 것으로 수정
    - 리스트의 크기보다 뽑을 아이템 수가 많을때 테스트 추가

--- __test__/Draw.test.js
+++ __test__/Draw.test.js
@@ -26,12 +26,25 @@ describe("뽑기 테스트", () => {
   test("리스트에서 중복되지 않는 아이템을 원하는 만큼 뽑는다.", () => {
     //given
     const list = [1, 2, 3];
+    const amount = 2;
 
     //when
-    const items = drawUniqueItems(list, list.length);
+    const items = drawUniqueItems(list, amount);
 
     //then
-    expect(items.sort()).toStrictEqual(list);
+    items.forEach((e) => expect(list).toContain(e));
+  });
+
+  test("리스트의 크기보다 뽑을 아이템 수가 많다면 에러를 반환한다.", () => {
+    //given
+    const list = [1, 2, 3];
+    const amount = 4;
+
+    //when
+    const whenTooManyAmount = () => drawUniqueItems(list, amount);
+
+    //then
+    expect(whenTooManyAmount).toThrow();
   });
 
   test("이미 중복이 있는 리스트라면 에러를 반환한다.", () => {
