9f0d4ed06caf33020807ece569f6010cf630a7ae
ChanwooPark <chanw9503@gmail.com>
2024-05-16 14:54:29 +0900

    Feat. 정렬 함수 추가

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,13 @@ export function generateRandomNumbers(range, radix) {
 export function filterArray(array, targetValue) {
   return array.filter((item) => item.result === targetValue).length;
 }
+
+export function sortArray(order = 'ASC' | 'DESC', array) {
+  return array.sort((a, b) => {
+    if (order === 'ASC') {
+      return a - b;
+    } else if (order === 'DESC') {
+      return b - a;
+    }
+  });
+}
