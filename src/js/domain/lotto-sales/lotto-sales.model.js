e23b7b668ede568df748fe2bf0470f0ab97c4c24
monat96 <42468323+monat96@users.noreply.github.com>
2024-05-17 04:47:52 +0900

    style: camelCase 적용
    - amountPaid

--- src/js/domain/lotto-sales/lotto-sales.model.js
+++ src/js/domain/lotto-sales/lotto-sales.model.js
@@ -4,10 +4,10 @@ import LOTTO_SALES from "./lotto-sales.constant.js";
 import { validateAmountPaid } from "./lotto-sales.contract.js";
 
 class LottoSales {
-  purchase(amount_paid) {
-    validateAmountPaid(amount_paid);
+  purchase(amountPaid) {
+    validateAmountPaid(amountPaid);
 
-    const availableLottoCount = Math.floor(amount_paid / LOTTO_SALES.PRICE);
+    const availableLottoCount = Math.floor(amountPaid / LOTTO_SALES.PRICE);
 
     return Array.from({ length: availableLottoCount }, () => new Lotto());
   }
