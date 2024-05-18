cfe87258350562eb32b559b41ffa0f864e63ee35
Paik <gwbaik9717@icloud.com>
2024-05-18 09:43:14 +0900

    refactor: step-console-index play 로직 수정

--- src/step-console-index.js
+++ src/step-console-index.js
@@ -6,85 +6,85 @@ import Output from "./js/view/Output";
 import repeatUntilNoError from "./utils/repeatUntilNoError";
 
 const play = async () => {
-  while (true) {
-    // 로또를 구입할 금액 입력
-    const purchasedAmount = await repeatUntilNoError(
-      Input.getLottoPurchasedAmount
-    );
+  // 로또를 구입할 금액 입력
+  const purchasedAmount = await repeatUntilNoError(
+    Input.getLottoPurchasedAmount
+  );
 
-    // 로또를 구입한 금액만큼 최대 개수의 로또 발급
-    const availableLottoCount =
-      LottoGame.getPurchasableLottoCount(purchasedAmount);
+  // 로또를 구입한 금액만큼 최대 개수의 로또 발급
+  const availableLottoCount =
+    LottoGame.getPurchasableLottoCount(purchasedAmount);
 
-    const lottos = [];
-    for (let i = 0; i < availableLottoCount; i++) {
-      const lottoNumbers = Lotto.generateRandomLottoNumbers();
-      lottos.push(new Lotto(lottoNumbers));
-    }
-
-    // 로또를 발급한 개수 출력
-    Output.printGeneratedLottosCount(lottos.length);
-
-    // 발급한 로또들의 각 로또 번호들을 오름차순으로 출력
-    const sortedLottosNumbers = lottos.map((lotto) =>
-      Lotto.sortLottoNumbersByAscendingOrder(lotto.numbers)
-    );
-    Output.printGeneratedLottosNumbers(sortedLottosNumbers);
-
-    // 당첨 번호 입력
-    const winningLotto = await repeatUntilNoError(Input.getWinningLotto);
-
-    // 보너스 번호 입력
-    const bonusNumber = await repeatUntilNoError(
-      async () => await Input.getBonusNumber(winningLotto)
-    );
-
-    // 로또 당첨 결과 생성
-    const lottoResult = new LottoResult(winningLotto, bonusNumber);
-
-    // 로또 당첨 결과 통계 출력
-    const lottoRankings = [
-      LottoResult.LottoRanking["FIFTH"],
-      LottoResult.LottoRanking["FOURTH"],
-      LottoResult.LottoRanking["THIRD"],
-      LottoResult.LottoRanking["SECOND"],
-      LottoResult.LottoRanking["FIRST"],
-    ];
-
-    const lottoRankingStatistics = lottoRankings.map((lottoRanking) => {
-      const lottoRankingInfo = LottoResult.LottoRankingInfo[lottoRanking];
-      const lottoRankingCount = lottoResult.getLottoRankingCount(
-        lottos,
-        lottoRanking
-      );
-
-      return {
-        count: lottoRankingCount,
-        rankingWinningPrice: lottoRankingInfo.winningPrice,
-        rankingCondition: lottoRankingInfo.condition,
-        isShowExtraMent: lottoRanking === LottoResult.LottoRanking["SECOND"],
-      };
-    });
-
-    Output.printLottoRankingStatistics(lottoRankingStatistics);
-
-    // 로또 수익률 출력
-    const totalLottoWinningPrice =
-      lottoResult.getTotalLottoWinningPrice(lottos);
-    const totalLottoProfitRate = LottoResult.getTotalLottoProfitRate(
-      totalLottoWinningPrice,
-      purchasedAmount
-    );
-    Output.printLottoProfitRate(totalLottoProfitRate);
-
-    //  재시작/종료 여부를 입력받는다.
-    const isRestartLottoGame = await repeatUntilNoError(
-      Input.getIsRestartLottoGame
-    );
-    if (!isRestartLottoGame) {
-      break;
-    }
+  const lottos = [];
+  for (let i = 0; i < availableLottoCount; i++) {
+    const lottoNumbers = Lotto.generateRandomLottoNumbers();
+    lottos.push(new Lotto(lottoNumbers));
   }
+
+  // 로또를 발급한 개수 출력
+  Output.printGeneratedLottosCount(lottos.length);
+
+  // 발급한 로또들의 각 로또 번호들을 오름차순으로 출력
+  const sortedLottosNumbers = lottos.map((lotto) =>
+    Lotto.sortLottoNumbersByAscendingOrder(lotto.numbers)
+  );
+  Output.printGeneratedLottosNumbers(sortedLottosNumbers);
+
+  // 당첨 번호 입력
+  const winningLotto = await repeatUntilNoError(Input.getWinningLotto);
+
+  // 보너스 번호 입력
+  const bonusNumber = await repeatUntilNoError(
+    async () => await Input.getBonusNumber(winningLotto)
+  );
+
+  // 로또 당첨 결과 생성
+  const lottoResult = new LottoResult(winningLotto, bonusNumber);
+
+  // 로또 당첨 결과 통계 출력
+  const lottoRankings = [
+    LottoResult.LottoRanking["FIFTH"],
+    LottoResult.LottoRanking["FOURTH"],
+    LottoResult.LottoRanking["THIRD"],
+    LottoResult.LottoRanking["SECOND"],
+    LottoResult.LottoRanking["FIRST"],
+  ];
+
+  const lottoRankingStatistics = lottoRankings.map((lottoRanking) => {
+    const lottoRankingInfo = LottoResult.LottoRankingInfo[lottoRanking];
+    const lottoRankingCount = lottoResult.getLottoRankingCount(
+      lottos,
+      lottoRanking
+    );
+
+    return {
+      count: lottoRankingCount,
+      rankingWinningPrice: lottoRankingInfo.winningPrice,
+      rankingCondition: lottoRankingInfo.condition,
+      isShowExtraMent: lottoRanking === LottoResult.LottoRanking["SECOND"],
+    };
+  });
+
+  Output.printLottoRankingStatistics(lottoRankingStatistics);
+
+  // 로또 수익률 출력
+  const totalLottoWinningPrice = lottoResult.getTotalLottoWinningPrice(lottos);
+  const totalLottoProfitRate = LottoResult.getTotalLottoProfitRate(
+    totalLottoWinningPrice,
+    purchasedAmount
+  );
+  Output.printLottoProfitRate(totalLottoProfitRate);
 };
 
-play();
+while (true) {
+  // 로또 게임 시작
+  await play();
+
+  //  재시작/종료 여부를 입력받는다.
+  const isRestartLottoGame = await repeatUntilNoError(
+    Input.getIsRestartLottoGame
+  );
+  if (!isRestartLottoGame) {
+    break;
+  }
+}
