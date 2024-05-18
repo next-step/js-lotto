3e1df40c90a4d690d039dc9ea621c76e20356561
oxlzlo <yeojin58@naver.com>
2024-05-18 23:36:51 +0900

    refactor: 불필요한 변수 제거 (바로 return으로 반환)

--- src/tests/Lotto.js
+++ src/tests/Lotto.js
@@ -12,8 +12,7 @@ class Lotto {
 
 	purchase(purchaseAmount) {
 		// 구매 금액을 로또 가격으로 나눈 몫이 quantity
-		this.quantity = purchaseAmount / this.price;
-		return this.quantity;
+		return purchaseAmount / this.price;
 	}
 
 	createLottoNumbers() {
