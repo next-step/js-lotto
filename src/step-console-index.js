8e95bd15481a55ae3e445a27fbb2b5e0d639d3bc
jiyeon <ejy1105@gmail.com>
2024-05-20 00:20:42 +0900

    fix: console view 호출부에서 도메인 로직 제거 및 수정

--- src/step-console-index.js
+++ src/step-console-index.js
@@ -4,46 +4,65 @@
  */
 
 import Lotto from "./domain/Lotto";
-import WinningLotto from "./domain/WinningLotto";
+import LottoMachine from "./domain/LottoMachine";
+import LottoStats from "./domain/LottoStats";
+import WinningLotto, { PRIZE } from "./domain/WinningLotto";
 import { readLineAsync } from "./util/readLine";
 
 async function play() {
   const pay = await readLineAsync("구입금액을 입력해 주세요. > ");
 
-  const buyLottoCount = Math.floor(pay / 1000); //정수가 아닐경우,
+  const lottoMachine = new LottoMachine(pay);
+  console.log(`${lottoMachine.amount}개를 구매했습니다.`);
+  console.log(lottoMachine.number);
 
-  console.log(`${buyLottoCount}개를 구매했습니다.`);
-
-  const arrayLotto = Array.from({ length: buyLottoCount }, () => {
-    const lotto = new Lotto();
-    console.log(lotto.number);
-
-    return lotto.number;
-  });
-
-  const winningLotto = await readLineAsync("당첨 번호를 입력해주세요. > ");
+  const winningLottoNumber = await readLineAsync(
+    "당첨 번호를 입력해주세요. > "
+  );
   const bonusLottoNum = await readLineAsync("보너스 번호를 입력해주세요. > ");
 
-  const winningNum = new WinningLotto(
-    winningLotto.split(",").map(Number),
+  const winningLotto = new WinningLotto(
+    winningLottoNumber.split(",").map(Number),
     bonusLottoNum
   );
 
-  const stats = arrayLotto.map((lotto) =>
-    lotto.filter((x) => winningNum.number.includes(x))
-  );
+  const prizeList = winningLotto.getResultPrize(lottoMachine.number);
 
-  const rank = winningNum.countLotto(stats);
+  console.log(prizeList);
+
+  const stats = new LottoStats(prizeList);
+
+  console.log(stats);
 
   console.log("당첨 통계");
   console.log("---------------");
-  console.log(`3개 일치 (5,000원) - ${rank.fifth}개`);
-  console.log(`4개 일치 (50,000원) - ${rank.fourth}개`);
-  console.log(`5개 일치 (1,500,000원) - ${rank.third}개`);
-  console.log(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${rank.second}개`);
-  console.log(`6개 일치 (2,000,000,000원) - ${rank.first}개`);
+  console.log(
+    `${PRIZE.FIFTH.matchCount}개 일치 (${PRIZE.FIFTH.prize}원) - ${
+      stats.stats[PRIZE.FIFTH.rank]
+    }개`
+  );
+  console.log(
+    `${PRIZE.FOURTH.matchCount}개 일치 (${PRIZE.FOURTH.prize}원) - ${
+      stats.stats[PRIZE.FOURTH.rank]
+    }개`
+  );
+  console.log(
+    `${PRIZE.THIRD.matchCount}개 일치 (${PRIZE.THIRD.prize}원) - ${
+      stats.stats[PRIZE.THIRD.rank]
+    }개`
+  );
+  console.log(
+    `${PRIZE.SECOND.matchCount}개 일치, 보너스 볼 일치 (${
+      PRIZE.SECOND.prize
+    }원) - ${stats.stats[PRIZE.SECOND.rank]}개`
+  );
+  console.log(
+    `${PRIZE.FIRST.matchCount}개 일치 (${PRIZE.FIRST.prize}원) - ${
+      stats.stats[PRIZE.FIRST.rank]
+    }개`
+  );
 
-  console.log(`총 수익률은${winningNum.totalReturn(pay)}`);
+  console.log(`총 수익률은${stats.totalReturn(pay)}`);
 }
 
 play();
