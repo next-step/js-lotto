99c2ae615a9d7ad7bcb9e1cd28531b1409533fd6
oxlzlo <yeojin58@naver.com>
2024-05-19 17:36:45 +0900

    refactor: 반복되는 테스트코드를 test.each api 사용으로 중복 제거

--- __tests__/Winning.test.js
+++ __tests__/Winning.test.js
@@ -16,38 +16,41 @@ describe('로또 당첨 테스트', () => {
 		// then
 		expect(matches).toBe(4);
 	});
+});
 
-	test('당첨은 1등부터 5등까지 있다', () => {
-		// given
-		const firstPlace = new Winning([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], 7);
-		const secondPlace = new Winning([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 7], 7);
-		const thirdPlace = new Winning([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 8], 7);
-		const fourthPlace = new Winning([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 8, 9], 7);
-		const fifthPlace = new Winning([1, 2, 3, 4, 5, 6], [1, 2, 3, 8, 9, 10], 7);
+describe('당첨은 1등부터 5등까지 있다', () => {
+	test.each([
+		{ lottoNumbers: [1, 2, 3, 4, 5, 6], userInputWinningNumber: [1, 2, 3, 4, 5, 6], userInputBonusNumber: 7, expectedPlace: WinningRank.FIRST_PLACE },
+		{ lottoNumbers: [1, 2, 3, 4, 5, 6], userInputWinningNumber: [1, 2, 3, 4, 5, 7], userInputBonusNumber: 7, expectedPlace: WinningRank.SECOND_PLACE },
+		{ lottoNumbers: [1, 2, 3, 4, 5, 6], userInputWinningNumber: [1, 2, 3, 4, 5, 8], userInputBonusNumber: 7, expectedPlace: WinningRank.THIRD_PLACE },
+		{ lottoNumbers: [1, 2, 3, 4, 5, 6], userInputWinningNumber: [1, 2, 3, 4, 8, 9], userInputBonusNumber: 7, expectedPlace: WinningRank.FOURTH_PLACE },
+		{ lottoNumbers: [1, 2, 3, 4, 5, 6], userInputWinningNumber: [1, 2, 3, 8, 9, 10], userInputBonusNumber: 7, expectedPlace: WinningRank.FIFTH_PLACE }
+	])('Lotto: %o, User winning: %o, User bonus: %i', ({ lottoNumbers, userInputWinningNumber, userInputBonusNumber, expectedPlace }) => {
+		const winning = new Winning(lottoNumbers, userInputWinningNumber, userInputBonusNumber);
 
 		// when
+		const place = winning.checkWinning();
+
 		// then
-		expect(firstPlace.checkWinning()).toBe(WinningRank.FIRST_PLACE);
-		expect(secondPlace.checkWinning()).toBe(WinningRank.SECOND_PLACE);
-		expect(thirdPlace.checkWinning()).toBe(WinningRank.THIRD_PLACE);
-		expect(fourthPlace.checkWinning()).toBe(WinningRank.FOURTH_PLACE);
-		expect(fifthPlace.checkWinning()).toBe(WinningRank.FIFTH_PLACE);
+		expect(place).toBe(expectedPlace);
 	});
+});
 
-	test('당첨 기준에 대한 금액 일치', () => {
+describe('당첨 기준에 대한 금액 일치', () => {
+	test.each([
+		{ lottoNumbers: [1, 2, 3, 4, 5, 6], userInputWinningNumber: [1, 2, 3, 4, 5, 6], userInputBonusNumber: 7, expectedPrized: WinningPrize.FIRST_PRIZE },
+		{ lottoNumbers: [1, 2, 3, 4, 5, 6], userInputWinningNumber: [1, 2, 3, 4, 5, 7], userInputBonusNumber: 7, expectedPrized: WinningPrize.SECOND_PRIZE },
+		{ lottoNumbers: [1, 2, 3, 4, 5, 6], userInputWinningNumber: [1, 2, 3, 4, 5, 8], userInputBonusNumber: 7, expectedPrized: WinningPrize.THIRD_PRIZE },
+		{ lottoNumbers: [1, 2, 3, 4, 5, 6], userInputWinningNumber: [1, 2, 3, 4, 8, 9], userInputBonusNumber: 7, expectedPrized: WinningPrize.FOURTH_PRIZE },
+		{ lottoNumbers: [1, 2, 3, 4, 5, 6], userInputWinningNumber: [1, 2, 3, 8, 9, 10], userInputBonusNumber: 7, expectedPrized: WinningPrize.FIFTH_PRIZE }
+	])('Lotto: %o, User winning: %o, User bonus: %i', ({ lottoNumbers, userInputWinningNumber, userInputBonusNumber, expectedPrized }) => {
 		// given
-		const firstPlace = new Winning([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6], 7);
-		const secondPlace = new Winning([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 7], 7);
-		const thirdPlace = new Winning([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 8], 7);
-		const fourthPlace = new Winning([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 8, 9], 7);
-		const fifthPlace = new Winning([1, 2, 3, 4, 5, 6], [1, 2, 3, 8, 9, 10], 7);
+		const winning = new Winning(lottoNumbers, userInputWinningNumber, userInputBonusNumber);
 
 		// when
+		const prize = winning.calculatePrize();
+
 		// then
-		expect(firstPlace.calculatePrize()).toBe(WinningPrize.FIRST_PRIZE);
-		expect(secondPlace.calculatePrize()).toBe(WinningPrize.SECOND_PRIZE);
-		expect(thirdPlace.calculatePrize()).toBe(WinningPrize.THIRD_PRIZE);
-		expect(fourthPlace.calculatePrize()).toBe(WinningPrize.FOURTH_PRIZE);
-		expect(fifthPlace.calculatePrize()).toBe(WinningPrize.FIFTH_PRIZE);
+		expect(prize).toBe(expectedPrized);
 	});
 });
