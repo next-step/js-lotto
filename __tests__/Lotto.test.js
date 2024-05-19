125f7b0860ad62f3be06dd2a484cb924e2d6286d
oxlzlo <yeojin58@naver.com>
2024-05-19 15:38:56 +0900

    refactor: 로또 구입만큼 로또를 발행하는 테스트 로또 실체를 만들어 테스트 진행

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -16,10 +16,12 @@ describe('로또 기능 테스트', () => {
 		const lotto = new Lotto();
 
 		// when
-		const quantity = lotto.purchase(5000);
+		// const quantity = lotto.purchase(5000);
+		const purchaseAmount = 5000;
+		const lottoQuantity = lotto.generatePurchasedLottoNumbers(purchaseAmount);
 
 		// then
-		expect(quantity).toBe(5);
+		expect(lottoQuantity.length).toBe(5);
 	});
 
 	test('로또 당첨 번호는 1 ~ 45 사이의 무작위 수 중 중복되지 않는 6개이다.', () => {
