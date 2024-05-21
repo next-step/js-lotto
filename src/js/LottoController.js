cc5d44f9e943ca4e8d3f1f8b986fbe4bc9a4d488
DongHyeonWon <dhe7700@naver.com>
2024-05-21 18:55:16 +0900

    feat: 번호 보기를 토글하면 로또 번호가 출력되거나 사라진다

--- src/js/LottoController.js
+++ src/js/LottoController.js
@@ -12,14 +12,24 @@ export default class LottoController {
 
   init() {
     this.initPurchasePrice();
+    this.initLottosToggle();
   }
 
   initPurchasePrice() {
     $("#purchase_price_button").addEventListener("click", (e) => {
       const purchasePrice = $("#purchase_price_input").value;
       this.#lotto = new Lotto(purchasePrice);
-      e.target.disabled = true;
+      if ($("#lottos_toggle_button").checked === false) {
+        $("#lotto_result_box").classList.add("d-none");
+      }
       output.lottoResult(this.#lotto.lottos);
+    });
+  }
+
+  initLottosToggle() {
+    $("#lottos_toggle_button").addEventListener("click", (e) => {
+      console.log($("#lottos_toggle_button").checked);
+      $("#lotto_result_box").classList.toggle("d-none");
     });
   }
 }
