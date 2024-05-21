d84b4fbb5d6c0d9b2b6d59d10a7716511fb964a5
Vespucci95 <donglonk@naver.com>
2024-05-22 00:23:21 +0900

    feat: 로또 요구사항 구현 (재시작, 사용자입력)

--- src/js/ConsoleApp.js
+++ src/js/ConsoleApp.js
@@ -1,28 +1,41 @@
 import LottoMachine from './domain/LottoMachine';
 import LottoCalculator from './domain/LottoCalculator';
-import { comma, readLineAsync } from './utils';
-import { convertLottoStringToLottoArray } from './utils/LottoUtil';
 import { readLottoNumbers, readROI } from './view/LottoIO';
+import { comma, prompt } from './utils';
+import ThrowMessage from './utils/ThrowMessage';
+import LottoThrowMessage from './utils/LottoThrowMessage';
+import { convertLottoStringToLottoArray } from './utils/LottoUtil';
+import { LOTTO } from './constants';
 
 async function ConsoleApp() {
   try {
-    const purchaseAmount = await readLineAsync('구입금액을 입력해 주세요. : ');
+    const purchaseAmount = await prompt('구입금액을 입력해 주세요. : ', {
+      format: Number,
+      validate: validatePurchaseAmount,
+    });
+
     const lottoTickets = LottoMachine.sellAutoLottoTicket(
       Number(purchaseAmount)
     );
 
     console.log(`${lottoTickets.length}개를 구매했습니다.`);
-    lottoTickets.forEach((lottoTicket) =>
-      console.log(readLottoNumbers(lottoTicket))
-    );
+    lottoTickets.forEach((lottoTicket) => {
+      console.log(readLottoNumbers(lottoTicket));
+    });
 
-    const winningNumbers = await readLineAsync('당첨 번호를 입력해 주세요. : ');
+    const winningNumbers = await prompt('당첨 번호를 입력해 주세요. : ', {
+      format: convertLottoStringToLottoArray,
+      validate: validateWinningNumbers,
+    });
 
-    const bonusNumber = await readLineAsync('보너스 번호를 입력해 주세요. : ');
+    const winningBonusNumber = await prompt('보너스 번호를 입력해 주세요. : ', {
+      format: Number,
+      validate: (value) => validateWinningBonusNumber(value, winningNumbers),
+    });
 
     const lottoCalculator = new LottoCalculator({
-      winningNumbers: convertLottoStringToLottoArray(winningNumbers),
-      winningBonusNumber: Number(bonusNumber),
+      winningNumbers,
+      winningBonusNumber,
     });
 
     const { profit, chart } = lottoCalculator.getStatistics(lottoTickets);
@@ -38,9 +51,44 @@ async function ConsoleApp() {
     console.log(
       `총 수익률을 ${readROI(profit, Number(purchaseAmount))}입니다.`
     );
+
+    const cmd = await prompt('다시 시작하시겠습니까? (y/n) : ', {
+      validate: validateRetryCmd,
+    });
+    if (['y', 'Y'].includes(cmd)) {
+      await ConsoleApp();
+    }
   } catch (e) {
     console.log(`${e.message}`);
   }
+}
+
+function validatePurchaseAmount(cost) {
+  new ThrowMessage(cost)
+    .maxSafeInteger()
+    .isInteger()
+    .callback((value) => {
+      if (value < LOTTO.PRICE) {
+        throw new Error("'돈이 부족합니다.'");
+      }
+    });
+}
+
+function validateWinningNumbers(lottoNumbers) {
+  new LottoThrowMessage(lottoNumbers).isValidLottoNumberArray();
+}
+
+function validateWinningBonusNumber(winningBonusNumber, winningNumbers) {
+  new LottoThrowMessage(winningBonusNumber)
+    .isValidLottoNumber()
+    .checkDuplicateLottoNumbers(winningNumbers);
+}
+
+function validateRetryCmd(input) {
+  new ThrowMessage(input).regex(
+    /^[yYnN]$/,
+    `\n- [y|Y] : 로또 게임 재시작\n- [n|N] : 로또 게임 종료\n`
+  );
 }
 
 export default ConsoleApp;
