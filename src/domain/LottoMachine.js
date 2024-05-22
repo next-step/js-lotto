f64ed8dc41174dea8dc678e354d3b9d3f7208e33
jiyeon <ejy1105@gmail.com>
2024-05-23 01:36:59 +0900

    fix: 1-45개의 로또 번호를 미리 만들어 두는 구조로 가보는것

--- src/domain/LottoMachine.js
+++ src/domain/LottoMachine.js
@@ -1,15 +1,26 @@
 import { ErrorLotto } from "../constants/error";
-import { MAX_NUMBER, MIN_NUMBER } from "../constants/number";
-import { randomNumber, sortingNumber } from "../util/random";
+import {
+  LOTTO_NUMBER_LENGTH,
+  MAX_NUMBER,
+  MIN_NUMBER,
+} from "../constants/number";
+import { randomNumber, shuffle, sortingNumber } from "../util/random";
 import Lotto from "./Lotto";
+import LottoNumber from "./LottoNumber";
 
 export const LOTTO_PRICE = 1000;
 
 class LottoMachine {
   price = LOTTO_PRICE;
+  #lottoNumberPool;
 
   constructor(price) {
-    this.price = price;
+    this.price = price; //빈값인지 숫자인지 에러를 만들
+
+    this.#lottoNumberPool = Array.from(
+      { length: MAX_NUMBER },
+      (v, i) => new LottoNumber(i + 1)
+    );
   }
 
   #validatePrice(amount) {
@@ -19,22 +30,24 @@ class LottoMachine {
     return buyLottoCount;
   }
 
-  #generateRandomNumber() {
-    const set = new Set();
-    while (set.size < 6) {
-      const num = randomNumber(MIN_NUMBER, MAX_NUMBER);
-      set.add(num);
-    }
+  #generateRandomLottoNumbers() {
+    // const set = new Set();
+    // while (set.size < 6) {
+    //   const num = randomNumber(MIN_NUMBER, MAX_NUMBER);
+    //   set.add(num);
+    // }
 
-    const randomLotto = Array.from(set);
-    return sortingNumber(randomLotto);
+    // const randomLotto = Array.from(set);
+    // return sortingNumber(randomLotto);
+    return shuffle(this.#lottoNumberPool).slice(0, 6);
   }
 
   #generateLottoList(amount) {
-    const randomLottoList = Array.from({ length: amount }, () =>
-      this.#generateRandomNumber()
-    );
-    return randomLottoList.map((lotto) => new Lotto(lotto).number);
+    const randomLottoList = Array.from({ length: amount }, () => {
+      const randomLottoNumbers = this.#generateRandomLottoNumbers();
+      return new Lotto(randomLottoNumbers);
+    });
+    return randomLottoList;
   }
 
   buyLottoList(pay) {
