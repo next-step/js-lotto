3ef627e452fdfaca638e4b07adb084f4e0e19515
Paik <gwbaik9717@icloud.com>
2024-05-23 06:14:53 +0900

    feat: 로또 당첨 결과 및 게임 재시작 기능 구현

--- src/js/index.js
+++ src/js/index.js
@@ -7,14 +7,81 @@ import WinningLotto from "./domain/WinningLotto.js";
 const $ = (selector) => document.querySelector(selector);
 const $all = (selector) => document.querySelectorAll(selector);
 
-const $showResultButton = document.querySelector(".open-result-modal-button");
-const $modalClose = document.querySelector(".modal-close");
-const $modal = document.querySelector(".modal");
+const $showResultButton = $(".open-result-modal-button");
+const $modalClose = $(".modal-close");
+const $modal = $(".modal");
+
+const winningNumberInputs = $all(".winning-number");
+const $bonusNumberInput = $(".bonus-number");
+
+const $purchasedLottosSection = $(".purchased-lottos-section");
+const $purchasedLottosCount = $(".purchased-lottos-count");
+
+const onRestartGame = () => {
+  // hide modal
+  onCloseModal();
+
+  // reset game
+  resetGame();
+};
+
+const showPurchasedLottosSection = () => {
+  $purchasedLottosSection.classList.remove("d-none");
+};
+
+const hidePurchasedLottosSection = () => {
+  $purchasedLottosSection.classList.add("d-none");
+};
+
+const resetPurchasedLottosSection = () => {
+  // reset toggle button
+  $lottoNumbersToggleButton.checked = false;
+};
+
+const $winningLottoForm = $(".winning-lotto-form");
+
+const showWinningLottoForm = () => {
+  $winningLottoForm.classList.remove("d-none");
+};
+
+const hideWinningLottoForm = () => {
+  $winningLottoForm.classList.add("d-none");
+};
+
+const resetWinningLottoForm = () => {
+  // reset winning number inputs
+  winningNumberInputs.forEach((winningNumberInput) => {
+    winningNumberInput.value = "";
+  });
+
+  // reset bonus number input
+  $bonusNumberInput.value = "";
+};
+
+const resetPurchaeAmountInput = () => {
+  $purchaseAmountInput.value = "";
+};
+
+const resetGame = () => {
+  resetPurchaeAmountInput();
+
+  hidePurchasedLottosSection();
+  resetPurchasedLottosCount();
+
+  hideWinningLottoForm();
+  resetWinningLottoForm();
+};
+
+const $restartGameButton = $(".restart-game-button");
+$restartGameButton.addEventListener("click", onRestartGame);
+
+// get purchase amount from inupt
+const $purchaseAmountInput = $(".purchase-amount-input");
+let purchaseAmount = 0;
+
+let lottos = [];
 
 const onClickShowResult = (e) => {
-  const winningNumberInputs = $all(".winning-number");
-  const $bonusNumberInput = $(".bonus-number");
-
   const isValidWinningNumbers = Array.from(winningNumberInputs).every(
     (winningNumberInput) => {
       return winningNumberInput.validity.valid;
@@ -38,17 +105,42 @@ const onClickShowResult = (e) => {
     const lotto = new Lotto(winningNumbers);
     const winningLotto = new WinningLotto(lotto, bonusNumber);
     const lottoResult = new LottoResult(winningLotto);
-    onModalShow(lottoResult);
+    showLottoResult(lottos, lottoResult);
   } catch (error) {
     alert(error.message);
   }
 };
 
-const onModalShow = (lottoResult) => {
+const showLottoResult = (lottos, lottoResult) => {
+  // open modal
   $modal.classList.add("open");
+
+  const winningCountElements = $all(".winning-count");
+  const lottoRankings = [
+    LottoResult.LottoRanking["FIFTH"],
+    LottoResult.LottoRanking["FOURTH"],
+    LottoResult.LottoRanking["THIRD"],
+    LottoResult.LottoRanking["SECOND"],
+    LottoResult.LottoRanking["FIRST"],
+  ];
+
+  lottoRankings.forEach((lottoRanking, index) => {
+    const winningCountElement = winningCountElements[index];
+    const winningCount = lottoResult.getLottoRankingCount(lottos, lottoRanking);
+    winningCountElement.textContent = winningCount;
+  });
+
+  const totalWinningPrice = lottoResult.getTotalLottoWinningPrice(lottos);
+  const totalProfitRate = LottoResult.getTotalLottoProfitRate(
+    totalWinningPrice,
+    purchaseAmount
+  );
+
+  const $profitRate = $("#profit-rate");
+  $profitRate.textContent = totalProfitRate.toFixed(2);
 };
 
-const onModalClose = () => {
+const onCloseModal = () => {
   $modal.classList.remove("open");
 };
 
@@ -66,8 +158,6 @@ const generateLottoTemplate = (lottoNumbers, hideLottoNumbers) => {
   }">${lottoNumbersTextContent}</span>
 </li>`;
 };
-
-let lottos = [];
 
 $lottoNumbersToggleButton.addEventListener("click", () => {
   const checked = $lottoNumbersToggleButton.checked;
@@ -95,11 +185,9 @@ $lottoNumbersToggleButton.addEventListener("click", () => {
 });
 
 const onPurchaseLotto = () => {
-  // get purchase amount from inupt
-  const $purchaseAmountInput = $(".purchase-amount-input");
-  const purchaseAmount = $purchaseAmountInput.value;
-
   try {
+    purchaseAmount = Number($purchaseAmountInput.value);
+
     purchasableLottosCount = LottoGame.getPurchasableLottoCount(purchaseAmount);
 
     lottos = generateLottos(purchasableLottosCount);
@@ -116,27 +204,18 @@ const onPurchaseLotto = () => {
   }
 };
 
-const $purchasedLottosSection = $(".purchased-lottos-section");
-const $purchasedLottosCountLabel = $(".purchased-lottos-count-label");
-
 const showPurchasedLottos = (lottos) => {
-  // show purchased lotto section
-  $purchasedLottosSection.classList.remove("d-none");
+  showPurchasedLottosSection();
 
   // update purchased lotto count
-  const purchasedLottosCountLabelContent = `총 ${lottos.length}개를 구매하였습니다.`;
-  $purchasedLottosCountLabel.textContent = purchasedLottosCountLabelContent;
+  const purchasedLottosCountLabelContent = lottos.length;
+  $purchasedLottosCount.textContent = purchasedLottosCountLabelContent;
 
   const lottoListTemplate = lottos.map((lotto) =>
     generateLottoTemplate(lotto.numbers, true)
   );
 
   $lottoList.innerHTML = lottoListTemplate.join("");
-};
-
-const $winningLottoForm = $(".winning-lotto-form");
-const showWinningLottoForm = () => {
-  $winningLottoForm.classList.remove("d-none");
 };
 
 const generateLottos = (lottosCount) => {
@@ -153,4 +232,4 @@ const $purchaseButton = $(".purchase-button");
 $purchaseButton.addEventListener("click", onPurchaseLotto);
 
 $showResultButton.addEventListener("click", onClickShowResult);
-$modalClose.addEventListener("click", onModalClose);
+$modalClose.addEventListener("click", onCloseModal);
