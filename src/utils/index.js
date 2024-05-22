4960df5f53c93eabc2146607d7b39f8c1cecadee
chanwoo <chanw9503@gmail.com>
2024-05-22 10:22:57 +0900

    Fix. merge 중 발생한 중복 버그 수정

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,18 +18,3 @@ export function sortArray(order = 'ASC' | 'DESC', array) {
         }
     });
 }
-
-export function countArrayResults(array, condition) {
-    return array.filter(condition).length;
-}
-
-export function sortArray(order = 'ASC' | 'DESC', array) {
-    validateOrder(order);
-    return array.sort((a, b) => {
-        if (order === 'ASC') {
-            return a - b;
-        } else if (order === 'DESC') {
-            return b - a;
-        }
-    });
-}
