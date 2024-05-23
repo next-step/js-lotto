63430c5721f604e31c8650e8a0f9848925bc8eca
Paik <gwbaik9717@icloud.com>
2024-05-23 10:29:13 +0900

    refactor: LottoGame object literal에서 클래스로 변경

--- src/js/domain/LottoGame.js
+++ src/js/domain/LottoGame.js
@@ -3,14 +3,34 @@ import {
   ErrorLottoPurchasedAmount,
 } from "../constants/error.js";
 
-const LottoGame = {
-  LOTTO_PRICE: 1000,
-  MIN_PURCHASED_AMOUNT: 0,
+class LottoGame {
+  #lottos; // 발행된 로또들 저장
+  #purchasedAmount;
 
-  RESTART_GAME_TRUE: "y",
-  RESTART_GAME_FALSE: "n",
+  static LOTTO_PRICE = 1000;
+  static MIN_PURCHASED_AMOUNT = 0;
 
-  validateLottoPurchasedAmount(purchasedAmount) {
+  static RESTART_GAME_TRUE = "y";
+  static RESTART_GAME_FALSE = "n";
+
+  set purchasedAmount(purchasedAmount) {
+    LottoGame.validateLottoPurchasedAmount(purchasedAmount);
+    this.#purchasedAmount = purchasedAmount;
+  }
+
+  get purchasedAmount() {
+    return this.#purchasedAmount;
+  }
+
+  get lottos() {
+    return [...this.#lottos];
+  }
+
+  set lottos(lottos) {
+    this.#lottos = lottos;
+  }
+
+  static validateLottoPurchasedAmount(purchasedAmount) {
     if (isNaN(purchasedAmount)) {
       throw new Error(
         ErrorLottoPurchasedAmount.ERROR_LOTTO_PURCHASED_AMOUNT_NOT_NUMBER
@@ -22,18 +42,18 @@ const LottoGame = {
         ErrorLottoPurchasedAmount.ERROR_LOTTO_PURCHASED_AMOUNT_NOT_POSITIVE
       );
     }
-  },
+  }
 
-  getPurchasableLottoCount(purchasedAmount) {
+  static getPurchasableLottoCount(purchasedAmount) {
     this.validateLottoPurchasedAmount(purchasedAmount);
     return Math.floor(Number(purchasedAmount) / LottoGame.LOTTO_PRICE);
-  },
+  }
 
-  validateIsRestartLottoGame(input) {
+  static validateIsRestartLottoGame(input) {
     if (![this.RESTART_GAME_FALSE, this.RESTART_GAME_TRUE].includes(input)) {
       throw new Error(ErrorLottoGame.ERROR_LOTTO_GAME_RESTART_NOT_VALID);
     }
-  },
-};
+  }
+}
 
 export default LottoGame;
