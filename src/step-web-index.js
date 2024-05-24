cd0dd982e84931327994df2a4e590264cde31df0
Paik <gwbaik9717@icloud.com>
2024-05-24 09:58:05 +0900

    feat: 웹 기반 로또 실행 코드 작성

--- src/step-web-index.js
+++ src/step-web-index.js
@@ -1,6 +1,99 @@
-/**
- * step 3의 시작점이 되는 파일입니다.
- * 노드 환경에서 사용하는 readline 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
- */
+import Lotto from "./js/domain/Lotto.js";
+import LottoGame from "./js/domain/LottoGame.js";
+import LottoMachine from "./js/domain/LottoMachine.js";
+import LottoResult from "./js/domain/LottoResult.js";
+import WinningLotto from "./js/domain/WinningLotto.js";
+import LottoListSection from "./js/view/web/LottoListSection.js";
+import LottoResultModal from "./js/view/web/LottoResultModal.js";
+import PurchaseAmountInputForm from "./js/view/web/PurchaseAmountInputForm.js";
+import WinningLottoForm from "./js/view/web/WinningLottoForm.js";
+import { $ } from "./utils/dom.js";
 
-console.log("Web Browser!");
+const lottoGame = new LottoGame();
+
+// 로또 게임 초기화
+const initializeLottoGame = () => {
+  const purchasedAmount = purchaseAmountInputForm.inputValue;
+  const purchasableLottosCount =
+    LottoGame.getPurchasableLottoCount(purchasedAmount);
+
+  lottoGame.purchasedAmount = Number(purchasedAmount);
+  lottoGame.lottos = LottoMachine.generateRandomLottos(purchasableLottosCount);
+};
+
+// 구매 금액 입력
+const onSubmitPurchaseAmount = (e) => {
+  const isValidInput = purchaseAmountInputForm.isValidInput;
+  if (!isValidInput) {
+    return;
+  }
+
+  e.preventDefault();
+
+  try {
+    initializeLottoGame();
+    lottoListSection.show(lottoGame.lottos);
+    winningLottoForm.show();
+  } catch (error) {
+    alert(error.message);
+  }
+};
+const purchaseAmountInputForm = new PurchaseAmountInputForm(
+  onSubmitPurchaseAmount
+);
+
+// 구매한 로또 목록
+const onToggleShowLottoNumbers = () => {
+  lottoListSection.toggleLottoNumbers(lottoGame.lottos);
+};
+const lottoListSection = new LottoListSection(onToggleShowLottoNumbers);
+
+// 당첨 번호 입력
+const winningLottoForm = new WinningLottoForm();
+
+// 로또 결과 모달
+const lottoResultModal = new LottoResultModal();
+
+// 결과 확인하기
+const onClickShowResult = (e) => {
+  const isValidWinningNumbers = winningLottoForm.isValidWinningNumbers;
+  const isValidBonusNumber = winningLottoForm.isValidBonusNumber;
+
+  if (!isValidWinningNumbers || !isValidBonusNumber) {
+    return;
+  }
+
+  // prevent form submit
+  e.preventDefault();
+
+  const winningNumbers = winningLottoForm.winningNumbers;
+  const bonusNumber = winningLottoForm.bonusNumber;
+
+  try {
+    const lotto = new Lotto(winningNumbers);
+    const winningLotto = new WinningLotto(lotto, bonusNumber);
+    const lottoResult = new LottoResult(winningLotto);
+
+    lottoResultModal.open();
+    lottoResultModal.renderLottoResult(lottoGame, lottoResult);
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+const $showResultButton = $(".open-result-modal-button");
+$showResultButton.addEventListener("click", onClickShowResult);
+
+// 게임 다시 시작하기
+const onRestartGame = () => {
+  lottoResultModal.close();
+
+  purchaseAmountInputForm.reset();
+  lottoListSection.hide();
+  lottoListSection.reset(lottoGame.lottos);
+  winningLottoForm.hide();
+  winningLottoForm.reset();
+};
+
+const $restartGameButton = $(".restart-game-button");
+$restartGameButton.addEventListener("click", onRestartGame);
