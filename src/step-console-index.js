e6796fa8bcdef8acab5c6a54c2ceb805659f2907
Donghoon Kang <d159123@naver.com>
2024-05-26 01:41:06 +0900

    docs: 모듈 제거

--- src/step-console-index.js
+++ src/step-console-index.js
@@ -1,6 +1,4 @@
-import { Lotto, LottoMachine } from "./js/domain/index.js";
 import { LottoController } from "./js/view/Controller.js";
-import { Input, Output } from "./js/view/index.js";
 
 const lottoController = new LottoController();
 
