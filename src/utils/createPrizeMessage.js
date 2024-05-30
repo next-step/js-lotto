f9ca5fc90b13bbdfb6efc52337b8ea9946411f1d
DongHyeonWon <dhe7700@naver.com>
2024-05-31 01:17:00 +0900

    refac: 보너스 볼 일치할때 순위 상수화

--- src/utils/createPrizeMessage.js
+++ src/utils/createPrizeMessage.js
@@ -1,7 +1,8 @@
+import { LOTTO_GAME } from "../constants/LottoGame";
 import { PRIZE_STATISTICS } from "../constants/message";
 
 export const createPrizeMessage = (prize, count) => {
   const { equalCount, price } = PRIZE_STATISTICS[prize];
-  const bonusText = prize === 2 ? ", 보너스 일치" : "";
+  const bonusText = prize === LOTTO_GAME.BONUS_RANK ? ", 보너스 일치" : "";
   return `${equalCount}개 일치${bonusText} (${price}원) - ${count}개`;
 };
