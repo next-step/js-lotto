9ef25130766426a04d775f1d1deb095cf11f2c72
sanghyuk-2i <desummit1@gmail.com>
2025-02-15 01:53:22 +0900

    refactor: getRandomLottoNumber & getLotto 개선
    - getLotto를 위한 getRandomLottoNumber I/O 구조 개선
    - getLotto 요구사항 적용

--- src/domains/common/constants.js
+++ src/domains/common/constants.js
@@ -1,2 +1,8 @@
 export const LOTTO_MIN_NUMBER = 1;
 export const LOTTO_MAX_NUMBER = 45;
+
+export const LOTTO_LENGTH = 6;
+
+export const LOTTO_NUMBERS = Array.from({ length: 45 }).map(
+  (_, index) => index + 1,
+);
--- src/domains/common/utils.js
+++ src/domains/common/utils.js
@@ -1,25 +1,47 @@
 import { isNumber, isPositiveInteger } from '../../utils';
-import { LOTTO_MAX_NUMBER, LOTTO_MIN_NUMBER } from './constants';
+import {
+  LOTTO_LENGTH,
+  LOTTO_MAX_NUMBER,
+  LOTTO_MIN_NUMBER,
+  LOTTO_NUMBERS,
+} from './constants';
 
 const isLottoNumberRange = (value) =>
   value >= LOTTO_MIN_NUMBER && value <= LOTTO_MAX_NUMBER;
 
-export const getRandomLottoNumber = (manualNumber) => {
-  const targetNumber =
-    manualNumber ??
-    Math.floor(Math.random() * LOTTO_MAX_NUMBER) + LOTTO_MIN_NUMBER;
+export const getRandomLottoNumber = (availableNumbers) => {
+  if (!Array.isArray(availableNumbers) || !availableNumbers.length > 0) {
+    console.log(availableNumbers);
+    throw new Error('로또 번호를 생성하면서 문제가 생겼습니다.');
+  }
+
+  const selectedIndex = Math.floor(Math.random() * availableNumbers.length);
+  const selectedNumber = availableNumbers[selectedIndex];
 
   if (
-    !isNumber(targetNumber) ||
-    !isPositiveInteger(targetNumber) ||
-    !isLottoNumberRange(targetNumber)
+    !isNumber(selectedNumber) ||
+    !isPositiveInteger(selectedNumber) ||
+    !isLottoNumberRange(selectedNumber)
   ) {
     throw new Error('로또 번호를 생성하면서 문제가 생겼습니다.');
   }
 
-  return targetNumber;
+  return selectedNumber;
 };
 
 export const getLotto = () => {
-  return Array.from([1, 2, 3, 4, 5, 6], (value) => getRandomLottoNumber(value));
+  const lottoNumberStocks = LOTTO_NUMBERS;
+
+  const selectedNumbers = Array.from({ length: LOTTO_LENGTH }, () => {
+    const selectedNumber = getRandomLottoNumber(lottoNumberStocks);
+    const removeIndex = lottoNumberStocks.indexOf(selectedNumber);
+
+    lottoNumberStocks.splice(removeIndex, 1);
+
+    return selectedNumber;
+  });
+
+  selectedNumbers.sort((a, b) => a - b);
+
+  return selectedNumbers;
 };
