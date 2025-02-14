69fdf42de9a9c02955c0786a2a688f5016800d04
mandarin-sep <vheh0901@gmail.com>
2025-02-15 00:50:29 +0900

    feat: 테스트를 통과하는 도메인 로직 작성성

--- src/lotto.js
+++ src/lotto.js
@@ -4,5 +4,28 @@ export default class Lotto {
     this.numbers = [];
   }
 
-  make() {}
+  //도메인 로직
+  makeNumbers() {
+    const LOTTO_NUMBER_COUNT = 6;
+    let numbers = [];
+    for (let i = 0; i < LOTTO_NUMBER_COUNT; i++) {
+      numbers.push(Math.floor(Math.random() * 99) + 1);
+    }
+    return numbers;
+  }
+
+  makeLotto() {
+    this.numbers.push(this.makeNumbers());
+  }
+
+  makeLottoByPayment() {
+    for (let i = 0; i < this.count; i++) {
+      this.makeLotto();
+    }
+  }
+
+  //UI로직
+  showLottos(index) {
+    console.log(this.numbers[index]);
+  }
 }
