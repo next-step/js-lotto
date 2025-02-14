72555b4b2ffe3ca576eb78b303a444722146dea7
sanghyuk-2i <desummit1@gmail.com>
2025-02-15 02:22:54 +0900

    refactor: calculateLottoCount 개선
    - 일부 부분 상수화

--- src/domains/order/utils.js
+++ src/domains/order/utils.js
@@ -1,9 +1,10 @@
 import { isNumber, isPositiveInteger } from '../../utils';
+import { LOTTO_PRICE } from './constants';
 
 export const calculateLottoCount = (amount) => {
   if (!isNumber(amount) || !isPositiveInteger(amount)) {
     throw new Error('로또를 구매하는 과정에서 문제가 생겼습니다.');
   }
 
-  return Math.floor(amount / 1000);
+  return Math.floor(amount / LOTTO_PRICE);
 };
