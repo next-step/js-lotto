db1c909351ea8baddd15252d3cb877cf087b5e0c
mandarin-sep <vheh0901@gmail.com>
2025-02-15 01:45:33 +0900

    feat: 당첨 번호 입력 ui 작성성

--- src/getUserInput.js
+++ src/getUserInput.js
@@ -4,6 +4,20 @@ const checkInputTypeIsNumber = (input) => {
   return !Number.isNaN(Number(input));
 };
 
+const checkAllInputsTypeisNumber = (input) => {
+  const inputArr = input.split(",");
+
+  return inputArr.every((item) => checkInputTypeIsNumber(item));
+};
+const checkInputsLengthValid = (input) => {
+  const inputArr = input.split(",");
+
+  return inputArr.length === 6;
+};
+const checkBounsValid = (input) => {
+  return input > 0 && input < 100;
+};
+
 export function readLineAsync(query) {
   return new Promise((resolve, reject) => {
     const rl = readline.createInterface({
@@ -15,6 +29,49 @@ export function readLineAsync(query) {
       rl.close();
       if (!checkInputTypeIsNumber(input)) {
         reject(new Error("숫자만 입력해주세요."));
+        return;
+      }
+      resolve(input);
+    });
+  });
+}
+export function getWinningNumbers(query) {
+  return new Promise((resolve, reject) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question(query, (input) => {
+      rl.close();
+      if (!checkInputsLengthValid(input)) {
+        reject(new Error("숫자를 쉼표(,)로 구분해서 6개 입력해주세요"));
+        return;
+      }
+      if (!checkAllInputsTypeisNumber(input)) {
+        reject(new Error("숫자만 입력해주세요."));
+        return;
+      }
+      resolve(input);
+    });
+  });
+}
+
+export function getBounsNumber(query) {
+  return new Promise((resolve, reject) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.question(query, (input) => {
+      rl.close();
+      if (!checkAllInputsTypeisNumber(input)) {
+        reject(new Error("숫자만 입력해주세요."));
+        return;
+      }
+      if (!checkBounsValid(input)) {
+        reject(new Error("1 - 99까지의 숫자만 입력해주세요."));
         return;
       }
       resolve(input);
--- src/step1-index.js
+++ src/step1-index.js
@@ -1,5 +1,9 @@
 import Lotto from "./lotto.js";
-import { readLineAsync } from "./getUserInput.js";
+import {
+  readLineAsync,
+  getWinningNumbers,
+  getBounsNumber,
+} from "./getUserInput.js";
 
 /**
  * step 1의 시작점이 되는 파일입니다.
@@ -7,12 +11,20 @@ import { readLineAsync } from "./getUserInput.js";
  */
 const startLottoGame = async () => {
   let lotto;
+  let winngNumbers = [];
+  let bounsNumber;
   await readLineAsync("구입금액을 입력해 주세요 : ").then((amount) => {
     lotto = new Lotto(amount);
     lotto.makeLottoByPayment();
     for (let i = 0; i < lotto.count; i++) {
       lotto.showLottos(i);
     }
+  });
+  await getWinningNumbers("당첨 번호를 입력해 주세요. : ").then((numbers) => {
+    winngNumbers = numbers.split(",");
+  });
+  await getBounsNumber("보너스 번호를 입력해 주세요. : ").then((number) => {
+    bounsNumber = number;
   });
 };
 
