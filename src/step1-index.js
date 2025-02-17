a5eac57d0f3d154233a8828ecc560b12406f94b2
Terry <kdjtiger@gmail.com>
2025-02-17 21:38:21 +0900

    Feat: 로또 추첨 결과 출력
    - 당첨 통계를 출력한다.
    
    - 당첨 내역과 수익률을 출력한다.

--- src/step1-index.js
+++ src/step1-index.js
@@ -3,10 +3,38 @@
  * 브라우저 환경에서 사용하는 css 파일 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
  */
 
+import LottoDraw from "./domain/LottoDraw.js";
 import LottoPurchase from "./domain/LottoPurchase.js";
 import Input from "./view/Input.js";
+import Output from "./view/output.js";
+
+// 당첨 번호를 받는다.
+// 보너스 번호를 받는다.
+
+// 당첨 통계 보여주기
 
 const input = new Input();
-const purchasePrice = await input.getPurchasePrice();
+const output = new Output();
 
-const lottoPurchase = new LottoPurchase(purchasePrice);
+try {
+  const purchasePrice = await input.getPurchasePrice();
+  const lottoPurchase = new LottoPurchase(purchasePrice);
+
+  const lottoWinningNumbersInput = await input.getLottoWinningNumbers();
+  const lottoWinningNumbers = lottoWinningNumbersInput
+    .split(",")
+    .map((num) => Number(num));
+
+  const lottoBonusNumber = await input.getLottoBonusNumber();
+  const lottoNumbers = {
+    lottoWinningNumbers,
+    lottoBonusNumber: Number(lottoBonusNumber),
+  };
+
+  const lottoDraw = new LottoDraw(lottoNumbers, lottoPurchase.lottoTickets);
+  const lottoDrawResult = lottoDraw.start();
+  output.printDrawResult(lottoDrawResult);
+  
+} catch (error) {
+  output.printErrorMessage(error);
+}
--- src/view/Output.js
+++ src/view/Output.js
@@ -1,5 +1,27 @@
+import { RESULT_TEXT } from "../utils/constants.js";
+
 class Output {
   constructor() {}
+
+  printDrawResult(result) {
+    console.log("당첨 통계");
+    console.log("--------------------");
+
+    this.printResultTextMessage(result);
+
+    this.printRateOfReturn(result.rateOfReturn);
+  }
+
+  printResultTextMessage(result) {
+    Object.entries(result.counts).forEach(([matchedCount, count]) => {
+      const resultText = RESULT_TEXT[matchedCount];
+      console.log(`${resultText}- ${count}개`);
+    });
+  }
+
+  printRateOfReturn(rate) {
+    console.log(`총 수익률은 ${rate}%입니다.`);
+  }
 
   printErrorMessage(error) {
     console.error(error);
