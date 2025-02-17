b79d38918fb4d1c7ae9d975ef0520a485f36e58c
etff <wish2rich@gmail.com>
2025-02-17 21:37:22 +0900

    feat: 수익률 기능 추가

--- src/domain/Budget.js
+++ src/domain/Budget.js
@@ -1,10 +1,15 @@
 class Budget {
   constructor(amount) {
     this.amount = amount;
+    this.totalWinningAmount = 0;
   }
 
   getLottoCount(lottoPrice) {
     return Math.floor(this.amount / lottoPrice);
+  }
+
+  getProfit() {
+    return ((this.totalWinningAmount / this.amount) * 100).toFixed(1);
   }
 }
 
