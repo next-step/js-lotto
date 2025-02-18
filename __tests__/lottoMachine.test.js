4a8b21036c8943968b0ecc33cf2f5ffad5630d28
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-18 12:12:59 +0900

    test: 경계값으로 테스트 하도록 수정

--- __tests__/lottoMachine.test.js
+++ __tests__/lottoMachine.test.js
@@ -2,10 +2,10 @@ import LottoMachine from "../src/domain/LottoMachine.js";
 
 describe("LottoMachine Class 테스트", () => {
     it("로또 머신에 금액을 넣을 시 1000원 이하면 안된다.", () => {
-        expect(() => new LottoMachine(800)).toThrowError(new Error(LottoMachine.MINIMUM_PRICE_MESSAGE));
+        expect(() => new LottoMachine(999)).toThrowError(new Error(LottoMachine.MINIMUM_PRICE_MESSAGE));
     });
     
-    it("1000원당 로또는 1장이다", () => {
+    it("2000원 입력 시 로또는 2장 생성된다.", () => {
         const lottoMachine = new LottoMachine(2000);
         expect(lottoMachine.lottos.length).toEqual(2);
     });
