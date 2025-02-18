659867a3098dc35b7ae746f1e3e4ce93b64cd092
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-18 14:57:00 +0900

    feat: 수동로또 입력받을 수 있도록

--- src/step-console-index.js
+++ src/step-console-index.js
@@ -1,30 +1,27 @@
-import * as io from "./view/inputoutput.js"
-import LottoMachine from "./domain/lottoMachine.js";
+import InputOutput from "./view/inputoutput.js";
+import LottoMachine from "./domain/LottoMachine.js";
 import LottoConfirmation from "./domain/lottoConfirmation.js";
 import PrizeLotto from "./domain/PrizeLotto.js";
 
 const main = async () => {
-    const readline = io.createInterface();
-    const price = await io.receivedPrice(readline);
+    const io = new InputOutput();
+    const price = await io.receivedPrice();
 
     const lottoMachine = new LottoMachine(price);
-
     io.printLottos(lottoMachine.getLottos);
 
-    const prizeNum = await io.receivedPrizeLottoNum(readline);
-    const bonusNum = await io.receivedBonusLottoNum(readline);
+    const prizeNum = await io.receivedPrizeLottoNum(); 
+    const bonusNum = await io.receivedBonusLottoNum();
 
     const prizeLotto = new PrizeLotto(prizeNum, bonusNum);
-
     const lottoConfirmation = new LottoConfirmation(lottoMachine.getLottos, prizeLotto);
 
     io.lottoResult(lottoConfirmation.getLottoResults);
-
-    console.log(price)
+    console.log(price);
     
     io.printRateOfReturn(lottoConfirmation.calculateRateOfReturn(price));
 
-    readline.close();
+    io.closeInterface(); 
 }
 
 main();
--- src/step-web-index.js
+++ src/step-web-index.js
@@ -1,33 +1,35 @@
-import * as io from "./view/inputoutput.js"
-import LottoMachine from "./domain/lottoMachine.js";
+import InputOutput from "./view/inputoutput.js";
+import LottoMachine from "./domain/LottoMachine.js";
 import LottoConfirmation from "./domain/lottoConfirmation.js";
 import PrizeLotto from "./domain/PrizeLotto.js";
+import Lotto from "./domain/Lotto.js";
 
 const main = async () => {
-    const readline = io.createInterface();
-
+    const io = new InputOutput();
     let continueRunning = true;
 
-    while (continueRunning) { 
-        const price = await io.receivedPrice(readline);
+    while (continueRunning) {
+        const price = await io.receivedPrice();
 
         const lottoMachine = new LottoMachine(price);
+
+        await io.buyAutoOrManual(lottoMachine);
+
         io.printLottos(lottoMachine.getLottos);
 
-        const prizeNum = await io.receivedPrizeLottoNum(readline);
-        const bonusNum = await io.receivedBonusLottoNum(readline);
+        const prizeNum = await io.receivedPrizeLottoNum();
+        const bonusNum = await io.receivedBonusLottoNum();
 
         const prizeLotto = new PrizeLotto(prizeNum, bonusNum);
         const lottoConfirmation = new LottoConfirmation(lottoMachine.getLottos, prizeLotto);
 
         io.lottoResult(lottoConfirmation.getLottoResults);
-        console.log(price);
         io.printRateOfReturn(lottoConfirmation.calculateRateOfReturn(price));
 
-        continueRunning = await io.restart(readline);
+        continueRunning = await io.restart();
     }
 
-    readline.close();
+    io.closeInterface();
 }
 
 main();
