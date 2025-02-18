fa07e83df30f15ebd1697b123a186f532cf28b26
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-18 11:46:07 +0900

    fix: 로또 생성 책임 옮기기

--- src/domain/lotto.js
+++ src/domain/lotto.js
@@ -6,27 +6,14 @@ class Lotto {
     #lottoNumbers;
 
     constructor(lottoNumbers) {
-        this.#lottoNumbers = this.outputAutoLotto();
+        this.#lottoNumbers = lottoNumbers;
     }
 
     get getLottoNumbers() {
         return this.#lottoNumbers;
     }
 
-    outputAutoLotto() {
-        const numberRange = [...Array(Lotto.LOTTO_NUMBER_RANGE).keys()].map(i => i + 1);
-        
-        this.shuffleArray(numberRange);
-        
-        return numberRange.slice(0, Lotto.LOTTO_SIZE).sort((a, b) => a - b);
-    }
-
-    shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]]; // Swap elements
-        }
-    }
+    
 }
 
 export default Lotto;
--- src/domain/lottoMachine.js
+++ src/domain/lottoMachine.js
@@ -10,7 +10,7 @@ class LottoMachine {
     }
 
     #createLotto(lottoNum) {
-        return Array.from({ length: lottoNum }, () => new Lotto());
+        return Array.from({ length: lottoNum }, () => new Lotto(this.#outputAutoLotto()));
     }
 
     #confirmMinimumPrice(price) {
@@ -21,6 +21,21 @@ class LottoMachine {
 
     #calculateLottoNum(price) {
         return price / LottoMachine.LOTTO_PRICE;
+    }
+
+    #outputAutoLotto() {
+        const numberRange = [...Array(Lotto.LOTTO_NUMBER_RANGE).keys()].map(i => i + 1);
+        
+        this.#shuffleArray(numberRange);
+        
+        return numberRange.slice(0, Lotto.LOTTO_SIZE).sort((a, b) => a - b);
+    }
+
+    #shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+        }
     }
 }
 
