e5df1f4fc01ac5485b931539e005cc3bdd1e24e8
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-18 11:46:23 +0900

    test: Lotto getLottoNumbers 테스트

--- __tests__/lotto.test.js
+++ __tests__/lotto.test.js
@@ -1,8 +1,8 @@
 import Lotto from "../src/domain/lotto.js";
 
 describe("Lotto Class 테스트", () => {
-    it("랜덤한 로또번호를 생성한다.", () => {
-        const lotto = new Lotto();
-        expect(lotto.getLottoNumbers.length).toEqual(6);
+    it("생성한 로또번호를 가져온다.", () => {
+        const lotto = new Lotto([1, 2, 3, 4, 5, 6]);
+        expect(lotto.getLottoNumbers).toEqual([1, 2, 3, 4, 5, 6]);
     });
 });
