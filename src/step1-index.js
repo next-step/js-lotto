b72b15fcfff45aba0aac36ba73b5d78fe1d79a92
minji Kong <minzyaaaaaa@gmail.com>
2025-02-19 19:06:54 +0900

    Feat: 당첨번호와 보너스 번호를 입력받는다.

--- src/step1-index.js
+++ src/step1-index.js
@@ -1,15 +1,22 @@
 import Lotto from "./domain/Lotto.js";
 
-import { inputPurchaseAmount } from "./view/input.js";
+import {
+  inputPurchaseAmount,
+  inputWinningNumber,
+  inputBonusNumber,
+} from "./view/input.js";
 import { printPurchaseResult } from "./view/output.js";
 
 async function run() {
   const purchaseAmount = await inputPurchaseAmount();
 
   const lotto = new Lotto();
-  const purchasedLottos = lotto.purchase(Number(purchaseAmount));
+  const purchasedLottos = lotto.purchase(purchaseAmount);
 
   printPurchaseResult(purchasedLottos.length, purchasedLottos);
+
+  const winningNumber = await inputWinningNumber();
+  const bonusNumber = await inputBonusNumber();
 }
 
 run();
--- src/view/input.js
+++ src/view/input.js
@@ -1,5 +1,16 @@
 import { readLineAsync } from "../utils/readLine.js";
 
 export async function inputPurchaseAmount() {
-  return await readLineAsync("> 구입금액을 입력해 주세요.");
+  const amount = await readLineAsync("> 구입금액을 입력해 주세요. ");
+  return Number(amount);
+}
+
+export async function inputWinningNumber() {
+  const numbers = await readLineAsync("\n> 당첨 번호를 입력해 주세요. ");
+  return numbers.split(",");
+}
+
+export async function inputBonusNumber() {
+  const number = await readLineAsync("\n> 보너스 번호를 입력해 주세요. ");
+  return Number(number);
 }
