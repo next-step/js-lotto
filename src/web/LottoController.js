4079c04618427f4a9d1041c933b87000966f0f77
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-21 00:32:11 +0900

    feat: 버튼 클릭시 동작 관리 Controller

---
+++ src/web/LottoController.js
@@ -0,0 +1,63 @@
+import LottoMachine from "../domain/LottoMachine.js";
+import LottoConfirmation from "../domain/lottoConfirmation.js";
+import PrizeLotto from "../domain/PrizeLotto.js";
+import RenderPage from "./RenderPage.js";
+import  * as elements from "./Elements";
+
+class LottoController {
+
+    #lottoMachine;
+    #renderPage;
+
+    init() {
+        this.initInputMoney();
+        this.initContrimPrize();
+        this.initRestart();
+        this.initClose();
+        this.#renderPage = new RenderPage();
+    }
+
+    initInputMoney() {
+        elements.getBuyButtonElements().addEventListener('click', () => {
+            this.buyLottoByLottoMachine();
+            this.#renderPage.renderLottoList(this.#lottoMachine);
+        });
+    }
+
+    initContrimPrize() {
+        elements.getConfirmButtonElements().addEventListener('click', () => {
+            this.#renderPage.openModal(this.confirmLottoPrize());
+        });
+    }
+
+    initRestart() {
+        elements.getRestartButtonElements().addEventListener('click', () => {
+            this.#renderPage.closeModal();
+            this.#renderPage.clearInput();
+        });
+    }
+
+    initClose() {
+        elements.getRestartButtonElements().addEventListener('click', () => {
+            this.#renderPage.closeModal();
+        });
+    }
+
+    confirmLottoPrize() {
+        const prizeNum = Array.from(elements.getWinningNumber()).map(num => Number(num.value));
+        const bonusNum = elements.getBonusNumElements().value;
+
+        const prizeLotto = new PrizeLotto(prizeNum, bonusNum);
+        return new LottoConfirmation(this.#lottoMachine.getLottos, prizeLotto);
+    }
+
+    buyLottoByLottoMachine() {
+        const money = elements.getMoneyElements().value;
+        this.#renderPage.clearInput();
+
+        this.#lottoMachine = new LottoMachine(money);
+        this.#lottoMachine.buyAuto();
+    };
+};
+
+export default LottoController;
