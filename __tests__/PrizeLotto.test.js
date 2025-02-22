93016e793a6845633dfce09b81a98194f1eec9df
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-22 19:35:01 +0900

    test: PrizeLotto 정상생성 테스트코드

--- __tests__/PrizeLotto.test.js
+++ __tests__/PrizeLotto.test.js
@@ -3,6 +3,12 @@ import LottoNumber from "../src/domain/LottoNumber.js";
 
 describe("LottoConfirmation Class 테스트", () => {
 
+    it("PrizeLotto를 생성한다.", () => {
+        const prizeLotto = new PrizeLotto([1, 2, 3, 7, 8, 9], 45);
+        expect(prizeLotto.value).toEqual([1, 2, 3, 7, 8, 9]);
+        expect(prizeLotto.bonusNum).toEqual(45);
+    });
+
     it("보너스 숫자가 1 ~ 45사이의 숫자가 아니면 예외가 발생한다.", () => {
         const prizeLotto = [1, 2, 3, 7, 8, 9];
         expect(() => new PrizeLotto(prizeLotto, 46)).toThrowError(new Error(LottoNumber.LOTTO_NUM_RANGE_MESSAGE));
