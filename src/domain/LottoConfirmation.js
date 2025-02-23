52934f71b1d0420bbc2e8d898345a1f0037fe423
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-23 22:18:18 +0900

    refactor: 숫자 표현방법 변경

--- src/domain/LottoConfirmation.js
+++ src/domain/LottoConfirmation.js
@@ -3,11 +3,11 @@ import LottoMachine from "./LottoMachine.js";
 
 class LottoConfirmation {
 
-    static THREE_MATCH_PRICE = 5000;
-    static FOUR_MATCH_PRICE = 50000;
-    static FIVE_MATCH_PRICE = 1500000;
-    static FIVE_BONUS_MATCH_PRICE = 30000000;
-    static SIX_MATCH_PRICE = 2000000000;
+    static THREE_MATCH_PRICE = 5_000;
+    static FOUR_MATCH_PRICE = 50_000;
+    static FIVE_MATCH_PRICE = 1_500_000;
+    static FIVE_BONUS_MATCH_PRICE = 30_000_000;
+    static SIX_MATCH_PRICE = 2_000_000_000;
 
     #lottos;
     #totalPrize;
