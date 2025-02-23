2a4b161611ffe4e2c7e137e15efcd1a6638612c0
etff <wish2rich@gmail.com>
2025-02-23 20:02:14 +0900

    refactor: budget 예외 케이스 추가

--- __tests__/domain/Budget.test.js
+++ __tests__/domain/Budget.test.js
@@ -1,6 +1,20 @@
 import Budget from "../../src/domain/Budget.js";
 
 describe("Budget 클래스는", () => {
+  describe("=== 생성에 대한 테스트 ===", () => {
+    it("예산으로 입력된 값이 0보다 작을때 예외를 던져야한다.", () => {
+      const givenBudget = -1000;
+      expect(() => new Budget(givenBudget)).toThrow();
+    });
+    it.each([undefined, "1000", null])(
+      "예산으로 입력된 값이 숫자가 아닐때 예외를 던져야한다.",
+      () => {
+        const givenBudget = "1000";
+        expect(() => new Budget(givenBudget)).toThrow();
+      },
+    );
+  });
+
   describe("=== 로또 갯수에 대한 테스트 ===", () => {
     it("예산과 로또 가격을 입력받으면 로또 갯수를 가지고 있어야 한다", () => {
       const givenBudget = 10000;
--- src/domain/Budget.js
+++ src/domain/Budget.js
@@ -1,6 +1,9 @@
 class Budget {
   static INVALID_AMOUNT = "올바른 금액을 입력해주세요.";
   constructor(amount) {
+    if (!amount || !Number.isNaN(amount) || amount < 0) {
+      throw new Error(Budget.INVALID_AMOUNT);
+    }
     this.amount = amount;
     this.totalWinningAmount = 0;
   }
@@ -18,14 +21,11 @@ class Budget {
   }
 
   static createBudget(budgetPrice) {
-    if (!budgetPrice) {
-      throw new Error(Budget.INVALID_AMOUNT);
-    }
     const amount = Number(budgetPrice);
-    if (Number.isNaN(amount)) {
+    if (!budgetPrice || Number.isNaN(amount)) {
       throw new Error(Budget.INVALID_AMOUNT);
     }
-    return new Budget(amount);
+    return new Budget(Number(budgetPrice));
   }
 }
 
