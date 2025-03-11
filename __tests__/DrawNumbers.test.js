9c8f18db52c8dad7d9bbedf92219439d6d0b0281
minji Kong <minzyaaaaaa@gmail.com>
2025-03-11 12:07:34 +0900

    Refactor: 테스트 결과 명시

--- __tests__/DrawNumbers.test.js
+++ __tests__/DrawNumbers.test.js
@@ -3,7 +3,7 @@ import BonusNumbers from "../src/domain/BonusNumbers.js";
 import DrawNumbers from "../src/domain/DrawNumbers.js";
 
 describe("추첨 번호 테스트", () => {
-  it("추첨 번호는 중복될 수 없다.", () => {
+  it("추첨 번호가 중복될 시 예외가 발생한다.", () => {
     expect(() => {
       new DrawNumbers({
         winningNumbers: new WinningNumbers({ numbers: [1, 2, 3, 4, 5, 6] }),
