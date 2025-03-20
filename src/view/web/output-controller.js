cf85d236fce36aca0bf8ca5cb55d749241fc7d87
minji Kong <minzyaaaaaa@gmail.com>
2025-03-20 13:28:26 +0900

    Fix: 로또 재구입 시 지난 결과 제거하도록 수정

--- src/view/web/output-controller.js
+++ src/view/web/output-controller.js
@@ -7,6 +7,7 @@ import {
 } from "./output";
 
 export function updatePurchasedResultView(quantity, purchasedLottos) {
+  removePurchaseResult();
   showPurchaseResultStep();
   printPurchaseResult(quantity, purchasedLottos);
 }
