8647a98cded02e8032efbff15139a5634e34d57b
Seung-wan <tmddhks0104@gmail.com>
2025-09-03 00:10:43 +0900

    test: green

--- src/domain/LottoGame.js
+++ src/domain/LottoGame.js
@@ -1,1 +1,16 @@
-export class LottoGame {}
+import { Lotto } from "./Lotto.js";
+
+export class LottoGame {
+  buy(price) {
+    const lottoCountToBuy = price / 1_000;
+    const lotto = new Lotto();
+
+    const lottos = [];
+
+    for (let i = 0; i < lottoCountToBuy; i += 1) {
+      lottos.push(lotto.issue());
+    }
+
+    return lottos;
+  }
+}
