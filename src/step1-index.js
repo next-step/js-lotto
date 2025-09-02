db8f4dc5ad78217aa81952ca43cad175bf7918a3
Seung-wan <tmddhks0104@gmail.com>
2025-09-03 00:15:47 +0900

    feat: 당첨 번호와 보너스 번호를 입력받는다.

--- src/step1-index.js
+++ src/step1-index.js
@@ -2,6 +2,9 @@ import { View } from "./view/View.js";
 
 async function main() {
   const purchasePrice = await View.read("구입금액을 입력해 주세요.");
+
+  const winningNumbers = await View.read("당첨 번호를 입력해 주세요.");
+  const bonusNumber = await View.read("보너스 번호를 입력해 주세요.");
 }
 
 main();
