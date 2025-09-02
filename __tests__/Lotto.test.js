e1e3bf55e9d1916b7b7aa55fee310c3fc94ba639
Seung-wan <tmddhks0104@gmail.com>
2025-09-03 01:29:16 +0900

    refactor:

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,7 +1,8 @@
 import { Lotto } from "../src/domain/Lotto.js";
 
 describe(Lotto.name, () => {
-  it(`로또는 발급하면 서로 겹치지 않는 ${Lotto.LOTTO_NUMBER_MIN}~${Lotto.LOTTO_NUMBER_MAX} 사이의 ${Lotto.LOTTO_NUMBER_COUNT_MAX}개의 숫자를 반환한다.`, () => {
+  it(`로또를 발급하면 서로 겹치지 않는 ${Lotto.LOTTO_NUMBER_MIN}~${Lotto.LOTTO_NUMBER_MAX} 사이의
+     ${Lotto.LOTTO_NUMBER_COUNT_MAX}개의 숫자를 반환한다.`, () => {
     const lottoNumbers = Lotto.issue();
     const lottoNumbersSet = new Set(lottoNumbers);
 
