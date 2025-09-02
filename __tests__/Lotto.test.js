8d4adaada93cac32cc102ea823de6f5adbf59535
Seung-wan <tmddhks0104@gmail.com>
2025-09-03 01:32:23 +0900

    refactor: 테스트코드 실행 횟수 조정

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -3,12 +3,14 @@ import { Lotto } from "../src/domain/Lotto.js";
 describe(Lotto.name, () => {
   it(`로또를 발급하면 서로 겹치지 않는 ${Lotto.LOTTO_NUMBER_MIN}~${Lotto.LOTTO_NUMBER_MAX} 사이의
      ${Lotto.LOTTO_NUMBER_COUNT_MAX}개의 숫자를 반환한다.`, () => {
-    const lottoNumbers = Lotto.issue();
-    const lottoNumbersSet = new Set(lottoNumbers);
+    for (let i = 0; i < 1_000; i += 1) {
+      const lottoNumbers = Lotto.issue();
+      const lottoNumbersSet = new Set(lottoNumbers);
 
-    const isValidLottoNumber = Lotto.isValidLottoNumber(lottoNumbers);
+      const isValidLottoNumber = Lotto.isValidLottoNumber(lottoNumbers);
 
-    expect(isValidLottoNumber).toBe(true);
-    expect(lottoNumbersSet.size).toBe(Lotto.LOTTO_NUMBER_COUNT_MAX);
+      expect(isValidLottoNumber).toBe(true);
+      expect(lottoNumbersSet.size).toBe(Lotto.LOTTO_NUMBER_COUNT_MAX);
+    }
   });
 });
