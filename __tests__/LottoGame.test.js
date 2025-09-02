56e860d2d518067e72508a97b7b046c754f05a6c
Seung-wan <tmddhks0104@gmail.com>
2025-09-03 00:08:46 +0900

    test: red

--- __tests__/LottoGame.test.js
+++ __tests__/LottoGame.test.js
@@ -1,5 +1,12 @@
 import { LottoGame } from "../src/domain/LottoGame.js";
 
 describe(LottoGame.name, () => {
-  it("", () => {});
+  it("구입 금액에 해당하는 만큼 로또를 발행한다.", () => {
+    const PURCHASE_PRICE = 8_000;
+    const lottoGame = new LottoGame();
+
+    const lottos = lottoGame.buy(PURCHASE_PRICE);
+
+    expect(lottos.length).toBe(PURCHASE_PRICE / 1000);
+  });
 });
