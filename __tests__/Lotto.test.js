5ac4a472711d728438fbe5ab1092c0e9e5213765
Seung-wan <tmddhks0104@gmail.com>
2025-09-03 00:02:40 +0900

    test: refactor

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,13 +1,16 @@
 import { Lotto } from "../src/domain/Lotto.js";
 
 describe(Lotto.name, () => {
-  it("로또는 발급하면 서로 겹치지 않는 1~45 사이의 6개의 숫자를 반환한다.", () => {
+  it(`로또는 발급하면 서로 겹치지 않는 ${Lotto.LOTTO_NUMBER_MIN}~${Lotto.LOTTO_NUMBER_MAX} 사이의 ${Lotto.LOTTO_NUMBER_COUNT_MAX}개의 숫자를 반환한다.`, () => {
     const lotto = new Lotto();
-    const lottoNumbers = lotto.issue();
 
+    const lottoNumbers = lotto.issue();
     const lottoNumbersSet = new Set(lottoNumbers);
+
     const isValidLottoNumber = lottoNumbers.every(
-      (lottoNumber) => lottoNumber >= 1 && lottoNumber <= 45
+      (lottoNumber) =>
+        lottoNumber >= Lotto.LOTTO_NUMBER_MIN &&
+        lottoNumber <= Lotto.LOTTO_NUMBER_MAX
     );
 
     expect(isValidLottoNumber).toBe(true);
