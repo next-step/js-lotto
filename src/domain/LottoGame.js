e7e4809ef4df84574fc7c4c78ac7d9c37de903ae
Seung-wan <tmddhks0104@gmail.com>
2025-09-03 00:46:56 +0900

    test: refactor

--- src/domain/LottoGame.js
+++ src/domain/LottoGame.js
@@ -17,12 +17,52 @@ export class LottoGame {
   }
 
   checkResult({ lottoNumbers, winningNumbers, bonusNumber }) {
-    return {
+    const result = {
       first: 0,
       second: 0,
       third: 0,
       fourth: 0,
-      fifth: 2,
+      fifth: 0,
     };
+
+    for (let i = 0; i < lottoNumbers.length; i += 1) {
+      const lottoNumber = lottoNumbers[i];
+      let matchCount = 0;
+
+      for (let j = 0; j < lottoNumber.length; j += 1) {
+        const number = lottoNumber[j];
+
+        if (winningNumbers.includes(number)) {
+          matchCount += 1;
+        }
+      }
+
+      if (matchCount < 3) {
+        continue;
+      }
+
+      if (matchCount === 3) {
+        result.fifth += 1;
+      }
+
+      if (matchCount === 4) {
+        result.fourth += 1;
+      }
+
+      if (matchCount === 5) {
+        if (lottoNumber.includes(bonusNumber)) {
+          result.second += 1;
+          return;
+        }
+
+        result.third += 1;
+      }
+
+      if (matchCount === 6) {
+        result.first += 1;
+      }
+    }
+
+    return result;
   }
 }
