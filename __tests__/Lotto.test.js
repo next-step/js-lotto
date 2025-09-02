322c08cf5818467f96efa604789e02f3d71eb7b4
Seung-wan <tmddhks0104@gmail.com>
2025-09-02 23:36:30 +0900

    test: red-green

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -1,5 +1,12 @@
 import { Lotto } from "../src/domain/Lotto.js";
 
 describe(Lotto.name, () => {
-  it("로또는 발급하면 서로 겹치지 않는 1~45 사이의 6개의 숫자를 반환한다.", () => {});
+  it("로또는 발급하면 서로 겹치지 않는 1~45 사이의 6개의 숫자를 반환한다.", () => {
+    const lotto = new Lotto();
+    const lottoNumbers = lotto.issue();
+
+    const lottoNumbersSet = new Set(lottoNumbers);
+
+    expect(lottoNumbersSet.size).toBe(6);
+  });
 });
--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -1,1 +1,5 @@
-export class Lotto {}
+export class Lotto {
+  issue() {
+    return [1, 2, 3, 4, 5, 6];
+  }
+}
