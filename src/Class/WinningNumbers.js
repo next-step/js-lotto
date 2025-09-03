c01473ffb5f44a6e8657eea4cbd5b56f1ea55ab1
unknown <syjin@in4ucloud.com>
2025-09-03 15:13:28 +0900

    feat: 당첨번호 및 보너스 번호가 1~45번 사이의 번호인지 확인하는 기능

--- src/Class/WinningNumbers.js
+++ src/Class/WinningNumbers.js
@@ -10,17 +10,17 @@ class WinningNumbers {
 
   construnctor() {
     this.#winningNumber = [];
-    this.#bonusWinningNumber = "";
+    this.#bonusWinningNumber = 0;
   }
 
   // 당첨번호 및 보너스 번화가 1~45번 사이의 번호인지 체크
-  isValidRangeNumber(lotto) {
+  isValidRangeNumber(winningNumber) {
     let isValid = false;
 
-    if (typeof lotto === "string") {
-      isValid = LOTTO_MIN_NUMBER && num <= LOTTO_MAX_NUMBER;
+    if (typeof winningNumber === "number") {
+      isValid = LOTTO_MIN_NUMBER && winningNumber <= LOTTO_MAX_NUMBER;
     } else {
-      const filterLottoNumbers = lotto.filter(
+      const filterLottoNumbers = winningNumber.filter(
         (num) => num >= LOTTO_MIN_NUMBER && num <= LOTTO_MAX_NUMBER
       );
 
