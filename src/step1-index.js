fed03344dbe532e4eecdb46c113ce953b03924a1
unknown <syjin@in4ucloud.com>
2025-09-03 10:27:06 +0900

    feat: lotto class 정의

---
+++ src/step1-index.js
@@ -0,0 +1,23 @@
+const LOTTO_PRICE = 1000;
+const LOTTO_COUNT = 6;
+const LOTTO_NUMBERS = Array.from({ length: 45 }, (_, i) => i + 1);
+
+class LottoPurchase {
+  #purchaseAmount;
+  #lottos;
+
+  constructor(money) {
+    this.#purchaseAmount = money;
+    this.#lottos = [];
+  }
+}
+
+class WinningNumbers {
+  #winningNumber;
+  #bonusWinningNumber;
+
+  construnctor() {
+    this.#winningNumber = "";
+    this.#bonusWinningNumber = "";
+  }
+}
