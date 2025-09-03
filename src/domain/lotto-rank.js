2dbc909032105f96c16eb9f77f577d9fc7637cea
강현석 <kekemusa37@gmail.com>
2025-09-03 23:06:45 +0900

    feat: LottoRank를 구현합니다.

---
+++ src/domain/lotto-rank.js
@@ -0,0 +1,21 @@
+export const LottoRank = Object.freeze({
+  FIRST: { match: 6, hasBonus: false, prize: 2_000_000_000 },
+  SECOND: { match: 5, hasBonus: true, prize: 30_000_000 },
+  THIRD: { match: 5, hasBonus: false, prize: 1_500_000 },
+  TOURTH: { match: 4, hasBonus: false, prize: 50_000 },
+  FIFTH: { match: 3, hasBonus: false, prize: 5_000 },
+  LAST: { match: 0, hasBonus: false, prize: 0 },
+});
+
+/**
+ *
+ * @param {number} match
+ * @param {boolean} hasBonus
+ */
+export function findLottoRank(match, hasBonus) {
+  return (
+    Object.values(LottoRank).find(
+      (rank) => rank.match === match && rank.hasBonus === hasBonus
+    ) || LottoRank.LAST
+  );
+}
