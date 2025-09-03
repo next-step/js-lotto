b4dbff2f8e560b375f2b437ef0bd4d21d6b0e8ab
강현석 <kekemusa37@gmail.com>
2025-09-04 00:28:40 +0900

    feat: 당첨 번호와 보너스 번호를 입력받는다.

--- src/step1-index.js
+++ src/step1-index.js
@@ -6,3 +6,6 @@ const lottoPrice = await readLineAsync("구매금액을 입력해 주세요.");
 const lottoStore = new LottoStore(lottoPrice);
 const lottoList = lottoStore.sell();
 printLotto(lottoList);
+
+const winningNumber = await readLineAsync("당첨 번호를 입력해 주세요.");
+const bonusNumber = await readLineAsync("보너스 번호를 입력해 주세요.");
