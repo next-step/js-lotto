999908a8546ce48d9dea6979975a316eb124eec5
unknown <syjin@in4ucloud.com>
2025-09-04 14:42:20 +0900

    feat: 통계 출력 함수 추가

--- src/Class/WinningNumbers.js
+++ src/Class/WinningNumbers.js
@@ -2,6 +2,11 @@ import {
   LOTTO_COUNT,
   LOTTO_MAX_NUMBER,
   LOTTO_MIN_NUMBER,
+  MATCH_FIVE_BONUS,
+  MATCH_FIVE,
+  MATCH_FIVE_PLUS_BONUS,
+  MATCHED_PRICE,
+  RESULTS_ORDER,
 } from "../constants/lotto.js";
 
 class WinningNumbers {
@@ -15,46 +20,75 @@ class WinningNumbers {
 
   // 당첨번호가 1~45번 사이의 번호인지 체크
   isValidRangeWinningNumber() {
-    let isValid = false;
-
     const filterLottoNumbers = this.#winningNumber.filter(
       (num) => num >= LOTTO_MIN_NUMBER && num <= LOTTO_MAX_NUMBER
     );
 
-    isValid = filterLottoNumbers.length === LOTTO_COUNT;
-
-    return isValid;
+    return filterLottoNumbers.length === LOTTO_COUNT;
   }
 
   // 보너스 번호가 1~45번 사이의 번호인지 체크
   isValidRangeBonusNumber() {
-    let isValid = false;
-
-    if (typeof this.#bonusNumber === "number") {
-      isValid = LOTTO_MIN_NUMBER && this.#bonusNumber <= LOTTO_MAX_NUMBER;
-    }
-
-    return isValid;
+    return (
+      typeof this.#bonusNumber === "number" &&
+      this.#bonusNumber >= LOTTO_MIN_NUMBER &&
+      this.#bonusNumber <= LOTTO_MAX_NUMBER
+    );
   }
 
   // 당첨번호가 6개인지 확인하는 함수
   isDigitCount() {
-    if (this.#winningNumber.length === LOTTO_COUNT) {
-      return true;
-    }
-
-    return false;
+    return this.#winningNumber.length === LOTTO_COUNT;
   }
 
   // 당첨번호에 보너스 번호가 포함되는지 체크
   isBonusNumberNotInWinningNumbers() {
-    if (
-      this.#winningNumber.filter((num) => num === this.#bonusNumber).length > 0
-    ) {
-      return false;
-    }
+    return this.#winningNumber.includes(this.#bonusNumber);
+  }
 
-    return true;
+  matchLottoPriceKey(sameNumberCount, sameBonusNumberCount) {
+    if (sameNumberCount < MATCH_THREE) return;
+
+    if (sameNumberCount === MATCH_FIVE && sameBonusNumberCount)
+      return MATCH_FIVE_BONUS;
+
+    return sameNumberCount;
+  }
+
+  updatePriceCheck(lotto, priceCheck) {
+    const sameBonusNumberCount = lotto.includes(this.#bonusNumber);
+    const sameNumberCount = lotto.filter((num) =>
+      this.#winningNumber.includes(num)
+    ).length;
+
+    const key = this.matchLottoPriceKey(sameNumberCount, sameBonusNumberCount);
+
+    if (!key) return;
+
+    priceCheck[key] = (priceCheck[key] || 0) + 1;
+  }
+
+  checkWinningStatistics(lottoTickets) {
+    console.log("당첨 통계");
+    console.log("--------------------");
+
+    const priceCheck = {};
+
+    lottoTickets.forEach((lotto) => this.updatePriceCheck(lotto, priceCheck));
+
+    this.resultsOrder(priceCheck);
+    this.rateOfReturn(priceCheck);
+  }
+
+  printStatistics(priceCheck) {
+    RESULTS_ORDER.forEach(({ key, label, price }) => {
+      const count = priceCheck[key] || 0;
+      console.log(`${label} (${price.toLocaleString()}원) - ${count}개`);
+    });
+  }
+
+  rateOfReturn(priceCheck) {
+    //
   }
 }
 
--- src/constants/lotto.js
+++ src/constants/lotto.js
@@ -8,9 +8,32 @@ export const LOTTO_NUMBERS = Array.from({ length: 45 }, (_, i) => i + 1);
 export const LOTTO_MIN_NUMBER = 1;
 export const LOTTO_MAX_NUMBER = 45;
 
+export const MATCH_THREE = 3;
+export const MATCH_FOUR = 4;
+export const MATCH_FIVE = 5;
+export const MATCH_SIX = 6;
+export const MATCH_FIVE_BONUS = "BONUS";
+
 export const MATCHED_PRICE = {
-  3: { price: 5000 },
-  4: { price: 50000 },
-  5: { price: 1500000, bonusPrice: 30000000 },
-  6: { price: 2000000000 },
+  MATCH_THREE: 5000,
+  MATCH_FOUR: 50000,
+  MATCH_FIVE: 1500000,
+  MATCH_FIVE_BONUS: 30000000,
+  MATCH_SIX: 2000000000,
 };
+
+export const RESULTS_ORDER = [
+  {
+    key: MATCH_THREE,
+    label: "3개 일치",
+    price: MATCHED_PRICE[MATCH_THREE],
+  },
+  { key: MATCH_FOUR, label: "4개 일치", price: MATCHED_PRICE[MATCH_FOUR] },
+  { key: MATCH_FIVE, label: "5개 일치", price: MATCHED_PRICE[MATCH_FIVE] },
+  {
+    key: MATCH_FIVE_BONUS,
+    label: "5개 일치, 보너스 볼 일치",
+    price: MATCHED_PRICE[MATCH_FIVE_BONUS],
+  },
+  { key: MATCH_SIX, label: "6개 일치", price: MATCHED_PRICE[MATCH_SIX] },
+];
