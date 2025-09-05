b3101a707a53fade8ec00a69d9c24cbe70fade4a
강현석 <kekemusa37@gmail.com>
2025-09-05 16:47:46 +0900

    review: 출력에 필요한 rank를 배열로 만들어서 코드를 간결하게 합니다.

--- src/view/output.js
+++ src/view/output.js
@@ -19,34 +19,19 @@ export function printLotto(lottoList) {
 export function printWinningStatistics(rankList) {
   console.log("당첨 통계");
   console.log("--------------------");
-  console.log(
-    `${LOTTO_RANK.FIFTH.match}개 일치 (${LOTTO_RANK.FIFTH.prize}원) - ${countRank(
-      rankList,
-      LOTTO_RANK.FIFTH
-    )}개`
-  );
-  console.log(
-    `${LOTTO_RANK.FOURTH.match}개 일치 (${
-      LOTTO_RANK.FOURTH.prize
-    }원) - ${countRank(rankList, LOTTO_RANK.FOURTH)}개`
-  );
-  console.log(
-    `${LOTTO_RANK.THIRD.match}개 일치 (${LOTTO_RANK.THIRD.prize}원) - ${countRank(
-      rankList,
-      LOTTO_RANK.THIRD
-    )}개`
-  );
-  console.log(
-    `${LOTTO_RANK.SECOND.match}개 일치, 보너스 볼 일치 (${
-      LOTTO_RANK.SECOND.prize
-    }원) - ${countRank(rankList, LOTTO_RANK.SECOND)}개`
-  );
-  console.log(
-    `${LOTTO_RANK.FIRST.match}개 일치 (${LOTTO_RANK.FIRST.prize}원) - ${countRank(
-      rankList,
-      LOTTO_RANK.FIRST
-    )}개`
-  );
+
+  const rankListForPrint = [
+    LOTTO_RANK.FIFTH,
+    LOTTO_RANK.FOURTH,
+    LOTTO_RANK.THIRD,
+    LOTTO_RANK.SECOND,
+    LOTTO_RANK.FIRST,
+  ];
+  rankListForPrint.forEach((rank) => {
+    console.log(
+      `${rank.match}개 일치 (${rank.prize}원) - ${countRank(rankList, rank)}개`
+    );
+  });
 }
 
 export function printRateOfReturn(rateOfReturn) {
