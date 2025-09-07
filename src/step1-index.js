d56d1be823e20fddb76e8c288fdf3526e63d76c5
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-09-07 20:52:16 +0900

    feat: 구입한 로또 출력 로직

--- src/step1-index.js
+++ src/step1-index.js
@@ -1,8 +1,25 @@
-import { readLineAsync } from "./utils/readLineAsync.js";
+import { BASE_LOTTO_PRICE } from "./domains/LottoMachine/constants/index.js";
+import { LottoMachine } from "./domains/LottoMachine/index.js";
+import { readLineAsync } from "./ui/utils/readLineAsync.js";
 
 const play = async () => {
   const amount = await readLineAsync("구입금액을 입력해 주세요.");
-  console.log(amount);
+
+  const lottoMachine = new LottoMachine(BASE_LOTTO_PRICE);
+  const lottos = lottoMachine.issueLottos(amount);
+
+  print(`${lottos.length}개를 구매했습니다.`);
+  printLottos(lottos);
 };
 
 play();
+
+function print(text) {
+  console.log(text);
+}
+
+function printLottos(lottos) {
+  lottos.forEach((lotto) => {
+    console.log(lotto.numbers);
+  });
+}
