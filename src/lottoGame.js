75de2374ae3b767ab1acaa0076b05f801177d375
SEMIN-97 <jsemin.1021@gmail.com>
2025-09-07 22:39:47 +0900

    fix: LOTTO_RANK_TABLE 2등 조건 오류 수정

--- src/lottoGame.js
+++ src/lottoGame.js
@@ -1,6 +1,6 @@
 const LOTTO_RANK_TABLE = [
   { rank: 1, matchCount: 6, prize: 2_000_000_000 },
-  { rank: 2, matchCount: 5, prize: 30_000_000, bonus: true },
+  { rank: 2, matchCount: 6, prize: 30_000_000, bonus: true },
   { rank: 3, matchCount: 5, prize: 1_500_000 },
   { rank: 4, matchCount: 4, prize: 50_000 },
   { rank: 5, matchCount: 3, prize: 5_000 },
