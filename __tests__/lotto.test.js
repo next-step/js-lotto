95fe914fccbc72778406594bd091c05b8757de8f
SEMIN-97 <jsemin.1021@gmail.com>
2025-09-07 16:33:03 +0900

    feat: 로또 1장은 1~45 범위의 중복되지 않은 6개의 숫자로 구성된다

--- __tests__/lotto.test.js
+++ __tests__/lotto.test.js
@@ -1,4 +1,4 @@
-import { getTicket } from '../src/lotto.js'
+import { generateLottoNumber, getTicket } from '../src/lotto.js'
 import { LottoError } from '../src/errors/lottoError.js'
 
 describe('getTicket 함수 테스트', () => {
@@ -22,5 +22,28 @@ describe('getTicket 함수 테스트', () => {
 
   it('금액을 입력하지 않으면 에러를 발생시킨다', () => {
     expect(() => getTicket()).toThrow(LottoError.TicketPriceTooLow())
+  })
+})
+
+describe('generateLottoNumber 함수 테스트', () => {
+  it('6개의 숫자로 구성된다', () => {
+    const numbers = generateLottoNumber()
+
+    expect(numbers).toHaveLength(6)
+  })
+
+  it('6개의 숫자는 중복되지 않는다', () => {
+    const numbers = new Set(generateLottoNumber())
+
+    expect(numbers.size).toBe(6)
+  })
+
+  it('1~45 범위의 숫자들로 구성된다', () => {
+    const numbers = generateLottoNumber()
+
+    numbers.forEach(number => {
+      expect(number).toBeGreaterThanOrEqual(1)
+      expect(number).toBeLessThanOrEqual(45)
+    })
   })
 })
--- src/lotto.js
+++ src/lotto.js
@@ -1,6 +1,7 @@
 import { LottoError } from './errors/lottoError.js'
 
 const LOTTO_PRICE = 1000
+const LOTTO_NUMBERS_LENGTH = 6
 
 export function getTicket(money) {
   if (!money || money < LOTTO_PRICE) {
@@ -8,4 +9,15 @@ export function getTicket(money) {
   }
 
   return Math.floor(money / LOTTO_PRICE)
+}
+
+export function generateLottoNumber() {
+  const numbers = new Set()
+
+  while (numbers.size < LOTTO_NUMBERS_LENGTH) {
+    const num = Math.floor(Math.random() * 45) + 1
+    numbers.add(num)
+  }
+
+  return Array.from(numbers)
 }
