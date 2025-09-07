9284a3c6cecccea518b98c6a4d7efc0afbaa356e
강현석 <kekemusa37@gmail.com>
2025-09-07 22:12:05 +0900

    feat: 사용자가 잘못된 값을 입력한 경우 throw문을 사용해 예외를 발생시키고, 에러 메시지를 출력 후 그 부분부터 입력을 다시 받습니다.

--- src/step1-index.js
+++ src/step1-index.js
@@ -1,7 +1,9 @@
-import { LottoNumber } from "./domain/lotto-number.js";
-import { LottoStore } from "./domain/lotto-store.js";
-import { Lotto } from "./domain/lotto.js";
-import { readLineAsync, askRetry } from "./view/input.js";
+import {
+  askRetry,
+  askLottoPrice,
+  askWinningLotto,
+  askBonusNumber,
+} from "./view/input.js";
 import {
   printLotto,
   printRateOfReturn,
@@ -10,21 +12,18 @@ import {
 
 let playAgain = true;
 while (playAgain) {
-  const lottoPrice = await readLineAsync("구매금액을 입력해 주세요.");
-  const lottoStore = new LottoStore(lottoPrice);
+  const lottoStore = await askLottoPrice();
   const lottoList = lottoStore.sell();
   printLotto(lottoList);
 
-  const winningNumbersInput = await readLineAsync("당첨 번호를 입력해 주세요.");
-  const winningLotto = Lotto.from(winningNumbersInput);
-  const bonusNumberInput = await readLineAsync("보너스 번호를 입력해 주세요.");
-  const bonusNumber = LottoNumber.from(bonusNumberInput);
-  lottoStore.validateBonusNumber(winningLotto, bonusNumber);
+  const winningLotto = await askWinningLotto();
+  const bonusNumber = await askBonusNumber(lottoStore, winningLotto);
   const rankList = lottoList.map((lotto) =>
     lotto.prize(winningLotto, bonusNumber)
   );
+
   printWinningStatistics(rankList);
   printRateOfReturn(lottoStore.rateOfReturn(rankList));
-  const retryInput = await readLineAsync("다시 시작하시겠습니까? (y/n)");
-  playAgain = askRetry(retryInput);
+  
+  playAgain = await askRetry();
 }
--- src/view/input.js
+++ src/view/input.js
@@ -1,11 +1,14 @@
 import readline from "readline";
+import { LottoStore } from "../domain/lotto-store.js";
+import { Lotto } from "../domain/lotto.js";
+import { LottoNumber } from "../domain/lotto-number.js";
 
 export const RESTART_INPUT = {
   YES: "y",
   NO: "n",
 };
 
-export function readLineAsync(query) {
+function readLineAsync(query) {
   return new Promise((resolve, reject) => {
     if (arguments.length !== 1) {
       reject(new Error("arguments must be 1"));
@@ -27,9 +30,64 @@ export function readLineAsync(query) {
   });
 }
 
-export function askRetry(input) {
-  if (!Object.values(RESTART_INPUT).includes(input)) {
-    throw new TypeError("y 또는 n을 입력해주세요.");
+export async function askRetry() {
+  while (true) {
+    const retry = await readLineAsync("다시 시작하시겠습니까? (y/n)");
+
+    try {
+      if (!Object.values(RESTART_INPUT).includes(retry)) {
+        throw new TypeError("y 또는 n을 입력해주세요.");
+      }
+      return retry === RESTART_INPUT.YES;
+    } catch (e) {
+      console.log(e.message);
+    }
   }
-  return input === RESTART_INPUT.YES;
+}
+
+export async function askLottoPrice() {
+  while (true) {
+    const lottoPrice = await readLineAsync("구매금액을 입력해 주세요.");
+
+    try {
+      return new LottoStore(lottoPrice);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+}
+
+export async function askWinningLotto() {
+  while (true) {
+    const winningNumbersInput = await readLineAsync(
+      "당첨 번호를 입력해 주세요."
+    );
+
+    try {
+      return Lotto.from(winningNumbersInput);
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+}
+
+/**
+ *
+ * @param {LottoStore} lottoStore
+ * @param {Lotto} winningLotto
+ */
+export async function askBonusNumber(lottoStore, winningLotto) {
+  while (true) {
+    const bonusNumberInput = await readLineAsync(
+      "보너스 번호를 입력해 주세요."
+    );
+
+    try {
+      const bonusNumber = LottoNumber.from(bonusNumberInput);
+      lottoStore.validateBonusNumber(winningLotto, bonusNumber);
+      return bonusNumber;
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
 }
