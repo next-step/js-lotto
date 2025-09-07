e419fac644922ddee7d4ed310e4a626c3aafeedd
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-09-07 19:57:00 +0900

    feat: 입출력 - 로또 구입 금액을 입력받는다.

--- src/step1-index.js
+++ src/step1-index.js
@@ -1,4 +1,8 @@
-/**
- * step 1의 시작점이 되는 파일입니다.
- * 브라우저 환경에서 사용하는 css 파일 등을 불러올 경우 정상적으로 빌드할 수 없습니다.
- */
+import { readLineAsync } from "./utils/readLineAsync.js";
+
+const play = async () => {
+  const amount = await readLineAsync("구입금액을 입력해 주세요.");
+  console.log(amount);
+};
+
+play();
