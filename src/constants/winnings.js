796bba8906641177dfd7e9b0e5c301bd522a5699
eun0leee <eun0leee@gmail.com>
2025-09-08 12:45:19 +0900

    refactor : numeric separator 사용

--- src/constants/winnings.js
+++ src/constants/winnings.js
@@ -1,7 +1,7 @@
 export const RANK_INFO = [
-  { name: 'fifth', matchCount: 3, money: 5000 },
-  { name: 'fourth', matchCount: 4, money: 50000 },
-  { name: 'third', matchCount: 5, money: 1500000 },
-  { name: 'second', matchCount: 5, money: 30000000 },
-  { name: 'first', matchCount: 6, money: 2000000000 },
+  { name: 'fifth', matchCount: 3, money: 5_000 },
+  { name: 'fourth', matchCount: 4, money: 50_000 },
+  { name: 'third', matchCount: 5, money: 1_500_000 },
+  { name: 'second', matchCount: 5, money: 30_000_000 },
+  { name: 'first', matchCount: 6, money: 2_000_000_000 },
 ];
