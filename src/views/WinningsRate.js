e21dab8476062e407a5c96376c11cf17ce28ef3e
eun0leee <eun0leee@gmail.com>
2025-09-08 12:50:11 +0900

    fix : 수익률 천단위 콤마로 출력

--- src/views/WinningsRate.js
+++ src/views/WinningsRate.js
@@ -3,7 +3,7 @@ import { calculateWinningsRate } from '../domain/winningsRate.js';
 const WinningsRate = (amountPaid, rankCountsMap) => {
   const winningRate = calculateWinningsRate(amountPaid, rankCountsMap);
 
-  console.log(`총 수익률은 ${winningRate}% 입니다.`);
+  console.log(`총 수익률은 ${winningRate.toLocaleString()}% 입니다.`);
 };
 
 export default WinningsRate;
