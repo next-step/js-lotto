699cfcba9abd4f8d6c98c1936cf7358d3db60d39
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-09-14 00:51:30 +0900

    refactor: generateLottoNumbers 로직 간소화

--- src/domains/LottoMachine/utils/generateLottoNumbers.js
+++ src/domains/LottoMachine/utils/generateLottoNumbers.js
@@ -1,17 +1,8 @@
-import {
-  MAX_LOTTO_NUMBER_COUNT,
-  MAX_LOTTO_NUMBER,
-} from "../../Lotto/constants/index.js";
+import { MAX_LOTTO_NUMBER } from "../../Lotto/constants/index.js";
 
 export const generateLottoNumbers = () => {
-  const numbers = [];
-
-  while (numbers.length < MAX_LOTTO_NUMBER_COUNT) {
-    const number = Math.floor(Math.random() * MAX_LOTTO_NUMBER) + 1;
-    if (!numbers.includes(number)) {
-      numbers.push(number);
-    }
-  }
-
-  return numbers.sort((a, b) => a - b);
+  return Array.from({ length: MAX_LOTTO_NUMBER }, (_, index) => index + 1)
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 6)
+    .sort((a, b) => a - b);
 };
