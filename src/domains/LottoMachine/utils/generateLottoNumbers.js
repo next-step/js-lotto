0a03660e09dad84ad30ee23f6f04c1208ccbcaef
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-09-14 02:08:00 +0900

    fix: 로또 번호 생성 로직 에러 수정

--- src/domains/LottoMachine/utils/generateLottoNumbers.js
+++ src/domains/LottoMachine/utils/generateLottoNumbers.js
@@ -1,7 +1,7 @@
 import { Lotto } from "../../Lotto/index.js";
 
 export const generateLottoNumbers = () => {
-  return Array.from({ length: Lotto.SIZE }, (_, index) => index + 1)
+  return Array.from({ length: 45 }, (_, index) => index + 1)
     .sort(() => Math.random() - 0.5)
     .slice(0, Lotto.SIZE)
     .sort((a, b) => a - b);
