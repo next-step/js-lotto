5bac4f497121d96001f988de01343aa0c54a3402
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-09-14 12:19:37 +0900

    fix: 깨진 테스트 코드 수정

--- __tests__/Lotto.test.js
+++ __tests__/Lotto.test.js
@@ -4,7 +4,7 @@ describe("로또는", () => {
   it("1~45 사이의 6개의 중복 없는 숫자를 가진다", () => {
     const lotto = new Lotto([1, 2, 3, 4, 5, 45]);
 
-    expect(lotto.numbers.size).toBe(Lotto.SIZE);
+    expect(lotto.numbers.length).toBe(Lotto.SIZE);
   });
 
   it("6개의 중복되지 않은 숫자를 가지지 않으면 에러를 반환한다", () => {
