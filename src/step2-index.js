ab04fc080af1680731ebb89bf2302a6714237e77
강현석 <kekemusa37@gmail.com>
2025-09-18 22:23:44 +0900

    review: 각 form의 submit 이벤트를 리스너로 독립적으로 등록하도록 합니다.

--- src/step2-index.js
+++ src/step2-index.js
@@ -1,41 +1,29 @@
 import { LottoNumber } from "./domain/lotto-number.js";
 import { LottoStore } from "./domain/lotto-store.js";
 import { Lotto } from "./domain/lotto.js";
-import { submitLottoPrice, submitWinningLotto } from "./view/input.web.js";
+import { onSubmitLottoPrice, onSubmitWinningLotto } from "./view/input.web.js";
 import { printLottoWeb, showWinningStatistics } from "./view/output.web.js";
 
-window.addEventListener("load", async () => {
+window.addEventListener("load", () => {
   let lottoStore;
-  while (true) {
-    try {
-      const lottoPrice = await submitLottoPrice();
-      lottoStore = new LottoStore(lottoPrice);
-      break;
-    } catch (error) {
-      alert(error.message);
-    }
-  }
-  const lottoList = lottoStore.sell();
-  printLottoWeb(lottoList);
+  let lottoList = [];
 
-  let winningLotto;
-  let bonusLottoNumber;
-  while (true) {
-    try {
-      const { winningNumbers, bonusNumber } = await submitWinningLotto();
-      winningLotto = new Lotto(winningNumbers);
-      bonusLottoNumber = new LottoNumber(bonusNumber);
-      lottoStore.validateBonusNumber(winningLotto, bonusLottoNumber);
-      break;
-    } catch (error) {
-      alert(error.message);
-    }
-  }
+  onSubmitLottoPrice((lottoPrice) => {
+    lottoStore = new LottoStore(lottoPrice);
+    lottoList = lottoStore.sell();
+    printLottoWeb(lottoList);
+  });
 
-  const rankList = lottoList.map((lotto) =>
-    lotto.prize(winningLotto, bonusLottoNumber)
-  );
-  const rateOfReturn = lottoStore.rateOfReturn(rankList);
+  onSubmitWinningLotto(({ winningNumbers, bonusNumber }) => {
+    const winningLotto = new Lotto(winningNumbers);
+    const bonusLottoNumber = new LottoNumber(bonusNumber);
+    lottoStore.validateBonusNumber(winningLotto, bonusLottoNumber);
 
-  showWinningStatistics(rankList, rateOfReturn);
+    const rankList = lottoList.map((lotto) =>
+      lotto.prize(winningLotto, bonusLottoNumber)
+    );
+    const rateOfReturn = lottoStore.rateOfReturn(rankList);
+
+    showWinningStatistics(rankList, rateOfReturn);
+  });
 });
