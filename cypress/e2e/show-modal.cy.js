3f285151eedb7e3210b1335f78f357d43e23a672
강현석 <kekemusa37@gmail.com>
2025-09-22 21:52:06 +0900

    review: 매직 넘버는 별도의 상수로 관리하도록 합니다.

--- cypress/e2e/show-modal.cy.js
+++ cypress/e2e/show-modal.cy.js
@@ -1,14 +1,18 @@
 describe("당첨 통계 모달 시나리오", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173");
+
     cy.get(".lotto-price form input").type("5000");
     cy.get(".lotto-price form button").click();
+
+    const winningNumber = [1, 2, 3, 4, 5, 6];
+    const bonusNumber = 7;
     cy.get('.numbers-row input[name="winning-number"]').each(
       ($input, index) => {
-        cy.wrap($input).type(index + 1);
+        cy.wrap($input).type(winningNumber[index]);
       }
     );
-    cy.get('.numbers-row input[name="bonus-number"]').type(45);
+    cy.get('.numbers-row input[name="bonus-number"]').type(bonusNumber);
     cy.get(".winning-lotto form button").click();
   });
   it("닫기 버튼을 누르면 모달이 닫히고 기존에 입력했던 정보가 초기화됩니다.", () => {
